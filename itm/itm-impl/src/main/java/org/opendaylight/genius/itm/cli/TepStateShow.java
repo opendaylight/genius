5d058d23fac6b5362dcfef4524f91b5dcac2ea00
Faseela K <faseela.k@ericsson.com>
2016-04-14 19:26:16 +0530

    Porting the modules from vpnservice to genius
    Change-Id: I8b4e63b28761485ef1d9bfe2f7e5171d0cb6a352
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

---
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cli/TepStateShow.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2015 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.itm.cli;
+
+import org.apache.karaf.shell.commands.Command;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
+
+@Command(scope = "tep", name = "state-show", description="Monitors tunnel state")
+
+    public class TepStateShow extends OsgiCommandSupport {
+
+       @Override
+
+       protected Object doExecute() throws Exception {
+
+              System.out.println("Executing show TEP states command");
+
+       return null;
+
+       }
+    }
