f85287399152ae899c3d690519fd41203965ce35
Faseela K <faseela.k@ericsson.com>
2016-04-14 19:26:16 +0530

    Porting the modules from vpnservice to genius
    Change-Id: I8b4e63b28761485ef1d9bfe2f7e5171d0cb6a352
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

---
+++ lockmanager/lockmanager-impl/src/main/java/org/opendaylight/lockmanager/LockManagerUtils.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2015 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.lockmanager;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.lockmanager.rev160413.Locks;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.lockmanager.rev160413.TimeUnits;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.lockmanager.rev160413.locks.Lock;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.lockmanager.rev160413.locks.LockBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.lockmanager.rev160413.locks.LockKey;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+import java.util.concurrent.TimeUnit;
+
+public class LockManagerUtils {
+
+    public static long convertToMillis(long waitTime, TimeUnit timeUnit) {
+        return timeUnit.toMillis(waitTime);
+    }
+
+    public static void sleep(long waitTime) {
+        try {
+            Thread.sleep(waitTime);
+        } catch (InterruptedException exception) {
+        }
+    }
+
+    public static TimeUnit convertToTimeUnit(TimeUnits timeUnit) {
+        switch (timeUnit) {
+        case Days:
+            return TimeUnit.DAYS;
+        case Microseconds:
+            return TimeUnit.MICROSECONDS;
+        case Hours:
+            return TimeUnit.HOURS;
+        case Minutes:
+            return TimeUnit.MINUTES;
+        case Nanoseconds:
+            return TimeUnit.NANOSECONDS;
+        case Seconds:
+            return TimeUnit.SECONDS;
+        case Milliseconds:
+        default:
+            return TimeUnit.MILLISECONDS;
+        }
+    }
+
+    public static InstanceIdentifier<Lock> getLockInstanceIdentifier(String lockName) {
+        return InstanceIdentifier
+                .builder(Locks.class).child(Lock.class,
+                        new LockKey(lockName)).build();
+    }
+
+    public static Lock buildLockData(String lockName) {
+        return new LockBuilder().setKey(new LockKey(lockName)).setLockName(lockName).setLockOwner(Thread.currentThread().getName()).build();
+    }
+}
