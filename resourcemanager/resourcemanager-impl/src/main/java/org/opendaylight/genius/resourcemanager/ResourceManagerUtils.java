dfb72f6b89ca48b703a4cb007e87bba3d7b7065f
karanrajsingh <karan.raj.s.singh@ericsson.com>
2016-07-14 16:56:55 +0530

    Implementation files for resourcemanager bundle
    Change-Id: I4278cc9cd121a9d32c48b8f83e1a3e7ff5965ec8
    Signed-off-by: karanrajsingh <karan.raj.s.singh@ericsson.com>

--- resourcemanager/resourcemanager-impl/src/main/java/org/opendaylight/genius/resourcemanager/ResourceManagerUtils.java
+++ resourcemanager/resourcemanager-impl/src/main/java/org/opendaylight/genius/resourcemanager/ResourceManagerUtils.java
@@ -7,10 +7,38 @@
  */
 package org.opendaylight.genius.resourcemanager;
 
+import com.google.common.net.InetAddresses;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.IdPools;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.id.pools.IdPool;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.id.pools.IdPoolKey;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.InetAddress;
+
 class ResourceManagerUtils {
     private static final Logger LOGGER = LoggerFactory.getLogger(ResourceManagerUtils.class);
+
+    private static int BLADE_ID;
+    static {
+        try {
+            BLADE_ID = InetAddresses.coerceToInteger(InetAddress.getLocalHost());
+        } catch (Exception e) {
+            LOGGER.error("ResourceManager - Exception - {}", e.getMessage());
+        }
+    }
+
+    protected static InstanceIdentifier<IdPool> getIdPoolInstance(String poolName) {
+        InstanceIdentifier.InstanceIdentifierBuilder<IdPool> idPoolBuilder = InstanceIdentifier
+                .builder(IdPools.class).child(IdPool.class,
+                        new IdPoolKey(poolName));
+        InstanceIdentifier<IdPool> id = idPoolBuilder.build();
+        return id;
+    }
+
+    protected static String getLocalPoolName(String poolName) {
+        return (poolName + "." + BLADE_ID);
+    }
 
 }
