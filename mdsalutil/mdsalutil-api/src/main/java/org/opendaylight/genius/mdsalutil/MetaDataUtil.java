5218e883cb77c8dc40c32ee0204833e71ce8c14f
Suraj Ranjan <suraj.ranjan@ericsson.com>
2016-07-18 13:32:36 +0530

    Addition for new constants
    Following commit includes few additional constants in following file:
    
    1> ITMConstants.Java
    2> MetaDataUtil.Java
    
    Change-Id: I96c47f0cd92986ae57479c3a7269b2e22e3b9c05
    Signed-off-by: Suraj Ranjan <suraj.ranjan@ericsson.com>

--- mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/MetaDataUtil.java
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/MetaDataUtil.java
@@ -20,6 +20,7 @@ public class MetaDataUtil {
     public static final BigInteger METADA_MASK_TUNNEL_ID =       new BigInteger("00000000FFFFFF00", 16);
     public static final BigInteger METADATA_MASK_SERVICE_SH_FLAG = new BigInteger("000000FFFF000001", 16);
     public static final BigInteger METADATA_MASK_LPORT_TAG_SH_FLAG =     new BigInteger("1FFFFF0000000001", 16);
+    public static final BigInteger METADATA_MASK_SUBNET_ROUTE =         new BigInteger("0000FFFFFFFFFFFF", 16);
 
     public static BigInteger getMetaDataForLPortDispatcher(int lportTag, short serviceIndex) {
         return getServiceIndexMetaData(serviceIndex).or(getLportTagMetaData(lportTag));
