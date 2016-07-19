be87aa22da02365408f459d5aeb65e35a43fd893
Dimple Jain <dimple.jain@ericsson.com>
2016-07-19 15:42:01 +0530

    Upstreaming ITM code
    Change-Id: I3865d0ffba58b48cd76a58e0d8cd9f20ae318ede
    Signed-off-by: Dimple Jain <dimple.jain@ericsson.com>

--- itm/itm-api/src/main/java/org/opendaylight/genius/itm/globals/ITMConstants.java
+++ itm/itm-api/src/main/java/org/opendaylight/genius/itm/globals/ITMConstants.java
@@ -23,6 +23,8 @@ public class ITMConstants{
   public static final boolean DEFAULT_MONITOR_ENABLED = true;
   public static final int DEFAULT_MONITOR_INTERVAL = 10000;
   public static final int BFD_DEFAULT_MONITOR_INTERVAL = 1000;
+  public static final int MIN_MONITOR_INTERVAL = 1000;
+  public static final int MAX_MONITOR_INTERVAL = 30000;
   public static final String DUMMY_IP_ADDRESS = "0.0.0.0";
   public static final String TUNNEL_TYPE_VXLAN = "VXLAN";
   public static final String TUNNEL_TYPE_GRE = "GRE";
