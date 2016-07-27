a1888719d471e63fdfd6fe6da2b400f6addd46b0
Suraj Ranjan <suraj.ranjan@ericsson.com>
2016-07-27 13:13:14 +0530

    Addition of new Table in NwConstants.java
    This review includes addition of new Tables in NwConstants.java file so
    that these constants can be used universally across all module.
    
    Change-Id: Iecf3c7e3bca107f5d638c6ebf88ad5fbb2fb1f99
    Signed-off-by: Suraj Ranjan <suraj.ranjan@ericsson.com>

--- mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/NwConstants.java
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/NwConstants.java
@@ -7,6 +7,8 @@
  */
 package org.opendaylight.genius.mdsalutil;
 
+import java.math.BigInteger;
+
 public class NwConstants {
 
     // EthType Values
@@ -44,20 +46,50 @@ public class NwConstants {
 
     public static final int DEFAULT_ARP_FLOW_PRIORITY = 100;
 
-    public static final int SCF_SERVICE_INDEX = 1;
-    public static final int L3VPN_SERVICE_INDEX = 2;
-    public static final int ELAN_SERVICE_INDEX = 3;
+    public static final int DHCP_SERVICE_INDEX = 1;
+    public static final int ACL_SERVICE_INDEX = 2;
+    public static final int IPV6_SERVICE_INDEX = 3;
+    public static final int SCF_SERVICE_INDEX = 4;
+    public static final int L3VPN_SERVICE_INDEX = 5;
+    public static final int ELAN_SERVICE_INDEX = 6;
     //Table IDs
-    public static final short L3_FIB_TABLE = 21;
-    public static final short L3_LFIB_TABLE = 20;
-    public static final short L3_SUBNET_ROUTE_TABLE=22;
+    public static final BigInteger VLAN_TABLE_COOKIE = new BigInteger("8000000", 16);
+    public static final BigInteger COOKIE_VM_INGRESS_TABLE = new BigInteger("8000001", 16);
+    public static final BigInteger COOKIE_VM_LFIB_TABLE = new BigInteger("8000002", 16);
+    static final BigInteger COOKIE_VM_FIB_TABLE =  new BigInteger("8000003", 16);
+    public static final BigInteger COOKIE_DNAT_TABLE = new BigInteger("8000004", 16);
+    public static final BigInteger COOKIE_TS_TABLE = new BigInteger("8000005", 16);
+    public static final BigInteger COOKIE_SNAT_TABLE = new BigInteger("8000006", 16);
+    public static final BigInteger COOKIE_VXLAN_TRUNK_L2_TABLE = new BigInteger("1200000", 16);
+    public static final BigInteger COOKIE_GRE_TRUNK_L2_TABLE = new BigInteger("1400000", 16);
+    public static final BigInteger COOKIE_ELAN_INGRESS_TABLE = new BigInteger("8040000", 16);
+    public static final BigInteger TUNNEL_TABLE_COOKIE = new BigInteger("9000000", 16);
 
-    public static final short L3_PROTOCOL_TABLE = 36;
-    public static final short L3_INTERFACE_TABLE = 80;
-    public static final short LPORT_DISPATCHER_TABLE = 17;
     public static final short VLAN_INTERFACE_INGRESS_TABLE = 0;
+    public static final short LPORT_DISPATCHER_TABLE = 17;
+    public static final short DHCP_TABLE_EXTERNAL_TUNNEL = 18;
+    public static final short DHCP_TABLE = 19;
+    public static final short L3_LFIB_TABLE = 20;
+    public static final short L3_FIB_TABLE = 21;
+    public static final short L3_SUBNET_ROUTE_TABLE=22;
+    public static final short PDNAT_TABLE = 25;
+    public static final short PSNAT_TABLE = 26;
+    public static final short DNAT_TABLE = 27;
+    public static final short SNAT_TABLE = 28;
     public static final short INTERNAL_TUNNEL_TABLE = 36;
     public static final short EXTERNAL_TUNNEL_TABLE = 38;
-    public static final short DHCP_TABLE = 25;
-    public static final short DHCP_TABLE_EXTERNAL_TUNNEL = 18;
+    public static final short EGRESS_ACL_TABLE_ID = 40;
+    public static final short EGRESS_ACL_NEXT_TABLE_ID = 41;
+    public static final short INBOUND_NAPT_TABLE = 44;
+    public static final short OUTBOUND_NAPT_TABLE = 46;
+    public static final short NAPT_PFIB_TABLE = 47;
+    public static final short ELAN_SMAC_TABLE = 50;
+    public static final short ELAN_DMAC_TABLE = 51;
+    public static final short ELAN_UNKNOWN_DMAC_TABLE = 52;
+    public static final short ELAN_FILTER_EQUALS_TABLE = 55;
+    public static final short L3_INTERFACE_TABLE = 80;
+    public static final short INGRESS_ACL_TABLE_ID = 251;
+    public static final short INGRESS_ACL_NEXT_TABLE_ID = 252;
+
+
  }
