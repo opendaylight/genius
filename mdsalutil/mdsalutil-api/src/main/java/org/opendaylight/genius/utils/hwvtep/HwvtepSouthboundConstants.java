21791ddc886d8562a1edae06212ead2ba1f78042
K.V Suneelu Verma <k.v.suneelu.verma@ericsson.com>
2016-08-31 10:37:09 +0530

    Bug 6589 adding support for hwvtep devices ha
    Added the following utility methods
    to add a node as ha child node of the other node
    to check if a node is ha child node or not
    to get list of child nodes for given parent node
    
    When two southbound devices are in mlag, the northbound l2gw configuration
    has to be synced in both the devices.
    a node is marked as ha child node based on other_config columnn key ha_id in
    manager table
    Two ha devices will have same ha_id value.
    
    Change-Id: I4e4d58074b8a4b7478fe7859dd436d6ca8cf2d37
    Signed-off-by: K.V Suneelu Verma <k.v.suneelu.verma@ericsson.com>
    (cherry picked from commit e963423ecb0069d35baaaff713dc2f155f7b017d)

--- mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/utils/hwvtep/HwvtepSouthboundConstants.java
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/utils/hwvtep/HwvtepSouthboundConstants.java
@@ -7,22 +7,23 @@
  */
 package org.opendaylight.genius.utils.hwvtep;
 
+import com.google.common.collect.ImmutableBiMap;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Uri;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.MacAddress;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.EncapsulationTypeBase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.EncapsulationTypeVxlanOverIpv4;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TopologyId;
 
-import com.google.common.collect.ImmutableBiMap;
-
 // TODO (eperefr) Remove this class once it's been added to hwvtep-southbound
 public class HwvtepSouthboundConstants {
 
-    public static final TopologyId HWVTEP_TOPOLOGY_ID = new TopologyId("hwvtep:1");
     public static final String HWVTEP_ENTITY_TYPE = "hwvtep";
-    public static final Object PSWITCH_URI_PREFIX = "physicalswitch";
     public static final ImmutableBiMap<Class<? extends EncapsulationTypeBase>, String> ENCAPS_TYPE_MAP = new ImmutableBiMap.Builder<Class<? extends EncapsulationTypeBase>, String>()
             .put(EncapsulationTypeVxlanOverIpv4.class, "vxlan_over_ipv4").build();
     public static final String ELAN_ENTITY_TYPE = "elan";
     public static final String ELAN_ENTITY_NAME = "elan";
     public static final String TEP_PREFIX = "vxlan_over_ipv4:";
+    public static final String PSWITCH_URI_PREFIX = "physicalswitch";
+    public static final TopologyId HWVTEP_TOPOLOGY_ID = new TopologyId(new Uri("hwvtep:1"));
 }
 
