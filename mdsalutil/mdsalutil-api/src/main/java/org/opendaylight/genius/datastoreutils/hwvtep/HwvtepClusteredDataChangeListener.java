47ab745f43689c5ef445a97964564a185c6f1e29
K.V Suneelu Verma <k.v.suneelu.verma@ericsson.com>
2016-08-31 10:37:09 +0530

    Bug 6589 adding support for hwvtep devices ha
    When two southbound devices are in mlag, the northbound l2gw configuration
    has to be synced in both the devices.
    Added utility methods to find out if a node is ha child or not.
    a node is marked as ha child node based on other_config columnn key ha_id in
    manager table
    Two ha devices will have same ha_id value.
    
    Change-Id: I4e4d58074b8a4b7478fe7859dd436d6ca8cf2d37
    Signed-off-by: K.V Suneelu Verma <k.v.suneelu.verma@ericsson.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/datastoreutils/hwvtep/HwvtepClusteredDataChangeListener.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2016 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.datastoreutils.hwvtep;
+
+import org.opendaylight.controller.md.sal.binding.api.ClusteredDataChangeListener;
+import org.opendaylight.genius.datastoreutils.AsyncClusteredDataChangeListenerBase;
+import org.opendaylight.genius.utils.hwvtep.HwvtepHACache;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+public abstract class HwvtepClusteredDataChangeListener<T extends DataObject, K extends ClusteredDataChangeListener> extends AsyncClusteredDataChangeListenerBase<T , K>  {
+    public HwvtepClusteredDataChangeListener(Class<T> clazz, Class<K> eventClazz) {
+        super(clazz, eventClazz);
+    }
+
+
+    @Override
+    protected void remove(InstanceIdentifier<T> identifier, T del) {
+        String nodeIdVal = identifier.firstKeyOf(Node.class).getNodeId().getValue();
+        if (HwvtepHACache.isHAEnabledDevice(nodeIdVal)) {
+            return;
+        }
+        removed(identifier,del);
+    }
+
+    @Override
+    protected void update(InstanceIdentifier<T> identifier, T original, T update) {
+        String nodeIdVal = identifier.firstKeyOf(Node.class).getNodeId().getValue();
+        if (HwvtepHACache.isHAEnabledDevice(nodeIdVal)) {
+            return;
+        }
+        updated(identifier,original, update);
+    }
+
+    @Override
+    protected void add(InstanceIdentifier<T> identifier, T add) {
+        String nodeIdVal = identifier.firstKeyOf(Node.class).getNodeId().getValue();
+        if (HwvtepHACache.isHAEnabledDevice(nodeIdVal)) {
+            return;
+        }
+        added(identifier, add);
+    }
+
+    protected abstract void added(InstanceIdentifier<T> identifier, T add);
+    protected abstract void removed(InstanceIdentifier<T> identifier, T del);
+    protected abstract void updated(InstanceIdentifier<T> identifier, T original, T update);
+
+
+}
