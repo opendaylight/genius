8ebfb4435ddf3ce70a496806f2b4fa50cb5799ca
Sathish Kumar Tippeshappa <sathishkumar.bt@altencalsoftlabs.com>
2016-09-02 11:08:39 +0530

    Default transport zone creation during the bootup
    Change-Id: I3e510238e097e3a2eb3ab36d871477c46f01509a
    Signed-off-by: Sathish Kumar Tippeshappa <sathishkumar.bt@altencalsoftlabs.com>

---
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/validator/TransportZoneNameAllowed.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2016 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.itm.validator;
+
+import org.opendaylight.genius.itm.globals.ITMConstants;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.rev160406.transport.zones.TransportZone;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TransportZoneNameAllowed implements TransportZoneValidator {
+    private static final Logger LOG = LoggerFactory.getLogger(TransportZoneNameAllowed.class);
+    @Override
+    public boolean validate(TransportZone zone) {
+        if(zone.getZoneName().equalsIgnoreCase(ITMConstants.DEFAULT_TRANSPORT_ZONE)){
+           LOG.warn(ITMConstants.DEFAULT_TRANSPORT_ZONE+ " are not processed ");
+           return false;
+        }
+        return true;
+    }
+}
