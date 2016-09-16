7f135c8b1331422d44a47497ab601b636c804a74
Tarun Thakur <tarun.t@altencalsoftlabs.com>
2016-09-16 14:34:08 +0530

    Code for TEP auto-config feature in ITM module
    - to add/update/delete TEP received from southbound into ITM config datastore.
    - to perform Datastore write operations in queue using DataStoreJobCoordinator.
    
    Change-Id: I77c5a2e3db7dfb9778d52522ce1e805c3e29cb1e
    Signed-off-by: Tarun Thakur <tarun.t@altencalsoftlabs.com>

---
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/confighelpers/OvsdbTepRemoveWorker.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2016 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.itm.confighelpers;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+public class OvsdbTepRemoveWorker implements Callable<List<ListenableFuture<Void>>> {
+    private static final Logger LOG = LoggerFactory.getLogger(OvsdbTepRemoveWorker.class ) ;
+    private String tepIp;
+    private String strDpnId;
+    private String tzName;
+    private DataBroker dataBroker;
+
+    public OvsdbTepRemoveWorker(String tepIp, String strDpnId, String tzName, DataBroker broker) {
+        this.tepIp = tepIp;
+        this.strDpnId = strDpnId;
+        this.tzName = tzName;
+        this.dataBroker = broker ;
+    }
+
+    @Override
+    public List<ListenableFuture<Void>> call() throws Exception {
+        List<ListenableFuture<Void>> futures = new ArrayList<>();
+        WriteTransaction wrTx = dataBroker.newWriteOnlyTransaction();
+
+        LOG.trace("Remove TEP task is picked from DataStoreJobCoordinator for execution.");
+
+        // remove TEP received from southbound OVSDB from ITM config DS.
+        OvsdbTepRemoveConfigHelper.removeTepReceivedFromOvsdb(tepIp, strDpnId, tzName, dataBroker, wrTx);
+
+        futures.add(wrTx.submit());
+        return futures;
+    }
+}
