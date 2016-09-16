ff79822374aee471a433ad41ff21f8c2f8468eb4
Tarun Thakur <tarun.t@altencalsoftlabs.com>
2016-09-16 14:34:08 +0530

    Removing TransportZoneListener.java changes added by Sathish and Nishchya which were committed by mistake.
    Fixed TEP-update and TEP-remove issues.
    Added support for TZ name update as well.
    
    CR-100 code changes to add/update/delete TEP received from southbound and
    add TEP into ITM config datastore.
    Included yang changes of Unknown TEPs into Transport Zones.
    
    Change-Id: I77c5a2e3db7dfb9778d52522ce1e805c3e29cb1e
    Signed-off-by: Tarun Thakur <tarun.t@altencalsoftlabs.com>

---
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/listeners/ItmListenerUtils.java
@@ -0,0 +1,158 @@
+/*
+ * Copyright (c) 2016 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.itm.listeners;
+
+import com.google.common.base.Optional;
+import java.math.BigInteger;
+import java.util.List;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.itm.impl.ItmUtils;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpPrefix;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.rev160406.TransportZones;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.rev160406.transport.zones.*;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.rev160406.transport.zones.tepsnothostedintransportzone.UnknownVteps;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.rev160406.transport.zones.tepsnothostedintransportzone.UnknownVtepsBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.rev160406.transport.zones.tepsnothostedintransportzone.UnknownVtepsKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbBridgeAugmentation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbNodeAugmentation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.node.attributes.ManagedNodeEntry;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ItmListenerUtils {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ItmListenerUtils.class);
+    public static final String dummyPrefix="255.255.255.255/32";
+    public static final String dummyGateway="0.0.0.0";
+    public static final String dummyPort="";
+    public static final int dummyVlanID=0;
+
+
+    public static String getStrDatapathId(Node node) {
+        OvsdbBridgeAugmentation ovsdbBridgeAugmentation = node.getAugmentation(OvsdbBridgeAugmentation.class);
+        String datapathId = null;
+        if (ovsdbBridgeAugmentation != null && ovsdbBridgeAugmentation.getDatapathId() != null) {
+            datapathId = ovsdbBridgeAugmentation.getDatapathId().getValue();
+        }
+        return datapathId;
+    }
+
+    public static TransportZone getTransportZoneFromITMConfigDS(String tzone,DataBroker dataBroker) {
+        InstanceIdentifier<TransportZone> tzonePath = InstanceIdentifier.builder(TransportZones.class)
+                .child(TransportZone.class, new TransportZoneKey(tzone)).build();
+        Optional<TransportZone> tZoneOptional = ItmUtils.read(LogicalDatastoreType.CONFIGURATION, tzonePath,
+                dataBroker);
+        if (tZoneOptional.isPresent()) {
+            return tZoneOptional.get();
+        }
+        return null;
+    }
+
+    public static TepsNotHostedInTransportZone getUnknownTransportZoneFromITMConfigDS(String unknownTz, DataBroker dataBroker) {
+        InstanceIdentifier<TepsNotHostedInTransportZone> unknownTzPath = InstanceIdentifier.builder(TransportZones.class)
+                .child(TepsNotHostedInTransportZone.class, new TepsNotHostedInTransportZoneKey(unknownTz)).build();
+        Optional<TepsNotHostedInTransportZone> unknownTzOptional = ItmUtils.read(LogicalDatastoreType.CONFIGURATION, unknownTzPath,
+                dataBroker);
+        if (unknownTzOptional.isPresent()) {
+            return unknownTzOptional.get();
+        }
+        return null;
+    }
+
+    public static void removeUnknownTzTepIntoTepsNotHosted(String tzName, IpAddress tepIpAddress, BigInteger dpnId, DataBroker dataBroker) {
+        List<UnknownVteps> vtepList = null;
+
+        TepsNotHostedInTransportZone unknownTz = ItmListenerUtils.getUnknownTransportZoneFromITMConfigDS(tzName, dataBroker);
+        if (unknownTz == null) {
+            LOG.info("Unknown TransportZone does not exist. Nothing to do for TEP removal.");
+            return;
+        } else {
+            vtepList = unknownTz.getUnknownVteps();
+            if (vtepList == null || vtepList.isEmpty()) {
+                //  case: vtep list does not exist or it has no elements
+                LOG.info("Remove TEP in unknown TZ ({}) when no vtep-list in the TZ. Nothing to do.", tzName);
+            } else {
+                //  case: vtep list has elements
+                boolean vtepFound = false;
+
+                for (UnknownVteps vtep : vtepList) {
+                    if (vtep.getDpnId().equals(dpnId)) {
+                        vtepFound = true;
+                        break;
+                    }
+                }
+                if (vtepFound) {
+                    // vtep is found, update it with tep-ip
+                    LOG.info("Remove TEP with IP ({}) in unknown TZ ({}) as new TEP into vtep-list in the TZ.", tepIpAddress, tzName);
+                    removeVtepIntoTepsNotHosted(tzName, dpnId, dataBroker);
+                }
+            }
+        }
+    }
+
+    public static void addVtepIntoTepsNotHosted(List<UnknownVteps> updatedVtepList, IpAddress tepIpAddress,
+                                                      String tzName, BigInteger dpnId, DataBroker dataBroker) {
+        //Create TZ node path
+        InstanceIdentifier<TepsNotHostedInTransportZone> tZonepath =
+                InstanceIdentifier.builder(TransportZones.class)
+                        .child(TepsNotHostedInTransportZone.class, new TepsNotHostedInTransportZoneKey(tzName)).build();
+
+
+        // create vtep
+        UnknownVtepsKey vtepkey = new UnknownVtepsKey(dpnId);
+        UnknownVteps vtepObj = new UnknownVtepsBuilder().setDpnId(dpnId).setIpAddress(tepIpAddress).setKey(vtepkey).build();
+
+        // Add vtep obtained into unknown TZ tep list
+        updatedVtepList.add(vtepObj);
+
+
+        // create unknown TZ node with updated vtep list
+        TepsNotHostedInTransportZone updatedTzone =
+                new TepsNotHostedInTransportZoneBuilder().setKey(new TepsNotHostedInTransportZoneKey(tzName))
+                        .setZoneName(tzName).setUnknownVteps(updatedVtepList).build();
+
+        LOG.info("Adding TEP into unknown (TZ: {}, DPID: {}, TEP IP: {}) in ITM Config DS.", tzName, dpnId, tepIpAddress);
+
+        // Update TZ in Config DS.
+        ItmUtils.asyncUpdate(LogicalDatastoreType.CONFIGURATION, tZonepath, updatedTzone, dataBroker,
+                ItmUtils.DEFAULT_CALLBACK);
+    }
+
+    public static void removeVtepIntoTepsNotHosted(String tzName, BigInteger dpnId, DataBroker dataBroker) {
+
+        UnknownVtepsKey unknownVtepkey = new UnknownVtepsKey(dpnId);
+        InstanceIdentifier<UnknownVteps> vTepPath =
+                InstanceIdentifier.builder(TransportZones.class)
+                        .child(TepsNotHostedInTransportZone.class, new TepsNotHostedInTransportZoneKey(tzName))
+                        .child(UnknownVteps.class, unknownVtepkey).build();
+
+        LOG.info("Removing TEP from unknown (TZ: {}, DPID: {}) in ITM Config DS.", tzName, dpnId);
+        // remove vtep
+        ItmUtils.asyncDelete(LogicalDatastoreType.CONFIGURATION, vTepPath, dataBroker, ItmUtils.DEFAULT_CALLBACK);
+    }
+
+    public static IpPrefix getDummySubnet() {
+
+        // set prefix to dummy-subnet
+        String dummyPrefix = "255.255.255.255/32";
+        IpPrefix subnetMaskObj = null;
+
+        // Get subnet prefix
+        try {
+            subnetMaskObj = new IpPrefix(dummyPrefix.toCharArray());
+        } catch (Exception e) {
+            LOG.error("Invalid Subnet Mask. Expected: 0.0.0.0/0 to 255.255.255.255/32");
+            return null;
+        }
+        return subnetMaskObj;
+    }
+}
