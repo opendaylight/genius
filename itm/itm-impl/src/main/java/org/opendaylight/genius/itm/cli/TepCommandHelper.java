15225ab9a3467427bf848d6321696fa66b20f12e
Tarun Thakur <tarun.t@altencalsoftlabs.com>
2016-09-16 14:34:08 +0530

    Code for TEP auto-config feature in ITM module
    - to add/update/delete TEP received from southbound into ITM config datastore.
    - to perform Datastore write operations in queue using DataStoreJobCoordinator.
    
    Change-Id: I77c5a2e3db7dfb9778d52522ce1e805c3e29cb1e
    Signed-off-by: Tarun Thakur <tarun.t@altencalsoftlabs.com>

--- itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cli/TepCommandHelper.java
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cli/TepCommandHelper.java
@@ -290,24 +290,6 @@ public class TepCommandHelper {
     }
 
     /**
-     * Gets the transport zone from config ds.
-     *
-     * @param tzone
-     *            the tzone
-     * @return the transport zone
-     */
-    public TransportZone getTransportZoneFromConfigDS(String tzone) {
-        InstanceIdentifier<TransportZone> tzonePath = InstanceIdentifier.builder(TransportZones.class)
-                .child(TransportZone.class, new TransportZoneKey(tzone)).build();
-        Optional<TransportZone> tZoneOptional = ItmUtils.read(LogicalDatastoreType.CONFIGURATION, tzonePath,
-                dataBroker);
-        if (tZoneOptional.isPresent()) {
-            return tZoneOptional.get();
-        }
-        return null;
-    }
-
-    /**
      * Gets all transport zones.
      *
      * @return all transport zones
@@ -753,7 +735,7 @@ public void showCache(String cacheName) {
     public void configureTunnelType(String tZoneName, String tunnelType) {
         LOG.debug("configureTunnelType {} for transportZone {}", tunnelType, tZoneName);
 
-        TransportZone tZoneFromConfigDS = getTransportZoneFromConfigDS(tZoneName);
+        TransportZone tZoneFromConfigDS = ItmUtils.getTransportZoneFromConfigDS(tZoneName, dataBroker);
         validateTunnelType(tZoneName, tunnelType,tZoneFromConfigDS);
 
         if (tZoneFromConfigDS != null) {
