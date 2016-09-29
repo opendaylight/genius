5f9711e39795fcda894abac013206caf20377cdf
Kency Kurian <kency.kurian@ericsson.com>
2016-09-29 18:00:06 +0530

    IdManager Performance Improvements
    In order to avoid the wait time to write to the DS during allocation of id
    each local id pool(pool per cluster) can be stored in memory using
    concurrent hash map. This can be later synced with the DS using
    DatastoreJobCoordinator. In order to achieve this the following has been
    done:
    
    - Created 5 new jobs to update the local pool info DS.
      * LocalPoolCreateJob - Writes the entire local pool content from the
        cache to the DS.
      * IdHolderSyncJob - Updates either of the idHolder(Available/Released)
      * CleanUpJob - Used to return back excessive ids if any to the parent
        pool. This will be scheduled after IDLE_TIME whenever an id is
        released since that is the only time there are chances of excessive
        ids to be present in the local pool.
      * UpdateIdEntryJob - To add/delete the idEntry in the parentPool.
      * LocalPoolDeleteJob - Deletes the local pool from DS.
    - New classes to hold local id pool objects.
      * IdHolder - An interface which provides methods to allocateId, addId
        etc. which is implemented by AvailableIdHolder and ReleaseIdHolder.
      * IdLocalPool - Holds the local pool name and IdHolders for each pool.
    - JUnits test cases have also been updated.
    - Added a CLI for printing the local cache values.
    
    Change-Id: Ic39cae3b4c01ce44d1624187d2bf3099e707611e
    Signed-off-by: Kency Kurian <kency.kurian@ericsson.com>

---
+++ idmanager/idmanager-impl/src/main/java/org/opendaylight/genius/idmanager/IdPoolListener.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2016 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.idmanager;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.genius.datastoreutils.AsyncClusteredDataTreeChangeListenerBase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.IdPools;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.id.pools.IdPool;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class IdPoolListener extends AsyncClusteredDataTreeChangeListenerBase<IdPool, IdPoolListener> implements AutoCloseable {
+
+    private static final Logger LOG = LoggerFactory.getLogger(IdPoolListener.class);
+    DataBroker broker;
+    IdManager idManager;
+    public IdPoolListener(DataBroker broker, IdManager idManager) {
+        super(IdPool.class, IdPoolListener.class);
+        this.broker = broker;
+        this.idManager = idManager;
+    }
+
+    @Override
+    protected void remove(InstanceIdentifier<IdPool> identifier, IdPool del) {
+        String parentPoolName = del.getParentPoolName();
+        String poolName = del.getPoolName();
+        if (parentPoolName != null && !parentPoolName.isEmpty()) {
+            idManager.poolDeleted(parentPoolName, poolName);
+        }
+    }
+
+    @Override
+    protected void update(InstanceIdentifier<IdPool> identifier,
+            IdPool original, IdPool update) {
+        if (update.getAvailableIdsHolder() != original.getAvailableIdsHolder() || update.getReleasedIdsHolder() != original.getReleasedIdsHolder()) {
+            String parentPoolName = update.getParentPoolName();
+            String poolName = update.getPoolName();
+            if (parentPoolName != null && !parentPoolName.isEmpty()) {
+                if (!IdUtils.getPoolUpdatedMap(poolName)) {
+                    LOG.info("Received update for NAME {} : {} - {}", update.getPoolName(), original, update);
+                    idManager.updateLocalIdPoolCache(update, parentPoolName);
+                } else {
+                    IdUtils.decrementPoolUpdatedMap(poolName);
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void add(InstanceIdentifier<IdPool> identifier, IdPool add) {
+        LOG.info("Received add for name {} : {}", add.getPoolName(), add);
+    }
+
+    @Override
+    protected InstanceIdentifier<IdPool> getWildCardPath() {
+        return InstanceIdentifier.create(IdPools.class).child(IdPool.class);
+    }
+
+    @Override
+    protected IdPoolListener getDataTreeChangeListener() {
+        return IdPoolListener.this;
+    }
+
+}
---
+++ idmanager/idmanager-impl/src/main/java/org/opendaylight/genius/idmanager/jobs/LocalPoolDeleteJob.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2016 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.genius.idmanager.jobs;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.idmanager.IdUtils;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.id.pools.IdPool;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+public class LocalPoolDeleteJob implements Callable<List<ListenableFuture<Void>>> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LocalPoolDeleteJob.class);
+    private String poolName;
+    private DataBroker broker;
+
+    public LocalPoolDeleteJob(String poolName, DataBroker broker) {
+        super();
+        this.poolName = poolName;
+        this.broker = broker;
+    }
+
+    @Override
+    public List<ListenableFuture<Void>> call() throws Exception {
+        ArrayList<ListenableFuture<Void>> futures = new ArrayList<ListenableFuture<Void>>();
+        InstanceIdentifier<IdPool> idPoolToBeDeleted = IdUtils.getIdPoolInstance(poolName);
+        WriteTransaction tx = broker.newWriteOnlyTransaction();
+        tx.delete(LogicalDatastoreType.CONFIGURATION, idPoolToBeDeleted);
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Deleted local pool {}", poolName);
+        }
+        futures.add(tx.submit());
+        return futures;
+    }
+}
