e95593aa9aec9453e9ac4369dd9c87ff6738bc11
Hema Gopalkrishnan <hema.gopalkrishnan@ericsson.com>
2016-10-07 11:27:46 +0530

    Upstreaming ITM cache impl and monitoring bug fix
    Removed the Add and Remove External End Point CLI from this review
    Fixed caches related failures in JUnit Tests
    Retaining the oper-state along with tunnel-state in tunnels_state
    Changing the ITM Monitoring Cache related listeners from
    DataChangeListener to DataTreeChangeListener
    
    Change-Id: Ia6ec4f6da43b37fc82d7b47f3aec6ffaae8d01ed
    Signed-off-by: Hema Gopalkrishnan <hema.gopalkrishnan@ericsson.com>

--- itm/itm-impl/src/main/java/org/opendaylight/genius/itm/listeners/cache/ItmMonitoringListener.java
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/listeners/cache/ItmMonitoringListener.java
@@ -7,28 +7,20 @@
  */
 package org.opendaylight.genius.itm.listeners.cache;
 
-import org.opendaylight.controller.md.sal.binding.api.ClusteredDataChangeListener;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
-import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
 import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
-import org.opendaylight.genius.datastoreutils.AsyncClusteredDataChangeListenerBase;
+import org.opendaylight.genius.datastoreutils.AsyncClusteredDataTreeChangeListenerBase;
 import org.opendaylight.genius.itm.globals.ITMConstants;
 import org.opendaylight.genius.itm.listeners.TunnelMonitorChangeListener;
 import org.opendaylight.genius.utils.cache.DataStoreCache;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.config.rev160406.TunnelMonitorParams;
-import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * Created by edimjai on 8/4/2016.
- */
-public class ItmMonitoringListener  extends AsyncClusteredDataChangeListenerBase<TunnelMonitorParams, ItmMonitoringListener>
+public class ItmMonitoringListener  extends AsyncClusteredDataTreeChangeListenerBase<TunnelMonitorParams, ItmMonitoringListener>
     implements AutoCloseable {
-
-  private ListenerRegistration<DataChangeListener> listenerRegistration;
 
   private static final Logger logger = LoggerFactory.getLogger(ItmMonitoringListener.class);
 
@@ -36,9 +28,7 @@ public class ItmMonitoringListener  extends AsyncClusteredDataChangeListenerBase
     super(TunnelMonitorParams.class, ItmMonitoringListener.class);
 
     try {
-      listenerRegistration = broker.registerDataChangeListener(LogicalDatastoreType.OPERATIONAL,
-          getWildCardPath(), this,
-          AsyncDataBroker.DataChangeScope.BASE);
+      registerListener(LogicalDatastoreType.OPERATIONAL, broker);
     } catch (final Exception e) {
       logger.error("ItmMonitoring DataChange listener registration fail!", e);
     }
@@ -46,14 +36,6 @@ public class ItmMonitoringListener  extends AsyncClusteredDataChangeListenerBase
 
   @Override
   public void close() {
-    if (listenerRegistration != null) {
-      try {
-        listenerRegistration.close();
-      } catch (final Exception e) {
-        logger.error("Error when cleaning up DataChangeListener.", e);
-      }
-      listenerRegistration = null;
-    }
     logger.info("ItmMonitoring listener Closed");
   }
 
@@ -79,19 +61,8 @@ public class ItmMonitoringListener  extends AsyncClusteredDataChangeListenerBase
     return InstanceIdentifier.create(TunnelMonitorParams.class);
   }
 
-  @Override
-  protected ClusteredDataChangeListener getDataChangeListener() {
+  protected ItmMonitoringListener getDataTreeChangeListener() {
     return this;
   }
 
-  @Override
-  protected AsyncDataBroker.DataChangeScope getDataChangeScope() {
-    return AsyncDataBroker.DataChangeScope.BASE;
-  }
-
-
-
 }
-
-
-
