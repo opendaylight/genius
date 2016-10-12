46e2f98469c0f409381e3c98ec8bdfa32108e102
Chintan Apte <chintan.apte@ericsson.com>
2016-10-12 16:06:11 +0530

    ODL Bug: 6095, bundle:diag failing for ITM bundle. UT:- RemoveExternalEndpoint is pointing to a vpnservice package which is causing the issue, Started the Karaf and checked the bundle status and diag. coming up jjst fine.
    Change-Id: I76613223e5819154e6f505e42ba435500a8f5a28
    Signed-off-by: Chintan Apte <chintan.apte@ericsson.com>

--- itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cli/RemoveExternalEndpoint.java
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cli/RemoveExternalEndpoint.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.vpnservice.itm.cli;
+package org.opendaylight.genius.itm.cli;
 import java.math.BigInteger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
