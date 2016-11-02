b3889453dec9c59d5bb2ac3392fd2551f402049a
Michael Vorburger <vorburger@redhat.com>
2016-11-02 15:59:28 +0100

    InterfaceManagerTest as a new-style component/API/end2end test
    Change-Id: I058b4cdeab4be2b01b4172a5376ad1fe4597ea27
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ interfacemanager/interfacemanager-impl/src/test/java/org/opendaylight/genius/interfacemanager/test/InterfaceManagerTestModule.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.interfacemanager.test;
+
+import org.opendaylight.infrautils.inject.guice.testutils.AbstractGuiceJsr250Module;
+
+/**
+ * Dependency Injectin Wiring for {@link InterfaceManagerTest}.
+ *
+ * @author Michael Vorburger
+ */
+public class InterfaceManagerTestModule extends AbstractGuiceJsr250Module {
+
+    @Override
+    protected void configureBindings() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
