42223985b1f1c1e239f82fc5379f45e004e11b7b
David Suarez <david.suarez.fuentes@ericsson.com>
2016-11-10 00:30:18 +0100

    Refactor and checkstyle changes.
    Change-Id: Icdbb507c06e677ff0968105f26f717261af0fa65
    Signed-off-by: David Suarez <david.suarez.fuentes@ericsson.com>

--- fcapsmanager/countermanager/src/main/java/org/opendaylight/genius/fcapsmanager/countermanager/PMRegistrationListener.java
+++ fcapsmanager/countermanager/src/main/java/org/opendaylight/genius/fcapsmanager/countermanager/PMRegistrationListener.java
@@ -8,10 +8,10 @@
 package org.opendaylight.genius.fcapsmanager.countermanager;
 
 import java.lang.management.ManagementFactory;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
-
 import javax.management.InstanceNotFoundException;
 import javax.management.MBeanServer;
 import javax.management.MBeanServerNotification;
@@ -20,70 +20,77 @@ import javax.management.Notification;
 import javax.management.NotificationFilterSupport;
 import javax.management.NotificationListener;
 import javax.management.ObjectName;
-
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PMRegistrationListener implements Runnable {
-    private final static org.slf4j.Logger LOG = LoggerFactory.getLogger(PMRegistrationListener.class);
-    static MBeanServer mbs = null;
+    private static final Logger LOG = LoggerFactory.getLogger(PMRegistrationListener.class);
+    private static MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
 
-    private static String DOMAIN = "SDNC.PM";
-    public static HashSet<ObjectName> beanNames = new HashSet<ObjectName>();
+    private boolean shouldContinue = true;
+    private static final String DOMAIN = "SDNC.PM";
+    private static final String MBEAN_REGISTERED = "JMX.mbean.registered";
+    private static final String MBEAN_UNREGISTERED = "JMX.mbean.unregistered";
+    protected static Collection<ObjectName> beanNames = new HashSet<>();
     private BundleContext context = null;
 
-    public PMRegistrationListener(BundleContext context){
-        this.context=context;
+    public PMRegistrationListener(BundleContext context) {
+        this.context = context;
+    }
+
+    public void setShouldContinue(boolean shouldContinue) {
+        this.shouldContinue = shouldContinue;
     }
 
     /**
-     * Gets register notification when a mbean is registered in platform Mbeanserver and checks if it is counter mbean and add it to the map.
+     * Gets register notification when a mbean is registered in platform
+     * Mbeanserver and checks if it is counter mbean and add it to the map.
      */
     public static class DelegateListener implements NotificationListener {
+
+        @Override
         public void handleNotification(Notification notification, Object obj) {
             if (notification instanceof MBeanServerNotification) {
-                MBeanServerNotification msnotification =
-                        (MBeanServerNotification) notification;
-                String nType = msnotification.getType();
+                MBeanServerNotification msnotification = (MBeanServerNotification) notification;
+                String notificationType = msnotification.getType();
                 ObjectName mbn = msnotification.getMBeanName();
-                if (nType.equals("JMX.mbean.registered")) {
+
+                if (MBEAN_REGISTERED.equals(notificationType)) {
                     String mbean = mbn.toString();
-                    if(mbean.contains(DOMAIN)) {
+                    if (mbean.contains(DOMAIN)) {
                         beanNames.add(mbn);
-                        LOG.debug("Beans are " +beanNames);
+                        LOG.debug("Beans are " + beanNames);
                     }
                 }
-                if (nType.equals("JMX.mbean.unregistered")) {
-                    if(mbn.toString().contains(DOMAIN)) {
-                        beanNames.remove(mbn);
-                        LOG.debug(mbn +" MBean has been unregistered");
-                    }
+                if (MBEAN_UNREGISTERED.equals(notificationType) && mbn.toString().contains(DOMAIN)) {
+                    beanNames.remove(mbn);
+                    LOG.error(mbn + " MBean has been unregistered");
                 }
             }
         }
     }
 
     @Override
-    public void run(){
-        mbs = ManagementFactory.getPlatformMBeanServer();
+    public void run() {
         queryMbeans();
         DelegateListener delegateListener = new DelegateListener();
         ObjectName delegate = null;
         try {
             delegate = new ObjectName("JMImplementation:type=MBeanServerDelegate");
         } catch (MalformedObjectNameException e) {
-            e.printStackTrace();
+            LOG.error("Malformed object name: {} ", e);
         }
         NotificationFilterSupport filter = new NotificationFilterSupport();
 
-        filter.enableType("JMX.mbean.registered");
-        filter.enableType("JMX.mbean.unregistered");
+        filter.enableType(MBEAN_REGISTERED);
+        filter.enableType(MBEAN_UNREGISTERED);
 
         LOG.debug("Add PM Registeration Notification Listener");
         try {
-            mbs.addNotificationListener(delegate, delegateListener, filter,null);
-        }catch (InstanceNotFoundException e) {
-            e.printStackTrace();
+            mbs.addNotificationListener(delegate, delegateListener, filter, null);
+        } catch (InstanceNotFoundException e) {
+            LOG.debug("Instance not found: {}", e);
         }
         Poller poller = new Poller(this.context);
         poller.polling();
@@ -91,26 +98,26 @@ public class PMRegistrationListener implements Runnable {
     }
 
     /**
-     * Prepovising case to handle all counter mbeans which are registered before the installation of framework bundle
-     * Queries the platform Mbeanserver to retrieve registered counter mbean and add it to the map
+     * Prepovising case to handle all counter mbeans which are registered before
+     * the installation of framework bundle Queries the platform Mbeanserver to
+     * retrieve registered counter mbean and add it to the map
      */
     public void queryMbeans() {
-        Set<ObjectName> names =
-                new TreeSet<ObjectName>(mbs.queryNames(null, null));
+        Set<ObjectName> names = new TreeSet<>(mbs.queryNames(null, null));
         LOG.debug("\nQueried MBeanServer for MBeans:");
         for (ObjectName name : names) {
-            if(name.toString().contains(DOMAIN)){
+            if (name.toString().contains(DOMAIN)) {
                 beanNames.add(name);
             }
         }
     }
 
     private void waitforNotification() {
-        while(true){
+        while (shouldContinue) {
             try {
                 Thread.sleep(50);
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                shouldContinue = false;
             }
         }
     }
