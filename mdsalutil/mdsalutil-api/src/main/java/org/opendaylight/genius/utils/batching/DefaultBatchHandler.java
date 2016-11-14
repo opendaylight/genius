2186bc4b818d48b4d4075506b5959fd1c51fab3a
Stephen Kitt <skitt@redhat.com>
2016-11-14 18:37:31 +0100

    Tighten DefaultBatchHandler properties
    * dataBroker, batchSize and batchInterval shouldn't be static.
    * None of the properties should be public.
    
    Change-Id: Ibd0ee0e35566824ea4aedfaa5766d5de8a830f95
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/utils/batching/DefaultBatchHandler.java
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/utils/batching/DefaultBatchHandler.java
@@ -8,24 +8,20 @@
 
 package org.opendaylight.genius.utils.batching;
 
-import org.opendaylight.controller.md.sal.binding.api.BindingTransactionChain;
+import java.util.List;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
-import java.util.List;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-
 
 public class DefaultBatchHandler implements ResourceHandler {
 
-    private static DataBroker dataBroker;
-    public static Integer batchSize;
-    public static Integer batchInterval;
-    public LogicalDatastoreType datastoreType;
+    private DataBroker dataBroker;
+    private Integer batchSize;
+    private Integer batchInterval;
+    private LogicalDatastoreType datastoreType;
 
     public DefaultBatchHandler(DataBroker  dataBroker,LogicalDatastoreType dataStoreType,Integer batchSize,Integer batchInterval) {
 
