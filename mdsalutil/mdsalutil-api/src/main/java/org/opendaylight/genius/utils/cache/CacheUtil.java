a2a35bbeb89499d7779109c7556bac00721b4442
Michael Vorburger <vorburger@redhat.com>
2016-11-24 20:13:36 +0100

    @deprecate CacheUtil
    For an example of how to simply replace this fairly useless utility
    class with a ConcurrentHashMap in a project, please see the upcoming
    next Gerrit doing that in idmanager.
    
    Change-Id: I2c512aafba34a8eef83d3935c12f7048b96af471
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/utils/cache/CacheUtil.java
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/utils/cache/CacheUtil.java
@@ -10,24 +10,52 @@ package org.opendaylight.genius.utils.cache;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
+/**
+ * Cache wannabe.
+ *
+ * <p>This class wanted to be Cache when it was grown up. Currently it's actually just
+ * a nested ConcurrentMap, and thus has fairly limited real value.
+ *
+ * <p>The usage of static methods here, instead of an (OSGi) "service"
+ * (dependency inject-able in tests!) makes it impossible to easily properly
+ * use code relying on this in component tests (as there would be no automatic
+ * reset between tests; you would have to manually {@link #destroyCache(String)}
+ * {@literal @}Before each test).
+ *
+ * <p>This class' "static" Singleton doesn't play nice with OSGi e.g. for hot reload.
+ *
+ * <p>This class' (necessary) use <?> generics causes @SuppressWarnings("unchecked") when used.
+ *
+ * <p>Perhaps you would like to use <a href="https://github.com/google/guava/wiki/CachesExplained">
+ * Google Guava's simple Caches</a>, if not a full blown JSR 107 javax.cache (JCache) implementation,
+ * such as <a href="http://infinispan.org">Infinispan</a> or <a href="http://www.ehcache.org">Ehcache</a>,
+ * instead of this class?
+ *
+ * @deprecated We now recommend you simply use your own new ConcurrentHashMap<>() instead.
+ *
+ * @author unascribed (Ericsson India?) - original code
+ * @author Michael Vorburger.ch - JavaDoc
+ */
+@Deprecated
 public class CacheUtil {
-    private static final ConcurrentMap<String, ConcurrentMap<?, ?>> m_mapCache =
-            new ConcurrentHashMap<>();
 
-    public static ConcurrentMap<?, ?> getCache(String sCacheName) {
-        return m_mapCache.get(sCacheName);
+    private static final ConcurrentMap<String, ConcurrentMap<?, ?>> MAP_OF_MAP = new ConcurrentHashMap<>();
+
+    public static ConcurrentMap<?, ?> getCache(String cacheName) {
+        return MAP_OF_MAP.get(cacheName);
     }
 
-    public static void createCache(String sCacheName) {
-        if (m_mapCache.get(sCacheName) == null)
-            m_mapCache.put(sCacheName, new ConcurrentHashMap<>());
+    public static void createCache(String cacheName) {
+        if (MAP_OF_MAP.get(cacheName) == null) {
+            MAP_OF_MAP.put(cacheName, new ConcurrentHashMap<>());
+        }
     }
 
-    public static boolean isCacheValid( String cacheName ) {
-        return m_mapCache.containsKey(cacheName) ;
+    public static boolean isCacheValid(String cacheName) {
+        return MAP_OF_MAP.containsKey(cacheName);
     }
 
-    public static void destroyCache(String sCacheName) {
-        m_mapCache.remove(sCacheName);
+    public static void destroyCache(String cacheName) {
+        MAP_OF_MAP.remove(cacheName);
     }
 }
