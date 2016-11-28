ca237e431963b134fd16aa5109116d4c09454f37
Stephen Kitt <skitt@redhat.com>
2016-11-28 13:46:21 +0100

    Add new ActionInfo implementations for reg load/move
    This illustrates a pattern for ActionInfo implementations with strict
    typing and less coupling in ActionInfo/ActionType.
    
    Change-Id: I5bcbe039ecc5d871ef9b1ee11c88822231e92ef7
    Signed-off-by: Stephen Kitt <skitt@redhat.com>
    (cherry picked from commit 5d441a8a25c2acb8becdf2299dfc22d83a1f8bf7)

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/actions/ActionRegMove.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright © 2016 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.actions;
+
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.genius.mdsalutil.ActionType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.match.rev140421.NxmNxReg;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.dst.choice.grouping.dst.choice.DstOfMplsLabelCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nodes.node.table.flow.instructions.instruction.instruction.apply.actions._case.apply.actions.action.action.NxActionRegMoveNodesNodeTableFlowApplyActionsCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.move.grouping.NxRegMoveBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.move.grouping.nx.reg.move.Src;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.move.grouping.nx.reg.move.SrcBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.src.choice.grouping.src.choice.SrcNxRegCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.move.grouping.nx.reg.move.Dst;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.move.grouping.nx.reg.move.DstBuilder;
+
+/**
+ * Action to move an NXM register.
+ */
+public class ActionRegMove extends ActionInfo {
+    private final Class<? extends NxmNxReg> register;
+    private final int start;
+    private final int end;
+
+    public ActionRegMove(Class<? extends NxmNxReg> register, int start, int end) {
+        super(ActionType.nx_load_reg, new String[0]);
+        this.register = register;
+        this.start = start;
+        this.end = end;
+    }
+
+    public ActionRegMove(int actionKey, Class<? extends NxmNxReg> register, int start, int end) {
+        super(ActionType.nx_load_reg, new String[0], actionKey);
+        this.register = register;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public Action buildAction() {
+        return buildAction(getActionKey());
+    }
+
+    public Action buildAction(int newActionKey) {
+        Src src = new SrcBuilder()
+                .setSrcChoice(new SrcNxRegCaseBuilder().setNxReg(register).build())
+                .setStart(start)
+                .setEnd(end)
+                .build();
+
+        Dst dst = new DstBuilder()
+                .setDstChoice(new DstOfMplsLabelCaseBuilder().setOfMplsLabel(true).build())
+                .setStart(start)
+                .setEnd(end)
+                .build();
+
+        NxRegMoveBuilder nxRegMoveBuilder = new NxRegMoveBuilder().setSrc(src).setDst(dst);
+        return new ActionBuilder()
+                .setAction(new NxActionRegMoveNodesNodeTableFlowApplyActionsCaseBuilder().setNxRegMove(
+                        nxRegMoveBuilder.build()).build())
+                .setKey(new ActionKey(newActionKey))
+                .build();
+    }
+
+    public Class<? extends NxmNxReg> getRegister() {
+        return register;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+}
