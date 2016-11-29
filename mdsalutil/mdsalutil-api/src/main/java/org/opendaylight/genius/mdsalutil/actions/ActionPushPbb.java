2c6fb044d0db3d374825d5d467726d8f52b892ab
Stephen Kitt <skitt@redhat.com>
2016-11-29 15:16:54 +0100

    Refactor push/pop MPLS/PBB/VLAN actions
    Change-Id: I98055271b0924c7828d1f1b96b863f8cbc96ac15
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/actions/ActionPushPbb.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright © 2016 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.actions;
+
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.genius.mdsalutil.ActionType;
+import org.opendaylight.genius.mdsalutil.NwConstants;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.PushPbbActionCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.push.pbb.action._case.PushPbbActionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionKey;
+
+/**
+ * Push PBB action.
+ */
+public class ActionPushPbb extends ActionInfo {
+    public ActionPushPbb() {
+        super(ActionType.push_pbb, new String[0]);
+    }
+
+    @Override
+    public Action buildAction() {
+        return buildAction(getActionKey());
+    }
+
+    public Action buildAction(int newActionKey) {
+        return new ActionBuilder().setAction(
+                new PushPbbActionCaseBuilder().setPushPbbAction(
+                        new PushPbbActionBuilder()
+                                .setEthernetType(NwConstants.ETHTYPE_PBB).build()).build())
+                .setKey(new ActionKey(newActionKey)).build();
+    }
+}
