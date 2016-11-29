48d8421171e0c451105d0f7fe855dc003b9f8c8e
Stephen Kitt <skitt@redhat.com>
2016-11-29 12:11:55 +0100

    Migrate ActionType.group to ActionGroup
    ActionType.group is backwards-compatible and can handle both
    ActionInfo (old-style) and ActionGroup (new-style).
    
    Change-Id: I963b80b68018aa3a827400381186695e97822cc3
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/test/java/org/opendaylight/genius/mdsalutil/actions/ActionGroupTest.java
@@ -0,0 +1,45 @@
+package org.opendaylight.genius.mdsalutil.actions;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import ch.vorburger.xtendbeans.XtendBeanGenerator;
+import org.junit.Test;
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.genius.mdsalutil.ActionType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.GroupActionCase;
+
+/**
+ * Test class for {@link ActionGroup}.
+ */
+public class ActionGroupTest {
+    XtendBeanGenerator generator = new XtendBeanGenerator();
+
+    @Test
+    public void actionInfoTestForGroupAction() {
+        ActionInfo actionInfo = new ActionGroup(123);
+        Action action = actionInfo.buildAction();
+        assertTrue(action.getAction() instanceof GroupActionCase);
+        GroupActionCase actionCase = (GroupActionCase) action.getAction();
+        assertEquals(123L, (long) actionCase.getGroupAction().getGroupId());
+    }
+
+    @Test
+    public void backwardsCompatibleAction() {
+        ActionInfo actionInfo = new ActionInfo(ActionType.group, new String[] {"123"});
+        Action action = actionInfo.buildAction();
+        assertTrue(action.getAction() instanceof GroupActionCase);
+        GroupActionCase actionCase = (GroupActionCase) action.getAction();
+        assertEquals(123L, (long) actionCase.getGroupAction().getGroupId());
+    }
+
+    @Test
+    public void generateActionGroup() {
+        ActionInfo actionGroup = new ActionGroup(123);
+        actionGroup.buildAction();
+        assertEquals("(new ActionGroupBuilder => [\n" +
+                "    groupId = 123L\n" +
+                "]).build()", generator.getExpression(actionGroup));
+    }
+}
