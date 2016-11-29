89944dafcf3ad11ced85c6567d95eee69cb604f7
Stephen Kitt <skitt@redhat.com>
2016-11-29 15:16:54 +0100

    Refactor push/pop MPLS/PBB/VLAN actions
    Change-Id: I98055271b0924c7828d1f1b96b863f8cbc96ac15
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/actions/ActionPushVlan.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright © 2016 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.actions;
+
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.genius.mdsalutil.ActionType;
+import org.opendaylight.genius.mdsalutil.NwConstants;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.PushVlanActionCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.push.vlan.action._case.PushVlanActionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionKey;
+
+/**
+ * Push VLAN action.
+ */
+public class ActionPushVlan extends ActionInfo {
+    public ActionPushVlan() {
+        super(ActionType.push_vlan, new String[0]);
+    }
+
+    public ActionPushVlan(int actionKey) {
+        super(ActionType.push_vlan, new String[0], actionKey);
+    }
+
+    @Override
+    public Action buildAction() {
+        return buildAction(getActionKey());
+    }
+
+    public Action buildAction(int newActionKey) {
+        return new ActionBuilder().setAction(
+                new PushVlanActionCaseBuilder().setPushVlanAction(
+                        new PushVlanActionBuilder().setEthernetType(
+                                NwConstants.ETHTYPE_802_1Q).build()).build())
+                .setKey(new ActionKey(newActionKey)).build();
+    }
+}
