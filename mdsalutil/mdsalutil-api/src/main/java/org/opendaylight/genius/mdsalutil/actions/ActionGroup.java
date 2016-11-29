560d309988f6a37ba4f3f360a1f5cb2fcd5794fd
Stephen Kitt <skitt@redhat.com>
2016-11-29 12:11:55 +0100

    Migrate ActionType.group to ActionGroup
    ActionType.group is backwards-compatible and can handle both
    ActionInfo (old-style) and ActionGroup (new-style).
    
    Change-Id: I963b80b68018aa3a827400381186695e97822cc3
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/actions/ActionGroup.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright © 2016 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.actions;
+
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.genius.mdsalutil.ActionType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.GroupActionCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.group.action._case.GroupActionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionKey;
+
+/**
+ * Group action.
+ */
+public class ActionGroup extends ActionInfo {
+    private final long groupId;
+
+    public ActionGroup(long groupId) {
+        super(ActionType.group, new String[] { Long.toString(groupId) });
+        this.groupId = groupId;
+    }
+
+    @Override
+    public Action buildAction() {
+        return buildAction(getActionKey());
+    }
+
+    public Action buildAction(int newActionKey) {
+        return new ActionBuilder().setAction(
+                new GroupActionCaseBuilder().setGroupAction(
+                        new GroupActionBuilder().setGroupId(groupId).build()).build())
+                .setKey(new ActionKey(newActionKey)).build();
+    }
+}
