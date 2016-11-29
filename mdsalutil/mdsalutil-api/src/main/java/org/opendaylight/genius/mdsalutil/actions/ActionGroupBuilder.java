b048900853ffb10153d2f332bbfe407405b6bdba
Stephen Kitt <skitt@redhat.com>
2016-11-29 12:11:55 +0100

    Migrate ActionType.group to ActionGroup
    ActionType.group is backwards-compatible and can handle both
    ActionInfo (old-style) and ActionGroup (new-style).
    
    Change-Id: I963b80b68018aa3a827400381186695e97822cc3
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/actions/ActionGroupBuilder.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright © 2016 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.actions;
+
+import org.opendaylight.yangtools.concepts.Builder;
+
+/**
+ * Builder for {@link ActionGroup}.
+ */
+public class ActionGroupBuilder implements Builder<ActionGroup> {
+    private long groupId;
+
+    @Override
+    public ActionGroup build() {
+        return new ActionGroup(groupId);
+    }
+
+    public long getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(long groupId) {
+        this.groupId = groupId;
+    }
+}
