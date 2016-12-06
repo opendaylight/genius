10db32d6868b742b8c58705dbe3b31d1ceba9b85
gowri <gowri.r@ericsson.com>
2016-12-06 13:51:27 +0530

    FcapsApplication module migration to blueprint
    Fcaps Port counter enhancement : Port counter to retain port count upon switch disconnection
    
    Change-Id: Iec2908d5aea02d9eb9e10c06b45179247b0a99b7
    Signed-off-by: gowri <gowri.r@ericsson.com>

--- fcapsapplication/fcapsapplication-impl/src/main/java/org/opendaylight/genius/fcapsapp/NodeEventListener.java
+++ fcapsapplication/fcapsapplication-impl/src/main/java/org/opendaylight/genius/fcapsapp/NodeEventListener.java
@@ -25,6 +25,11 @@ import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Singleton;
+
+@Singleton
 public class NodeEventListener<D extends DataObject> implements ClusteredDataTreeChangeListener<D>,AutoCloseable {
     private static final Logger LOG = LoggerFactory.getLogger(NodeEventListener.class);
     public static final AlarmAgent alarmAgent = new AlarmAgent();
@@ -34,10 +39,15 @@ public class NodeEventListener<D extends DataObject> implements ClusteredDataTre
 
     /**
      * Construcor set EntityOwnershipService
-     * @param eos
+     * @param entityOwnershipService
      */
-    public NodeEventListener(final EntityOwnershipService eos) {
-        this.entityOwnershipService = eos;
+    public NodeEventListener(final EntityOwnershipService entityOwnershipService) {
+        this.entityOwnershipService = entityOwnershipService;
+    }
+
+    @PostConstruct
+    public void start() throws Exception {
+        LOG.info("NodeEventListener started");
     }
 
     @Override
@@ -122,7 +132,9 @@ public class NodeEventListener<D extends DataObject> implements ClusteredDataTre
         return this.entityOwnershipService.getOwnershipState(entity).transform(EntityOwnershipState::isOwner).or(false);
     }
 
+    @PreDestroy
     @Override
     public void close() throws Exception {
+        LOG.info("NodeEventListener closed");
     }
 }
