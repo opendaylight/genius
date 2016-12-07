2725e918e481f404c4adaa7de92736e8cfb838a6
Stephen Kitt <skitt@redhat.com>
2016-12-07 11:45:03 +0100

    Action redesign: ActionType.set_field_...
    This patch switches MPLS labels, PBB ISID, VLAN and tunnel id handling
    to the new model.
    
    Change-Id: I18b621016f490bf84b4f68d1bfb78e14addc6030
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/actions/ActionSetFieldTunnelId.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.actions;
+
+import java.math.BigInteger;
+import javax.annotation.Nullable;
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.genius.mdsalutil.ActionType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.SetFieldCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.set.field._case.SetFieldBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.match.TunnelBuilder;
+
+/**
+ * Set tunnel id field action.
+ */
+public class ActionSetFieldTunnelId extends ActionInfo {
+    private final BigInteger tunnelId;
+    @Nullable private final BigInteger tunnelMask;
+
+    public ActionSetFieldTunnelId(BigInteger tunnelId) {
+        this(0, tunnelId);
+    }
+
+    public ActionSetFieldTunnelId(int actionKey, BigInteger tunnelId) {
+        this(actionKey, tunnelId, null);
+    }
+
+    public ActionSetFieldTunnelId(BigInteger tunnelId, BigInteger tunnelMask) {
+        this(0, tunnelId, tunnelMask);
+    }
+
+    public ActionSetFieldTunnelId(int actionKey, BigInteger tunnelId, BigInteger tunnelMask) {
+        super(ActionType.set_field_tunnel_id,
+            tunnelMask == null ? new BigInteger[] {tunnelId} : new BigInteger[] {tunnelId, tunnelMask}, actionKey);
+        this.tunnelId = tunnelId;
+        this.tunnelMask = tunnelMask;
+    }
+
+    @Deprecated
+    public ActionSetFieldTunnelId(ActionInfo actionInfo) {
+        this(actionInfo.getActionKey(), actionInfo.getBigActionValues()[0],
+            (actionInfo.getBigActionValues().length > 1) ? actionInfo.getBigActionValues()[1] : null);
+    }
+
+    @Override
+    public Action buildAction() {
+        return buildAction(getActionKey());
+    }
+
+    public Action buildAction(int newActionKey) {
+        TunnelBuilder tunnelBuilder = new TunnelBuilder()
+            .setTunnelId(tunnelId);
+        if (tunnelMask != null) {
+            tunnelBuilder.setTunnelMask(tunnelMask);
+        }
+        return new ActionBuilder()
+            .setAction(
+                new SetFieldCaseBuilder()
+                    .setSetField(
+                        new SetFieldBuilder()
+                            .setTunnel(tunnelBuilder.build())
+                            .build())
+                    .build())
+            .setKey(new ActionKey(newActionKey))
+            .build();
+    }
+}
