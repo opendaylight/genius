e1a10c3b3320a6a95cdb5389d5e81e79b4b63188
David Suarez <david.suarez.fuentes@ericsson.com>
2016-12-07 23:17:30 +0100

    Declare logger properly.
    Declare logger properly and other formatting and cleanup changes.
    
    Change-Id: Iecea96b426c1e7b30868eebe124c34427bc06f9b
    Signed-off-by: David Suarez <david.suarez.fuentes@ericsson.com>

--- itm/itm-impl/src/main/java/org/opendaylight/genius/itm/monitoring/JMXAlarmAgent.java
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/monitoring/JMXAlarmAgent.java
@@ -7,20 +7,21 @@
  */
 package org.opendaylight.genius.itm.monitoring;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.management.ManagementFactory;
 import javax.management.MBeanServer;
 import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
-import java.lang.management.ManagementFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JMXAlarmAgent {
-    static Logger s_logger = LoggerFactory.getLogger(JMXAlarmAgent.class);
-    private MBeanServer mbs = null;
-    private ObjectName alarmName = null;
+    private static final Logger LOG = LoggerFactory.getLogger(JMXAlarmAgent.class);
     private static final String BEANNAME = "SDNC.FM:name=DataPathAlarmBean";
-    private static DataPathAlarm alarmBean= new DataPathAlarm();
+
+    private final MBeanServer mbs;
+    private ObjectName alarmName;
+
+    private static DataPathAlarm alarmBean = new DataPathAlarm();
 
     public JMXAlarmAgent() {
         // Get the platform MBeanServer
@@ -28,49 +29,52 @@ public class JMXAlarmAgent {
         try {
             alarmName = new ObjectName(BEANNAME);
         } catch (MalformedObjectNameException e) {
-            s_logger.error("ObjectName instance creation failed for BEANAME {} : {}",BEANNAME, e);
+            LOG.error("ObjectName instance creation failed for BEANAME {} : {}", BEANNAME, e);
         }
     }
 
     public void registerMbean() {
         // Unique identification of MBeans
         try {
-            // Uniquely identify the MBeans and register them with the platform MBeanServer
-            if(!mbs.isRegistered(alarmName)) {
+            // Uniquely identify the MBeans and register them with the platform
+            // MBeanServer
+            if (!mbs.isRegistered(alarmName)) {
                 mbs.registerMBean(alarmBean, alarmName);
-                s_logger.debug("Registered Mbean {} successfully", alarmName);
+                LOG.debug("Registered Mbean {} successfully", alarmName);
             }
-        } catch(Exception e) {
-            s_logger.error("Registeration failed for Mbean {} :{}", alarmName,e);
+        } catch (Exception e) {
+            LOG.error("Registeration failed for Mbean {} :{}", alarmName, e);
         }
     }
 
     public void unregisterMbean() {
         try {
-            if(mbs.isRegistered(alarmName)) {
+            if (mbs.isRegistered(alarmName)) {
                 mbs.unregisterMBean(alarmName);
-                s_logger.debug("Unregistered Mbean {} successfully", alarmName);
+                LOG.debug("Unregistered Mbean {} successfully", alarmName);
             }
         } catch (Exception e) {
-            s_logger.error("UnRegisteration failed for Mbean {} :{}", alarmName,e);
+            LOG.error("UnRegisteration failed for Mbean {} :{}", alarmName, e);
         }
     }
 
-    public void invokeFMraisemethod(String alarmId,String text,String src) {
+    public void invokeFMraisemethod(String alarmId, String text, String src) {
         try {
-            mbs.invoke(alarmName, "raiseAlarm", new Object[]{alarmId, text, src}, new String[]{String.class.getName(), String.class.getName(), String.class.getName()});
-            s_logger.trace("Invoked raiseAlarm function for Mbean {} with source {}", BEANNAME, src);
+            mbs.invoke(alarmName, "raiseAlarm", new Object[] { alarmId, text, src },
+                    new String[] { String.class.getName(), String.class.getName(), String.class.getName() });
+            LOG.trace("Invoked raiseAlarm function for Mbean {} with source {}", BEANNAME, src);
         } catch (Exception e) {
-            s_logger.error("Invoking raiseAlarm method failed for Mbean {} :{}", alarmName,e);
+            LOG.error("Invoking raiseAlarm method failed for Mbean {} :{}", alarmName, e);
         }
     }
 
-    public void invokeFMclearmethod(String alarmId,String text,String src) {
+    public void invokeFMclearmethod(String alarmId, String text, String src) {
         try {
-            mbs.invoke(alarmName, "clearAlarm", new Object[]{alarmId, text, src}, new String[]{String.class.getName(), String.class.getName(), String.class.getName()});
-            s_logger.trace("Invoked clearAlarm function for Mbean {} with source {}",BEANNAME,src);
+            mbs.invoke(alarmName, "clearAlarm", new Object[] { alarmId, text, src },
+                    new String[] { String.class.getName(), String.class.getName(), String.class.getName() });
+            LOG.trace("Invoked clearAlarm function for Mbean {} with source {}", BEANNAME, src);
         } catch (Exception e) {
-            s_logger.error("Invoking clearAlarm method failed for Mbean {} :{}", alarmName,e);
+            LOG.error("Invoking clearAlarm method failed for Mbean {} :{}", alarmName, e);
         }
     }
 }
