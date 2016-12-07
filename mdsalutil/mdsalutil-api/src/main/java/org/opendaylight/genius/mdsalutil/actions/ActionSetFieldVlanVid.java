87ad1c5fb68dcfaba9a4e9b6bc7f205b25687cea
Stephen Kitt <skitt@redhat.com>
2016-12-07 11:45:03 +0100

    Action redesign: ActionType.set_field_...
    This patch switches MPLS labels, PBB ISID, VLAN and tunnel id handling
    to the new model.
    
    Change-Id: I18b621016f490bf84b4f68d1bfb78e14addc6030
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/actions/ActionSetFieldVlanVid.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.actions;
+
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.genius.mdsalutil.ActionType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.SetFieldCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.set.field._case.SetFieldBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.l2.types.rev130827.VlanId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.match.VlanMatchBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.vlan.match.fields.VlanIdBuilder;
+
+/**
+ * Set VLAN VID field action.
+ */
+public class ActionSetFieldVlanVid extends ActionInfo {
+    private final int vlanId;
+
+    public ActionSetFieldVlanVid(int vlanId) {
+        this(0, vlanId);
+    }
+
+    public ActionSetFieldVlanVid(int actionKey, int vlanId) {
+        super(ActionType.set_field_pbb_isid, new String[] {Long.toString(vlanId)}, actionKey);
+        this.vlanId = vlanId;
+    }
+
+    public ActionSetFieldVlanVid(ActionInfo actionInfo) {
+        this(actionInfo.getActionKey(), Integer.parseInt(actionInfo.getActionValues()[0]));
+    }
+
+    @Override
+    public Action buildAction() {
+        return buildAction(getActionKey());
+    }
+
+    public Action buildAction(int newActionKey) {
+        return new ActionBuilder()
+            .setAction(
+                new SetFieldCaseBuilder()
+                    .setSetField(
+                        new SetFieldBuilder()
+                            .setVlanMatch(
+                                new VlanMatchBuilder()
+                                    .setVlanId(
+                                        new VlanIdBuilder()
+                                            .setVlanId(new VlanId(vlanId))
+                                            .setVlanIdPresent(true)
+                                            .build())
+                                    .build())
+                            .build())
+                    .build())
+            .setKey(new ActionKey(newActionKey))
+            .build();
+    }
+}
