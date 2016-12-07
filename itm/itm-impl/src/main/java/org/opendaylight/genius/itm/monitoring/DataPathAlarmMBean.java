b2c6dfb5776629b0bc2fbcd245a893545a8b8774
David Suarez <david.suarez.fuentes@ericsson.com>
2016-12-07 23:17:30 +0100

    Fix the JMXAlarmAgent
    - Declare logger properly.
    - More generic interface.
    - Fix lifecycle: registration and de-registration.
    - Fix logging of exceptions.
    - The ItmTunnelEventListener can still work even if the system couldn't
      initialize the JMX agent properly.
    - Cleanup and formatting.
    
    Change-Id: Iecea96b426c1e7b30868eebe124c34427bc06f9b
    Signed-off-by: David Suarez <david.suarez.fuentes@ericsson.com>

--- itm/itm-impl/src/main/java/org/opendaylight/genius/itm/monitoring/DataPathAlarmMBean.java
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/monitoring/DataPathAlarmMBean.java
@@ -7,13 +7,25 @@
  */
 package org.opendaylight.genius.itm.monitoring;
 
-import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * This is the interface for the DataPath Alarm MBean. It basically allows for
+ * raising and clearing alarms.
+ *
+ * @author Ericsson India Global Services Pvt Ltd. and others
+ *
+ */
 public interface DataPathAlarmMBean {
-    void setRaiseAlarmObject(ArrayList<String> raiseAlarmObject);
-    ArrayList<String> getRaiseAlarmObject();
-    void setClearAlarmObject(ArrayList<String> clearAlarmObject);
-    ArrayList<String> getClearAlarmObject();
+    void setRaiseAlarmObject(List<String> raiseAlarmObject);
+
+    List<String> getRaiseAlarmObject();
+
+    void setClearAlarmObject(List<String> clearAlarmObject);
+
+    List<String> getClearAlarmObject();
+
     void raiseAlarm(String alarmName, String additionalText, String source);
+
     void clearAlarm(String alarmName, String additionalText, String source);
 }
