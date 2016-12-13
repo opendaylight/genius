7800d5589b34cce342a82ce9cf4f63d2619a70bb
Michael Vorburger <vorburger@redhat.com>
2016-12-13 19:22:42 +0100

    InterfaceManagerConfigurationTest w. AsyncEventsWaiter, not Thread.sleep
    incl. significant enhancements to the AsyncEventsWaiter asynchronous
    testing infrastructure related utility classes.. this is actually
    turning out to be very cool now! ;)
    
    This change has 0 impact on run-time production code performance; its
    just test infrastructure which registered chained listeners, in test
    wiring/configuration (Guice), only.
    
    Change-Id: I8ffe2e6c02523bc53b807529fffd9d7f54ec9e2c
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/datastoreutils/ChainableClusteredDataTreeChangeListenerBase.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.datastoreutils;
+
+import java.util.Collection;
+import org.opendaylight.controller.md.sal.binding.api.ClusteredDataTreeChangeListener;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeChangeListener;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeModification;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+
+/**
+ * {@link ClusteredDataTreeChangeListener} which is chain-able ({@link ChainableDataTreeChangeListener}).
+ *
+ * <p>Do *NOT* use this just in order to be able to wait for change event processing of listeners extending this
+ * in tests with the AsyncEventsWaiter.  That is not necessary - because this listener is not asynchronous.
+ * This is just intended to be used for any other cases where you would like a CDTL to be chainable, not
+ * for tests and the AsyncEventsWaiter.
+ *
+ * @author Michael Vorburger
+ */
+public abstract class ChainableClusteredDataTreeChangeListenerBase<T extends DataObject>
+        implements ClusteredDataTreeChangeListener<T>, ChainableDataTreeChangeListener<T> {
+
+    private final ChainableDataTreeChangeListenerImpl<T> chainingDelegate = new ChainableDataTreeChangeListenerImpl<>();
+
+    @Override
+    public void addAfterListener(DataTreeChangeListener<T> listener) {
+        chainingDelegate.addAfterListener(listener);
+    }
+
+    @Override
+    public final void onDataTreeChanged(Collection<DataTreeModification<T>> changes) {
+        onDataTreeChanged2(changes);
+        chainingDelegate.notifyAfterOnDataTreeChanged(changes);
+    }
+
+    protected abstract void onDataTreeChanged2(Collection<DataTreeModification<T>> changes);
+}
