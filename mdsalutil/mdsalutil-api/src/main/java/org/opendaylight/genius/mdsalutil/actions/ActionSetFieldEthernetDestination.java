ff3013ffec1f001f452944f94048aebe70b3265c
Stephen Kitt <skitt@redhat.com>
2016-12-14 15:00:50 +0100

    Action redesign: migrate set_field_eth_{dest,src}
    Change-Id: I77b1ed6a9e76d7b66b7cd43941ead50ee01482fb
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/actions/ActionSetFieldEthernetDestination.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright © 2016 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.actions;
+
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.genius.mdsalutil.ActionType;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.MacAddress;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.SetFieldCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.set.field._case.SetFieldBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.ethernet.match.fields.EthernetDestinationBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.match.EthernetMatchBuilder;
+
+/**
+ * Set Ethernet destination field action.
+ */
+public class ActionSetFieldEthernetDestination extends ActionInfo {
+    private final MacAddress destination;
+
+    public ActionSetFieldEthernetDestination(MacAddress destination) {
+        this(0, destination);
+    }
+
+    public ActionSetFieldEthernetDestination(int actionKey, MacAddress destination) {
+        super(ActionType.set_field_eth_dest, new String[] {destination.getValue()}, actionKey);
+        this.destination = destination;
+    }
+
+    @Deprecated
+    public ActionSetFieldEthernetDestination(ActionInfo actionInfo) {
+        this(actionInfo.getActionKey(), new MacAddress(actionInfo.getActionValues()[0]));
+    }
+
+    @Override
+    public Action buildAction() {
+        return buildAction(getActionKey());
+    }
+
+    public Action buildAction(int newActionKey) {
+        return new ActionBuilder()
+            .setAction(new SetFieldCaseBuilder()
+                .setSetField(new SetFieldBuilder()
+                    .setEthernetMatch(new EthernetMatchBuilder()
+                        .setEthernetDestination(new EthernetDestinationBuilder().setAddress(destination).build())
+                        .build())
+                    .build())
+                .build())
+            .setKey(new ActionKey(newActionKey))
+            .build();
+    }
+
+    public MacAddress getDestination() {
+        return destination;
+    }
+}
