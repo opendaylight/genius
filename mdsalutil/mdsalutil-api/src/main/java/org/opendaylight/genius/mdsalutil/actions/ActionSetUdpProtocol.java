4e8c475042327a998dced0c0ebac9b9327c4e921
Stephen Kitt <skitt@redhat.com>
2016-12-14 15:54:15 +0100

    Action redesign: migrate set_udp_protocol
    Change-Id: Id5d73de28e83f48840e591cea7a3240ddffb9147
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/actions/ActionSetUdpProtocol.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright © 2016 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.actions;
+
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.genius.mdsalutil.ActionType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.SetFieldCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.set.field._case.SetFieldBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.match.IpMatchBuilder;
+
+/**
+ * Set UDP protocol action.
+ */
+public class ActionSetUdpProtocol extends ActionInfo {
+    public ActionSetUdpProtocol() {
+        this(0);
+    }
+
+    public ActionSetUdpProtocol(int actionKey) {
+        super(ActionType.set_udp_protocol, new String[0], actionKey);
+    }
+
+    @Deprecated
+    public ActionSetUdpProtocol(ActionInfo actionInfo) {
+        this(actionInfo.getActionKey());
+    }
+
+    @Override
+    public Action buildAction() {
+        return buildAction(getActionKey());
+    }
+
+    public Action buildAction(int newActionKey) {
+        return new ActionBuilder()
+            .setAction(new SetFieldCaseBuilder()
+                .setSetField(new SetFieldBuilder()
+                    .setIpMatch(new IpMatchBuilder()
+                        .setIpProtocol((short) 17)
+                        .build())
+                    .build())
+                .build())
+            .setKey(new ActionKey(newActionKey))
+            .build();
+    }
+}
