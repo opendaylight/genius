f08e194b90ec6a7d970f5d132d00ec0073dcf43c
Stephen Kitt <skitt@redhat.com>
2016-12-14 15:00:50 +0100

    Action redesign: migrate set_field_eth_{dest,src}
    Change-Id: I77b1ed6a9e76d7b66b7cd43941ead50ee01482fb
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/test/java/org/opendaylight/genius/mdsalutil/actions/ActionSetFieldEthernetDestinationTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright © 2016 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.actions;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import ch.vorburger.xtendbeans.XtendBeanGenerator;
+import org.junit.Test;
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.genius.mdsalutil.ActionType;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.MacAddress;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.GroupActionCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.SetFieldCase;
+
+/**
+ * Test class for {@link ActionSetFieldEthernetDestination}.
+ */
+public class ActionSetFieldEthernetDestinationTest {
+    private static final String MAC_ADDRESS = "11:22:33:44:55:66";
+
+    private XtendBeanGenerator generator = new XtendBeanGenerator();
+
+    @Test
+    public void actionInfoTest() {
+        verifyAction(new ActionSetFieldEthernetDestination(new MacAddress(MAC_ADDRESS)).buildAction());
+    }
+
+    @Test
+    public void backwardsCompatibleAction() {
+        verifyAction(new ActionInfo(ActionType.set_field_eth_dest, new String[] {MAC_ADDRESS}).buildAction());
+    }
+
+    private void verifyAction(Action action) {
+        assertTrue(action.getAction() instanceof SetFieldCase);
+        SetFieldCase actionCase = (SetFieldCase) action.getAction();
+        assertNotNull(actionCase.getSetField().getEthernetMatch());
+        assertNotNull(actionCase.getSetField().getEthernetMatch().getEthernetDestination());
+        assertEquals(MAC_ADDRESS,
+            actionCase.getSetField().getEthernetMatch().getEthernetDestination().getAddress().getValue());
+    }
+
+    @Test
+    public void generateAction() {
+        ActionInfo actionInfo = new ActionSetFieldEthernetDestination(new MacAddress(MAC_ADDRESS));
+        actionInfo.buildAction();
+        assertEquals(
+            "new ActionSetFieldEthernetDestination(0, new MacAddress(\"" + MAC_ADDRESS + "\")" + System.lineSeparator()
+                + ")" + System.lineSeparator(), generator.getExpression(actionInfo));
+    }
+}
