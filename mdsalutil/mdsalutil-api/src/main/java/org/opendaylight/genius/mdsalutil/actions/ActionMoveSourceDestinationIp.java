3e40dcedfc4649011bba34067f3aef6fec8a5b4d
Stephen Kitt <skitt@redhat.com>
2016-12-16 11:18:18 +0100

    Action redesign: migrate move_src_dst_{eth,ip}
    Change-Id: Idda8f0ee6b0ebb91514bb2562e483e47362fb973
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/actions/ActionMoveSourceDestinationIp.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright © 2016 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.actions;
+
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.genius.mdsalutil.ActionType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.dst.choice.grouping.dst.choice.DstOfIpDstCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nodes.node.table.flow.instructions.instruction.instruction.apply.actions._case.apply.actions.action.action.NxActionRegMoveNodesNodeTableFlowApplyActionsCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.move.grouping.NxRegMove;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.move.grouping.NxRegMoveBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.move.grouping.nx.reg.move.DstBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.move.grouping.nx.reg.move.SrcBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.src.choice.grouping.src.choice.SrcOfIpSrcCaseBuilder;
+
+/**
+ * Move source/destination IP action.
+ */
+public class ActionMoveSourceDestinationIp extends ActionInfo {
+    public ActionMoveSourceDestinationIp() {
+        this(0);
+    }
+
+    public ActionMoveSourceDestinationIp(int actionKey) {
+        super(ActionType.move_src_dst_ip, new String[0], actionKey);
+    }
+
+    @Deprecated
+    public ActionMoveSourceDestinationIp(ActionInfo actionInfo) {
+        this();
+    }
+
+    @Override
+    public Action buildAction() {
+        return buildAction(getActionKey());
+    }
+
+    public Action buildAction(int newActionKey) {
+        ActionBuilder ab = new ActionBuilder();
+        NxRegMove regMove = new NxRegMoveBuilder()
+            .setSrc(new SrcBuilder()
+                .setSrcChoice(new SrcOfIpSrcCaseBuilder().setOfIpSrc(Boolean.TRUE).build())
+                .setStart(0)
+                .build())
+            .setDst(new DstBuilder()
+                .setDstChoice(new DstOfIpDstCaseBuilder().setOfIpDst(Boolean.TRUE).build())
+                .setStart(0)
+                .setEnd(31)
+                .build())
+            .build();
+        ab.setAction(new NxActionRegMoveNodesNodeTableFlowApplyActionsCaseBuilder().setNxRegMove(regMove).build());
+        ab.setKey(new ActionKey(newActionKey));
+        return ab.build();
+    }
+}
