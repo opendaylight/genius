b550f5d8e6c0e1175eb64de02afbcf9d250c06db
Michael Vorburger <vorburger@redhat.com>
2016-12-16 18:43:46 +0100

    ITM new style component test (empty)
    It's empty, but still already very useful as-is, because even without
    real test methods in it yet it will already ensure non-regression at
    least of all the internal binding wiring start-up.
    
    This change all introduces dumb hard-coded cache invalidation.  This is
    required because we've seen that without doing this @After ItmTest,
    other tests start to fail, because there are isolation issues.  The real
    solution is to de-static-ify all this stuff, but that's a bit more work,
    so for now, and in order to unblock and enable people to write new style
    component tests in ITM, this "hack" seems a good short term solution.
    
    Change-Id: Ie12e43d6097bc50ff95d2edbff1926e5d4520193
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ itm/itm-impl/src/test/java/org/opendaylight/genius/itm/tests/ItmTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.itm.tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.MethodRule;
+import org.opendaylight.genius.itm.impl.ItmTestUtils;
+import org.opendaylight.genius.utils.cache.CacheTestUtil;
+import org.opendaylight.infrautils.inject.guice.testutils.GuiceRule;
+
+/**
+ * Component tests for ITM.
+ */
+public class ItmTest {
+
+    public @Rule MethodRule guice = new GuiceRule(ItmTestModule.class);
+
+    @Before
+    public void before() {
+        // so that any @Test methods here which do something real
+        // are isolated from any other test that run before this ItmTest
+        clearCaches();
+    }
+
+    @After
+    public void after() {
+        // so that any other *Test which will run after us has a clean slate again
+        clearCaches();
+    }
+
+    private void clearCaches() {
+        // Explicitly clear stupid static caches
+        // (TODO which really need to be de-static-ified instead of doing this..)
+        ItmTestUtils.clearAllItmCaches();
+        CacheTestUtil.clearAllCaches();
+    }
+
+    @Test
+    public void testWiring() throws Exception {
+        // do nothing; still valuable to check that ItmTestModule works and everything comes up
+    }
+}
---
+++ itm/itm-impl/src/test/java/org/opendaylight/genius/itm/tests/ItmTestModule.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.itm.tests;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.test.DataBrokerTestModule;
+import org.opendaylight.genius.idmanager.IdManager;
+import org.opendaylight.genius.itm.impl.ITMManager;
+import org.opendaylight.genius.itm.impl.ItmProvider;
+import org.opendaylight.genius.itm.listeners.InterfaceStateListener;
+import org.opendaylight.genius.itm.listeners.TransportZoneListener;
+import org.opendaylight.genius.itm.listeners.TunnelMonitorChangeListener;
+import org.opendaylight.genius.itm.listeners.TunnelMonitorIntervalListener;
+import org.opendaylight.genius.itm.listeners.VtepConfigSchemaListener;
+import org.opendaylight.genius.itm.listeners.cache.DpnTepsInfoListener;
+import org.opendaylight.genius.itm.listeners.cache.ItmMonitoringIntervalListener;
+import org.opendaylight.genius.itm.listeners.cache.ItmMonitoringListener;
+import org.opendaylight.genius.itm.listeners.cache.StateTunnelListListener;
+import org.opendaylight.genius.itm.monitoring.ItmTunnelEventListener;
+import org.opendaylight.genius.itm.rpc.ItmManagerRpcService;
+import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
+import org.opendaylight.genius.mdsalutil.interfaces.testutils.TestIMdsalApiManager;
+import org.opendaylight.infrautils.inject.guice.testutils.AbstractGuiceJsr250Module;
+import org.opendaylight.lockmanager.LockManager;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.IdManagerService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.rpcs.rev160406.ItmRpcService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.lockmanager.rev160413.LockManagerService;
+
+/**
+ * Dependency Injection Wiring for {@link ItmTest}.
+ *
+ * @author Michael Vorburger
+ */
+public class ItmTestModule extends AbstractGuiceJsr250Module {
+
+    @Override
+    protected void configureBindings() throws Exception {
+        // Bindings for services from this project
+        bind(ItmRpcService.class).to(ItmManagerRpcService.class);
+        bind(ITMManager.class);
+        bind(ItmProvider.class);
+
+        bind(ItmMonitoringIntervalListener.class);
+        bind(DpnTepsInfoListener.class);
+        bind(StateTunnelListListener.class);
+        bind(ItmMonitoringListener.class);
+
+        bind(TunnelMonitorIntervalListener.class);
+        bind(TransportZoneListener.class);
+        bind(InterfaceStateListener.class);
+        bind(VtepConfigSchemaListener.class);
+        bind(TunnelMonitorChangeListener.class);
+        bind(ItmTunnelEventListener.class);
+
+        // Bindings for external services to "real" implementations
+        bind(IdManagerService.class).to(IdManager.class);
+        bind(LockManagerService.class).to(LockManager.class);
+        bind(DataBroker.class).toInstance(DataBrokerTestModule.dataBroker());
+
+        // Bindings to test infra (fakes & mocks)
+        TestIMdsalApiManager mdsalManager = TestIMdsalApiManager.newInstance();
+        bind(IMdsalApiManager.class).toInstance(mdsalManager);
+        bind(TestIMdsalApiManager.class).toInstance(mdsalManager);
+    }
+
+}
