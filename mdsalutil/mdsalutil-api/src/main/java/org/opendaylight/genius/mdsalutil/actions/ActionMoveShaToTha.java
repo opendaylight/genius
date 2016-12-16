fc401e088e29a088868ac142e69d6cb1a8876ba4
Stephen Kitt <skitt@redhat.com>
2016-12-16 16:06:41 +0100

    Action redesign: migrate NICIRA ARP extensions
    Change-Id: I64583a3f57028af371ff1f0c7a8cb2359c384501
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/actions/ActionMoveShaToTha.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.actions;
+
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.genius.mdsalutil.ActionType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.dst.choice.grouping.dst.choice.DstNxArpThaCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nodes.node.table.flow.instructions.instruction.instruction.apply.actions._case.apply.actions.action.action.NxActionRegMoveNodesNodeTableFlowApplyActionsCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.move.grouping.NxRegMoveBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.move.grouping.nx.reg.move.DstBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.move.grouping.nx.reg.move.SrcBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.src.choice.grouping.src.choice.SrcNxArpShaCaseBuilder;
+
+/**
+ * Move Source Hardware address to Destination address, to where the ARP
+ * response need to be addressed to.
+ */
+public class ActionMoveShaToTha extends ActionInfo {
+    public ActionMoveShaToTha() {
+        this(0);
+    }
+
+    public ActionMoveShaToTha(int actionKey) {
+        super(ActionType.move_sha_to_tha, new String[0], actionKey);
+    }
+
+    @Deprecated
+    public ActionMoveShaToTha(ActionInfo actionInfo) {
+        this(actionInfo.getActionKey());
+    }
+
+    @Override
+    public Action buildAction() {
+        return buildAction(getActionKey());
+    }
+
+    public Action buildAction(int newActionKey) {
+        // Length of the SHA is 6 byte, hence the end offset bit is 47
+        return new ActionBuilder()
+            .setAction(new NxActionRegMoveNodesNodeTableFlowApplyActionsCaseBuilder()
+                .setNxRegMove(new NxRegMoveBuilder()
+                    .setSrc(new SrcBuilder()
+                        .setSrcChoice(new SrcNxArpShaCaseBuilder().setNxArpSha(true).build())
+                        .setStart(0)
+                        .setEnd(47)
+                        .build())
+                    .setDst(new DstBuilder()
+                        .setDstChoice(new DstNxArpThaCaseBuilder().setNxArpTha(true).build())
+                        .setStart(0)
+                        .setEnd(47)
+                        .build())
+                    .build())
+                .build())
+            .setKey(new ActionKey(newActionKey))
+            .build();
+    }
+}
---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/actions/ActionSetArpOp.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.actions;
+
+import java.math.BigInteger;
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.genius.mdsalutil.ActionType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.dst.choice.grouping.dst.choice.DstOfArpOpCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nodes.node.table.flow.instructions.instruction.instruction.apply.actions._case.apply.actions.action.action.NxActionRegLoadNodesNodeTableFlowApplyActionsCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.load.grouping.NxRegLoadBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.load.grouping.nx.reg.load.DstBuilder;
+
+/**
+ * Set ARP Operation Type that is Request or Replay.
+ */
+public class ActionSetArpOp extends ActionInfo {
+    private final int value;
+
+    public ActionSetArpOp(int value) {
+        this(0, value);
+    }
+
+    public ActionSetArpOp(int actionKey, int value) {
+        super(ActionType.set_arp_op, new String[] {Integer.toString(value)}, actionKey);
+        this.value = value;
+    }
+
+    @Deprecated
+    public ActionSetArpOp(ActionInfo actionInfo) {
+        this(actionInfo.getActionKey(), Integer.parseInt(actionInfo.getActionValues()[0]));
+    }
+
+    @Override
+    public Action buildAction() {
+        return buildAction(getActionKey());
+    }
+
+    public Action buildAction(int newActionKey) {
+        // The length of ARP operation field is 2 bytes, hence end offset bit is 15
+        return new ActionBuilder()
+            .setAction(new NxActionRegLoadNodesNodeTableFlowApplyActionsCaseBuilder()
+                .setNxRegLoad(new NxRegLoadBuilder()
+                    .setDst(new DstBuilder()
+                        .setDstChoice(new DstOfArpOpCaseBuilder().setOfArpOp(true).build())
+                        .setStart(0)
+                        .setEnd(15)
+                        .build())
+                    .setValue(BigInteger.valueOf(value))
+                    .build())
+                .build())
+            .setKey(new ActionKey(newActionKey))
+            .build();
+    }
+}
