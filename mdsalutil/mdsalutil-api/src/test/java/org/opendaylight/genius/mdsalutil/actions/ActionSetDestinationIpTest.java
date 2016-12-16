dd0f58dc3b73c2730375d0102d35a662a396c524
Stephen Kitt <skitt@redhat.com>
2016-12-16 14:39:15 +0100

    Action redesign: migrate set_{destination,source}_ip
    Change-Id: I557ad1d1b79a224c5d4adc9b71c2b0aa00652165
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/test/java/org/opendaylight/genius/mdsalutil/actions/ActionSetDestinationIpTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright © 2016, 2017 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.actions;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import ch.vorburger.xtendbeans.XtendBeanGenerator;
+import org.junit.Test;
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.genius.mdsalutil.ActionType;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv4Prefix;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.SetFieldCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.match.layer._3.match.Ipv4Match;
+
+/**
+ * Test class for {@link ActionSetDestinationIp}.
+ */
+public class ActionSetDestinationIpTest {
+    private static final String IP_ADDRESS = "1.2.3.4";
+    private static final String IP_MASK = "32";
+
+    private XtendBeanGenerator generator = new XtendBeanGenerator();
+
+    @Test
+    public void actionInfoTest() {
+        verifyAction(new ActionSetDestinationIp(IP_ADDRESS).buildAction());
+        verifyAction(new ActionSetDestinationIp(IP_ADDRESS, IP_MASK).buildAction());
+        verifyAction(new ActionSetDestinationIp(new Ipv4Prefix(IP_ADDRESS + "/" + IP_MASK)).buildAction());
+    }
+
+    @Test
+    public void backwardsCompatibleAction() {
+        verifyAction(new ActionInfo(ActionType.set_destination_ip,
+            new String[] {IP_ADDRESS}).buildAction());
+        verifyAction(new ActionInfo(ActionType.set_destination_ip,
+            new String[] {IP_ADDRESS, IP_MASK}).buildAction());
+    }
+
+    private void verifyAction(Action action) {
+        assertTrue(action.getAction() instanceof SetFieldCase);
+        SetFieldCase actionCase = (SetFieldCase) action.getAction();
+        assertNotNull(actionCase.getSetField().getLayer3Match());
+        assertTrue(actionCase.getSetField().getLayer3Match() instanceof Ipv4Match);
+        Ipv4Match ipv4Match = (Ipv4Match) actionCase.getSetField().getLayer3Match();
+        assertEquals(new Ipv4Prefix(IP_ADDRESS + "/" + IP_MASK), ipv4Match.getIpv4Destination());
+    }
+
+    @Test
+    public void generateAction() {
+        ActionInfo actionInfo = new ActionSetDestinationIp(IP_ADDRESS);
+        assertEquals("new ActionSetDestinationIp(0, new Ipv4Prefix(\"" + IP_ADDRESS + "/" + IP_MASK + "\"))",
+                generator.getExpression(actionInfo));
+    }
+}
