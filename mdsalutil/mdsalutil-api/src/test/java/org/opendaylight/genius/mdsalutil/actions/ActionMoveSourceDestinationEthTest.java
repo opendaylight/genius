224b605e51b8e44076e2bc80f8fa7969322a0c26
Stephen Kitt <skitt@redhat.com>
2016-12-16 11:18:18 +0100

    Action redesign: migrate move_src_dst_{eth,ip}
    Change-Id: Idda8f0ee6b0ebb91514bb2562e483e47362fb973
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/test/java/org/opendaylight/genius/mdsalutil/actions/ActionMoveSourceDestinationEthTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright © 2016, 2017 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.actions;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import ch.vorburger.xtendbeans.XtendBeanGenerator;
+import org.junit.Test;
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.genius.mdsalutil.ActionType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.dst.choice.grouping.dst.choice.DstOfEthDstCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nodes.node.table.flow.instructions.instruction.instruction.apply.actions._case.apply.actions.action.action.NxActionRegMoveNodesNodeTableFlowApplyActionsCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.move.grouping.NxRegMove;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.src.choice.grouping.src.choice.SrcOfEthSrcCase;
+
+/**
+ * Test class for {@link ActionMoveSourceDestinationEth}.
+ */
+public class ActionMoveSourceDestinationEthTest {
+    private XtendBeanGenerator generator = new XtendBeanGenerator();
+
+    @Test
+    public void actionInfoTest() {
+        verifyAction(new ActionMoveSourceDestinationEth().buildAction());
+    }
+
+    @Test
+    public void backwardsCompatibleAction() {
+        verifyAction(new ActionInfo(ActionType.move_src_dst_eth, new String[0]).buildAction());
+    }
+
+    private void verifyAction(Action action) {
+        assertTrue(action.getAction() instanceof NxActionRegMoveNodesNodeTableFlowApplyActionsCase);
+        NxActionRegMoveNodesNodeTableFlowApplyActionsCase actionCase =
+            (NxActionRegMoveNodesNodeTableFlowApplyActionsCase) action.getAction();
+        NxRegMove nxRegMove = actionCase.getNxRegMove();
+        assertTrue(nxRegMove.getSrc().getSrcChoice() instanceof SrcOfEthSrcCase);
+        SrcOfEthSrcCase srcChoice = (SrcOfEthSrcCase) nxRegMove.getSrc().getSrcChoice();
+        assertTrue(srcChoice.isOfEthSrc());
+        assertEquals(0, nxRegMove.getSrc().getStart().intValue());
+        assertTrue(nxRegMove.getDst().getDstChoice() instanceof DstOfEthDstCase);
+        DstOfEthDstCase dstChoice = (DstOfEthDstCase) nxRegMove.getDst().getDstChoice();
+        assertTrue(dstChoice.isOfEthDst());
+        assertEquals(0, nxRegMove.getDst().getStart().intValue());
+        assertEquals(47, nxRegMove.getDst().getEnd().intValue());
+    }
+
+    @Test
+    public void generateAction() {
+        ActionInfo actionInfo = new ActionMoveSourceDestinationEth();
+        assertEquals("new ActionMoveSourceDestinationEth", generator.getExpression(actionInfo));
+    }
+}
---
+++ mdsalutil/mdsalutil-api/src/test/java/org/opendaylight/genius/mdsalutil/actions/ActionMoveSourceDestinationIpTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright © 2016, 2017 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.actions;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import ch.vorburger.xtendbeans.XtendBeanGenerator;
+import org.junit.Test;
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.genius.mdsalutil.ActionType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.dst.choice.grouping.dst.choice.DstOfIpDstCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nodes.node.table.flow.instructions.instruction.instruction.apply.actions._case.apply.actions.action.action.NxActionRegMoveNodesNodeTableFlowApplyActionsCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.move.grouping.NxRegMove;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.src.choice.grouping.src.choice.SrcOfIpSrcCase;
+
+/**
+ * Test class for {@link ActionMoveSourceDestinationIp}.
+ */
+public class ActionMoveSourceDestinationIpTest {
+    private XtendBeanGenerator generator = new XtendBeanGenerator();
+
+    @Test
+    public void actionInfoTest() {
+        verifyAction(new ActionMoveSourceDestinationIp().buildAction());
+    }
+
+    @Test
+    public void backwardsCompatibleAction() {
+        verifyAction(new ActionInfo(ActionType.move_src_dst_ip, new String[0]).buildAction());
+    }
+
+    private void verifyAction(Action action) {
+        assertTrue(action.getAction() instanceof NxActionRegMoveNodesNodeTableFlowApplyActionsCase);
+        NxActionRegMoveNodesNodeTableFlowApplyActionsCase actionCase =
+            (NxActionRegMoveNodesNodeTableFlowApplyActionsCase) action.getAction();
+        NxRegMove nxRegMove = actionCase.getNxRegMove();
+        assertTrue(nxRegMove.getSrc().getSrcChoice() instanceof SrcOfIpSrcCase);
+        SrcOfIpSrcCase srcChoice = (SrcOfIpSrcCase) nxRegMove.getSrc().getSrcChoice();
+        assertTrue(srcChoice.isOfIpSrc());
+        assertEquals(0, nxRegMove.getSrc().getStart().intValue());
+        assertTrue(nxRegMove.getDst().getDstChoice() instanceof DstOfIpDstCase);
+        DstOfIpDstCase dstChoice = (DstOfIpDstCase) nxRegMove.getDst().getDstChoice();
+        assertTrue(dstChoice.isOfIpDst());
+        assertEquals(0, nxRegMove.getDst().getStart().intValue());
+        assertEquals(31, nxRegMove.getDst().getEnd().intValue());
+    }
+
+    @Test
+    public void generateAction() {
+        ActionInfo actionInfo = new ActionMoveSourceDestinationIp();
+        assertEquals("new ActionMoveSourceDestinationIp", generator.getExpression(actionInfo));
+    }
+}
