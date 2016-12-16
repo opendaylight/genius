7b9eb20465ad8b27027ce026b238ed985c707cd8
Stephen Kitt <skitt@redhat.com>
2016-12-16 16:06:41 +0100

    Action redesign: migrate NICIRA ARP extensions
    Change-Id: I64583a3f57028af371ff1f0c7a8cb2359c384501
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/test/java/org/opendaylight/genius/mdsalutil/actions/ActionLoadIpToSpaTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright © 2016, 2017 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.actions;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import ch.vorburger.xtendbeans.XtendBeanGenerator;
+import com.google.common.net.InetAddresses;
+import java.math.BigInteger;
+import org.junit.Test;
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.genius.mdsalutil.ActionType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.dst.choice.grouping.dst.choice.DstOfArpSpaCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nodes.node.table.flow.instructions.instruction.instruction.apply.actions._case.apply.actions.action.action.NxActionRegLoadNodesNodeTableFlowApplyActionsCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.load.grouping.NxRegLoad;
+
+/**
+ * Test for {@link ActionLoadIpToSpa}.
+ */
+public class ActionLoadIpToSpaTest {
+    private static final String IP_ADDRESS = "1.2.3.4";
+
+    private XtendBeanGenerator generator = new XtendBeanGenerator();
+
+    @Test
+    public void actionInfoTest() {
+        verifyAction(new ActionLoadIpToSpa(IP_ADDRESS).buildAction());
+    }
+
+    @Test
+    public void backwardsCompatibleAction() {
+        verifyAction(new ActionInfo(ActionType.load_ip_to_spa, new String[] {IP_ADDRESS}).buildAction());
+    }
+
+    private void verifyAction(Action action) {
+        assertTrue(action.getAction() instanceof NxActionRegLoadNodesNodeTableFlowApplyActionsCase);
+        NxActionRegLoadNodesNodeTableFlowApplyActionsCase actionCase =
+            (NxActionRegLoadNodesNodeTableFlowApplyActionsCase) action.getAction();
+        assertNotNull(actionCase.getNxRegLoad());
+        NxRegLoad nxRegLoad = actionCase.getNxRegLoad();
+        assertTrue(nxRegLoad.getDst().getDstChoice() instanceof DstOfArpSpaCase);
+        DstOfArpSpaCase dstOfArpSpaCase = (DstOfArpSpaCase) nxRegLoad.getDst().getDstChoice();
+        assertTrue(dstOfArpSpaCase.isOfArpSpa());
+        assertEquals(0, nxRegLoad.getDst().getStart().intValue());
+        assertEquals(31, nxRegLoad.getDst().getEnd().intValue());
+        assertEquals(
+            BigInteger.valueOf(InetAddresses.coerceToInteger(InetAddresses.forString(IP_ADDRESS)) & 0xffffffffL),
+            nxRegLoad.getValue());
+    }
+
+    @Test
+    public void generateAction() {
+        ActionInfo actionInfo = new ActionLoadIpToSpa(IP_ADDRESS);
+        assertEquals("new ActionLoadIpToSpa(0, \"" + IP_ADDRESS + "\")", generator.getExpression(actionInfo));
+    }
+}
---
+++ mdsalutil/mdsalutil-api/src/test/java/org/opendaylight/genius/mdsalutil/actions/ActionLoadMacToShaTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright © 2016, 2017 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.actions;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import ch.vorburger.xtendbeans.XtendBeanGenerator;
+import java.math.BigInteger;
+import org.junit.Test;
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.genius.mdsalutil.ActionType;
+import org.opendaylight.genius.mdsalutil.NWUtil;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.MacAddress;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.dst.choice.grouping.dst.choice.DstNxArpShaCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nodes.node.table.flow.instructions.instruction.instruction.apply.actions._case.apply.actions.action.action.NxActionRegLoadNodesNodeTableFlowApplyActionsCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.load.grouping.NxRegLoad;
+
+/**
+ * Test for {@link ActionLoadMacToSha}.
+ */
+public class ActionLoadMacToShaTest {
+    private static final String MAC_ADDRESS = "11:22:33:44:55:66";
+
+    private XtendBeanGenerator generator = new XtendBeanGenerator();
+
+    @Test
+    public void actionInfoTest() {
+        verifyAction(new ActionLoadMacToSha(new MacAddress(MAC_ADDRESS)).buildAction());
+    }
+
+    @Test
+    public void backwardsCompatibleAction() {
+        verifyAction(new ActionInfo(ActionType.load_mac_to_sha, new String[] {MAC_ADDRESS}).buildAction());
+    }
+
+    private void verifyAction(Action action) {
+        assertTrue(action.getAction() instanceof NxActionRegLoadNodesNodeTableFlowApplyActionsCase);
+        NxActionRegLoadNodesNodeTableFlowApplyActionsCase actionCase =
+            (NxActionRegLoadNodesNodeTableFlowApplyActionsCase) action.getAction();
+        assertNotNull(actionCase.getNxRegLoad());
+        NxRegLoad nxRegLoad = actionCase.getNxRegLoad();
+        assertTrue(nxRegLoad.getDst().getDstChoice() instanceof DstNxArpShaCase);
+        DstNxArpShaCase dstNxArpShaCase = (DstNxArpShaCase) nxRegLoad.getDst().getDstChoice();
+        assertTrue(dstNxArpShaCase.isNxArpSha());
+        assertEquals(0, nxRegLoad.getDst().getStart().intValue());
+        assertEquals(47, nxRegLoad.getDst().getEnd().intValue());
+        assertEquals(BigInteger.valueOf(NWUtil.macToLong(new MacAddress(MAC_ADDRESS))), nxRegLoad.getValue());
+    }
+
+    @Test
+    public void generateAction() {
+        ActionInfo actionInfo = new ActionLoadMacToSha(new MacAddress(MAC_ADDRESS));
+        assertEquals("new ActionLoadMacToSha(0, new MacAddress(\"" + MAC_ADDRESS + "\"))",
+                generator.getExpression(actionInfo));
+    }
+}
