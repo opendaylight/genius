87822ce520c890672b2d70e042af51ed35c4a083
Periyasamy Palanisamy <periyasamy.palanisamy@ericsson.com>
2016-12-19 12:16:56 +0530

    DataStoreJobCoordinator API for pending tasks count (useful for testing)
    * added counter for jobs_incomplete
    * exposing API for getIncompleteTaskCount
    
    TDD incl. self unit test
    
    Change-Id: I6b1d824804ab73c0b57929d8fe057bc783231308
    Signed-off-by: Periyasamy Palanisamy <periyasamy.palanisamy@ericsson.com>
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/datastoreutils/DataStoreJobCoordinator.java
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/datastoreutils/DataStoreJobCoordinator.java
@@ -22,6 +22,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -79,6 +80,10 @@ public class DataStoreJobCoordinator {
         enqueueJob(job.getJobQueueKey(), job);
     }
 
+    public long getIncompleteTaskCount() {
+        return DataStoreJobCoordinatorCounters.jobs_incomplete.get();
+    }
+
     /**
      *    This is used by the external applications to enqueue a Job
      *    with an appropriate key. A JobEntry is created and queued
@@ -101,6 +106,8 @@ public class DataStoreJobCoordinator {
             jobEntriesMap.put(key, jobQueue);
 
             DataStoreJobCoordinatorCounters.jobs_pending.inc();
+            DataStoreJobCoordinatorCounters.jobs_incomplete.inc();
+            DataStoreJobCoordinatorCounters.jobs_created.inc();
         }
         reentrantLock.lock();
         try {
@@ -126,6 +133,7 @@ public class DataStoreJobCoordinator {
             }
         }
         DataStoreJobCoordinatorCounters.jobs_cleared.inc();
+        DataStoreJobCoordinatorCounters.jobs_incomplete.dec();
     }
 
     /**
@@ -158,11 +166,11 @@ public class DataStoreJobCoordinator {
         }
 
         /**
-         *    This method is used to handle failure callbacks. If more
-         *    retry needed, the retrycount is decremented and mainworker
-         *    is executed again. After retries completed, rollbackworker
-         *    is executed. If rollbackworker fails, this is a
-         *    double-fault. Double fault is logged and ignored.
+         * This method is used to handle failure callbacks. If more
+         * retry needed, the retrycount is decremented and mainworker
+         * is executed again. After retries completed, rollbackworker
+         * is executed. If rollbackworker fails, this is a
+         * double-fault. Double fault is logged and ignored.
          */
         @Override
         public void onFailure(Throwable throwable) {
@@ -306,7 +314,6 @@ public class DataStoreJobCoordinator {
 
                                 } else {
                                     it.remove();
-                                    DataStoreJobCoordinatorCounters.jobs_remove_entry.inc();
                                 }
                             }
                         }
