5348cc9463d3f858cdd4e241e7e603231103e750
Stephen Kitt <skitt@redhat.com>
2017-01-05 15:22:35 +0100

    MatchInfo redesign
    Change-Id: I83cb7d9a0ab04116f8796dd1b53cc125e237d981
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/matches/MatchArpSpa.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright © 2017 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.matches;
+
+import java.math.BigInteger;
+import org.opendaylight.genius.mdsalutil.MatchFieldType;
+import org.opendaylight.genius.mdsalutil.NWUtil;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv4Prefix;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.flow.MatchBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.match.layer._3.match.ArpMatch;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.match.layer._3.match.ArpMatchBuilder;
+
+/**
+ * ARP source transport address match.
+ */
+public class MatchArpSpa extends MatchInfoHelper<ArpMatch, ArpMatchBuilder> {
+    private final Ipv4Prefix address;
+
+    public MatchArpSpa(Ipv4Prefix address) {
+        super(MatchFieldType.arp_spa, address.getValue().split("/"));
+        this.address = address;
+    }
+
+    public MatchArpSpa(long ip, long mask) {
+        this(new Ipv4Prefix(NWUtil.longToIpv4(ip, mask)));
+    }
+
+    public MatchArpSpa(String ip, String mask) {
+        this(new Ipv4Prefix(ip + "/" + mask));
+    }
+
+    /**
+     * Create an instance; this constructor is only present for XtendBeanGenerator and must not be used.
+     */
+    @Deprecated
+    public MatchArpSpa(Ipv4Prefix address, BigInteger[] bigMatchValues, MatchFieldType matchField, long[] matchValues,
+            String[] stringMatchValues) {
+        this(address);
+    }
+
+    @Override
+    protected void applyValue(MatchBuilder matchBuilder, ArpMatch value) {
+        matchBuilder.setLayer3Match(value);
+    }
+
+    @Override
+    protected void populateBuilder(ArpMatchBuilder builder) {
+        builder.setArpSourceTransportAddress(address);
+    }
+
+    public Ipv4Prefix getAddress() {
+        return address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        MatchArpSpa that = (MatchArpSpa) o;
+
+        return address != null ? address.equals(that.address) : that.address == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (address != null ? address.hashCode() : 0);
+        return result;
+    }
+}
---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/matches/MatchIpv6Source.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright © 2017 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.matches;
+
+import java.math.BigInteger;
+import org.opendaylight.genius.mdsalutil.MatchFieldType;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv6Prefix;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.flow.MatchBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.match.layer._3.match.Ipv6Match;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.match.layer._3.match.Ipv6MatchBuilder;
+
+/**
+ * IPv6 source match.
+ */
+public class MatchIpv6Source extends MatchInfoHelper<Ipv6Match, Ipv6MatchBuilder> {
+    private final Ipv6Prefix prefix;
+
+    public MatchIpv6Source(String address) {
+        super(MatchFieldType.ipv6_source, new String[] {address});
+        this.prefix = new Ipv6Prefix(address);
+    }
+
+    public MatchIpv6Source(Ipv6Prefix prefix) {
+        super(MatchFieldType.ipv6_source, new String[] {prefix.getValue()});
+        this.prefix = prefix;
+    }
+
+    /**
+     * Create an instance; this constructor is only present for XtendBeanGenerator and must not be used.
+     */
+    @Deprecated
+    public MatchIpv6Source(BigInteger[] bigMatchValues, MatchFieldType matchField, long[] matchValues,
+            Ipv6Prefix prefix, String[] stringMatchValues) {
+        this(prefix);
+    }
+
+    @Override
+    protected void applyValue(MatchBuilder matchBuilder, Ipv6Match value) {
+        matchBuilder.setLayer3Match(value);
+    }
+
+    @Override
+    protected void populateBuilder(Ipv6MatchBuilder builder) {
+        builder.setIpv6Source(prefix);
+    }
+
+    public Ipv6Prefix getPrefix() {
+        return prefix;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        MatchIpv6Source that = (MatchIpv6Source) o;
+
+        return prefix != null ? prefix.equals(that.prefix) : that.prefix == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (prefix != null ? prefix.hashCode() : 0);
+        return result;
+    }
+}
