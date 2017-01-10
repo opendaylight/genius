0999a3714176f7fe98ca5cdb87d987d1edfda9a0
Edwin Anthony <n.edwin.anthony@ericsson.com>
2017-01-10 19:59:50 +0530

    Interfacemanager blueprint migration
    Change-Id: I4b3f7ea54ef290546a4e7e031a0f3b5b3023c7aa
    Signed-off-by: Edwin Anthony <n.edwin.anthony@ericsson.com>

---
+++ interfacemanager/interfacemanager-impl/src/test/java/org/opendaylight/genius/interfacemanager/test/infra/TestAlivenessMonitorService.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2016 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.interfacemanager.test.infra;
+
+import org.mockito.Mockito;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.alivenessmonitor.rev160411.AlivenessMonitorService;
+
+import static org.opendaylight.yangtools.testutils.mockito.MoreAnswers.realOrException;
+
+/**
+ * To facilitate Listener bindings in InterfaceMangerTestModule
+ *
+ * @author N Edwin Anthony
+ */
+public abstract class TestAlivenessMonitorService implements AlivenessMonitorService {
+
+    public static TestAlivenessMonitorService newInstance() {
+        return Mockito.mock(TestAlivenessMonitorService.class, realOrException());
+    }
+}
