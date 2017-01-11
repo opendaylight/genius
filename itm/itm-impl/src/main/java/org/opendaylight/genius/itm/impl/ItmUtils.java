60c289b0c7d443830204453c186477718730cec7
Tarun Thakur <tarun.t@altencalsoftlabs.com>
2017-01-11 13:30:00 +0530

    Blueprint XML Config support in ITM.
    - Added genius-itm-config.xml into ITM as per Blueprint standard.
    - Introduced 2 params in config - def-tz-enabled and def-tz-tunnel-type
    - Used new 2 config params to create/delete default-transport-zone
    
    Change-Id: I83fd74f88ea3d6b3c30b120f76d97aa92fb6d611
    Signed-off-by: Tarun Thakur <tarun.t@altencalsoftlabs.com>

--- itm/itm-impl/src/main/java/org/opendaylight/genius/itm/impl/ItmUtils.java
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/impl/ItmUtils.java
@@ -1127,4 +1127,55 @@ public class ItmUtils {
         return zones;
     }
 
+    /**
+     * Returns the transport zone from Configuration datastore.
+     *
+     * @param tzName transport zone name
+     * @param dataBroker data broker handle to perform operations on datastore
+     */
+    public static TransportZone getTransportZoneFromConfigDS(String tzName, DataBroker dataBroker) {
+        InstanceIdentifier<TransportZone> tzonePath = InstanceIdentifier.builder(TransportZones.class)
+            .child(TransportZone.class, new TransportZoneKey(tzName)).build();
+        Optional<TransportZone> tZoneOptional = ItmUtils.read(LogicalDatastoreType.CONFIGURATION, tzonePath,
+            dataBroker);
+        if (tZoneOptional.isPresent()) {
+            return tZoneOptional.get();
+        }
+        return null;
+    }
+
+    /**
+     * Deletes the transport zone from Configuration datastore.
+     *
+     * @param tzName transport zone name
+     * @param dataBroker data broker handle to perform operations on datastore
+     */
+    public static void deleteTransportZoneFromConfigDS(String tzName, DataBroker dataBroker) {
+        // check whether transport-zone exists in config DS.
+        TransportZone tZoneFromConfigDS = ItmUtils.getTransportZoneFromConfigDS(tzName, dataBroker);
+        if (tZoneFromConfigDS != null) {
+            // it exists, delete default-TZ now
+            InstanceIdentifier<TransportZone> path = InstanceIdentifier.builder(TransportZones.class)
+                .child(TransportZone.class,
+                    new TransportZoneKey(tzName)).build();
+            LOG.debug("Removing {} transport-zone from config DS.", tzName);
+            ItmUtils.asyncDelete(LogicalDatastoreType.CONFIGURATION, path, dataBroker, ItmUtils.DEFAULT_CALLBACK);
+        }
+    }
+
+    public static Class<? extends TunnelTypeBase> getTunnelType(String tunnelType) {
+        String tunType = null;
+
+        if (tunnelType == null || tunnelType.isEmpty()) {
+            tunType = ITMConstants.TUNNEL_TYPE_VXLAN;
+        } else if (!tunnelType.equals(ITMConstants.TUNNEL_TYPE_VXLAN) &&
+            !tunnelType.equals(ITMConstants.TUNNEL_TYPE_GRE)) {
+            tunType = ITMConstants.TUNNEL_TYPE_VXLAN;
+        }
+
+        if( tunType.equals(ITMConstants.TUNNEL_TYPE_GRE))
+            return TunnelTypeGre.class;
+        else
+            return TunnelTypeVxlan.class;
+    }
 }
