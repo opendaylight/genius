c133aa7fa94a947135564dee37fdcc3b90f833c9
Tarun Thakur <tarun.t@altencalsoftlabs.com>
2017-01-12 20:37:35 +0530

    ITM Tep Auto Config Unit test using Guice.
    Added cases for:
    - Change for def-tz-enabled and def-tz-tunnel-type config paramters
    - Default-TZ creation and deletion based on def-tz-enabled flag
    - TEP addition and deletion into/from default-TZ
    - TZ creation and TEP addition into TZ
    - TZ deletion and TEP deletion from TZ
    - TEP addition and deletion into/from default-TZ via southbound
    - TEP addition and deletion into/from northbound configured TZ via southbound
    - TEP addition and deletion into/from NotHostedList via southbound
    - Move TEP from one TZ to other TZ by changing tzname parameter in
      ExternalsIds list in Ovsdb node from southbound
    - Update TEP for its bridge by changing dpn-br-name parameter in
      ExternalsIds list in Ovsdb node from southbound
    
    Change-Id: Iea4279a992bf80a95d04e39f08cc59188327c9b5
    Signed-off-by: Tarun Thakur <tarun.t@altencalsoftlabs.com>

--- itm/itm-impl/src/test/java/org/opendaylight/genius/itm/tests/ItmTestConstants.java
+++ itm/itm-impl/src/test/java/org/opendaylight/genius/itm/tests/ItmTestConstants.java
@@ -38,4 +38,30 @@ public interface ItmTestConstants {
     Class<? extends TunnelTypeBase> TUNNEL_TYPE_VXLAN = TunnelTypeVxlan.class;
     Class<? extends TunnelMonitoringTypeBase> monitorProtocol = ITMConstants.DEFAULT_MONITOR_PROTOCOL;
     String  DPID_STR_ONE="1";
+
+    String EXTERNAL_ID_TEP_IP_KEY = "tep-ip";
+    String EXTERNAL_ID_TZNAME_KEY = "tzname";
+    String EXTERNAL_ID_DPN_BR_NAME_KEY = "dpn-br-name";
+
+    String LOCALHOST_IP = "127.0.0.1";
+    int OVSDB_CONN_PORT = 6640;
+
+    String DEF_TZ_TEP_IP = "192.168.56.30";
+    String NB_TZ_TEP_IP = "192.168.56.40";
+
+    String DEF_BR_NAME = "br-int";
+    String DEF_BR_DPID = "00:00:00:00:00:00:00:01";
+    BigInteger INT_DEF_BR_DPID = BigInteger.valueOf(1);
+
+    String BR2_NAME = "br2";
+    String BR2_DPID = "00:00:00:00:00:00:00:02";
+    BigInteger INT_BR2_DPID = BigInteger.valueOf(2);
+
+    //not hosted tz constants
+    String NOT_HOSTED_TZ_TEP_IP = "192.168.10.20";
+    String NOT_HOSTED_TZ_TEPDPN_ID = "0";
+    BigInteger NOT_HOSTED_INT_TZ_TEPDPN_ID = BigInteger.valueOf(0);
+    String NOT_HOSTED_TZ_NAME = "NotHostedTZ";
+    Boolean OF_TUNNEL = false;
+    String NOT_HOSTED_DEF_BR_DPID = "00:00:00:00:00:00:00:00";
 }
