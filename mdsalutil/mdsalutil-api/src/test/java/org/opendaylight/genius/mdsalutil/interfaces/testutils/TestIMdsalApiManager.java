1758be47a8f1528fa39c693f0c9a549ab7546e0e
Michael Vorburger <vorburger@redhat.com>
2017-01-14 03:56:37 +0100

    TestIMdsalApiManager with assertFlowsInAnyOrder()
    Moved here from netvirt's AclServiceTestBase
    
    Change-Id: Ia4410d2b9bab840ec63f08c8cef8a684291f32cc
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- mdsalutil/mdsalutil-api/src/test/java/org/opendaylight/genius/mdsalutil/interfaces/testutils/TestIMdsalApiManager.java
+++ mdsalutil/mdsalutil-api/src/test/java/org/opendaylight/genius/mdsalutil/interfaces/testutils/TestIMdsalApiManager.java
@@ -7,7 +7,9 @@
  */
 package org.opendaylight.genius.mdsalutil.interfaces.testutils;
 
+import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.opendaylight.mdsal.binding.testutils.AssertDataObjects.assertEqualBeans;
 import static org.opendaylight.yangtools.testutils.mockito.MoreAnswers.realOrException;
 
 import com.google.common.collect.ImmutableList;
@@ -23,6 +25,8 @@ import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFaile
 import org.opendaylight.genius.mdsalutil.FlowEntity;
 import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
 import org.opendaylight.mdsal.binding.testutils.AssertDataObjects;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Fake IMdsalApiManager useful for tests.
@@ -38,6 +42,8 @@ import org.opendaylight.mdsal.binding.testutils.AssertDataObjects;
  * @author Michael Vorburger
  */
 public abstract class TestIMdsalApiManager implements IMdsalApiManager {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestIMdsalApiManager.class);
 
     private List<FlowEntity> flows;
 
@@ -62,6 +68,7 @@ public abstract class TestIMdsalApiManager implements IMdsalApiManager {
     }
 
     public synchronized void assertFlows(Iterable<FlowEntity> expectedFlows) {
+        checkNonEmptyFlows(expectedFlows);
         List<FlowEntity> nonNullFlows = getOrNewFlows();
         if (!Iterables.isEmpty(expectedFlows)) {
             assertTrue("No Flows created (bean wiring may be broken?)", !nonNullFlows.isEmpty());
@@ -69,6 +76,44 @@ public abstract class TestIMdsalApiManager implements IMdsalApiManager {
         // TODO Support Iterable <-> List directly within XtendBeanGenerator
         List<FlowEntity> expectedFlowsAsNewArrayList = Lists.newArrayList(expectedFlows);
         AssertDataObjects.assertEqualBeans(expectedFlowsAsNewArrayList, nonNullFlows);
+    }
+
+
+    private synchronized void checkNonEmptyFlows(Iterable<FlowEntity> expectedFlows) {
+        if (!Iterables.isEmpty(expectedFlows)) {
+            assertTrue("No Flows created (bean wiring may be broken?)", !getOrNewFlows().isEmpty());
+        }
+    }
+
+    public synchronized void assertFlowsInAnyOrder(Iterable<FlowEntity> expectedFlows) {
+        checkNonEmptyFlows(expectedFlows);
+        // TODO Support Iterable <-> List directly within XtendBeanGenerator
+        List<FlowEntity> expectedFlowsAsNewArrayList = Lists.newArrayList(expectedFlows);
+
+        // FYI: This containsExactlyElementsIn() assumes that FlowEntity, and everything in it,
+        // has correctly working equals() implementations.  assertEqualBeans() does not assume
+        // that, and would work even without equals, because it only uses property reflection.
+        // Normally this will lead to the same result, but if one day it doesn't (because of
+        // a bug in an equals() implementation somewhere), then it's worth to keep this diff
+        // in mind.
+
+        // FTR: This use of G Truth and then catch AssertionError and using assertEqualBeans iff NOK
+        // (thus discarding the message from G Truth) is a bit of a hack, but it works well...
+        // If you're tempted to improve this, please remember that correctly re-implementing
+        // containsExactlyElementsIn (or Hamcrest's similar containsInAnyOrder) isn't a 1 line
+        // trivia... e.g. a.containsAll(b) && b.containsAll(a) isn't sufficient, because it
+        // won't work for duplicates (which we frequently have here); and ordering before is
+        // not viable because FlowEntity is not Comparable, and Comparator based on hashCode
+        // is not a good idea (different instances can have same hashCode), and e.g. on
+        // System#identityHashCode even less so.
+        try {
+            assertThat(flows).containsExactlyElementsIn(expectedFlowsAsNewArrayList);
+        } catch (AssertionError e) {
+            // The point of this is basically just that our assertEqualBeans output,
+            // in case of a comparison failure, is *A LOT* more clearly readable
+            // than what G Truth (or Hamcrest) can do based on toString.
+            assertEqualBeans(expectedFlowsAsNewArrayList, flows);
+        }
     }
 
     @Override
