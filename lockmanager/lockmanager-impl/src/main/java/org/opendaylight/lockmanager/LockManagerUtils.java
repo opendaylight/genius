5839dd1f2d5c79fd5ab4fa76628910ad62a634cb
David Suarez <david.suarez.fuentes@ericsson.com>
2017-01-23 12:54:14 +0100

    Enforce checkstyle in lockmanager-impl module
    Checkstyle enforcement for lockmanager-impl module.
    
    Change-Id: I75fa5c9f0baf4e3c10a269b593d7227238394cef
    Signed-off-by: David Suarez <david.suarez.fuentes@ericsson.com>

--- lockmanager/lockmanager-impl/src/main/java/org/opendaylight/lockmanager/LockManagerUtils.java
+++ lockmanager/lockmanager-impl/src/main/java/org/opendaylight/lockmanager/LockManagerUtils.java
@@ -8,14 +8,13 @@
 
 package org.opendaylight.lockmanager;
 
+import java.util.concurrent.TimeUnit;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.lockmanager.rev160413.Locks;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.lockmanager.rev160413.TimeUnits;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.lockmanager.rev160413.locks.Lock;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.lockmanager.rev160413.locks.LockBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.lockmanager.rev160413.locks.LockKey;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-
-import java.util.concurrent.TimeUnit;
 
 public class LockManagerUtils {
 
@@ -25,31 +24,30 @@ public class LockManagerUtils {
 
     public static TimeUnit convertToTimeUnit(TimeUnits timeUnit) {
         switch (timeUnit) {
-        case Days:
-            return TimeUnit.DAYS;
-        case Microseconds:
-            return TimeUnit.MICROSECONDS;
-        case Hours:
-            return TimeUnit.HOURS;
-        case Minutes:
-            return TimeUnit.MINUTES;
-        case Nanoseconds:
-            return TimeUnit.NANOSECONDS;
-        case Seconds:
-            return TimeUnit.SECONDS;
-        case Milliseconds:
-        default:
-            return TimeUnit.MILLISECONDS;
+            case Days:
+                return TimeUnit.DAYS;
+            case Microseconds:
+                return TimeUnit.MICROSECONDS;
+            case Hours:
+                return TimeUnit.HOURS;
+            case Minutes:
+                return TimeUnit.MINUTES;
+            case Nanoseconds:
+                return TimeUnit.NANOSECONDS;
+            case Seconds:
+                return TimeUnit.SECONDS;
+            case Milliseconds:
+            default:
+                return TimeUnit.MILLISECONDS;
         }
     }
 
     public static InstanceIdentifier<Lock> getLockInstanceIdentifier(String lockName) {
-        return InstanceIdentifier
-                .builder(Locks.class).child(Lock.class,
-                        new LockKey(lockName)).build();
+        return InstanceIdentifier.builder(Locks.class).child(Lock.class, new LockKey(lockName)).build();
     }
 
     public static Lock buildLockData(String lockName) {
-        return new LockBuilder().setKey(new LockKey(lockName)).setLockName(lockName).setLockOwner(Thread.currentThread().getName()).build();
+        return new LockBuilder().setKey(new LockKey(lockName)).setLockName(lockName)
+                .setLockOwner(Thread.currentThread().getName()).build();
     }
 }
