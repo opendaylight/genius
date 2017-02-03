f5e7f848e8cdfad633beeae4f4054c92f75a2595
Gowri R <gowri.r@ericsson.com>
2017-02-03 15:39:17 +0530

    Fcaps enchancement in NumberOfOFPorts counter
    Current design removes port count for switch, if switch gets disconnected.
    This fix ensures that the port count is retained upon switch disconnection.
    
    Change-Id: If58554a39e751c1a0a75bdb1e67f1a9f3db48284
    Signed-off-by: Gowri R <gowri.r@ericsson.com>

--- fcapsapplication/fcapsapplication-impl/src/main/java/org/opendaylight/genius/fcapsapp/performancecounter/FlowNodeConnectorInventoryTranslatorImpl.java
+++ fcapsapplication/fcapsapplication-impl/src/main/java/org/opendaylight/genius/fcapsapp/performancecounter/FlowNodeConnectorInventoryTranslatorImpl.java
@@ -7,6 +7,7 @@
  */
 package org.opendaylight.genius.fcapsapp.performancecounter;
 
+import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Iterables;
@@ -16,6 +17,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
+import org.opendaylight.controller.md.sal.binding.api.ReadOnlyTransaction;
 import org.opendaylight.controller.md.sal.common.api.clustering.Entity;
 import org.opendaylight.controller.md.sal.common.api.clustering.EntityOwnershipService;
 import org.opendaylight.controller.md.sal.common.api.clustering.EntityOwnershipState;
@@ -23,10 +25,13 @@ import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.genius.fcapsapp.portinfo.PortNameMapping;
 import org.opendaylight.openflowplugin.common.wait.SimpleTaskRetryLooper;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNodeConnector;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.node.NodeConnector;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeKey;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -105,10 +110,12 @@ public class FlowNodeConnectorInventoryTranslatorImpl extends NodeConnectorEvent
             String sNodeConnectorIdentifier = getNodeConnectorId(String.valueOf(nodeConnIdent.firstKeyOf(NodeConnector.class).getId()));
             long nDpId = getDpIdFromPortName(sNodeConnectorIdentifier);
             if (dpnToPortMultiMap.containsKey(nDpId)) {
-                LOG.debug("Node Connector {} removed", sNodeConnectorIdentifier);
-                dpnToPortMultiMap.remove(nDpId, sNodeConnectorIdentifier);
-                sendNodeConnectorUpdation(nDpId);
-                PortNameMapping.updatePortMap("openflow:" + nDpId + ":" + del.getName(), sNodeConnectorIdentifier, "DELETE");
+                if (getSwitchStatus(nDpId)) {
+                    LOG.debug("Node Connector {} removed", sNodeConnectorIdentifier);
+                    dpnToPortMultiMap.remove(nDpId, sNodeConnectorIdentifier);
+                    sendNodeConnectorUpdation(nDpId);
+                    PortNameMapping.updatePortMap("openflow:" + nDpId + ":" + del.getName(), sNodeConnectorIdentifier, "DELETE");
+                }
             }
         }
     }
@@ -148,7 +155,7 @@ public class FlowNodeConnectorInventoryTranslatorImpl extends NodeConnectorEvent
                     sendNodeConnectorUpdation(nDpId);
                     PortNameMapping.updatePortMap("openflow:" + nDpId + ":" + add.getName(), sNodeConnectorIdentifier, "ADD");
                 } else {
-                    LOG.error("Duplicate Event.Node Connector already added");
+                    LOG.debug("Node Connector {} already added for dpn {}", sNodeConnectorIdentifier, nDpId);
                 }
             }
         }
@@ -197,5 +204,31 @@ public class FlowNodeConnectorInventoryTranslatorImpl extends NodeConnectorEvent
         }
         LOG.debug("NumberOfOFPorts:" + nodeListPortsCountStr + " portlistsize " + portname.size());
         pMAgent.connectToPMAgentForNOOfPorts(nodeConnectorCountermap);
+    }
+
+    public boolean getSwitchStatus(long switchId) {
+        NodeId nodeId = new NodeId("openflow:" + switchId);
+        LOG.debug("Querying switch with dpnId {} is up/down", nodeId);
+        InstanceIdentifier<Node> nodeInstanceId = InstanceIdentifier.builder(Nodes.class)
+                .child(Node.class, new NodeKey(nodeId)).build();
+        Optional<Node> nodeOptional = read(dataBroker, LogicalDatastoreType.OPERATIONAL, nodeInstanceId);
+        if (nodeOptional.isPresent()) {
+            LOG.debug("Switch {} is up", nodeId);
+            return true;
+        }
+        LOG.debug("Switch {} is down", nodeId);
+        return false;
+    }
+
+    public static <T extends DataObject> Optional<T> read(DataBroker broker, LogicalDatastoreType datastoreType,
+                                                          InstanceIdentifier<T> path) {
+        ReadOnlyTransaction tx = broker.newReadOnlyTransaction();
+        Optional<T> result = Optional.absent();
+        try {
+            result = tx.read(datastoreType, path).checkedGet();
+        } catch (Exception e) {
+            LOG.error("Exception occured while reading from Datastore", e.getMessage());
+        }
+        return result;
     }
 }
