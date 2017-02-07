1f4b46519f464ca5fe137e43857fc8d91fb75814
Akash <a.k.sahu@ericsson.com>
2017-02-07 18:07:28 +0530

    Bug 7599 hwvtep ucast mac consumption performance
    Adding batching support by adding BlockingQueue for each shrad
    which can be used for batching resources across whole application
    easily without creating there own batching queue.
    
    Change-Id: I9b2fcab374b59295bf92f5a000a85dc111425a1f
    Signed-off-by: Akash <a.k.sahu@ericsson.com>

--- mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/utils/batching/ResourceBatchingManager.java
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/utils/batching/ResourceBatchingManager.java
@@ -15,6 +15,7 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import org.apache.commons.lang3.tuple.ImmutablePair;
@@ -36,10 +37,10 @@ public class ResourceBatchingManager implements AutoCloseable {
     private static final TimeUnit TIME_UNIT = TimeUnit.MILLISECONDS;
 
     private final ConcurrentHashMap<String, Pair<BlockingQueue, ResourceHandler>>
-        resourceHandlerMapper = new ConcurrentHashMap<>();
+            resourceHandlerMapper = new ConcurrentHashMap<>();
 
     private final ConcurrentHashMap<String, ScheduledThreadPoolExecutor>
-        resourceBatchingThreadMapper = new ConcurrentHashMap<>();
+            resourceBatchingThreadMapper = new ConcurrentHashMap<>();
 
     private static ResourceBatchingManager instance;
 
@@ -52,7 +53,7 @@ public class ResourceBatchingManager implements AutoCloseable {
     }
 
     @Override
-    public void close() {
+    public void close() throws Exception {
         LOG.trace("ResourceBatchingManager Closed");
     }
 
@@ -64,14 +65,77 @@ public class ResourceBatchingManager implements AutoCloseable {
             throw new RuntimeException("Resource type already registered");
         }
         resourceHandlerMapper.put(resourceType, new ImmutablePair<>(resQueue, resHandler));
-        ScheduledThreadPoolExecutor resDelegatorService = (ScheduledThreadPoolExecutor)
-                Executors.newScheduledThreadPool(1);
+        ScheduledThreadPoolExecutor resDelegatorService = (ScheduledThreadPoolExecutor) Executors.newScheduledThreadPool(1);
         resourceBatchingThreadMapper.put(resourceType, resDelegatorService);
         LOG.info("Registered resourceType {} with batchSize {} and batchInterval {}", resourceType,
                 resHandler.getBatchSize(), resHandler.getBatchInterval());
         if (resDelegatorService.getPoolSize() == 0 ) {
             resDelegatorService.scheduleWithFixedDelay(
                     new Batcher(resourceType), INITIAL_DELAY, resHandler.getBatchInterval(), TIME_UNIT);
+        }
+    }
+
+    public void registerDefaults(DataBroker broker) {
+        LOG.error("check3 register defaults called ");
+        for (SHARD_RESOURCE shardResource : SHARD_RESOURCE.values()) {
+            if (resourceHandlerMapper.containsKey(shardResource.name())) {
+                continue;
+            }
+            DefaultBatchHandler batchHandler = new DefaultBatchHandler(broker, shardResource.datastoreType, batchSize, batchInterval);
+            registerBatchableResource(shardResource.name(), shardResource.getQueue(), batchHandler);
+        }
+    }
+
+    private static final int PERIODICITY = 500;
+    private static final int BATCH_SIZE = 1000;
+    private static Integer batchSize = BATCH_SIZE;
+    private static Integer batchInterval = PERIODICITY;
+
+    public enum SHARD_RESOURCE {
+        CONFIG_TOPOLOGY(LogicalDatastoreType.CONFIGURATION),
+        OPERATIONAL_TOPOLOGY(LogicalDatastoreType.OPERATIONAL),
+        CONFIG_INVENTORY(LogicalDatastoreType.CONFIGURATION),
+        OPERATIONAL_INVENTORY(LogicalDatastoreType.OPERATIONAL);
+
+        BlockingQueue<ActionableResource> queue = new LinkedBlockingQueue<>();
+        LogicalDatastoreType datastoreType;
+
+        SHARD_RESOURCE(LogicalDatastoreType datastoreType) {
+            this.datastoreType = datastoreType;
+        }
+
+        public LogicalDatastoreType getDatastoreType(){
+            return datastoreType;
+        }
+        BlockingQueue<ActionableResource> getQueue() {
+            return queue;
+        }
+    }
+
+    public void put(SHARD_RESOURCE shardResource, InstanceIdentifier identifier, DataObject updatedData) {
+        BlockingQueue<ActionableResource> queue = shardResource.getQueue();
+        if (queue != null) {
+            ActionableResource actResource = new ActionableResourceImpl(identifier.toString(),
+                    identifier, ActionableResource.CREATE, updatedData, null/*oldData*/);
+            queue.add(actResource);
+        }
+    }
+
+    public void merge(SHARD_RESOURCE shardResource, InstanceIdentifier identifier, DataObject updatedData) {
+        BlockingQueue<ActionableResource> queue = shardResource.getQueue();
+        if (queue != null) {
+            ActionableResource actResource = new ActionableResourceImpl(identifier.toString(),
+                    identifier, ActionableResource.UPDATE, updatedData, null/*oldData*/);
+            queue.add(actResource);
+        }
+    }
+
+    public void delete(SHARD_RESOURCE shardResource, InstanceIdentifier identifier) {
+        BlockingQueue<ActionableResource> queue = shardResource.getQueue();
+        if (queue != null) {
+            ActionableResource actResource = new ActionableResourceImpl(identifier.toString(),
+                    identifier, ActionableResource.DELETE, null, null/*oldData*/);
+            queue.add(actResource);
         }
     }
 
@@ -176,7 +240,6 @@ public class ResourceBatchingManager implements AutoCloseable {
             this.actResourceList = actResourceList;
         }
 
-        @SuppressWarnings("checkstyle:IllegalCatch")
         public void process() {
             InstanceIdentifier<T> identifier;
             Object instance;
@@ -231,22 +294,20 @@ public class ResourceBatchingManager implements AutoCloseable {
                         WriteTransaction writeTransaction = broker.newWriteOnlyTransaction();
                         switch (object.getAction()) {
                             case SubTransaction.CREATE :
-                                writeTransaction.put(dsType, object.getInstanceIdentifier(),
-                                    (DataObject) object.getInstance(), true);
+                                writeTransaction.put(dsType, object.getInstanceIdentifier(), (DataObject)object.getInstance(), true);
                                 break;
                             case SubTransaction.DELETE :
                                 writeTransaction.delete(dsType, object.getInstanceIdentifier());
                                 break;
                             case SubTransaction.UPDATE :
-                                writeTransaction.merge(dsType, object.getInstanceIdentifier(),
-                                    (DataObject) object.getInstance(), true);
+                                writeTransaction.merge(dsType, object.getInstanceIdentifier(), (DataObject)object.getInstance(), true);
                                 break;
                             default:
-                                LOG.error("Unable to determine Action for transaction object with id {}",
-                                    object.getInstanceIdentifier());
+                                LOG.error("Unable to determine Action for transaction object with id {}", object.getInstanceIdentifier());
                         }
+                        CheckedFuture<Void, TransactionCommitFailedException> futureOperation = writeTransaction.submit();
                         try {
-                            writeTransaction.submit().get();
+                            futureOperation.get();
                         } catch (InterruptedException | ExecutionException exception) {
                             LOG.error("Error {} to datastore (path, data) : ({}, {})", object.getAction(),
                                     object.getInstanceIdentifier(), object.getInstance(), exception);
