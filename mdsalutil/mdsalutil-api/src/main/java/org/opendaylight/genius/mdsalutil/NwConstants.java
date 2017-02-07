fea5c229f23f8c4eed22a6d790490c58a15b826f
Guy Regev <guy.regev@hpe.com>
2017-02-07 16:24:50 +0200

    Added consts for counter service
    Change-Id: Ifa12f3353c4fe4b8917bc36c99e37a3f823c0ce5
    Signed-off-by: Guy Regev <guy.regev@hpe.com>

--- mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/NwConstants.java
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/NwConstants.java
@@ -12,13 +12,13 @@ import java.math.BigInteger;
 public final class NwConstants {
 
     // EthType Values
-    public static final int ETHTYPE_802_1Q            = 0X8100;
-    public static final int ETHTYPE_IPV4              = 0X0800;
-    public static final int ETHTYPE_IPV6              = 0x86dd;
-    public static final int ETHTYPE_ARP               = 0X0806;
+    public static final int ETHTYPE_802_1Q = 0X8100;
+    public static final int ETHTYPE_IPV4 = 0X0800;
+    public static final int ETHTYPE_IPV6 = 0x86dd;
+    public static final int ETHTYPE_ARP = 0X0806;
 
-    public static final int ETHTYPE_MPLS_UC           = 0X8847;
-    public static final int ETHTYPE_PBB               = 0X88E7;
+    public static final int ETHTYPE_MPLS_UC = 0X8847;
+    public static final int ETHTYPE_PBB = 0X88E7;
 
     public static final String MACADDR_SEP = ":";
 
@@ -27,19 +27,18 @@ public final class NwConstants {
     public static final String IPV4_SEP = ".";
     public static final String IPV6_SEP = ":";
 
-    //Protocol Type
+    // Protocol Type
     public static final int IP_PROT_ICMP = 1;
     public static final int IP_PROT_TCP = 6;
     public static final int IP_PROT_UDP = 17;
     public static final int IP_PROT_GRE = 47;
 
-    //ARP TYPE
+    // ARP TYPE
     public static final int ARP_REQUEST = 1;
     public static final int ARP_REPLY = 2;
 
-    //Default Port
+    // Default Port
     public static final int UDP_DEFAULT_PORT = 4789;
-
 
     // Flow Actions
     public static final int ADD_FLOW = 0;
@@ -55,15 +54,16 @@ public final class NwConstants {
 
     // Ingress (w.r.t switch) service indexes
     public static final short DEFAULT_SERVICE_INDEX = 0;
-    public static final short ACL_SERVICE_INDEX = 1;
-    public static final short DHCP_SERVICE_INDEX = 2;
-    public static final short QOS_SERVICE_INDEX = 3;
-    public static final short IPV6_SERVICE_INDEX = 4;
-    public static final short SCF_SERVICE_INDEX = 5;
-    public static final short SFC_SERVICE_INDEX = 5;
-    public static final short L3VPN_SERVICE_INDEX = 6;
-    public static final short ELAN_SERVICE_INDEX = 7;
-    public static final short DEFAULT_EGRESS_SERVICE_INDEX = 7;
+    public static final short INGRESS_COUNTERS_SERVICE_INDEX = 1;
+    public static final short ACL_SERVICE_INDEX = 2;
+    public static final short DHCP_SERVICE_INDEX = 3;
+    public static final short QOS_SERVICE_INDEX = 4;
+    public static final short IPV6_SERVICE_INDEX = 5;
+    public static final short SCF_SERVICE_INDEX = 6;
+    public static final short SFC_SERVICE_INDEX = 6;
+    public static final short L3VPN_SERVICE_INDEX = 7;
+    public static final short ELAN_SERVICE_INDEX = 8;
+    public static final short DEFAULT_EGRESS_SERVICE_INDEX = 8;
 
     public static final String DHCP_SERVICE_NAME = "DHCP_SERVICE";
     public static final String ACL_SERVICE_NAME = "ACL_SERVICE";
@@ -77,15 +77,19 @@ public final class NwConstants {
 
     // Egress (w.r.t switch) service indexes
     public static final short EGRESS_ACL_SERVICE_INDEX = 6;
+    public static final short EGRESS_COUNTERS_SERVICE_INDEX = 7;
 
     public static final String EGRESS_ACL_SERVICE_NAME = "EGRESS_ACL_SERVICE";
+
+    public static final String INGRESS_COUNTERS_SERVICE_NAME = "INGRESS_COUNTERS_SERVICE";
+    public static final String EGRESS_COUNTERS_SERVICE_NAME = "EGRESS_COUNTERS_SERVICE";
 
     public static final BigInteger COOKIE_IPV6_TABLE = new BigInteger("4000000", 16);
     public static final BigInteger COOKIE_QOS_TABLE = new BigInteger("4000001", 16);
     public static final BigInteger VLAN_TABLE_COOKIE = new BigInteger("8000000", 16);
     public static final BigInteger COOKIE_VM_INGRESS_TABLE = new BigInteger("8000001", 16);
     public static final BigInteger COOKIE_VM_LFIB_TABLE = new BigInteger("8000002", 16);
-    public static final BigInteger COOKIE_VM_FIB_TABLE =  new BigInteger("8000003", 16);
+    public static final BigInteger COOKIE_VM_FIB_TABLE = new BigInteger("8000003", 16);
     public static final BigInteger COOKIE_DNAT_TABLE = new BigInteger("8000004", 16);
     public static final BigInteger COOKIE_TS_TABLE = new BigInteger("8000005", 16);
     public static final BigInteger COOKIE_SNAT_TABLE = new BigInteger("8000006", 16);
@@ -98,7 +102,7 @@ public final class NwConstants {
     public static final BigInteger TUNNEL_TABLE_COOKIE = new BigInteger("9000000", 16);
     public static final BigInteger COOKIE_ARP_RESPONDER = new BigInteger("8220000", 16);
 
-    //Table IDs
+    // Table IDs
     public static final short VLAN_INTERFACE_INGRESS_TABLE = 0;
     public static final short VXLAN_TRUNK_INTERFACE_TABLE = 10;
     public static final short TRUNK_L2_TABLE = 11;
@@ -116,6 +120,7 @@ public final class NwConstants {
     public static final short SNAT_TABLE = 28;
     public static final short INTERNAL_TUNNEL_TABLE = 36;
     public static final short EXTERNAL_TUNNEL_TABLE = 38;
+    public static final short INGRESS_COUNTERS_TABLE = 39;
     public static final short INGRESS_ACL_TABLE = 40;
     public static final short INGRESS_ACL_FILTER_TABLE = 41;
     public static final short INGRESS_LEARN_TABLE = 41;
@@ -148,34 +153,31 @@ public final class NwConstants {
     public static final short EGRESS_ACL_FILTER_TABLE = 252;
     public static final short EGRESS_LEARN_TABLE = 252;
     public static final short EGRESS_LEARN2_TABLE = 253;
+    public static final short EGRESS_COUNTERS_TABLE = 230;
 
     public enum NxmOfFieldType {
-        NXM_OF_IN_PORT(0x0000, 0, 2, 16),
-        NXM_OF_ETH_DST(0x0000, 1, 6, 48),
-        NXM_OF_ETH_SRC(0x0000, 2, 6, 48),
-        NXM_OF_ETH_TYPE(0x0000, 3, 2, 16),
-        NXM_OF_VLAN_TCI(0x0000, 4, 2, 12),
-        NXM_OF_IP_TOS(0x0000, 5, 1, 8),
-        NXM_OF_IP_PROTO(0x0000, 6, 1, 8),
-        NXM_OF_IP_SRC(0x0000, 7, 4, 32),
-        NXM_OF_IP_DST(0x0000, 8, 4, 32),
-        NXM_OF_TCP_SRC(0x0000, 9, 2, 16),
-        NXM_OF_TCP_DST(0x0000, 10, 2, 16),
-        NXM_OF_UDP_SRC(0x0000, 11, 2, 16),
-        NXM_OF_UDP_DST(0x0000, 12, 2, 16),
-        NXM_OF_ICMP_TYPE(0x0000, 13, 1, 8),
-        NXM_OF_ICMP_CODE(0x0000, 14, 1, 8),
-        NXM_OF_ARP_OP(0x0000, 15, 2, 16),
-        NXM_OF_ARP_SPA(0x0000, 16, 4, 16),
-        NXM_OF_ARP_TPA(0x0000, 17, 4, 16),
-        NXM_NX_REG0(0x0001, 0, 4, -1),
-        NXM_NX_REG1(0x0001, 1, 4, -1),
-        NXM_NX_REG2(0x0001, 2, 4, -1),
-        NXM_NX_REG3(0x0001, 3, 4, -1),
-        NXM_NX_REG4(0x0001, 4, 4, -1),
-        NXM_NX_REG5(0x0001, 5, 4, -1),
-        NXM_NX_REG6(0x0001, 6, 4, -1),
-        NXM_NX_REG7(0x0001, 7, 4, -1);
+        NXM_OF_IN_PORT(0x0000, 0, 2, 16), NXM_OF_ETH_DST(0x0000, 1, 6, 48), NXM_OF_ETH_SRC(0x0000, 2, 6,
+                48), NXM_OF_ETH_TYPE(0x0000, 3, 2, 16), NXM_OF_VLAN_TCI(0x0000, 4, 2, 12), NXM_OF_IP_TOS(0x0000, 5, 1,
+                        8), NXM_OF_IP_PROTO(0x0000, 6, 1, 8), NXM_OF_IP_SRC(0x0000, 7, 4, 32), NXM_OF_IP_DST(0x0000, 8,
+                                4, 32), NXM_OF_TCP_SRC(0x0000, 9, 2, 16), NXM_OF_TCP_DST(0x0000, 10, 2,
+                                        16), NXM_OF_UDP_SRC(0x0000, 11, 2, 16), NXM_OF_UDP_DST(0x0000, 12, 2,
+                                                16), NXM_OF_ICMP_TYPE(0x0000, 13, 1, 8), NXM_OF_ICMP_CODE(0x0000, 14, 1,
+                                                        8), NXM_OF_ARP_OP(0x0000, 15, 2, 16), NXM_OF_ARP_SPA(0x0000, 16,
+                                                                4, 16), NXM_OF_ARP_TPA(0x0000, 17, 4, 16), NXM_NX_REG0(
+                                                                        0x0001, 0, 4,
+                                                                        -1), NXM_NX_REG1(0x0001, 1, 4, -1), NXM_NX_REG2(
+                                                                                0x0001, 2, 4, -1), NXM_NX_REG3(0x0001,
+                                                                                        3, 4, -1), NXM_NX_REG4(0x0001,
+                                                                                                4, 4, -1), NXM_NX_REG5(
+                                                                                                        0x0001, 5, 4,
+                                                                                                        -1), NXM_NX_REG6(
+                                                                                                                0x0001,
+                                                                                                                6, 4,
+                                                                                                                -1), NXM_NX_REG7(
+                                                                                                                        0x0001,
+                                                                                                                        7,
+                                                                                                                        4,
+                                                                                                                        -1);
 
         long type;
         int flowModHeaderLen;
