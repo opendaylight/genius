44e9c4f9e6e027b0188c37cf2b868ea3070f8d9c
David <david.suarez.fuentes@ericsson.com>
2017-02-15 19:55:02 +0100

    Fix issues related to checkstyle compliance
    Fix issues related to checkstyle compliance in module
    fcapsappjmx:
    - Copyright sections
    - Line lengths
    - Order imports
    - Declaration of constants: name, modifiers
    - Formatting
    - Checkstyle enforcement activated
    
    Change-Id: If4e6131025190ba4c38504053273f7114a3bbc09
    Signed-off-by: David <david.suarez.fuentes@ericsson.com>

--- fcapsapplication/fcapsapplication-jmxapi/src/main/java/org/opendaylight/genius/fcapsappjmx/ControlPathFailureAlarmMBean.java
+++ fcapsapplication/fcapsapplication-jmxapi/src/main/java/org/opendaylight/genius/fcapsappjmx/ControlPathFailureAlarmMBean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ * Copyright (c) 2016, 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -8,13 +8,18 @@
 
 package org.opendaylight.genius.fcapsappjmx;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public interface ControlPathFailureAlarmMBean {
-    void setRaiseAlarmObject(ArrayList<String> raiseAlarmObject);
-    ArrayList<String> getRaiseAlarmObject();
-    void setClearAlarmObject(ArrayList<String> clearAlarmObject);
-    ArrayList<String> getClearAlarmObject();
+    void setRaiseAlarmObject(List<String> raiseAlarmObject);
+
+    List<String> getRaiseAlarmObject();
+
+    void setClearAlarmObject(List<String> clearAlarmObject);
+
+    List<String> getClearAlarmObject();
+
     void raiseAlarm(String alarmName, String additionalText, String source);
+
     void clearAlarm(String alarmName, String additionalText, String source);
 }
