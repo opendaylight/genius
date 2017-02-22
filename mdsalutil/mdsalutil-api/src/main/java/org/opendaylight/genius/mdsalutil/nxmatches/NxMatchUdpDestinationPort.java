c1b629b74f0b531e998256e2d7b5a8e39a04cc1d
Stephen Kitt <skitt@redhat.com>
2017-02-22 17:48:55 +0100

    NxMatchInfo redesign
    Change-Id: Iae27c287dae8d9fe400e07d1429cd20bc94a14c6
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/nxmatches/NxMatchUdpDestinationPort.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright © 2017 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.nxmatches;
+
+import org.opendaylight.genius.mdsalutil.NxMatchFieldType;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.PortNumber;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.match.rev140714.NxAugMatchNodesNodeTableFlowBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.match.rev140714.NxmOfUdpDstKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.match.rev140714.nxm.of.udp.dst.grouping.NxmOfUdpDst;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.match.rev140714.nxm.of.udp.dst.grouping.NxmOfUdpDstBuilder;
+
+/**
+ * Nicira extension UDP destination port match.
+ */
+public class NxMatchUdpDestinationPort extends NxMatchInfoHelper<NxmOfUdpDst, NxmOfUdpDstBuilder> {
+    private final int port;
+    private final int mask;
+
+    public NxMatchUdpDestinationPort(int port, int mask) {
+        super(NxmOfUdpDstKey.class, NxMatchFieldType.nx_udp_dst_with_mask, new long[] {port, mask});
+        this.port = port;
+        this.mask = mask;
+    }
+
+    @Override
+    protected void applyValue(NxAugMatchNodesNodeTableFlowBuilder matchBuilder, NxmOfUdpDst value) {
+        matchBuilder.setNxmOfUdpDst(value);
+    }
+
+    @Override
+    protected void populateBuilder(NxmOfUdpDstBuilder builder) {
+        builder.setPort(new PortNumber(port));
+        builder.setMask(mask);
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public int getMask() {
+        return mask;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other == null || getClass() != other.getClass()) {
+            return false;
+        }
+        if (!super.equals(other)) {
+            return false;
+        }
+
+        NxMatchUdpDestinationPort that = (NxMatchUdpDestinationPort) other;
+
+        if (port != that.port) {
+            return false;
+        }
+        return mask == that.mask;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + port;
+        result = 31 * result + mask;
+        return result;
+    }
+}
