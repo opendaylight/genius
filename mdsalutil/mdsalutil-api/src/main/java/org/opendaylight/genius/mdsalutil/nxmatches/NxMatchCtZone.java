f58fbf632fa4afc2becaa01fefc8a864948a1a0b
Stephen Kitt <skitt@redhat.com>
2017-02-22 17:48:55 +0100

    NxMatchInfo redesign
    Change-Id: Iae27c287dae8d9fe400e07d1429cd20bc94a14c6
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/nxmatches/NxMatchCtZone.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright © 2017 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.nxmatches;
+
+import org.opendaylight.genius.mdsalutil.NxMatchFieldType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.match.rev140714.NxAugMatchNodesNodeTableFlowBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.match.rev140714.NxmNxCtZoneKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.match.rev140714.nxm.nx.ct.zone.grouping.NxmNxCtZone;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.match.rev140714.nxm.nx.ct.zone.grouping.NxmNxCtZoneBuilder;
+
+/**
+ * Nicira extension CT zone match.
+ */
+public class NxMatchCtZone extends NxMatchInfoHelper<NxmNxCtZone, NxmNxCtZoneBuilder> {
+    private final int zone;
+
+    public NxMatchCtZone(int zone) {
+        super(NxmNxCtZoneKey.class, NxMatchFieldType.ct_zone, new long[] {zone});
+        this.zone = zone;
+    }
+
+    @Override
+    protected void applyValue(NxAugMatchNodesNodeTableFlowBuilder matchBuilder, NxmNxCtZone value) {
+        matchBuilder.setNxmNxCtZone(value);
+    }
+
+    @Override
+    protected void populateBuilder(NxmNxCtZoneBuilder builder) {
+        builder.setCtZone(zone);
+    }
+
+    public int getZone() {
+        return zone;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other == null || getClass() != other.getClass()) {
+            return false;
+        }
+        if (!super.equals(other)) {
+            return false;
+        }
+
+        NxMatchCtZone that = (NxMatchCtZone) other;
+
+        return zone == that.zone;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + zone;
+        return result;
+    }
+}
---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/nxmatches/NxMatchTunnelSourceIp.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright © 2017 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.nxmatches;
+
+import org.opendaylight.genius.mdsalutil.NxMatchFieldType;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv4Address;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.match.rev140714.NxAugMatchNodesNodeTableFlowBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.match.rev140714.NxmNxTunIpv4SrcKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.match.rev140714.nxm.nx.tun.ipv4.src.grouping.NxmNxTunIpv4Src;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.match.rev140714.nxm.nx.tun.ipv4.src.grouping.NxmNxTunIpv4SrcBuilder;
+
+/**
+ * Nicira extension tunnel source IP match.
+ */
+public class NxMatchTunnelSourceIp extends NxMatchInfoHelper<NxmNxTunIpv4Src, NxmNxTunIpv4SrcBuilder> {
+    private final Ipv4Address address;
+
+    public NxMatchTunnelSourceIp(String address) {
+        this(new Ipv4Address(address));
+    }
+
+    public NxMatchTunnelSourceIp(Ipv4Address address) {
+        super(NxmNxTunIpv4SrcKey.class, NxMatchFieldType.tun_src_ip, new String[] {address.getValue()});
+        this.address = address;
+    }
+
+    @Override
+    protected void applyValue(NxAugMatchNodesNodeTableFlowBuilder matchBuilder, NxmNxTunIpv4Src value) {
+        matchBuilder.setNxmNxTunIpv4Src(value);
+    }
+
+    @Override
+    protected void populateBuilder(NxmNxTunIpv4SrcBuilder builder) {
+        builder.setIpv4Address(address);
+    }
+
+    public Ipv4Address getAddress() {
+        return address;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other == null || getClass() != other.getClass()) {
+            return false;
+        }
+        if (!super.equals(other)) {
+            return false;
+        }
+
+        NxMatchTunnelSourceIp that = (NxMatchTunnelSourceIp) other;
+
+        return address != null ? address.equals(that.address) : that.address == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (address != null ? address.hashCode() : 0);
+        return result;
+    }
+}
