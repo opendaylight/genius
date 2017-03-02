a667a9c398d02f2782815c4f5b4d08e0ddc1405a
Michael Vorburger <vorburger@redhat.com>
2017-03-02 13:24:51 +0100

    Fix missing alivenessmonitor-impl-protocols in features.xml
    Somehow forgot to uncomment this in c/52020
    (a202894405787214fd9271902af55525b1324f09) - and nobody noticed?!  (Not
    in code review, and no failing CSIT - our processes are broken...)
    
    Change-Id: Ic47c721c3468619e55cc888c29abcafd259486d8
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ alivenessmonitor/alivenessmonitor-impl/src/main/java/org/opendaylight/genius/alivenessmonitor/protocols/internal/AlivenessProtocolHandlerRegistryImpl.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.alivenessmonitor.protocols.internal;
+
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.concurrent.ThreadSafe;
+import javax.inject.Singleton;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.genius.alivenessmonitor.protocols.AlivenessProtocolHandler;
+import org.opendaylight.genius.alivenessmonitor.protocols.AlivenessProtocolHandlerRegistry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.alivenessmonitor.rev160411.EtherTypes;
+
+/**
+ * Implementation of AlivenessProtocolHandlerRegistry.
+ *
+ * @author Michael Vorburger.ch
+ */
+@Singleton
+@ThreadSafe
+public class AlivenessProtocolHandlerRegistryImpl implements AlivenessProtocolHandlerRegistry {
+
+    private final Map<EtherTypes, AlivenessProtocolHandler> ethTypeToProtocolHandler = new EnumMap<>(EtherTypes.class);
+    private final Map<Class<?>, AlivenessProtocolHandler> packetTypeToProtocolHandler = new HashMap<>();
+
+    @Override
+    public synchronized void register(EtherTypes etherType, AlivenessProtocolHandler protocolHandler) {
+        ethTypeToProtocolHandler.put(etherType, protocolHandler);
+        packetTypeToProtocolHandler.put(protocolHandler.getPacketClass(), protocolHandler);
+    }
+
+    @Override
+    public synchronized @Nullable AlivenessProtocolHandler getOpt(EtherTypes etherType) {
+        return ethTypeToProtocolHandler.get(etherType);
+    }
+
+    @Override
+    public synchronized @Nullable AlivenessProtocolHandler getOpt(Class<? extends Object> packetClass) {
+        return packetTypeToProtocolHandler.get(packetClass);
+    }
+
+    @Override
+    public @NonNull AlivenessProtocolHandler get(EtherTypes etherType) {
+        AlivenessProtocolHandler handler = getOpt(etherType);
+        if (handler == null) {
+            throw new IllegalStateException("No handler registered for etherType: " + etherType);
+        }
+        return handler;
+    }
+
+    public @NonNull AlivenessProtocolHandler get(Class<?> packetClass) {
+        AlivenessProtocolHandler handler = getOpt(packetClass);
+        if (handler == null) {
+            throw new IllegalStateException("No handler registered for packetClass: " + packetClass);
+        }
+        return handler;
+    }
+
+}
