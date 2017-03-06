3dacb15bd9e5c3e8d69586c9c7bb1ebbf4401799
Olga Schukin <olga.schukin@hpe.com>
2017-03-06 10:26:37 +0200

    Multiple VxLAN tunnels - initial support
    - ITM - global configuration option to guard the feature
    - ITM - creation of multiple tunnels between pair of DPNs
    - Maintain interface-child-info model for tunnel hierarchy
    - Logical tunnel group creation
    - Prepare OF select group for egress service
    
    Change-Id: I3eca1a7995922feafa4135bdaff83b44a6f1ed0a
    Signed-off-by: Olga Schukin <olga.schukin@hpe.com>

--- itm/itm-impl/src/main/java/org/opendaylight/genius/itm/confighelpers/ItmInternalTunnelAddWorker.java
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/confighelpers/ItmInternalTunnelAddWorker.java
@@ -12,9 +12,12 @@ import com.google.common.util.concurrent.ListenableFuture;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.datastoreutils.DataStoreJobCoordinator;
 import org.opendaylight.genius.itm.impl.ITMBatchingUtils;
 import org.opendaylight.genius.itm.impl.ItmUtils;
 import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
@@ -24,6 +27,8 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rev160406.TunnelMonitoringTypeBase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rev160406.TunnelMonitoringTypeLldp;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rev160406.TunnelTypeBase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rev160406.TunnelTypeLogicalGroup;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rev160406.TunnelTypeVxlan;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.DpnEndpoints;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.DpnEndpointsBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.TunnelList;
@@ -97,7 +102,6 @@ public class ItmInternalTunnelAddWorker {
                                           IdManagerService idManagerService, IMdsalApiManager mdsalManager,
                                           WriteTransaction transaction, List<ListenableFuture<Void>> futures) {
         LOG.trace("Building tunnels from DPN {} " , srcDpn);
-
         if (null == meshedDpnList || 0 == meshedDpnList.size()) {
             LOG.debug("No DPN in the mesh ");
             return ;
@@ -105,7 +109,7 @@ public class ItmInternalTunnelAddWorker {
         for (DPNTEPsInfo dstDpn: meshedDpnList) {
             if (!srcDpn.equals(dstDpn)) {
                 wireUpWithinTransportZone(srcDpn, dstDpn, dataBroker, idManagerService, mdsalManager,
-                        transaction, futures) ;
+                        transaction, futures);
             }
         }
 
@@ -126,8 +130,10 @@ public class ItmInternalTunnelAddWorker {
                         // wire them up
                         wireUpBidirectionalTunnel(srcte, dstte, srcDpn.getDPNID(), dstDpn.getDPNID(), dataBroker,
                                 idManagerService, mdsalManager, transaction, futures);
-                        // CHECK THIS -- Assumption -- One end point per Dpn per transport zone
-                        break ;
+                        if (!ItmTunnelAggregationHelper.isTunnelAggregationEnabled()) {
+                            // CHECK THIS -- Assumption -- One end point per Dpn per transport zone
+                            break;
+                        }
                     }
                 }
             }
@@ -145,51 +151,73 @@ public class ItmInternalTunnelAddWorker {
             ItmUtils.setUpOrRemoveTerminatingServiceTable(dstDpnId, mdsalManager, true);
         }
         // Create the forward direction tunnel
-        if (!wireUp(srcte, dstte, srcDpnId, dstDpnId, dataBroker, idManagerService, transaction, futures)) {
+        if (!wireUp(srcte, dstte, srcDpnId, dstDpnId, dataBroker, idManagerService, mdsalManager,
+                    transaction, futures)) {
             LOG.error("Could not build tunnel between end points {}, {} " , srcte, dstte);
         }
 
         // CHECK IF FORWARD IS NOT BUILT , REVERSE CAN BE BUILT
         // Create the tunnel for the reverse direction
-        if (!wireUp(dstte, srcte, dstDpnId, srcDpnId, dataBroker, idManagerService, transaction, futures)) {
+        if (!wireUp(dstte, srcte, dstDpnId, srcDpnId, dataBroker, idManagerService, mdsalManager,
+                    transaction, futures)) {
             LOG.error("Could not build tunnel between end points {}, {} " , dstte, srcte);
         }
     }
 
-    private static boolean wireUp(TunnelEndPoints srcte, TunnelEndPoints dstte, BigInteger srcDpnId,
-                                  BigInteger dstDpnId , DataBroker dataBroker, IdManagerService idManagerService,
-                                  WriteTransaction transaction, List<ListenableFuture<Void>> futures) {
+    private static boolean wireUp(TunnelEndPoints srcte, TunnelEndPoints dstte,
+                                  BigInteger srcDpnId, BigInteger dstDpnId,
+                                  DataBroker dataBroker, IdManagerService idManagerService,
+                                  IMdsalApiManager mdsalManager, WriteTransaction transaction,
+                                  List<ListenableFuture<Void>> futures) {
         // Wire Up logic
-        LOG.trace("Wiring between source tunnel end points {}, destination tunnel end points {} " , srcte, dstte);
-        String interfaceName = srcte.getInterfaceName() ;
+        LOG.trace("Wiring between source tunnel end points {}, destination tunnel end points {}", srcte, dstte);
+        String interfaceName = srcte.getInterfaceName();
         Class<? extends TunnelTypeBase> tunType = srcte.getTunnelType();
         String tunTypeStr = srcte.getTunnelType().getName();
         // Form the trunk Interface Name
+
         String trunkInterfaceName = ItmUtils.getTrunkInterfaceName(idManagerService, interfaceName,
                 new String(srcte.getIpAddress().getValue()),
                 new String(dstte.getIpAddress().getValue()),
                 tunTypeStr);
+        String parentInterfaceName = null;
+        if (tunType.isAssignableFrom(TunnelTypeVxlan.class)) {
+            parentInterfaceName = createLogicalGroupTunnel(srcte, dstte, srcDpnId, dstDpnId, dataBroker);
+        }
+        createTunnelInterface(srcte, dstte, srcDpnId, dstDpnId, tunType, trunkInterfaceName, parentInterfaceName);
+        // also update itm-state ds?
+        createInternalTunnel(srcDpnId, dstDpnId, tunType, trunkInterfaceName, transaction);
+        return true;
+    }
+
+    private static void createTunnelInterface(TunnelEndPoints srcte, TunnelEndPoints dstte,
+                                             BigInteger srcDpnId, BigInteger dstDpnId,
+                                             Class<? extends TunnelTypeBase> tunType,
+                                             String trunkInterfaceName, String parentInterfaceName) {
         String gateway = srcte.getIpAddress().getIpv4Address() != null ? "0.0.0.0" : "::";
         IpAddress gatewayIpObj = new IpAddress(gateway.toCharArray());
         IpAddress gwyIpAddress =
                 srcte.getSubnetMask().equals(dstte.getSubnetMask()) ? gatewayIpObj : srcte.getGwIpAddress() ;
         LOG.debug(" Creating Trunk Interface with parameters trunk I/f Name - {}, parent I/f name - {}, "
                 + "source IP - {}, destination IP - {} gateway IP - {}",
-                trunkInterfaceName, interfaceName, srcte.getIpAddress(), dstte.getIpAddress(), gwyIpAddress) ;
+                trunkInterfaceName, srcte.getInterfaceName(), srcte.getIpAddress(), dstte.getIpAddress(), gwyIpAddress);
         boolean useOfTunnel = ItmUtils.falseIfNull(srcte.isOptionOfTunnel());
         Interface iface = ItmUtils.buildTunnelInterface(srcDpnId, trunkInterfaceName,
-                String.format("%s %s",ItmUtils.convertTunnelTypetoString(srcte.getTunnelType()), "Trunk Interface"),
+                String.format("%s %s",ItmUtils.convertTunnelTypetoString(tunType), "Trunk Interface"),
                 true, tunType, srcte.getIpAddress(), dstte.getIpAddress(), gwyIpAddress, srcte.getVLANID(), true,
-                monitorEnabled, monitorProtocol, monitorInterval, useOfTunnel);
-        LOG.debug(" Trunk Interface builder - {} ", iface) ;
+                monitorEnabled, monitorProtocol, monitorInterval, useOfTunnel, parentInterfaceName);
+        LOG.debug(" Trunk Interface builder - {} ", iface);
         InstanceIdentifier<Interface> trunkIdentifier = ItmUtils.buildId(trunkInterfaceName);
-        LOG.debug(" Trunk Interface Identifier - {} ", trunkIdentifier) ;
-        LOG.trace(" Writing Trunk Interface to Config DS {}, {} ", trunkIdentifier, iface) ;
+        LOG.debug(" Trunk Interface Identifier - {} ", trunkIdentifier);
+        LOG.trace(" Writing Trunk Interface to Config DS {}, {} ", trunkIdentifier, iface);
         ITMBatchingUtils.update(trunkIdentifier, iface, ITMBatchingUtils.EntityType.DEFAULT_CONFIG);
         ItmUtils.itmCache.addInterface(iface);
-        // also update itm-state ds?
-        InstanceIdentifier<InternalTunnel> path = InstanceIdentifier.create(
-                TunnelList.class)
+    }
+
+    private static void createInternalTunnel(BigInteger srcDpnId, BigInteger dstDpnId,
+                                             Class<? extends TunnelTypeBase> tunType,
+                                             String trunkInterfaceName, WriteTransaction transaction) {
+        InstanceIdentifier<InternalTunnel> path = InstanceIdentifier.create(TunnelList.class)
                 .child(InternalTunnel.class, new InternalTunnelKey(dstDpnId, srcDpnId, tunType));
         InternalTunnel tnl = ItmUtils.buildInternalTunnel(srcDpnId, dstDpnId, tunType, trunkInterfaceName);
         // Switching to individual transaction submit as batching latencies is causing ELAN failures.
@@ -197,6 +225,62 @@ public class ItmInternalTunnelAddWorker {
         // ITMBatchingUtils.update(path, tnl, ITMBatchingUtils.EntityType.DEFAULT_CONFIG);
         transaction.merge(LogicalDatastoreType.CONFIGURATION,path, tnl, true) ;
         ItmUtils.itmCache.addInternalTunnel(tnl);
-        return true;
+    }
+
+    private static String createLogicalGroupTunnel(TunnelEndPoints srcte, TunnelEndPoints dstte,
+                                                   BigInteger srcDpnId, BigInteger dstDpnId,
+                                                   DataBroker dataBroker) {
+        String logicTunnelGroupName = null;
+        boolean tunnelAggregationEnabled = ItmTunnelAggregationHelper.isTunnelAggregationEnabled();
+        if (!tunnelAggregationEnabled) {
+            return logicTunnelGroupName;
+        }
+        logicTunnelGroupName = ItmUtils.getLogicalTunnelGroupName(srcDpnId, dstDpnId);
+        DataStoreJobCoordinator coordinator = DataStoreJobCoordinator.getInstance();
+        ItmTunnelAggregationWorker addWorker =
+                new ItmTunnelAggregationWorker(logicTunnelGroupName, srcte, dstte, srcDpnId, dstDpnId, dataBroker);
+        coordinator.enqueueJob(logicTunnelGroupName, addWorker);
+        return logicTunnelGroupName;
+    }
+
+    private static class ItmTunnelAggregationWorker implements Callable<List<ListenableFuture<Void>>> {
+
+        private final String logicTunnelGroupName;
+        private final TunnelEndPoints srcTe;
+        private final TunnelEndPoints dstTe;
+        private final BigInteger srcDpnId;
+        private final BigInteger dstDpnId;
+        private final DataBroker dataBroker;
+
+        ItmTunnelAggregationWorker(String logicGroupName, TunnelEndPoints srcte, TunnelEndPoints dstte,
+                BigInteger srcDpnId, BigInteger dstDpnId, DataBroker broker) {
+            this.logicTunnelGroupName = logicGroupName;
+            this.srcTe = srcte;
+            this.dstTe = dstte;
+            this.srcDpnId = srcDpnId;
+            this.dstDpnId = dstDpnId;
+            this.dataBroker = broker;
+        }
+
+        @Override
+        public List<ListenableFuture<Void>> call() throws Exception {
+            List<ListenableFuture<Void>> futures = new ArrayList<>();
+            //The logical tunnel interface be created only when the first tunnel interface on each OVS is created
+            InternalTunnel tunnel = ItmUtils.itmCache.getInternalTunnel(logicTunnelGroupName);
+            if (tunnel == null) {
+                WriteTransaction tx = dataBroker.newWriteOnlyTransaction();
+                LOG.info("MULTIPLE_VxLAN_TUNNELS: add the logical tunnel group {} because a first tunnel"
+                        + " interface on srcDpnId {}:{} dstDpnId {}:{} is created", logicTunnelGroupName, srcDpnId,
+                        srcTe.getIpAddress(), dstDpnId, dstTe.getIpAddress());
+
+                createTunnelInterface(srcTe, dstTe, srcDpnId, dstDpnId, TunnelTypeLogicalGroup.class,
+                                     logicTunnelGroupName, null);
+                createInternalTunnel(srcDpnId, dstDpnId, TunnelTypeLogicalGroup.class, logicTunnelGroupName, tx);
+                futures.add(tx.submit());
+            } else {
+                LOG.debug("MULTIPLE_VxLAN_TUNNELS: not first tunnel on srcDpnId {} dstDpnId {}",srcDpnId, dstDpnId);
+            }
+            return futures;
+        }
     }
 }
