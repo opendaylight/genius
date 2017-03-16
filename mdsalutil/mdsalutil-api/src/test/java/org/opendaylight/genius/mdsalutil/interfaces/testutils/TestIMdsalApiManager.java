cb2db988c1308ac2aab34e3bcb31cc0090f7a338
Michael Vorburger <vorburger@redhat.com>
2017-03-16 13:33:47 +0100

    [WIP] TestIMdsalApiManager with naturally sorted flows
    This leads to much clearer failure messages in assertFlowsInAnyOrder()
    
    TODO write better Comparable utility to compare several fields correctly
    
    TODO rebase after https://git.opendaylight.org/gerrit/#/c/53394/ is
    merged
    
    Change-Id: Ic7c07d7eb29a8937b88d3b094c2fcdcdad755ee4
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- mdsalutil/mdsalutil-api/src/test/java/org/opendaylight/genius/mdsalutil/interfaces/testutils/TestIMdsalApiManager.java
+++ mdsalutil/mdsalutil-api/src/test/java/org/opendaylight/genius/mdsalutil/interfaces/testutils/TestIMdsalApiManager.java
@@ -19,11 +19,13 @@ import com.google.common.util.concurrent.CheckedFuture;
 import com.google.common.util.concurrent.Futures;
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import org.mockito.Mockito;
 import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
 import org.opendaylight.genius.mdsalutil.FlowEntity;
 import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
+import org.opendaylight.infrautils.utils.CompareUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -89,6 +91,9 @@ public abstract class TestIMdsalApiManager implements IMdsalApiManager {
         // TODO Support Iterable <-> List directly within XtendBeanGenerator
         List<FlowEntity> expectedFlowsAsNewArrayList = Lists.newArrayList(expectedFlows);
 
+        List<FlowEntity> sortedFlows = sortFlows(flows);
+        List<FlowEntity> sortedExpectedFlows = sortFlows(expectedFlowsAsNewArrayList);
+
         // FYI: This containsExactlyElementsIn() assumes that FlowEntity, and everything in it,
         // has correctly working equals() implementations.  assertEqualBeans() does not assume
         // that, and would work even without equals, because it only uses property reflection.
@@ -106,20 +111,28 @@ public abstract class TestIMdsalApiManager implements IMdsalApiManager {
         // is not a good idea (different instances can have same hashCode), and e.g. on
         // System#identityHashCode even less so.
         try {
-            assertThat(flows).containsExactlyElementsIn(expectedFlowsAsNewArrayList);
+            assertThat(sortedFlows).containsExactlyElementsIn(sortedExpectedFlows);
         } catch (AssertionError e) {
             // We LOG the expected and actual flow in case of a failed assertion
             // because, even though that is typically just a HUGE String that's
             // hard to read (the diff printed subsequently by assertEqualBeans
             // is, much, more readable), there are cases when looking more closely
             // at the full toString() output of the flows is still useful, so:
-            LOG.warn("assert fail; expected flows: {}", expectedFlowsAsNewArrayList);
-            LOG.warn("assert fail; actual flows  : {}", flows);
+            LOG.warn("assert fail; expected flows: {}", sortedExpectedFlows);
+            LOG.warn("assert fail; actual flows  : {}", sortedFlows);
             // The point of this is basically just that our assertEqualBeans output,
             // in case of a comparison failure, is *A LOT* more clearly readable
             // than what G Truth (or Hamcrest) can do based on toString.
-            assertEqualBeans(expectedFlowsAsNewArrayList, flows);
+            assertEqualBeans(sortedExpectedFlows, sortedFlows);
         }
+    }
+
+    private List<FlowEntity> sortFlows(Iterable<FlowEntity> flows) {
+        List<FlowEntity> sortedFlows = Lists.newArrayList(flows);
+        Collections.sort(sortedFlows,
+            (flow1, flow2) -> CompareUtil.safeCompareTo(flow1.getTableId(), flow2.getTableId()));
+            // TODO next also compare to getPriority() etc.
+        return sortedFlows;
     }
 
     @Override
