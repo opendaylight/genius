4d388588973a5cb1378eefae2f89e82111bfdd2c
Michael Vorburger <vorburger@redhat.com>
2017-03-23 12:39:53 +0100

    [WIP] TestIMdsalApiManager now ignores "duplicate" flows
    TODO we need to agree on a strict definition of "duplicate" here...
    which of the two proposed new tests do we want to pass?
    
    Change-Id: Ic2017acd2c008d5ab30840b1533b51bd9b82453b
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- mdsalutil/mdsalutil-api/src/test/java/org/opendaylight/genius/mdsalutil/interfaces/testutils/tests/TestIMdsalApiManagerTest.java
+++ mdsalutil/mdsalutil-api/src/test/java/org/opendaylight/genius/mdsalutil/interfaces/testutils/tests/TestIMdsalApiManagerTest.java
@@ -44,8 +44,39 @@ public class TestIMdsalApiManagerTest {
         mdsalApiManager.assertFlowsInAnyOrder(ImmutableList.of(getNewFlow2(), getNewFlow1()));
     }
 
+    @Test
+    public void testInstallingIdenticalFlowTwice() {
+        mdsalApiManager.installFlow(getNewFlow1());
+        mdsalApiManager.installFlow(getNewFlow2());
+        mdsalApiManager.installFlow(getNewFlow1());
+        mdsalApiManager.assertFlowsInAnyOrder(ImmutableList.of(getNewFlow2(), getNewFlow1()));
+        mdsalApiManager.assertFlows(ImmutableList.of(getNewFlow1(), getNewFlow2()));
+    }
+
+    @Test
+    public void testInstallingNearIdenticalDifferentDpnFlowTwice() {
+        mdsalApiManager.installFlow(getNewFlow1(123));
+        mdsalApiManager.installFlow(getNewFlow2());
+        mdsalApiManager.installFlow(getNewFlow1(456));
+        mdsalApiManager.assertFlows(ImmutableList.of(getNewFlow1(123), getNewFlow2(), getNewFlow1(456)));
+        mdsalApiManager.assertFlowsInAnyOrder(ImmutableList.of(getNewFlow1(456), getNewFlow2(), getNewFlow1(123)));
+    }
+
+    @Test
+    public void testInstallingSimilarFlowWithEqualNaturalIdentitiyTwice() {
+        mdsalApiManager.installFlow(getNewFlow1(123));
+        mdsalApiManager.installFlow(getNewFlow2());
+        mdsalApiManager.installFlow(getNewFlowWithEqualNaturalIdentitiyTo1());
+        mdsalApiManager.assertFlowsInAnyOrder(ImmutableList.of(getNewFlow2(), getNewFlow1()));
+        mdsalApiManager.assertFlows(ImmutableList.of(getNewFlow1(), getNewFlow2()));
+    }
+
     private FlowEntity getNewFlow1() {
-        FlowEntity flow1 = new FlowEntity(123);
+        return getNewFlow1(123);
+    }
+
+    private FlowEntity getNewFlow1(long dpnId) {
+        FlowEntity flow1 = new FlowEntity(dpnId);
         flow1.setTableId(1);
         flow1.setCookie(BigInteger.valueOf(456789));
         flow1.setFlowId("ThisIsFlow1");
@@ -63,11 +94,33 @@ public class TestIMdsalApiManagerTest {
         return flow1;
     }
 
+    /**
+     * This flow has the same "key" (TODO what is that???) as the {@link #getNewFlow1()} but is not Java equals() to it.
+     */
+    private FlowEntity getNewFlowWithEqualNaturalIdentitiyTo1() {
+        FlowEntity flow1 = new FlowEntity(123);
+        flow1.setTableId(1);
+        flow1.setCookie(BigInteger.valueOf(456789));
+        flow1.setFlowId("ThisIsFlow1");
+        flow1.setHardTimeOut(789);
+        flow1.setIdleTimeOut(456);
+        flow1.setPriority(1);
+        flow1.setSendFlowRemFlag(false);
+        flow1.setStrictFlag(false);
+        List<InstructionInfo> instructionInfos = ImmutableList.of(
+                new InstructionClearActions(), new InstructionGotoTable((short) 7));
+        flow1.setInstructionInfoList(instructionInfos);
+        List<MatchInfoBase> matchInfos = ImmutableList.of(
+                new MatchArpSpa("192.168.10.1", "24"));
+        flow1.setMatchInfoList(matchInfos);
+        return flow1;
+    }
+
     private FlowEntity getNewFlow2() {
         FlowEntity flow2 = new FlowEntity(321);
         flow2.setTableId(2);
         flow2.setCookie(BigInteger.valueOf(987654));
-        flow2.setFlowId("ThisIsflow2");
+        flow2.setFlowId("ThisIsFlow2");
         flow2.setHardTimeOut(654);
         flow2.setIdleTimeOut(987);
         flow2.setPriority(2);
