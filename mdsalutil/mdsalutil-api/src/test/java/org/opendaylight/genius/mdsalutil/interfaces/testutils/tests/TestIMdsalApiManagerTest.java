6f3fa292a1def24a206aa9190e4cd845c83a2db8
Michael Vorburger <vorburger@redhat.com>
2017-03-24 03:17:59 +0100

    @Immutable FlowEntity
    This change has impacts in downstream project netvirt, which are handled
    in https://git.opendaylight.org/gerrit/#/c/54390/ (must be merged
    simultaneously)
    
    Change-Id: I3d58caa5cbeafd1df2458942692b9271a650a23c
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- mdsalutil/mdsalutil-api/src/test/java/org/opendaylight/genius/mdsalutil/interfaces/testutils/tests/TestIMdsalApiManagerTest.java
+++ mdsalutil/mdsalutil-api/src/test/java/org/opendaylight/genius/mdsalutil/interfaces/testutils/tests/TestIMdsalApiManagerTest.java
@@ -10,11 +10,9 @@ package org.opendaylight.genius.mdsalutil.interfaces.testutils.tests;
 import com.google.common.collect.ImmutableList;
 import com.google.common.testing.EqualsTester;
 import java.math.BigInteger;
-import java.util.List;
 import org.junit.Test;
 import org.opendaylight.genius.mdsalutil.FlowEntity;
-import org.opendaylight.genius.mdsalutil.InstructionInfo;
-import org.opendaylight.genius.mdsalutil.MatchInfoBase;
+import org.opendaylight.genius.mdsalutil.FlowEntityBuilder;
 import org.opendaylight.genius.mdsalutil.instructions.InstructionClearActions;
 import org.opendaylight.genius.mdsalutil.instructions.InstructionGotoTable;
 import org.opendaylight.genius.mdsalutil.interfaces.testutils.TestIMdsalApiManager;
@@ -45,41 +43,37 @@ public class TestIMdsalApiManagerTest {
     }
 
     private FlowEntity getNewFlow1() {
-        FlowEntity flow1 = new FlowEntity(123);
-        flow1.setTableId(1);
-        flow1.setCookie(BigInteger.valueOf(456789));
-        flow1.setFlowId("ThisIsFlow1");
-        flow1.setHardTimeOut(456);
-        flow1.setIdleTimeOut(789);
-        flow1.setPriority(1);
-        flow1.setSendFlowRemFlag(true);
-        flow1.setStrictFlag(true);
-        List<InstructionInfo> instructionInfos = ImmutableList.of(
-                new InstructionClearActions(), new InstructionGotoTable((short) 2));
-        flow1.setInstructionInfoList(instructionInfos);
-        List<MatchInfoBase> matchInfos = ImmutableList.of(
-                new MatchArpSpa("192.168.1.1", "24"));
-        flow1.setMatchInfoList(matchInfos);
-        return flow1;
+        return new FlowEntityBuilder()
+            .setDpnId(BigInteger.valueOf(123))
+            .setTableId((short) 1)
+            .setCookie(BigInteger.valueOf(456789))
+            .setFlowId("ThisIsFlow1")
+            .setHardTimeOut(456)
+            .setIdleTimeOut(789)
+            .setPriority(1)
+            .setSendFlowRemFlag(true)
+            .setStrictFlag(true)
+            .addInstructionInfoList(
+                new InstructionClearActions(), new InstructionGotoTable((short) 2))
+            .addMatchInfoList(new MatchArpSpa("192.168.1.1", "24"))
+        .build();
     }
 
     private FlowEntity getNewFlow2() {
-        FlowEntity flow2 = new FlowEntity(321);
-        flow2.setTableId(2);
-        flow2.setCookie(BigInteger.valueOf(987654));
-        flow2.setFlowId("ThisIsflow2");
-        flow2.setHardTimeOut(654);
-        flow2.setIdleTimeOut(987);
-        flow2.setPriority(2);
-        flow2.setSendFlowRemFlag(false);
-        flow2.setStrictFlag(false);
-        List<InstructionInfo> instructionInfos = ImmutableList.of(
-                new InstructionGotoTable((short) 1), new InstructionClearActions());
-        flow2.setInstructionInfoList(instructionInfos);
-        List<MatchInfoBase> matchInfos = ImmutableList.of(
-                new MatchArpSpa("10.11.12.30", "24"));
-        flow2.setMatchInfoList(matchInfos);
-        return flow2;
+        return new FlowEntityBuilder()
+                .setDpnId(BigInteger.valueOf(321))
+                .setTableId((short) 2)
+                .setCookie(BigInteger.valueOf(987654))
+                .setFlowId("ThisIsFlow2")
+                .setHardTimeOut(654)
+                .setIdleTimeOut(987)
+                .setPriority(2)
+                .setSendFlowRemFlag(false)
+                .setStrictFlag(false)
+                .addInstructionInfoList(
+                    new InstructionGotoTable((short) 1), new InstructionClearActions())
+                .addMatchInfoList(new MatchArpSpa("10.11.12.30", "24"))
+            .build();
     }
 
 }
