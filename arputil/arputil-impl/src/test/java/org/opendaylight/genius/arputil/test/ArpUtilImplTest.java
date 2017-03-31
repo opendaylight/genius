7356eed2e7d62a9c87b0cc38358105ff59fcee2c
Edwin Anthony <n.edwin.anthony@ericsson.com>
2017-03-31 12:11:24 +0530

    ArpUtil test framework
    Change-Id: I26f7f879cc99286ce43640092ad3836bf04241e7
    Signed-off-by: Edwin Anthony <n.edwin.anthony@ericsson.com>

---
+++ arputil/arputil-impl/src/test/java/org/opendaylight/genius/arputil/test/ArpUtilImplTest.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.arputil.test;
+
+import java.math.BigInteger;
+import javax.inject.Inject;
+import org.junit.Rule;
+import org.junit.rules.MethodRule;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.infrautils.inject.guice.testutils.GuiceRule;
+import org.opendaylight.infrautils.testutils.LogRule;
+
+public class ArpUtilImplTest {
+
+    public @Rule LogRule logRule = new LogRule();
+    public @Rule MethodRule guice = new GuiceRule(new ArputilTestModule());
+
+    static final BigInteger DPN_ID = BigInteger.valueOf(1);
+
+    @Inject DataBroker dataBroker;
+}
