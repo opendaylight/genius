ed82071d3fcca2bd0d4b6976a9877ca2d8b8ad58
Olga Schukin <olga.schukin@hpe.com>
2017-04-06 11:28:42 +0300

    <WIP> Multiple VxLAN tunnels - junit tests
    Change-Id: I9abdf0840276872c5b16e5a4bcb82b9e8673c0f3
    Signed-off-by: Olga Schukin <olga.schukin@hpe.com>

---
+++ itm/itm-impl/src/test/java/org/opendaylight/genius/itm/tests/AbstractItmTestModule.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.itm.tests;
+
+import static org.mockito.Mockito.mock;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.test.DataBrokerTestModule;
+import org.opendaylight.controller.md.sal.common.api.clustering.EntityOwnershipService;
+import org.opendaylight.genius.idmanager.IdManager;
+import org.opendaylight.genius.interfacemanager.InterfacemgrProvider;
+import org.opendaylight.genius.interfacemanager.interfaces.IInterfaceManager;
+import org.opendaylight.genius.itm.impl.ITMManager;
+import org.opendaylight.genius.itm.listeners.InterfaceStateListener;
+import org.opendaylight.genius.itm.listeners.OvsdbNodeListener;
+import org.opendaylight.genius.itm.listeners.TransportZoneListener;
+import org.opendaylight.genius.itm.listeners.TunnelMonitorChangeListener;
+import org.opendaylight.genius.itm.listeners.TunnelMonitorIntervalListener;
+import org.opendaylight.genius.itm.listeners.VtepConfigSchemaListener;
+import org.opendaylight.genius.itm.listeners.cache.DpnTepsInfoListener;
+import org.opendaylight.genius.itm.listeners.cache.ItmMonitoringIntervalListener;
+import org.opendaylight.genius.itm.listeners.cache.ItmMonitoringListener;
+import org.opendaylight.genius.itm.listeners.cache.StateTunnelListListener;
+import org.opendaylight.genius.itm.monitoring.ItmTunnelEventListener;
+import org.opendaylight.genius.itm.rpc.ItmManagerRpcService;
+import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
+import org.opendaylight.genius.mdsalutil.interfaces.testutils.TestIMdsalApiManager;
+import org.opendaylight.infrautils.inject.guice.testutils.AbstractGuiceJsr250Module;
+import org.opendaylight.lockmanager.LockManager;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.IdManagerService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.rpcs.rev160406.ItmRpcService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.lockmanager.rev160413.LockManagerService;
+
+public class AbstractItmTestModule extends AbstractGuiceJsr250Module {
+
+    @Override
+    protected void configureBindings() {
+        // Bindings for services from this project
+        bind(ItmRpcService.class).to(ItmManagerRpcService.class);
+        bind(ITMManager.class);
+        bind(ItmMonitoringIntervalListener.class);
+        bind(DpnTepsInfoListener.class);
+        bind(StateTunnelListListener.class);
+        bind(ItmMonitoringListener.class);
+        bind(TunnelMonitorIntervalListener.class);
+        bind(TransportZoneListener.class);
+        bind(OvsdbNodeListener.class);
+        bind(InterfaceStateListener.class);
+        bind(VtepConfigSchemaListener.class);
+        bind(TunnelMonitorChangeListener.class);
+        bind(ItmTunnelEventListener.class);
+
+        // Bindings for external services to "real" implementations
+        bind(EntityOwnershipService.class).toInstance(mock(EntityOwnershipService.class));
+        bind(IdManagerService.class).to(IdManager.class);
+        bind(LockManagerService.class).to(LockManager.class);
+        bind(DataBroker.class).toInstance(DataBrokerTestModule.dataBroker());
+        bind(IInterfaceManager.class).to(InterfacemgrProvider.class);
+
+        // Bindings to test infra (fakes & mocks)
+        TestIMdsalApiManager mdsalManager = TestIMdsalApiManager.newInstance();
+        bind(IMdsalApiManager.class).toInstance(mdsalManager);
+        bind(TestIMdsalApiManager.class).toInstance(mdsalManager);
+    }
+
+}
+
+
---
+++ itm/itm-impl/src/test/java/org/opendaylight/genius/itm/tests/ItmTunnelAggregationTestModule.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2017 Hewlett Packard Enterprise, Co. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.itm.tests;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opendaylight.genius.datastoreutils.testutils.JobCoordinatorEventsWaiter;
+import org.opendaylight.genius.datastoreutils.testutils.TestableJobCoordinatorEventsWaiter;
+import org.opendaylight.genius.itm.confighelpers.ItmTunnelAggregationHelper;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.config.rev160406.ItmConfig;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.config.rev160406.ItmConfigBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.config.rev160406.itm.config.TunnelAggregation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.config.rev160406.itm.config.TunnelAggregationBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.config.rev160406.itm.config.TunnelAggregationKey;
+
+public class ItmTunnelAggregationTestModule extends AbstractItmTestModule {
+
+    @Override
+    protected void configureBindings() {
+
+        super.configureBindings();
+
+        List<TunnelAggregation> tunnelValues = new ArrayList<>();
+        tunnelValues.add(new TunnelAggregationBuilder().setKey(new TunnelAggregationKey("vxlan"))
+                .setEnabled(true).setTunnelType("vxlan").build());
+        ItmConfig itmConfig = new ItmConfigBuilder().setTunnelAggregation(tunnelValues).build();
+
+        bind(ItmConfig.class).toInstance(itmConfig);
+        bind(ItmTunnelAggregationHelper.class);
+        bind(JobCoordinatorEventsWaiter.class).to(TestableJobCoordinatorEventsWaiter.class);
+    }
+
+}
