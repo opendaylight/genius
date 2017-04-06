110f3d98a6aaa403ea3bfc0030e8ec4d21993147
Periyasamy Palanisamy <periyasamy.palanisamy@ericsson.com>
2017-04-06 17:07:00 +0530

    adding wait-notify for acquiring clustered lock
    * LockManager manager uses busy spin with thread.sleep to acquire a lock
    if there is a contention for it.
    * Now thread.sleep is replaced with wait-notify using ACDTCN on the lock
    
    Change-Id: I4ce3816e150f1e75fedf3093d96ed41efc444fa5
    Signed-off-by: Periyasamy Palanisamy <periyasamy.palanisamy@ericsson.com>

---
+++ lockmanager/lockmanager-impl/src/main/java/org/opendaylight/lockmanager/LockListener.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.lockmanager;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Singleton;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.datastoreutils.AsyncClusteredDataTreeChangeListenerBase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.lockmanager.rev160413.Locks;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.lockmanager.rev160413.locks.Lock;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+public class LockListener extends AsyncClusteredDataTreeChangeListenerBase<Lock, LockListener>
+        implements AutoCloseable {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LockListener.class);
+    private final DataBroker broker;
+
+    public LockListener(DataBroker broker) {
+        this.broker = broker;
+    }
+
+    @PostConstruct
+    public void start() throws Exception {
+        registerListener(LogicalDatastoreType.OPERATIONAL, broker);
+        LOG.info("LockListener listener Started");
+    }
+
+    @Override
+    protected InstanceIdentifier<Lock> getWildCardPath() {
+        return InstanceIdentifier.create(Locks.class).child(Lock.class);
+    }
+
+    @Override
+    protected void remove(InstanceIdentifier<Lock> key, Lock remove) {
+        String lockName = remove.getLockName();
+        LOG.info("Received add for lock {} : {}", lockName, remove);
+        String lockNameSynchronizer = LockManagerUtils.getLockSynchronizerName(lockName);
+        synchronized (lockNameSynchronizer) {
+            lockNameSynchronizer.notifyAll();
+        }
+    }
+
+    @Override
+    protected void update(InstanceIdentifier<Lock> key, Lock dataObjectModificationBefore,
+            Lock dataObjectModificationAfter) {
+    }
+
+    @Override
+    protected void add(InstanceIdentifier<Lock> key, Lock add) {
+        LOG.info("Received add for lock {} : {}", add.getLockName(), add);
+    }
+
+    @Override
+    protected LockListener getDataTreeChangeListener() {
+        return this;
+    }
+
+}
