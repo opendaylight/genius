5c5aba802cc5053bbd3d73cf5747cddefa05400e
Faseela K <faseela.k@ericsson.com>
2017-04-10 13:30:21 +0530

    Enhancing lockmanager junits
    Change-Id: I87c21a7a24229b931c3cdfe2dd939a99c2aaa1e3
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

--- lockmanager/lockmanager-impl/src/test/java/org/opendaylight/lockmanager/tests/LockManagerTest.java
+++ lockmanager/lockmanager-impl/src/test/java/org/opendaylight/lockmanager/tests/LockManagerTest.java
@@ -9,12 +9,14 @@ package org.opendaylight.lockmanager.tests;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.opendaylight.controller.md.sal.binding.test.AbstractConcurrentDataBrokerTest;
@@ -23,6 +25,9 @@ import org.opendaylight.lockmanager.LockManager;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.lockmanager.rev160413.LockInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.lockmanager.rev160413.LockInputBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.lockmanager.rev160413.LockManagerService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.lockmanager.rev160413.TimeUnits;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.lockmanager.rev160413.TryLockInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.lockmanager.rev160413.TryLockInputBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.lockmanager.rev160413.UnlockInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.lockmanager.rev160413.UnlockInputBuilder;
 import org.opendaylight.yangtools.yang.common.RpcResult;
@@ -57,20 +62,65 @@ public class LockManagerTest extends AbstractConcurrentDataBrokerTest {
     }
 
     @Test
-    @Ignore // TODO make this work... currently it leads to an infinite loop
-    // and never gives up, and if it would give up, it would not return an error is expected;
-    // so retry seems to be completely broken; FIXME
+    // test re-lock of already locked key.
+    // lock() RPC will infinitely retry, and it will only come out when the key is unlocked
     public void testLockAndReLockSameAgain() throws InterruptedException, ExecutionException, TimeoutException {
         LockInput lockInput = new LockInputBuilder().setLockName("testLock").build();
         assertSuccessfulFutureRpcResult(lockManager.lock(lockInput));
+        runUnlockTimerTask("testLock", 3000);
+
+        // This will retry infinitely since the other lock is not released!
+        // After 5 seconds, the parallel thread will unlock the key, and the below TC will pass
         assertSuccessfulFutureRpcResult(lockManager.lock(lockInput));
     }
 
-    // TODO testTryLock()
+    @Test
+    // test re-lock of already locked key using tryLock() RPC.
+    // tryLock() RPC will retry only specific number of times, and it will only return after that
+    public void testTryLock() throws InterruptedException, ExecutionException, TimeoutException {
+        TryLockInput lockInput = new TryLockInputBuilder().setLockName("testTryLock").setTime(3L)
+            .setTimeUnit(TimeUnits.Seconds).build();
+        assertSuccessfulFutureRpcResult(lockManager.tryLock(lockInput));
+
+        // The second acquireLock request will retry for 3 seconds
+        // and since the first lock is not unlocked, the request will fail.
+        lockInput = new TryLockInputBuilder().setLockName("testTryLock").setTime(3000L)
+            .setTimeUnit(TimeUnits.Milliseconds).build();
+        assertFailedFutureRpcResult(lockManager.tryLock(lockInput));
+
+        // Try to unlock the key in a separate thread before retry expires, and see
+        // if lock gets acquired.
+        runUnlockTimerTask("testTryLock", 2000);
+
+        lockInput = new TryLockInputBuilder().setLockName("testTryLock").setTime(4000000L)
+            .setTimeUnit(TimeUnits.Microseconds).build();
+        assertSuccessfulFutureRpcResult(lockManager.tryLock(lockInput));
+    }
+
 
     private void assertSuccessfulFutureRpcResult(Future<RpcResult<Void>> futureRpcResult)
             throws InterruptedException, ExecutionException, TimeoutException {
         assertThat(futureRpcResult.get(5, TimeUnit.SECONDS).isSuccessful()).isTrue();
         assertThat(futureRpcResult.get(5, TimeUnit.SECONDS).getErrors()).isEmpty();
+    }
+
+    private void assertFailedFutureRpcResult(Future<RpcResult<Void>> futureRpcResult)
+    throws InterruptedException, ExecutionException, TimeoutException {
+        assertThat(futureRpcResult.get(5, TimeUnit.SECONDS).isSuccessful()).isFalse();
+    }
+
+    private void runUnlockTimerTask(String lockKey, long delay) {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                UnlockInput unlockInput = new UnlockInputBuilder().setLockName(lockKey).build();
+                try {
+                    assertSuccessfulFutureRpcResult(lockManager.unlock(unlockInput));
+                } catch (InterruptedException | ExecutionException | TimeoutException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }, delay);
     }
 }
