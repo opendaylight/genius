46a8386e8baa8cb1cffb77f0e23eb13275083b6e
Faseela K <faseela.k@ericsson.com>
2017-04-11 11:26:14 +0530

    Renaming IFM utility getInterfaceStateFromOperDS
    The utility was first doing a cache read, and in cache miss
    cases, it tends to read from oper DS. So, ideally both the utils should
    be separated so that if someone does not want to use the cache read, they
    should be free to use the same.
    
    Change-Id: Ie9b4371c466e57f75b357e7e6c39ecfb87480478
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

--- interfacemanager/interfacemanager-impl/src/main/java/org/opendaylight/genius/interfacemanager/commons/InterfaceManagerCommonUtils.java
+++ interfacemanager/interfacemanager-impl/src/main/java/org/opendaylight/genius/interfacemanager/commons/InterfaceManagerCommonUtils.java
@@ -165,21 +165,39 @@ public final class InterfaceManagerCommonUtils {
         return interfaceStateMap.get(interfaceName);
     }
 
+    /**
+     * This utility tries to fetch interface-state from cache first,
+     * and if not present tries to read it from operational DS.
+     *
+     *
+     * @param interfaceName
+     *            name of the logical interface.
+     * @param dataBroker
+     *            dataBroker handle to read from datastore
+     *
+     * @return If the data at the supplied path exists, returns interfaces-state object;
+     * if the data at the supplied path does not exist, returns null;
+     *
+     */
     public static org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.state
-        .Interface  getInterfaceStateFromOperDS(String interfaceName, DataBroker dataBroker) {
+        .Interface getInterfaceState(String interfaceName, DataBroker dataBroker) {
         org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.state.Interface
             ifState = getInterfaceStateFromCache(interfaceName);
         if (ifState != null) {
             return ifState;
         }
-        Optional<org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.state
-                .Interface> ifStateOptional = IfmUtil.read(LogicalDatastoreType.OPERATIONAL,
-                        IfmUtil.buildStateInterfaceId(interfaceName), dataBroker);
-        if (ifStateOptional.isPresent()) {
-            ifState = ifStateOptional.get();
+        ifState = getInterfaceStateFromOperDS(interfaceName, dataBroker);
+        if (ifState != null) {
             interfaceStateMap.put(ifState.getName(), ifState);
         }
         return ifState;
+    }
+
+    public static org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang
+        .ietf.interfaces.rev140508.interfaces.state.Interface getInterfaceStateFromOperDS(String interfaceName,
+        DataBroker dataBroker) {
+        return IfmUtil.read(LogicalDatastoreType.OPERATIONAL,
+            IfmUtil.buildStateInterfaceId(interfaceName), dataBroker).orNull();
     }
 
     @Deprecated
--- interfacemanager/interfacemanager-impl/src/main/java/org/opendaylight/genius/interfacemanager/servicebindings/flowbased/config/helpers/FlowBasedIngressServicesConfigBindHelper.java
+++ interfacemanager/interfacemanager-impl/src/main/java/org/opendaylight/genius/interfacemanager/servicebindings/flowbased/config/helpers/FlowBasedIngressServicesConfigBindHelper.java
@@ -77,7 +77,7 @@ public class FlowBasedIngressServicesConfigBindHelper implements FlowBasedServic
 
         org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang
             .ietf.interfaces.rev140508.interfaces.state.Interface ifState = InterfaceManagerCommonUtils
-                .getInterfaceStateFromOperDS(interfaceName, dataBroker);
+                .getInterfaceState(interfaceName, dataBroker);
         if (ifState == null) {
             LOG.warn("Interface not operational, not binding Service for Interface: {}", interfaceName);
             return futures;
