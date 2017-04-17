1b33f7c1f550788e9db0dc3d9c21c422a07000d4
Riyazahmed D Talikoti <riyazahmed.d.talikoti@ericsson.com>
2017-04-17 17:18:30 +0530

    Helper file to move the duplicate methods used.
    getEndpointIpAddressForDPN method is used by multiple modules
    elanmanager, fibmanager, vpnmanager, natservice
    Hence moving this api to itm-api in a helper file.
    We can use this same method in all the above mentioned modules.
    In future we can add such helper methods into this file and continue to
    use.
    
    Change-Id: I2b0c615b3f19b8f09e212a87174ae7ed05dd40ce
    Signed-off-by: Riyazahmed D Talikoti <riyazahmed.d.talikoti@ericsson.com>

---
+++ itm/itm-api/src/main/java/org/opendaylight/genius/itm/helper/ITMHelper.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright © 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.genius.itm.helper;
+
+
+import com.google.common.base.Optional;
+import java.math.BigInteger;
+import java.util.List;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.ReadOnlyTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.DpnEndpoints;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.dpn.endpoints.DPNTEPsInfo;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.dpn.endpoints.DPNTEPsInfoKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.dpn.endpoints.dpn.teps.info.TunnelEndPoints;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class ITMHelper {
+    private static final Logger LOG = LoggerFactory.getLogger(ITMHelper.class);
+
+    @SuppressWarnings("checkstyle:IllegalCatch")
+    public static <T extends DataObject> Optional<T> read(DataBroker broker, LogicalDatastoreType datastoreType,
+                                                          InstanceIdentifier<T> path) {
+        ReadOnlyTransaction tx = broker.newReadOnlyTransaction();
+        Optional<T> result = Optional.absent();
+        try {
+            result = tx.read(datastoreType, path).get();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return result;
+    }
+
+    private static String getEndpointIpAddressForDPN(DataBroker broker, BigInteger dpnId) {
+        String nextHopIp = null;
+        InstanceIdentifier<DPNTEPsInfo> tunnelInfoId =
+                InstanceIdentifier.builder(DpnEndpoints.class).child(DPNTEPsInfo.class,
+                        new DPNTEPsInfoKey(dpnId)).build();
+        Optional<DPNTEPsInfo> tunnelInfo = read(broker, LogicalDatastoreType.CONFIGURATION, tunnelInfoId);
+        if (tunnelInfo.isPresent()) {
+            List<TunnelEndPoints> nexthopIpList = tunnelInfo.get().getTunnelEndPoints();
+            if (nexthopIpList != null && !nexthopIpList.isEmpty()) {
+                nextHopIp = nexthopIpList.get(0).getIpAddress().getValue().toString();
+            }
+        }
+        return nextHopIp;
+    }
+}
