6cdf30ca1445e7bcd93be22d39ed9897248721fd
Edwin Anthony <n.edwin.anthony@ericsson.com>
2017-04-20 11:09:06 +0530

    ArpUtil Test
    Change-Id: I084e89b7cf8af6090b3fde30412ac8a809c24647
    Signed-off-by: Edwin Anthony <n.edwin.anthony@ericsson.com>

--- arputil/arputil-impl/src/test/java/org/opendaylight/genius/arputil/test/ArpUtilTestModule.java
+++ arputil/arputil-impl/src/test/java/org/opendaylight/genius/arputil/test/ArpUtilTestModule.java
@@ -28,8 +28,10 @@ public class ArpUtilTestModule extends AbstractGuiceJsr250Module {
         DataBroker dataBroker = DataBrokerTestModule.dataBroker();
         bind(DataBroker.class).toInstance(dataBroker);
 
-        bind(OdlInterfaceRpcService.class).toInstance(mock(OdlInterfaceRpcService.class));
-        bind(PacketProcessingService.class).toInstance(mock(PacketProcessingService.class));
+        OdlInterfaceRpcTestModule odlInterfaceRpcTestModule = OdlInterfaceRpcTestModule.newInstance();
+        bind(OdlInterfaceRpcService.class).toInstance(odlInterfaceRpcTestModule);
+        PacketProcessingTestModule packetProcessingService = PacketProcessingTestModule.newInstance();
+        bind(PacketProcessingService.class).toInstance(packetProcessingService);
         bind(NotificationService.class).toInstance(mock(NotificationService.class));
         bind(NotificationPublishService.class).toInstance(mock(NotificationPublishService.class));
         bind(OdlArputilService.class).to(ArpUtilImpl.class);
