844d60d374abd6dc3920b65d9091a9ddb6487d70
Edwin Anthony <n.edwin.anthony@ericsson.com>
2017-04-20 10:56:47 +0530

    ArpUtil Test
    Change-Id: I084e89b7cf8af6090b3fde30412ac8a809c24647
    Signed-off-by: Edwin Anthony <n.edwin.anthony@ericsson.com>

---
+++ arputil/arputil-impl/src/test/java/org/opendaylight/genius/arputil/test/PacketProcessingTestModule.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.arputil.test;
+
+import static org.opendaylight.yangtools.testutils.mockito.MoreAnswers.realOrException;
+
+import com.google.common.util.concurrent.Futures;
+import java.util.concurrent.Future;
+import org.mockito.Mockito;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketProcessingService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.TransmitPacketInput;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
+
+public class PacketProcessingTestModule implements PacketProcessingService {
+
+    public static PacketProcessingTestModule newInstance() {
+        return Mockito.mock(PacketProcessingTestModule.class, realOrException());
+    }
+
+    @Override
+    public Future<RpcResult<Void>> transmitPacket(TransmitPacketInput input) {
+        RpcResultBuilder<Void> rpcResultBuilder = RpcResultBuilder.success();
+        return Futures.immediateFuture(rpcResultBuilder.build());
+    }
+}
