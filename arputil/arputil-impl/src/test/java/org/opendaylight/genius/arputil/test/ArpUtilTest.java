0c750bc34fd4eedba03b302109e1ed9820ce2b8d
Edwin Anthony <n.edwin.anthony@ericsson.com>
2017-04-21 12:18:31 +0530

    ArpUtil Test
    Change-Id: I084e89b7cf8af6090b3fde30412ac8a809c24647
    Signed-off-by: Edwin Anthony <n.edwin.anthony@ericsson.com>
    (cherry picked from commit bb268e54fde36185dae9766847a99fdbea212ea3)

--- arputil/arputil-impl/src/test/java/org/opendaylight/genius/arputil/test/ArpUtilTest.java
+++ arputil/arputil-impl/src/test/java/org/opendaylight/genius/arputil/test/ArpUtilTest.java
@@ -7,21 +7,87 @@
  */
 package org.opendaylight.genius.arputil.test;
 
+import static org.opendaylight.genius.arputil.test.ArpUtilTestUtil.INTERFACE_NAME;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Future;
 import javax.inject.Inject;
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.MethodRule;
-import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.genius.arputil.internal.ArpUtilCounters;
+import org.opendaylight.genius.arputil.internal.ArpUtilImpl;
 import org.opendaylight.infrautils.inject.guice.testutils.GuiceRule;
 import org.opendaylight.infrautils.testutils.LogRule;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv4Address;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.PhysAddress;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.arputil.rev160406.GetMacInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.arputil.rev160406.GetMacInputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.arputil.rev160406.GetMacOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.arputil.rev160406.OdlArputilService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.arputil.rev160406.SendArpResponseInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.arputil.rev160406.SendArpResponseInputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.arputil.rev160406.interfaces.InterfaceAddress;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.arputil.rev160406.interfaces.InterfaceAddressBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketReceived;
+import org.opendaylight.yangtools.yang.common.RpcResult;
 
 public class ArpUtilTest {
 
     public @Rule LogRule logRule = new LogRule();
     public @Rule MethodRule guice = new GuiceRule(new ArpUtilTestModule());
 
-    @Inject DataBroker dataBroker;
+    @Inject ArpUtilImpl arpUtil;
+    @Inject OdlArputilService odlArputilService;
 
     @Test
-    public void testWiring(){ }
+    public void testGetMac() throws Exception {
+
+        final InterfaceAddress interfaceAddress = new InterfaceAddressBuilder()
+                .setInterface(INTERFACE_NAME)
+                .setIpAddress(new IpAddress(Ipv4Address.getDefaultInstance("192.168.0.1")))
+                .setMacaddress(new PhysAddress("1F:1F:1F:1F:1F:1F")).build();
+
+        final List<InterfaceAddress> itf = Arrays.asList(interfaceAddress);
+
+        GetMacInput getMacInput = new GetMacInputBuilder()
+                .setIpaddress(new IpAddress(Ipv4Address.getDefaultInstance("192.168.0.2")))
+                .setInterfaceAddress(itf).build();
+
+        PacketReceived packetReceived = ArpUtilTestUtil.createPayload(0); //request payload
+
+        Future<RpcResult<GetMacOutput>> output = odlArputilService.getMac(getMacInput);
+
+        arpUtil.onPacketReceived(packetReceived);
+
+        Assert.assertEquals("00:01:02:03:04:05", output.get().getResult().getMacaddress().getValue());
+    }
+
+    @Test
+    public void testRespRecvdNotification() {
+
+        PacketReceived packetReceived = ArpUtilTestUtil.createPayload(1); //response payload
+
+        Assert.assertEquals(0,ArpUtilCounters.arp_res_rcv_notification.get());
+
+        arpUtil.onPacketReceived(packetReceived);//onPacketReceived triggers counter.inc()
+
+        Assert.assertEquals(1,ArpUtilCounters.arp_res_rcv_notification.get());
+    }
+
+    @Test
+    public void testSendArpResponse() throws Exception {
+
+        SendArpResponseInput builder = new SendArpResponseInputBuilder().setInterface(INTERFACE_NAME)
+                .setSrcIpaddress(new IpAddress(Ipv4Address.getDefaultInstance("192.168.0.1")))
+                .setDstIpaddress(new IpAddress(Ipv4Address.getDefaultInstance("192.168.0.2")))
+                .setSrcMacaddress(new PhysAddress("1F:1F:1F:1F:1F:1F"))
+                .setDstMacaddress(new PhysAddress("00:01:02:03:04:05")).build();
+
+        Assert.assertEquals(true , odlArputilService.sendArpResponse(builder).get().isSuccessful());
+
+    }
 }
---
+++ arputil/arputil-impl/src/test/java/org/opendaylight/genius/arputil/test/ArpUtilTestUtil.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.arputil.test;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Node;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeConnectorRef;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.match.MetadataBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketReceived;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketReceivedBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.SendToController;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.packet.received.MatchBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.table.types.rev131026.TableId;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+public final class ArpUtilTestUtil {
+
+
+    public static final BigInteger DPN_ID = BigInteger.ONE;
+    public static final BigInteger META_DATA = BigInteger.TEN;
+    public static final long PORT_NUMBER = 2;
+    public static final String URI = "2";
+    public static final String INTERFACE_NAME = "23701c04-7e58-4c65-9425-78a80d49a218";
+    public static final String[] OP_CODE = new String[]{"0 1", "0 2"}; //array to store opCodes
+
+    private ArpUtilTestUtil() {}
+
+    public static PacketReceived createPayload(int oc) {
+
+        final short ID = 2;
+        byte [] payload = bytePayload(
+                "1F 1F 1F 1F 1F 1F",                               // Destination MAC
+                "00 01 02 03 04 05",                               // Source MAC
+                "08 06",                                           // Ethernet type
+                "0 1",                                             // Hardware type
+                "8 0",                                             // Protocol type
+                "6",                                               // Hardware size
+                "4",                                               // Protocol size
+                OP_CODE[oc],                                       // Opcode
+                "00 01 02 03 04 05",                               // Sender MAC Address
+                "C0 A8 0 2",                                       // Sender IP Address
+                "00 01 02 03 04 05",                               // Target MAC Address
+                "C0 A8 0 2"                                        // Target IP Address
+                );
+
+        return new PacketReceivedBuilder().setPacketInReason(SendToController.class)
+                .setTableId(new TableId(ID)).setPayload(payload)
+                .setIngress(new NodeConnectorRef(InstanceIdentifier.create(Node.class)))
+                .setMatch(new MatchBuilder().setMetadata(new MetadataBuilder().setMetadata(META_DATA).build()).build())
+                .build();
+    }
+
+    private static byte[] bytePayload(String... contents) {
+
+        List<String[]> splitContents = new ArrayList<>();
+        int packetLength = 0;
+        for (String content : contents) {
+            String[] split = content.split(" ");
+            packetLength += split.length;
+            splitContents.add(split);
+        }
+        byte[] packet = new byte[packetLength];
+        int index = 0;
+        for (String[] split : splitContents) {
+            for (String component : split) {
+                packet[index] = (byte) Integer.parseInt(component, 16);
+                index++;
+            }
+        }
+        return packet;
+    }
+}
