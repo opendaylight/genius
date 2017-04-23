0f95f8b188cee8937fb916ab30083e366c09d7ff
Faseela K <faseela.k@ericsson.com>
2017-04-23 23:37:47 +0530

    [WIP] Bug 7451 - Leftovers in dispatcher table when unbind and ietf-state delete occurs simultaneously
    Adding service-state-information to cache, whenever a service is bound to
    an interface, and this information will be deleted only when the last
    service on an interface is unbound. To make the first and last service
    counting more robust, moving the boundservices config listener, one layer
    up, at the services-info level, so that the event itself will come with
    the number of services currently bound an interface. Reading the same info
    from DS later on during the event processing, can cause incorrect data
    being read from DS, when multiple parallel services might be getting bound
    and unbound without any synchornization.
    
    Change-Id: Iec1fcf12f71f34fed01158aae483a1d36bac6757
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

--- interfacemanager/interfacemanager-impl/src/main/java/org/opendaylight/genius/interfacemanager/servicebindings/flowbased/config/helpers/FlowBasedIngressServicesConfigBindHelper.java
+++ interfacemanager/interfacemanager-impl/src/main/java/org/opendaylight/genius/interfacemanager/servicebindings/flowbased/config/helpers/FlowBasedIngressServicesConfigBindHelper.java
@@ -8,6 +8,7 @@
 package org.opendaylight.genius.interfacemanager.servicebindings.flowbased.config.helpers;
 
 import com.google.common.util.concurrent.ListenableFuture;
+
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,11 +26,8 @@ import org.opendaylight.genius.mdsalutil.NwConstants;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.iana._if.type.rev140508.L2vlan;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.iana._if.type.rev140508.Tunnel;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.Interface;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.servicebinding.rev160406.ServiceModeBase;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.servicebinding.rev160406.service.bindings.ServicesInfo;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.servicebinding.rev160406.service.bindings.services.info.BoundServices;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeConnectorId;
-import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -66,14 +64,10 @@ public class FlowBasedIngressServicesConfigBindHelper implements FlowBasedServic
     }
 
     @Override
-    public List<ListenableFuture<Void>> bindService(InstanceIdentifier<BoundServices> instanceIdentifier,
-            BoundServices boundServiceNew) {
+    public List<ListenableFuture<Void>> bindService(String interfaceName, BoundServices boundServiceNew,
+                                                    List<BoundServices> allServices) {
         List<ListenableFuture<Void>> futures = new ArrayList<>();
         DataBroker dataBroker = interfaceMgrProvider.getDataBroker();
-        String interfaceName = InstanceIdentifier.keyOf(instanceIdentifier.firstIdentifierOf(ServicesInfo.class))
-                .getInterfaceName();
-        Class<? extends ServiceModeBase> serviceMode = InstanceIdentifier
-                .keyOf(instanceIdentifier.firstIdentifierOf(ServicesInfo.class)).getServiceMode();
 
         org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang
             .ietf.interfaces.rev140508.interfaces.state.Interface ifState = InterfaceManagerCommonUtils
@@ -83,18 +77,8 @@ public class FlowBasedIngressServicesConfigBindHelper implements FlowBasedServic
             return futures;
         }
 
-        // Get the Parent ServiceInfo
-
-        ServicesInfo servicesInfo = FlowBasedServicesUtils.getServicesInfoForInterface(interfaceName, serviceMode,
-                dataBroker);
-        if (servicesInfo == null) {
-            LOG.error("Reached Impossible part 1 in the code during bind service for: {}", boundServiceNew);
-            return futures;
-        }
-
-        List<BoundServices> allServices = servicesInfo.getBoundServices();
         if (allServices.isEmpty()) {
-            LOG.error("Reached Impossible part 2 in the code during bind service for: {}", boundServiceNew);
+            LOG.error("Reached Impossible part 1 in the code during bind service for: {}", boundServiceNew);
             return futures;
         }
         // Split based on type of interface...
