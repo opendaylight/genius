4fe5b42703f5b490f5c0c2657d217f783ef82abc
Faseela K <faseela.k@ericsson.com>
2017-04-24 17:07:53 +0530

    [WIP] Bug7451 - Phase2
    Maintaining state information for bound-services in cache,
    and use the same while unbinding service so as to avoid race conditions
    when interface-state and service-unbinding happens exactly at the same
    time
    
    Change-Id: I39f6b3cef932d624de42d4cb56651fc8bb1de762
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

--- interfacemanager/interfacemanager-impl/src/main/java/org/opendaylight/genius/interfacemanager/servicebindings/flowbased/utilities/FlowBasedServicesUtils.java
+++ interfacemanager/interfacemanager-impl/src/main/java/org/opendaylight/genius/interfacemanager/servicebindings/flowbased/utilities/FlowBasedServicesUtils.java
@@ -213,7 +213,7 @@ public class FlowBasedServicesUtils {
         }
 
         String serviceRef = boundServiceNew.getServiceName();
-        String flowRef = getFlowRef(dpId, NwConstants.VLAN_INTERFACE_INGRESS_TABLE, iface.getName(), boundServiceNew,
+        String flowRef = getFlowRef(dpId, NwConstants.VLAN_INTERFACE_INGRESS_TABLE, iface.getName(),
                 boundServiceNew.getServicePriority());
         StypeOpenflow stypeOpenflow = boundServiceNew.getAugmentation(StypeOpenflow.class);
         Flow ingressFlow = MDSALUtil.buildFlowNew(tableId, flowRef, stypeOpenflow.getFlowPriority(), serviceRef, 0, 0,
@@ -240,7 +240,6 @@ public class FlowBasedServicesUtils {
 
     public static void installLPortDispatcherFlow(BigInteger dpId, BoundServices boundService, String interfaceName,
             WriteTransaction writeTransaction, int interfaceTag, short currentServiceIndex, short nextServiceIndex) {
-        LOG.debug("Installing LPort Dispatcher Flow {}, {}", dpId, interfaceName);
         String serviceRef = boundService.getServiceName();
         List<MatchInfo> matches = FlowBasedServicesUtils.getMatchInfoForDispatcherTable(dpId, interfaceTag,
                 currentServiceIndex);
@@ -274,17 +273,14 @@ public class FlowBasedServicesUtils {
             }
         }
 
-        //////////////////////////////////////////
-        // FIXME: workaround for https://bugs.opendaylight.org/show_bug.cgi?id=7451
-        int flowPriority = DEFAULT_DISPATCHER_PRIORITY;
-        //////////////////////////////////////////
-
         // build the flow and install it
-        String flowRef = getFlowRef(dpId, NwConstants.LPORT_DISPATCHER_TABLE, interfaceName, boundService,
-                currentServiceIndex);
+        String flowRef = getFlowRef(dpId, NwConstants.LPORT_DISPATCHER_TABLE, interfaceName,
+            boundService.getServicePriority());
         Flow ingressFlow = MDSALUtil.buildFlowNew(NwConstants.LPORT_DISPATCHER_TABLE, flowRef,
-                flowPriority, serviceRef, 0, 0, stypeOpenFlow.getFlowCookie(), matches,
+                DEFAULT_DISPATCHER_PRIORITY, serviceRef, 0, 0, stypeOpenFlow.getFlowCookie(), matches,
                 instructions);
+        LOG.debug("Installing LPort Dispatcher Flow on DPN {}, for interface {}, with flowRef {}", dpId,
+            interfaceName, flowRef);
         installFlow(dpId, ingressFlow, writeTransaction);
     }
 
@@ -346,8 +342,8 @@ public class FlowBasedServicesUtils {
         }
 
         // build the flow and install it
-        String flowRef = getFlowRef(dpId, NwConstants.EGRESS_LPORT_DISPATCHER_TABLE, interfaceName, boundService,
-                currentServiceIndex);
+        String flowRef = getFlowRef(dpId, NwConstants.EGRESS_LPORT_DISPATCHER_TABLE, interfaceName,
+            boundService.getServicePriority());
         Flow egressFlow = MDSALUtil.buildFlowNew(NwConstants.EGRESS_LPORT_DISPATCHER_TABLE, flowRef,
                 boundService.getServicePriority(), serviceRef, 0, 0, stypeOpenFlow.getFlowCookie(), matches,
                 instructions);
@@ -469,7 +465,7 @@ public class FlowBasedServicesUtils {
 
     public static void removeIngressFlow(String name, BoundServices serviceOld, BigInteger dpId,
             WriteTransaction writeTransaction) {
-        String flowKeyStr = getFlowRef(dpId, NwConstants.VLAN_INTERFACE_INGRESS_TABLE, name, serviceOld,
+        String flowKeyStr = getFlowRef(dpId, NwConstants.VLAN_INTERFACE_INGRESS_TABLE, name,
                 serviceOld.getServicePriority());
         LOG.debug("Removing Ingress Flow {}", flowKeyStr);
         FlowKey flowKey = new FlowKey(new FlowId(flowKeyStr));
@@ -488,8 +484,8 @@ public class FlowBasedServicesUtils {
 
         boundServicesOld.getAugmentation(StypeOpenflow.class);
         // build the flow and install it
-        String flowRef = getFlowRef(dpId, NwConstants.LPORT_DISPATCHER_TABLE, iface, boundServicesOld,
-                currentServiceIndex);
+        String flowRef = getFlowRef(dpId, NwConstants.LPORT_DISPATCHER_TABLE, iface,
+            boundServicesOld.getServicePriority());
         FlowKey flowKey = new FlowKey(new FlowId(flowRef));
         Node nodeDpn = buildInventoryDpnNode(dpId);
         InstanceIdentifier<Flow> flowInstanceId = InstanceIdentifier.builder(Nodes.class)
@@ -510,8 +506,8 @@ public class FlowBasedServicesUtils {
     private static void removeEgressDispatcherFlow(BigInteger dpId, String iface, WriteTransaction writeTransaction,
             short currentServiceIndex, BoundServices boundServicesOld) {
         // build the flow and install it
-        String flowRef = getFlowRef(dpId, NwConstants.EGRESS_LPORT_DISPATCHER_TABLE, iface, boundServicesOld,
-                currentServiceIndex);
+        String flowRef = getFlowRef(dpId, NwConstants.EGRESS_LPORT_DISPATCHER_TABLE, iface,
+            boundServicesOld.getServicePriority());
         FlowKey flowKey = new FlowKey(new FlowId(flowRef));
         Node nodeDpn = buildInventoryDpnNode(dpId);
         InstanceIdentifier<Flow> flowInstanceId = InstanceIdentifier.builder(Nodes.class)
@@ -542,10 +538,10 @@ public class FlowBasedServicesUtils {
         return String.format("%d:%s:%s", tableId, dpnId, infName);
     }
 
-    private static String getFlowRef(BigInteger dpnId, short tableId, String iface, BoundServices service,
-            short currentServiceIndex) {
+    private static String getFlowRef(BigInteger dpnId, short tableId, String iface,
+            short servicePriority) {
         return String.valueOf(dpnId) + tableId + NwConstants.FLOWID_SEPARATOR + iface + NwConstants.FLOWID_SEPARATOR
-                + currentServiceIndex;
+                + servicePriority;
     }
 
     private static String getSplitHorizonFlowRef(BigInteger dpnId, short tableId, String iface,
@@ -649,6 +645,10 @@ public class FlowBasedServicesUtils {
         WriteTransaction inventoryConfigShardTransaction = dataBroker.newWriteOnlyTransaction();
         installFlow(dpId, ingressFlow, inventoryConfigShardTransaction);
         futures.add(inventoryConfigShardTransaction.submit());
+    }
+
+    public static String getBoundServiceCacheKey(Class<? extends ServiceModeBase> serviceMode, String interfaceName) {
+        return new StringBuilder().append(serviceMode).append(interfaceName).toString();
     }
 
     public static InterfaceBoundServicesState getBoundServicesStateFromCache(String interfaceName) {
