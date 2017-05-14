e9a973b64a0dcf3e3fffd2a49cab1438bb1e7487
Sridhar Gaddam <sgaddam@redhat.com>
2017-05-15 02:24:24 +0530

    Support for moving Src/Dest IPv6 addresses in NxRegMove action
    Depends-On: https://git.opendaylight.org/gerrit/#/c/56969
    
    Change-Id: Ib4ba7baf50416b4e6e43705fbf796f65f4e00aa7
    Signed-off-by: Sridhar Gaddam <sgaddam@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/test/java/org/opendaylight/genius/mdsalutil/actions/ActionMoveSourceDestinationIpv6Test.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright © 2017 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.actions;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import ch.vorburger.xtendbeans.XtendBeanGenerator;
+import org.junit.Test;
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.dst.choice.grouping.dst.choice.DstNxIpv6DstCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nodes.node.table.flow.instructions.instruction.instruction.apply.actions._case.apply.actions.action.action.NxActionRegMoveNodesNodeTableFlowApplyActionsCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.move.grouping.NxRegMove;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.src.choice.grouping.src.choice.SrcNxIpv6SrcCase;
+
+/**
+ * Test class for {@link ActionMoveSourceDestinationIpv6}.
+ */
+public class ActionMoveSourceDestinationIpv6Test {
+    private XtendBeanGenerator generator = new XtendBeanGenerator();
+
+    @Test
+    public void actionInfoTest() {
+        verifyAction(new ActionMoveSourceDestinationIpv6().buildAction());
+    }
+
+    private void verifyAction(Action action) {
+        assertTrue(action.getAction() instanceof NxActionRegMoveNodesNodeTableFlowApplyActionsCase);
+        NxActionRegMoveNodesNodeTableFlowApplyActionsCase actionCase =
+            (NxActionRegMoveNodesNodeTableFlowApplyActionsCase) action.getAction();
+        NxRegMove nxRegMove = actionCase.getNxRegMove();
+        assertTrue(nxRegMove.getSrc().getSrcChoice() instanceof SrcNxIpv6SrcCase);
+        SrcNxIpv6SrcCase srcChoice = (SrcNxIpv6SrcCase) nxRegMove.getSrc().getSrcChoice();
+        assertTrue(srcChoice.isNxIpv6Src());
+        assertEquals(0, nxRegMove.getSrc().getStart().intValue());
+        assertTrue(nxRegMove.getDst().getDstChoice() instanceof DstNxIpv6DstCase);
+        DstNxIpv6DstCase dstChoice = (DstNxIpv6DstCase) nxRegMove.getDst().getDstChoice();
+        assertTrue(dstChoice.isNxIpv6Dst());
+        assertEquals(0, nxRegMove.getDst().getStart().intValue());
+        assertEquals(127, nxRegMove.getDst().getEnd().intValue());
+    }
+
+    @Test
+    public void generateAction() {
+        ActionInfo actionInfo = new ActionMoveSourceDestinationIp();
+        assertEquals("new ActionMoveSourceDestinationIp", generator.getExpression(actionInfo));
+    }
+}
