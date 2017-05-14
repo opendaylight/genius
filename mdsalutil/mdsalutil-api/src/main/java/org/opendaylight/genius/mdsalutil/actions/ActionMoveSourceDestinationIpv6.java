a02b3ae106087e8d4200c10a36665ea24fa946d5
Sridhar Gaddam <sgaddam@redhat.com>
2017-05-15 02:24:24 +0530

    Support for moving Src/Dest IPv6 addresses in NxRegMove action
    Depends-On: https://git.opendaylight.org/gerrit/#/c/56969
    
    Change-Id: Ib4ba7baf50416b4e6e43705fbf796f65f4e00aa7
    Signed-off-by: Sridhar Gaddam <sgaddam@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/actions/ActionMoveSourceDestinationIpv6.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright © 2017 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.actions;
+
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.dst.choice.grouping.dst.choice.DstNxIpv6DstCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nodes.node.table.flow.instructions.instruction.instruction.apply.actions._case.apply.actions.action.action.NxActionRegMoveNodesNodeTableFlowApplyActionsCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.move.grouping.NxRegMove;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.move.grouping.NxRegMoveBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.move.grouping.nx.reg.move.DstBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.move.grouping.nx.reg.move.SrcBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.src.choice.grouping.src.choice.SrcNxIpv6SrcCaseBuilder;
+
+/**
+ * Move source/destination IPv6 action.
+ */
+public class ActionMoveSourceDestinationIpv6 extends ActionInfo {
+    public ActionMoveSourceDestinationIpv6() {
+        this(0);
+    }
+
+    public ActionMoveSourceDestinationIpv6(int actionKey) {
+        super(actionKey);
+    }
+
+    @Override
+    public Action buildAction() {
+        return buildAction(getActionKey());
+    }
+
+    public Action buildAction(int newActionKey) {
+        ActionBuilder ab = new ActionBuilder();
+        NxRegMove regMove = new NxRegMoveBuilder()
+            .setSrc(new SrcBuilder()
+                .setSrcChoice((new SrcNxIpv6SrcCaseBuilder()).setNxIpv6Src(Boolean.TRUE).build())
+                .setStart(0)
+                .build())
+            .setDst(new DstBuilder()
+                .setDstChoice((new DstNxIpv6DstCaseBuilder()).setNxIpv6Dst(Boolean.TRUE).build())
+                .setStart(0)
+                .setEnd(127)
+                .build())
+            .build();
+        ab.setAction(new NxActionRegMoveNodesNodeTableFlowApplyActionsCaseBuilder().setNxRegMove(regMove).build());
+        ab.setKey(new ActionKey(newActionKey));
+        return ab.build();
+    }
+}
