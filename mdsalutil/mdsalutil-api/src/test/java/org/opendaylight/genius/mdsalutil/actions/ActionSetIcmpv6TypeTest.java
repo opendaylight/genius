0aad230ba0248268ca94999e29804cef88e79aa1
Sridhar Gaddam <sgaddam@redhat.com>
2017-05-15 20:25:43 +0530

    Utility api to configure icmpv6 type
    Change-Id: I6c7f2def23e043872957d5272f0fe1217f323c8b
    Signed-off-by: Sridhar Gaddam <sgaddam@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/test/java/org/opendaylight/genius/mdsalutil/actions/ActionSetIcmpv6TypeTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright © 2017 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.actions;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import ch.vorburger.xtendbeans.XtendBeanGenerator;
+import org.junit.Test;
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.SetFieldCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.match.Icmpv6Match;
+
+/**
+ * Test class for {@link ActionSetIcmpv6Type}.
+ */
+public class ActionSetIcmpv6TypeTest {
+    private static final short TYPE = (short) 129;
+
+    private XtendBeanGenerator generator = new XtendBeanGenerator();
+
+    @Test
+    public void actionInfoTest() {
+        verifyAction(new ActionSetIcmpv6Type(TYPE).buildAction());
+    }
+
+    private void verifyAction(Action action) {
+        assertTrue(action.getAction() instanceof SetFieldCase);
+        SetFieldCase actionCase = (SetFieldCase) action.getAction();
+        assertNotNull(actionCase.getSetField().getIcmpv6Match());
+        Icmpv6Match icmpv6Match = actionCase.getSetField().getIcmpv6Match();
+        assertEquals(TYPE, icmpv6Match.getIcmpv6Type().shortValue());
+    }
+
+    @Test
+    public void generateAction() {
+        ActionInfo actionInfo = new ActionSetIcmpv6Type(TYPE);
+        assertEquals("new ActionSetIcmpv6Type(0, 129 as short)", generator.getExpression(actionInfo));
+    }
+}
