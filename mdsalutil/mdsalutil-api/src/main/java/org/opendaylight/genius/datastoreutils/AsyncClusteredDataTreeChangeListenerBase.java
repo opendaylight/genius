c315fe515366062e1a8135436fca1c38495be2d7
Michael Vorburger <vorburger@redhat.com>
2017-05-17 12:36:46 +0200

    Listener base classes should not throw exception if close()'d
    for AsyncClusteredDataTreeChangeListenerBase and
    AsyncDataTreeChangeListenerBase, by introducing a new
    ShutdownLoggingExecutorService infra util. related to -1 discussion in
    https://git.opendaylight.org/gerrit/#/c/57004/
    
    This is an alternative proposal to the ShutdownLoggingExecutorService
    idea from https://git.opendaylight.org/gerrit/#/c/57156/
    
    Change-Id: I5884a188fb5ea3581f99cc559acb158ed370ed7b
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/datastoreutils/AsyncClusteredDataTreeChangeListenerBase.java
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/datastoreutils/AsyncClusteredDataTreeChangeListenerBase.java
@@ -22,6 +22,7 @@ import org.opendaylight.controller.md.sal.binding.api.DataTreeChangeListener;
 import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
 import org.opendaylight.controller.md.sal.binding.api.DataTreeModification;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.infra.LoggingRejectedExecutionHandler;
 import org.opendaylight.genius.utils.SuperTypeUtil;
 import org.opendaylight.infrautils.utils.concurrent.ThreadFactoryProvider;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
@@ -53,7 +54,8 @@ public abstract class AsyncClusteredDataTreeChangeListenerBase
             ThreadFactoryProvider.builder()
                 .namePrefix("AsyncClusteredDataTreeChangeListenerBase-DataTreeChangeHandler")
                 .logger(LOG)
-                .build().get(), rejectedExecutionHandler);
+                .build().get(),
+            new LoggingRejectedExecutionHandler());
 
     protected final Class<T> clazz;
 
