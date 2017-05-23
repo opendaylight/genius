0cc2482e5ecec1854fa18af213711b560510496b
Stephen Kitt <skitt@redhat.com>
2017-05-23 14:45:40 +0200

    Fix vlanIdModified test
    OvsVlanMemberConfigUpdateHelper.vlanIdModified() has an apparent
    copy-paste error in its null/non-null check. The whole test simplifies
    down to !Object.equals(), so this patch just uses that.
    
    Change-Id: I6b3548da5441e8318cf20eab3f4817d162c97fa6
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ interfacemanager/interfacemanager-impl/src/test/java/org/opendaylight/genius/interfacemanager/renderer/ovs/confighelpers/OvsVlanMemberConfigUpdateHelperTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright © 2017 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.interfacemanager.renderer.ovs.confighelpers;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.l2.types.rev130827.VlanId;
+
+/**
+ * Unit tests for {@link OvsVlanMemberConfigUpdateHelper}.
+ */
+public class OvsVlanMemberConfigUpdateHelperTest {
+    @Test
+    public void testVlanIdModified() {
+        VlanId vlanId1 = new VlanId(1);
+        VlanId vlanId2 = new VlanId(2);
+        assertFalse(OvsVlanMemberConfigUpdateHelper.vlanIdModified(null, null));
+        assertTrue(OvsVlanMemberConfigUpdateHelper.vlanIdModified(null, vlanId2));
+        assertTrue(OvsVlanMemberConfigUpdateHelper.vlanIdModified(vlanId1, null));
+        assertFalse(OvsVlanMemberConfigUpdateHelper.vlanIdModified(vlanId1, vlanId1));
+        assertTrue(OvsVlanMemberConfigUpdateHelper.vlanIdModified(vlanId1, vlanId2));
+    }
+}
