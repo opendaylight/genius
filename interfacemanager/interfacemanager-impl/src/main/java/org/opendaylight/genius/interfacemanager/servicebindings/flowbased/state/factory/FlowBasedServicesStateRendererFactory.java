862d8f70c333b7c9b5fe1070485ed8bf3bd25a8f
Faseela K <faseela.k@ericsson.com>
2017-06-05 16:07:19 +0530

    Remove FlowBasedServicesInterfaceStateListener
    Now that all applications are properly calling unbind-service,
    the interfacestatelistener which by itself tries to remove
    dispatcher table entries can be removed for a cleaner implementation
    
    Change-Id: Ic37f45d8c923e8a8f8800d331c3666f50bd7a9bd
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

--- interfacemanager/interfacemanager-impl/src/main/java/org/opendaylight/genius/interfacemanager/servicebindings/flowbased/state/factory/FlowBasedServicesStateRendererFactory.java
+++ interfacemanager/interfacemanager-impl/src/main/java/org/opendaylight/genius/interfacemanager/servicebindings/flowbased/state/factory/FlowBasedServicesStateRendererFactory.java
@@ -11,5 +11,4 @@ public abstract class FlowBasedServicesStateRendererFactory {
 
     public abstract FlowBasedServicesStateAddable getFlowBasedServicesStateAddRenderer();
 
-    public abstract FlowBasedServicesStateRemovable getFlowBasedServicesStateRemoveRenderer();
 }
