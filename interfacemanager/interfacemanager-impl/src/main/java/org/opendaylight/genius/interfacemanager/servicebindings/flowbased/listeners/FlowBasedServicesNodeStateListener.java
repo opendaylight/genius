a6b67665c4e103176f046f039f1910b0c78bc429
Faseela K <faseela.k@ericsson.com>
2017-06-05 16:07:19 +0530

    Remove FlowBasedServicesInterfaceStateListener
    Now that all applications are properly calling unbind-service,
    the interfacestatelistener which by itself tries to remove
    dispatcher table entries can be removed for a cleaner implementation
    
    Change-Id: Ic37f45d8c923e8a8f8800d331c3666f50bd7a9bd
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

--- interfacemanager/interfacemanager-impl/src/main/java/org/opendaylight/genius/interfacemanager/servicebindings/flowbased/listeners/FlowBasedServicesNodeStateListener.java
+++ interfacemanager/interfacemanager-impl/src/main/java/org/opendaylight/genius/interfacemanager/servicebindings/flowbased/listeners/FlowBasedServicesNodeStateListener.java
@@ -8,6 +8,7 @@
 package org.opendaylight.genius.interfacemanager.servicebindings.flowbased.listeners;
 
 import com.google.common.util.concurrent.ListenableFuture;
+
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,11 +16,11 @@ import java.util.concurrent.Callable;
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
 import javax.inject.Singleton;
+
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.genius.datastoreutils.listeners.AbstractSyncDataTreeChangeListener;
 import org.opendaylight.genius.interfacemanager.servicebindings.flowbased.state.factory.FlowBasedServicesStateAddable;
-import org.opendaylight.genius.interfacemanager.servicebindings.flowbased.state.factory.FlowBasedServicesStateRemovable;
 import org.opendaylight.genius.interfacemanager.servicebindings.flowbased.state.factory.FlowBasedServicesStateRendererFactoryResolver;
 import org.opendaylight.genius.interfacemanager.servicebindings.flowbased.utilities.FlowBasedServicesUtils;
 import org.opendaylight.infrautils.jobcoordinator.JobCoordinator;
@@ -39,41 +40,38 @@ public class FlowBasedServicesNodeStateListener extends AbstractSyncDataTreeChan
     private final FlowBasedServicesStateRendererFactoryResolver flowBasedServicesStateRendererFactoryResolver;
 
     @Inject
-    public FlowBasedServicesNodeStateListener(DataBroker dataBroker, JobCoordinator jobCoordinator,
-            FlowBasedServicesStateRendererFactoryResolver flowBasedServicesStateRendererFactoryResolver) {
+    public FlowBasedServicesNodeStateListener(final DataBroker dataBroker, final JobCoordinator jobCoordinator,
+                                              final FlowBasedServicesStateRendererFactoryResolver
+                                                      flowBasedServicesStateRendererFactoryResolver) {
         super(dataBroker, LogicalDatastoreType.OPERATIONAL, InstanceIdentifier.create(Nodes.class).child(Node.class));
         this.jobCoordinator = jobCoordinator;
         this.flowBasedServicesStateRendererFactoryResolver = flowBasedServicesStateRendererFactoryResolver;
     }
 
     @Override
-    public void remove(@Nonnull Node node) {
-        BigInteger dpId = getDpnID(node);
-        if (dpId == null) {
-            return;
-        }
-        unbindServicesOnTunnelType(dpId);
+    public void remove(@Nonnull final Node node) {
+        // Do nothing
     }
 
     @Override
-    public void update(@Nonnull Node originalNode, @Nonnull Node updatedNode) {
+    public void update(@Nonnull final Node originalNode, @Nonnull final Node updatedNode) {
         // Nothing to do
     }
 
     @Override
-    public void add(@Nonnull Node node) {
-        BigInteger dpId = getDpnID(node);
+    public void add(@Nonnull final Node node) {
+        final BigInteger dpId = getDpnID(node);
         if (dpId == null) {
             return;
         }
         bindServicesOnTunnelType(dpId);
     }
 
-    private void bindServicesOnTunnelType(BigInteger dpId) {
+    private void bindServicesOnTunnelType(final BigInteger dpId) {
         LOG.debug("Received node add event for {}", dpId);
-        for (Class<?extends ServiceModeBase> serviceMode : FlowBasedServicesUtils.SERVICE_MODE_MAP.values()) {
-            for (String interfaceName : FlowBasedServicesUtils.INTERFACE_TYPE_BASED_SERVICE_BINDING_KEYWORDS) {
-                FlowBasedServicesStateAddable flowBasedServicesStateAddable =
+        for (final Class<?extends ServiceModeBase> serviceMode : FlowBasedServicesUtils.SERVICE_MODE_MAP.values()) {
+            for (final String interfaceName : FlowBasedServicesUtils.INTERFACE_TYPE_BASED_SERVICE_BINDING_KEYWORDS) {
+                final FlowBasedServicesStateAddable flowBasedServicesStateAddable =
                     flowBasedServicesStateRendererFactoryResolver
                         .getFlowBasedServicesStateRendererFactory(serviceMode).getFlowBasedServicesStateAddRenderer();
                 jobCoordinator.enqueueJob(interfaceName,
@@ -83,27 +81,14 @@ public class FlowBasedServicesNodeStateListener extends AbstractSyncDataTreeChan
         }
     }
 
-    private void unbindServicesOnTunnelType(BigInteger dpId) {
-        LOG.debug("Received node add event for {}", dpId);
-        for (Class<?extends ServiceModeBase> serviceMode : FlowBasedServicesUtils.SERVICE_MODE_MAP.values()) {
-            for (String interfaceName : FlowBasedServicesUtils.INTERFACE_TYPE_BASED_SERVICE_BINDING_KEYWORDS) {
-                FlowBasedServicesStateRemovable flowBasedServicesStateRemovable =
-                    flowBasedServicesStateRendererFactoryResolver.getFlowBasedServicesStateRendererFactory(serviceMode)
-                        .getFlowBasedServicesStateRemoveRenderer();
-                jobCoordinator.enqueueJob(interfaceName,
-                                          new RendererStateInterfaceUnbindWorker(flowBasedServicesStateRemovable, dpId,
-                                                                                 interfaceName));
-            }
-        }
-    }
-
     private static class RendererStateInterfaceBindWorker implements Callable<List<ListenableFuture<Void>>> {
         private final String iface;
         final BigInteger dpnId;
         final FlowBasedServicesStateAddable flowBasedServicesStateAddable;
 
-        RendererStateInterfaceBindWorker(FlowBasedServicesStateAddable flowBasedServicesStateAddable, BigInteger dpnId,
-                                         String iface) {
+        RendererStateInterfaceBindWorker(final FlowBasedServicesStateAddable flowBasedServicesStateAddable,
+                                         final BigInteger dpnId,
+                                         final String iface) {
             this.flowBasedServicesStateAddable = flowBasedServicesStateAddable;
             this.dpnId = dpnId;
             this.iface = iface;
@@ -111,34 +96,14 @@ public class FlowBasedServicesNodeStateListener extends AbstractSyncDataTreeChan
 
         @Override
         public List<ListenableFuture<Void>> call() {
-            List<ListenableFuture<Void>> futures = new ArrayList<>();
+            final List<ListenableFuture<Void>> futures = new ArrayList<>();
             flowBasedServicesStateAddable.bindServicesOnInterfaceType(futures, dpnId, iface);
             return futures;
         }
     }
 
-    private static class RendererStateInterfaceUnbindWorker implements Callable<List<ListenableFuture<Void>>> {
-        private final String iface;
-        final BigInteger dpnId;
-        final FlowBasedServicesStateRemovable flowBasedServicesStateRemovable;
-
-        RendererStateInterfaceUnbindWorker(FlowBasedServicesStateRemovable flowBasedServicesStateRemovable,
-                                           BigInteger dpnId, String iface) {
-            this.flowBasedServicesStateRemovable = flowBasedServicesStateRemovable;
-            this.dpnId = dpnId;
-            this.iface = iface;
-        }
-
-        @Override
-        public List<ListenableFuture<Void>> call() {
-            List<ListenableFuture<Void>> futures = new ArrayList<>();
-            flowBasedServicesStateRemovable.unbindServicesOnInterfaceType(futures, dpnId, iface);
-            return futures;
-        }
-    }
-
-    private BigInteger getDpnID(Node id) {
-        String[] node =  id.getId().getValue().split(":");
+    private BigInteger getDpnID(final Node id) {
+        final String[] node =  id.getId().getValue().split(":");
         if (node.length < 2) {
             LOG.warn("Unexpected nodeId {}", id.getId().getValue());
             return null;
