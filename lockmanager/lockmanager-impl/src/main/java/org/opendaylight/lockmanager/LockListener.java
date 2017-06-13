d959ebf153e5d5a996f4e189c27b9bab1a09832e
Faseela K <faseela.k@ericsson.com>
2017-06-13 15:38:23 +0530

    LockManager - too many info logs
    Change-Id: I4d616da0de2bceb4ca7f80a7e9199db8535a6210
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

--- lockmanager/lockmanager-impl/src/main/java/org/opendaylight/lockmanager/LockListener.java
+++ lockmanager/lockmanager-impl/src/main/java/org/opendaylight/lockmanager/LockListener.java
@@ -49,7 +49,7 @@ public class LockListener extends AsyncClusteredDataTreeChangeListenerBase<Lock,
     @Override
     protected void remove(InstanceIdentifier<Lock> key, Lock remove) {
         String lockName = remove.getLockName();
-        LOG.info("Received remove for lock {} : {}", lockName, remove);
+        LOG.trace("Received remove for lock {} : {}", lockName, remove);
         java.util.Optional.ofNullable(lockManager.getSynchronizerForLock(lockName))
             .ifPresent(future -> future.complete(null));
     }
@@ -61,7 +61,7 @@ public class LockListener extends AsyncClusteredDataTreeChangeListenerBase<Lock,
 
     @Override
     protected void add(InstanceIdentifier<Lock> key, Lock add) {
-        LOG.info("Received add for lock {} : {}", add.getLockName(), add);
+        LOG.trace("Received add for lock {} : {}", add.getLockName(), add);
     }
 
     @Override
