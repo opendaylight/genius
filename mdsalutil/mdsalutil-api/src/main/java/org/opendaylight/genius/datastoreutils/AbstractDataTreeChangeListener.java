51ffdd062ec2ce1536683070050467fab61afa80
David Suarez <david.suarez.fuentes@ericsson.com>
2017-06-19 16:03:27 +0200

    Add a basic framework to create DataTreeChangeListeners
    WIP: Add a basic framework to create DataTreeChangeListeners.
    
    Change-Id: Iaf9ddea9bf312ba7659a57f1d63d5d93fa949938
    Signed-off-by: David Suarez <david.suarez.fuentes@ericsson.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/datastoreutils/AbstractDataTreeChangeListener.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2016, 2017 Ericsson Spain and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.datastoreutils;
+
+import java.util.Collection;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeChangeListener;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeModification;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+
+/**
+ * Abstract class providing some common functionality to specific listeners.
+ *
+ * @author David Suárez (david.suarez.fuentes@ericsson.com)
+ *
+ * @param <T>
+ *            type of the data object the listener is registered to.
+ */
+public abstract class AbstractDataTreeChangeListener<T extends DataObject>
+        implements DataTreeChangeListenerActions<T>, DataTreeChangeListener<T>, AutoCloseable {
+
+    /* (non-Javadoc)
+     * @see org.opendaylight.genius.datastoreutils.DataTreeChangeListenerActions#onDataTreeChanged(java.util.Collection)
+     */
+    @Override
+    public void onDataTreeChanged(Collection<DataTreeModification<T>> collection) {
+        DataTreeChangeListenerActions.super.onDataTreeChanged(collection);
+    }
+}
