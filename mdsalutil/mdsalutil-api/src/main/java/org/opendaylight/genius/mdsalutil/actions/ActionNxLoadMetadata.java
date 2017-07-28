9c9e7e31aaafc1abfced5958a3513ff69983db43
Aswin Suryanarayanan <asuryana@redhat.com>
2017-07-28 17:46:48 +0530

    Bug 8882 - With conntrack SNAT communication with PNF fails
    Added support for load metadata action.
    
    depends on https://git.opendaylight.org/gerrit/#/c/60869
    
    Change-Id: Iafa22368dbeea9e76b3d5f5f43ae5f615f476bcc
    Signed-off-by: Aswin Suryanarayanan <asuryana@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/actions/ActionNxLoadMetadata.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.actions;
+
+import java.math.BigInteger;
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.dst.choice.grouping.dst.choice.DstOfMetadataCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nodes.node.table.flow.instructions.instruction.instruction.apply.actions._case.apply.actions.action.action.NxActionRegLoadNodesNodeTableFlowApplyActionsCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.load.grouping.NxRegLoadBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.load.grouping.nx.reg.load.DstBuilder;
+
+/**
+ * NX load metadata action.
+ */
+public class ActionNxLoadMetadata extends ActionInfo {
+    private static final long serialVersionUID = 1L;
+
+    private final BigInteger value;
+
+    public ActionNxLoadMetadata(BigInteger value) {
+        this(0, value);
+    }
+
+    public ActionNxLoadMetadata(int actionKey, BigInteger value) {
+        super(actionKey);
+        this.value = value;
+    }
+
+    @Override
+    public Action buildAction() {
+        return buildAction(getActionKey());
+    }
+
+    @Override
+    public Action buildAction(int newActionKey) {
+        return new ActionBuilder()
+            .setAction(new NxActionRegLoadNodesNodeTableFlowApplyActionsCaseBuilder()
+                .setNxRegLoad(new NxRegLoadBuilder()
+                    .setDst(new DstBuilder()
+                        .setDstChoice(new DstOfMetadataCaseBuilder().setOfMetadata(true).build())
+                        .setStart(1)
+                        .setEnd(23)
+                        .build())
+                    .setValue(value)
+                    .build())
+                .build())
+            .setKey(new ActionKey(newActionKey))
+            .build();
+    }
+
+    public BigInteger getValue() {
+        return value;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!super.equals(obj)) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        ActionNxLoadMetadata other = (ActionNxLoadMetadata) obj;
+        if (value == null) {
+            if (other.value != null) {
+                return false;
+            }
+        } else if (!value.equals(other.value)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ActionNxLoadMetadata [value=" + value + ", getValue()=" + getValue() + "]";
+    }
+
+}
