e5e3b5f36c4efc1caa1747b171b29017147831ef
Aswin Suryanarayanan <asuryana@redhat.com>
2017-07-28 17:46:48 +0530

    Bug 8882 - With conntrack SNAT communication with PNF fails
    Added support for load metadata action.
    
    depends on https://git.opendaylight.org/gerrit/#/c/60869
    
    Change-Id: Iafa22368dbeea9e76b3d5f5f43ae5f615f476bcc
    Signed-off-by: Aswin Suryanarayanan <asuryana@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/test/java/org/opendaylight/genius/mdsalutil/actions/ActionNxLoadMetadataTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright © 2017 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.actions;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import ch.vorburger.xtendbeans.XtendBeanGenerator;
+import java.math.BigInteger;
+import org.junit.Test;
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.dst.choice.grouping.dst.choice.DstOfMetadataCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nodes.node.table.flow.instructions.instruction.instruction.apply.actions._case.apply.actions.action.action.NxActionRegLoadNodesNodeTableFlowApplyActionsCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.load.grouping.NxRegLoad;
+
+/**
+ * Test for {@link ActionNxLoadMetadata}.
+ */
+public class ActionNxLoadMetadataTest {
+    private static final BigInteger VALUE = BigInteger.TEN;
+
+    private XtendBeanGenerator generator = new XtendBeanGenerator();
+
+    @Test
+    public void actionInfoTest() {
+        verifyAction(new ActionNxLoadMetadata(VALUE).buildAction());
+    }
+
+    private void verifyAction(Action action) {
+        assertTrue(action.getAction() instanceof NxActionRegLoadNodesNodeTableFlowApplyActionsCase);
+        NxActionRegLoadNodesNodeTableFlowApplyActionsCase actionCase =
+            (NxActionRegLoadNodesNodeTableFlowApplyActionsCase) action.getAction();
+        assertNotNull(actionCase.getNxRegLoad());
+        NxRegLoad nxRegLoad = actionCase.getNxRegLoad();
+        assertTrue(nxRegLoad.getDst().getDstChoice() instanceof DstOfMetadataCase);
+        DstOfMetadataCase dstOfMetadataCase = (DstOfMetadataCase) nxRegLoad.getDst().getDstChoice();
+        assertTrue(dstOfMetadataCase.isOfMetadata());
+        assertEquals(1, nxRegLoad.getDst().getStart().intValue());
+        assertEquals(23, nxRegLoad.getDst().getEnd().intValue());
+        assertEquals(VALUE, nxRegLoad.getValue());
+    }
+
+    @Test
+    public void generateAction() {
+        ActionInfo actionInfo = new ActionNxLoadMetadata(VALUE);
+        assertEquals(
+            "new ActionNxLoadMetadata(0, " + VALUE + "bi)", generator.getExpression(actionInfo));
+    }
+}
