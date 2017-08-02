3d4d3820ff62eabec4bee1441d38149eda3d9ede
David Suarez <david.suarez.fuentes@gmail.com>
2017-08-02 11:30:10 +0200

    Remove start() method
    - Remove start() method to avoid the use of a
    useless PostConstruct method, following the recommendations in [1].
    
    - Replace info by debug
    - Replace magic number by constant
    
    [1] https://wiki.opendaylight.org/view/BestPractices/DI_Guidelines
    
    Change-Id: If28fc67742e9107d6129ddca6816b1dc2fd58652
    Signed-off-by: David Suarez <david.suarez.fuentes@gmail.com>

--- lockmanager/lockmanager-impl/src/main/java/org/opendaylight/lockmanager/LockManager.java
+++ lockmanager/lockmanager-impl/src/main/java/org/opendaylight/lockmanager/LockManager.java
@@ -19,7 +19,6 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -41,13 +40,14 @@ import org.slf4j.LoggerFactory;
 @Singleton
 public class LockManager implements LockManagerService {
 
+    private static final int DEFAULT_NUMBER_LOCKING_ATTEMPS = 30;
+    private static final int DEFAULT_RETRY_COUNT = 3;
+    private static final int DEFAULT_WAIT_TIME_IN_MILLIS = 1000;
+
     private final ConcurrentHashMap<String, CompletableFuture<Void>> lockSynchronizerMap =
             new ConcurrentHashMap<>();
 
     private static final Logger LOG = LoggerFactory.getLogger(LockManager.class);
-
-    private static final int DEFAULT_RETRY_COUNT = 3;
-    private static final int DEFAULT_WAIT_TIME_IN_MILLIS = 1000;
 
     private final DataBroker broker;
 
@@ -56,14 +56,9 @@ public class LockManager implements LockManagerService {
         this.broker = dataBroker;
     }
 
-    @PostConstruct
-    public void start() {
-        LOG.info("{} start", getClass().getSimpleName());
-    }
-
     @PreDestroy
     public void close() {
-        LOG.info("{} close", getClass().getSimpleName());
+        LOG.debug("{} close", getClass().getSimpleName());
     }
 
     @Override
@@ -155,7 +150,7 @@ public class LockManager implements LockManagerService {
                 if (readWriteLock(lockInstanceIdentifier, lockData)) {
                     return;
                 } else {
-                    if (retry >= 30) {
+                    if (retry >= DEFAULT_NUMBER_LOCKING_ATTEMPS) {
                         LOG.debug("Already locked for {} after waiting {}ms, try {}",
                                 lockName, DEFAULT_WAIT_TIME_IN_MILLIS, retry);
                     } else {
