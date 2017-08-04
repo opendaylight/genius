cccc6c2d13b835b8f749938d0c560b271da4f875
Faseela K <faseela.k@ericsson.com>
2017-08-05 00:04:21 +0530

    Bug 8859 : Table 220 programmed with wrong service-index
    Fix synchronization issues with service binding transaction submissions
    
    Change-Id: I1cab1590b7333513f079f062c07de483895c316c
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

--- interfacemanager/interfacemanager-impl/src/main/java/org/opendaylight/genius/interfacemanager/servicebindings/flowbased/config/factory/FlowBasedServicesConfigRemovable.java
+++ interfacemanager/interfacemanager-impl/src/main/java/org/opendaylight/genius/interfacemanager/servicebindings/flowbased/config/factory/FlowBasedServicesConfigRemovable.java
@@ -14,7 +14,8 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.ser
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.servicebinding.rev160406.service.bindings.services.info.BoundServices;
 
 public interface FlowBasedServicesConfigRemovable {
-    List<ListenableFuture<Void>> unbindService(String interfaceName,BoundServices boundServiceNew,
-                                               List<BoundServices> boundServicesList,
-                                               BoundServicesState boundServicesState);
+    void unbindService(List<ListenableFuture<Void>> futures,
+                       String interfaceName,BoundServices boundServiceNew,
+                       List<BoundServices> boundServicesList,
+                       BoundServicesState boundServicesState);
 }
