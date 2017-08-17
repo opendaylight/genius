c9f824a84d59bd73ac5b0d800172b6deb729bbc6
Vishal Thapar <vishal.thapar@ericsson.com>
2017-08-18 01:42:06 +0530

    SRM: Add implementation code
    This adds the following:
    1. RPC Implementation in impl and required util class
    2. CLI for SRM as mentioned in spec.
    3. Debug CLI to clear out SRM operDS.
    
    These cover Work Items 3 and 4 of SRM Spec.
    
    Change-Id: I04553678580ff53217c7757594cdd836aa8abfae
    Signed-off-by: Vishal Thapar <vishal.thapar@ericsson.com>

--- srm/impl/src/main/java/org/opendaylight/genius/srm/SrmRpcProvider.java
+++ srm/impl/src/main/java/org/opendaylight/genius/srm/SrmRpcProvider.java
@@ -8,6 +8,7 @@
 
 package org.opendaylight.genius.srm;
 
+import com.google.common.util.concurrent.Futures;
 import java.util.concurrent.Future;
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -16,8 +17,11 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.srm.rpcs.rev170711.R
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.srm.rpcs.rev170711.RecoverOutput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.srm.rpcs.rev170711.ReinstallInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.srm.rpcs.rev170711.ReinstallOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.srm.rpcs.rev170711.RpcSuccess;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.srm.rpcs.rev170711.SrmRpcsService;
+import org.opendaylight.yangtools.yang.common.RpcError.ErrorType;
 import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,13 +39,51 @@ public class SrmRpcProvider implements SrmRpcsService {
 
     @Override
     public Future<RpcResult<RecoverOutput>> recover(RecoverInput input) {
-        LOG.trace("recover() called with {}", input);
-        return null;
+        LOG.trace("recover called for {}", input);
+        RecoverOutput output = null;
+        RpcResultBuilder<RecoverOutput> resultBuilder;
+        try {
+            output = SrmRpcUtils.callSrmOp(dataBroker, input);
+            LOG.trace("recover output: {}", output);
+            if (RpcSuccess.class.equals(output.getResponse())) {
+                resultBuilder = RpcResultBuilder.<RecoverOutput>success()
+                    .withResult(output);
+            } else {
+                resultBuilder = RpcResultBuilder.<RecoverOutput>failed()
+                    .withResult(output)
+                    .withError(ErrorType.APPLICATION, output.getMessage());
+            }
+        } catch (NullPointerException e) {
+            LOG.error("Exception calling SrmRecoveryOp", e);
+            resultBuilder = RpcResultBuilder.<RecoverOutput>failed()
+                .withError(ErrorType.APPLICATION, output.getMessage());
+        }
+        return Futures.immediateFuture(resultBuilder.build());
     }
 
     @Override
     public Future<RpcResult<ReinstallOutput>> reinstall(ReinstallInput input) {
-        LOG.trace("reinstall() called with {}", input);
-        return null;
+        LOG.trace("reinstall called for {}", input);
+
+        ReinstallOutput output = null;
+        RpcResultBuilder<ReinstallOutput> resultBuilder;
+        try {
+            output = SrmRpcUtils.callSrmOp(dataBroker, input);
+            LOG.trace("reinstall output: {}", output);
+            if (output.isSuccessful()) {
+                resultBuilder = RpcResultBuilder.<ReinstallOutput>success()
+                    .withResult(output);
+            } else {
+                resultBuilder = RpcResultBuilder.<ReinstallOutput>failed()
+                    .withResult(output)
+                    .withError(ErrorType.APPLICATION, output.getMessage());
+            }
+        } catch (NullPointerException e) {
+            LOG.error("Exception calling SrmReinstallOp", e);
+            resultBuilder = RpcResultBuilder.<ReinstallOutput>failed()
+                .withError(ErrorType.APPLICATION, output.getMessage());
+        }
+        return Futures.immediateFuture(resultBuilder.build());
     }
+
 }
