a9478aac66809f76da40416f65673f6c49992282
Vishal Thapar <vishal.thapar@ericsson.com>
2017-08-18 01:42:06 +0530

    SRM: Add implementation code
    This adds the following:
    1. RPC Implementation in impl and required util class
    2. CLI for SRM as mentioned in spec.
    3. Debug CLI to clear out SRM operDS.
    
    These cover Work Items 3 and 4 of SRM Spec.
    
    Change-Id: I04553678580ff53217c7757594cdd836aa8abfae
    Signed-off-by: Vishal Thapar <vishal.thapar@ericsson.com>

--- srm/shell/src/main/java/org/opendaylight/genius/srm/shell/RecoverCommand.java
+++ srm/shell/src/main/java/org/opendaylight/genius/srm/shell/RecoverCommand.java
@@ -8,18 +8,26 @@
 
 package org.opendaylight.genius.srm.shell;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import org.apache.karaf.shell.commands.Argument;
 import org.apache.karaf.shell.commands.Command;
 import org.apache.karaf.shell.console.OsgiCommandSupport;
 import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.srm.rpcs.rev170711.RecoverInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.srm.rpcs.rev170711.RecoverInputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.srm.rpcs.rev170711.RecoverOutput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.srm.rpcs.rev170711.SrmRpcsService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.srm.types.rev170711.EntityNameBase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.srm.types.rev170711.EntityTypeBase;
+import org.opendaylight.yangtools.yang.common.RpcResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Command(scope = "srm", name = "recover", description = "Recover service or instance")
 public class RecoverCommand extends OsgiCommandSupport {
 
-    final Logger logger = LoggerFactory.getLogger(RecoverCommand.class);
+    final Logger LOG = LoggerFactory.getLogger(RecoverCommand.class);
 
     private RpcProviderRegistry rpcProviderRegistry;
     private SrmRpcsService srmRpcService;
@@ -28,13 +36,13 @@ public class RecoverCommand extends OsgiCommandSupport {
         this.rpcProviderRegistry = rpcProviderRegistry;
     }
 
-    @Argument(index = 0, name = "type", description = "EntityType, required", required = false, multiValued = false)
+    @Argument(index= 0, name= "type", description= "EntityType, required", required= false, multiValued= false)
     String type;
 
-    @Argument(index = 1, name = "name", description = "EntityName, required", required = false, multiValued = false)
+    @Argument(index= 1, name= "name", description = "EntityName, required", required = false, multiValued = false)
     String name;
 
-    @Argument(index = 2, name = "id", description = "EntityId, optional", required = false, multiValued = false)
+    @Argument(index= 2, name = "id", description = "EntityId, optional", required = false, multiValued = false)
     String id;
 
     @Override
@@ -43,7 +51,20 @@ public class RecoverCommand extends OsgiCommandSupport {
         if (rpcProviderRegistry != null) {
             srmRpcService = rpcProviderRegistry.getRpcService(SrmRpcsService.class);
             if (srmRpcService != null) {
-                session.getConsole().println(getHelp());
+                try {
+                    RecoverInput input = getInput();
+                    if (input == null) {
+                        // We've already shown the relevant error msg
+                        return null;
+                    }
+                    Future<RpcResult<RecoverOutput>> result = srmRpcService.recover(input);
+                    RpcResult<RecoverOutput> recoverResult = result.get();
+                    printResult(recoverResult);
+                } catch (InterruptedException | ExecutionException e) {
+                    LOG.error("Recovery interrupted", e);
+                } catch (NullPointerException e) {
+                    LOG.error("Unexpected error", e);
+                }
             } else {
                 session.getConsole().println("SrmRPCService not initialized");
             }
@@ -53,9 +74,48 @@ public class RecoverCommand extends OsgiCommandSupport {
         return null;
     }
 
+    private void printResult(RpcResult<RecoverOutput> recoverResult) {
+        StringBuilder strResult = new StringBuilder("");
+        if(recoverResult.isSuccessful()) {
+            strResult.append("RPC call to recover was successful");
+            LOG.trace("RPC Result: ", recoverResult.getResult());
+        } else {
+            strResult.append("RPC Call to recover failed.\n")
+                .append("ErrorCode: ").append(recoverResult.getResult().getResponse().getSimpleName())
+                .append("ErrorMsg: ").append(recoverResult.getResult().getMessage());
+            LOG.trace("RPC Result: ", recoverResult.getResult());
+        }
+        session.getConsole().println(strResult.toString());
+    }
+
+    private RecoverInput getInput() {
+        if (type == null || name == null) {
+            session.getConsole().println(getHelp());
+            return null;
+        }
+        Class<? extends EntityTypeBase> entityType = SrmCliUtils.getEntityType(type);
+        if (entityType == null) {
+            session.getConsole().println(SrmCliUtils.getTypeHelp());
+            return null;
+        }
+        Class<? extends EntityNameBase> entityName = SrmCliUtils.getEntityName(entityType, name);
+        if (entityName == null) {
+            session.getConsole().println(SrmCliUtils.getNameHelp(entityType));
+            return null;
+        }
+        RecoverInputBuilder inputBuilder = new RecoverInputBuilder();
+        inputBuilder.setEntityType(entityType);
+        inputBuilder.setEntityName(entityName);
+        if (id != null) {
+            inputBuilder.setEntityId(id);
+        }
+        return inputBuilder.build();
+    }
+
+
     private String getHelp() {
         StringBuilder help = new StringBuilder("Usage:");
-        help.append("srm:recover <type> <name> [ <id> ]\n");
+        help.append("exec recover <type> <name> [ <id> ]\n");
         return help.toString();
     }
 
