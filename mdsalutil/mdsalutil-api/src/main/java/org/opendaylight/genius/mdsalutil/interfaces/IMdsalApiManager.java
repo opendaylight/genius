a31dafef401596b1b85557e007ee9e6b7cd1fa0f
Stephen Kitt <skitt@redhat.com>
2017-09-06 15:41:18 +0200

    Clean up MDSALManager.removeGroup()
    Removing a group only needs a DPN and group identifier; deprecate the
    other forms, add a simple variant of removeGroup() and remove
    duplicates.
    
    This patch also moves the test package so that we avoid needing to
    make methods public.
    
    Change-Id: I70491f96890f2d3d0c1b82d23b298fd18fc40ce7
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/interfaces/IMdsalApiManager.java
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/interfaces/IMdsalApiManager.java
@@ -115,7 +115,30 @@ public interface IMdsalApiManager {
 
     void modifyGroup(GroupEntity groupEntity);
 
+    /**
+     * Remove a group.
+     *
+     * @deprecated Use {@link #removeGroup(BigInteger, long)}
+     */
+    @Deprecated
     void removeGroup(GroupEntity groupEntity);
+
+    /**
+     * Remove a group.
+     *
+     * @param dpnId The DP id.
+     * @param groupId The group id.
+     */
+    void removeGroup(BigInteger dpnId, long groupId);
+
+    /**
+     * Remove a group, using an existing transaction.
+     *
+     * @param dpnId The DP id.
+     * @param groupId The group id.
+     * @param tx The transaction.
+     */
+    void removeGroup(BigInteger dpnId, long groupId, WriteTransaction tx);
 
     /**
      * Remove a Group using batched transaction
@@ -125,7 +148,10 @@ public interface IMdsalApiManager {
      *            group being removed
      * @param tx
      *            batched transaction
+     *
+     * @deprecated Use {@link #removeGroup(BigInteger, long, WriteTransaction)}
      */
+    @Deprecated
     void removeGroupToTx(GroupEntity groupEntity, WriteTransaction tx);
 
     /**
@@ -138,7 +164,10 @@ public interface IMdsalApiManager {
      *            group being removed
      * @param tx
      *            batched transaction
+     *
+     * @deprecated Use {@link #removeGroup(BigInteger, long, WriteTransaction)}
      */
+    @Deprecated
     void removeGroupToTx(BigInteger dpId, Group group, WriteTransaction tx);
 
     /**
