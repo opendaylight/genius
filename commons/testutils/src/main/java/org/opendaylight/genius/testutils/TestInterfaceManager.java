78ae75344d2b3017234d06dba0dcb7459983e681
Sam Hague <shague@redhat.com>
2017-09-11 19:35:43 -0400

    bug 7380: Add getIfaceInfoFromConfigDataStore
    Add getInterfaceInfoFromConfigDataStore to
    TestInterfaceManager
    
    Change-Id: I4b28882cf79594a3cf0684682e4d32e98f529553
    Signed-off-by: Sam Hague <shague@redhat.com>
    (cherry picked from commit 194d57f444c90d8d3ebfb98c53ccf2969896e75a)

--- commons/testutils/src/main/java/org/opendaylight/genius/testutils/TestInterfaceManager.java
+++ commons/testutils/src/main/java/org/opendaylight/genius/testutils/TestInterfaceManager.java
@@ -14,6 +14,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.mockito.Mockito;
 import org.opendaylight.genius.interfacemanager.globals.InterfaceInfo;
 import org.opendaylight.genius.interfacemanager.interfaces.IInterfaceManager;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.Interface;
 
 /**
  * IInterfaceManager implementation for tests.
@@ -27,13 +28,19 @@ public abstract class TestInterfaceManager implements IInterfaceManager {
     public static TestInterfaceManager newInstance() {
         TestInterfaceManager testInterfaceManager = Mockito.mock(TestInterfaceManager.class, realOrException());
         testInterfaceManager.interfaceInfos = new ConcurrentHashMap<>();
+        testInterfaceManager.interfaces = new ConcurrentHashMap<>();
         return testInterfaceManager;
     }
 
     private Map<String, InterfaceInfo> interfaceInfos;
+    private Map<String, Interface> interfaces;
 
     public void addInterfaceInfo(InterfaceInfo interfaceInfo) {
         interfaceInfos.put(interfaceInfo.getInterfaceName(), interfaceInfo);
+    }
+
+    public void addInterface(Interface iface) {
+        interfaces.put(iface.getName(), iface);
     }
 
     @Override
@@ -44,5 +51,27 @@ public abstract class TestInterfaceManager implements IInterfaceManager {
                     "must addInterfaceInfo() to TestInterfaceManager before getInterfaceInfo: " + interfaceName);
         }
         return interfaceInfo;
+    }
+
+    @Override
+    public InterfaceInfo getInterfaceInfoFromOperationalDataStore(String interfaceName) {
+        return getInterfaceInfo(interfaceName);
+    }
+
+    @Override
+    public InterfaceInfo getInterfaceInfoFromOperationalDSCache(String interfaceName) {
+        return getInterfaceInfo(interfaceName);
+    }
+
+    @Override
+    public Interface getInterfaceInfoFromConfigDataStore(String interfaceName) {
+        Interface iface = interfaces.get(interfaceName);
+        if (iface == null) {
+            throw new IllegalStateException(
+                    "must addInterface() to TestInterfaceManager before getInterfaceInfoFromConfigDataStore: "
+                    + interfaceName);
+        }
+        return iface;
+
     }
 }
