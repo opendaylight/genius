47a1a7d591b11eb17b913aa30ef298ef19b7074c
David Suarez <david.suarez.fuentes@gmail.com>
2017-09-19 23:13:07 +0200

    Fix problems detected by the FindBugs plugin
    - FindBugs enforcement.
    
    Change-Id: Ibfb739a1e74c64a85bafc49b0b1d0d61e5005b04
    Signed-off-by: David Suarez <david.suarez.fuentes@gmail.com>

--- arputil/arputil-impl/src/main/java/org/opendaylight/genius/arputil/internal/ArpUtilImpl.java
+++ arputil/arputil-impl/src/main/java/org/opendaylight/genius/arputil/internal/ArpUtilImpl.java
@@ -11,12 +11,12 @@ package org.opendaylight.genius.arputil.internal;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import com.google.common.base.Optional;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.JdkFutureAdapters;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.SettableFuture;
+import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -37,7 +37,6 @@ import org.opendaylight.controller.liblldp.PacketException;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.NotificationPublishService;
 import org.opendaylight.controller.md.sal.binding.api.NotificationService;
-import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.genius.arputil.api.ArpConstants;
 import org.opendaylight.genius.mdsalutil.MDSALUtil;
 import org.opendaylight.genius.mdsalutil.MetaDataUtil;
@@ -46,10 +45,6 @@ import org.opendaylight.genius.mdsalutil.packet.ARP;
 import org.opendaylight.genius.mdsalutil.packet.Ethernet;
 import org.opendaylight.infrautils.inject.AbstractLifecycle;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.Interfaces;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.InterfacesState;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.Interface;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.InterfaceKey;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.PhysAddress;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.Action;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.arputil.rev160406.ArpRequestReceivedBuilder;
@@ -71,7 +66,6 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rpc
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rpcs.rev160406.GetPortFromInterfaceInputBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rpcs.rev160406.GetPortFromInterfaceOutput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rpcs.rev160406.OdlInterfaceRpcService;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeConnectorId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeConnectorRef;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeRef;
@@ -89,7 +83,6 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.Tr
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.TransmitPacketInputBuilder;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-import org.opendaylight.yangtools.yang.binding.InstanceIdentifier.InstanceIdentifierBuilder;
 import org.opendaylight.yangtools.yang.common.RpcError.ErrorType;
 import org.opendaylight.yangtools.yang.common.RpcResult;
 import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
@@ -212,8 +205,6 @@ public class ArpUtilImpl extends AbstractLifecycle implements OdlArputilService,
                 interfaceName = interfaceAddress.getInterface();
                 srcIpBytes = getIpAddressBytes(interfaceAddress.getIpAddress());
 
-                NodeConnectorId id = getNodeConnectorFromInterfaceName(interfaceName);
-
                 GetPortFromInterfaceOutput portResult = getPortFromInterface(interfaceName);
                 checkNotNull(portResult);
                 dpnId = portResult.getDpid();
@@ -260,7 +251,6 @@ public class ArpUtilImpl extends AbstractLifecycle implements OdlArputilService,
     }
 
     public Future<RpcResult<Void>> sendPacketOut(BigInteger dpnId, byte[] payload, NodeConnectorRef ref) {
-
         NodeConnectorRef nodeConnectorRef = MDSALUtil.getNodeConnRef(dpnId, "0xfffffffd");
         return packetProcessingService.transmitPacket(new TransmitPacketInputBuilder().setPayload(payload)
                 .setNode(new NodeRef(InstanceIdentifier.builder(Nodes.class)
@@ -268,9 +258,8 @@ public class ArpUtilImpl extends AbstractLifecycle implements OdlArputilService,
                 .setIngress(nodeConnectorRef).setEgress(ref).build());
     }
 
-    public Future<RpcResult<Void>> sendPacketOutWithActions(BigInteger dpnId, byte[] payload, NodeConnectorRef ref,
-            List<Action> actions) {
-
+    private Future<RpcResult<Void>> sendPacketOutWithActions(BigInteger dpnId, byte[] payload, NodeConnectorRef ref,
+                                                             List<Action> actions) {
         NodeConnectorRef nodeConnectorRef = MDSALUtil.getNodeConnRef(dpnId, "0xfffffffd");
         TransmitPacketInput transmitPacketInput = new TransmitPacketInputBuilder().setPayload(payload)
                 .setNode(new NodeRef(InstanceIdentifier.builder(Nodes.class)
@@ -329,7 +318,7 @@ public class ArpUtilImpl extends AbstractLifecycle implements OdlArputilService,
             byte[] srcIpBytes = getIpAddressBytes(input.getSrcIpaddress());
             byte[] dstIpBytes = getIpAddressBytes(input.getDstIpaddress());
             if (input.getSrcMacaddress() == null) {
-                srcMac = portResult.getPhyAddress().getBytes();
+                srcMac = portResult.getPhyAddress().getBytes("UTF-8");
             } else {
                 String macAddr = input.getSrcMacaddress().getValue();
                 srcMac = HexEncode.bytesFromHexString(macAddr);
@@ -344,7 +333,8 @@ public class ArpUtilImpl extends AbstractLifecycle implements OdlArputilService,
                     input.getSrcIpaddress().getIpv4Address().getValue(), HexEncode.bytesToHexStringFormat(srcMac),
                     HexEncode.bytesToHexStringFormat(dstMac), input.getDstIpaddress().getIpv4Address().getValue(),
                     dpnId);
-        } catch (UnknownHostException | PacketException | InterruptedException | ExecutionException e) {
+        } catch (UnknownHostException | PacketException | InterruptedException | UnsupportedEncodingException
+                | ExecutionException e) {
             LOG.error("failed to send arp response for {}: ", input.getSrcIpaddress(), e);
             return RpcResultBuilder.<Void>failed().withError(ErrorType.APPLICATION, e.getMessage(), e).buildFuture();
         }
@@ -358,7 +348,6 @@ public class ArpUtilImpl extends AbstractLifecycle implements OdlArputilService,
         LOG.trace("Packet Received {}", packetReceived);
 
         if (pktInReason == SendToController.class) {
-
             try {
                 int tableId = packetReceived.getTableId().getValue();
 
@@ -374,11 +363,6 @@ public class ArpUtilImpl extends AbstractLifecycle implements OdlArputilService,
                 ARP arp = (ARP) pkt;
                 InetAddress srcInetAddr = InetAddress.getByAddress(arp.getSenderProtocolAddress());
                 InetAddress dstInetAddr = InetAddress.getByAddress(arp.getTargetProtocolAddress());
-                InetAddress addr = srcInetAddr;
-                // For GARP learn target IP
-                if (srcInetAddr.getHostAddress().equalsIgnoreCase(dstInetAddr.getHostAddress())) {
-                    addr = dstInetAddr;
-                }
                 byte[] srcMac = ethernet.getSourceMACAddress();
                 byte[] dstMac = ethernet.getDestinationMACAddress();
 
@@ -396,9 +380,8 @@ public class ArpUtilImpl extends AbstractLifecycle implements OdlArputilService,
                             dstInetAddr, dstMac);
                 }
                 if (macAddrs.get(srcInetAddr.getHostAddress()) != null) {
-                    threadPool.submit(new MacResponderTask(arp));
+                    threadPool.execute(new MacResponderTask(arp));
                 }
-
             } catch (PacketException | UnknownHostException | InterruptedException | ExecutionException e) {
                 LOG.trace("Failed to decode packet", e);
             }
@@ -436,7 +419,7 @@ public class ArpUtilImpl extends AbstractLifecycle implements OdlArputilService,
         return interfaceFromIfIndexOutput.getInterfaceName();
     }
 
-    class MacResponderTask implements Runnable {
+    private class MacResponderTask implements Runnable {
         ARP arp;
 
         MacResponderTask(ARP arp) {
@@ -448,27 +431,22 @@ public class ArpUtilImpl extends AbstractLifecycle implements OdlArputilService,
             InetAddress srcAddr;
             GetMacOutputBuilder outputBuilder;
             String srcMac;
-            SettableFuture<RpcResult<GetMacOutput>> future = null;
-            RpcResultBuilder<GetMacOutput> resultBuilder;
             try {
                 srcAddr = InetAddress.getByAddress(arp.getSenderProtocolAddress());
                 srcMac = NWUtil.toStringMacAddress(arp.getSenderHardwareAddress());
-                future = macAddrs.remove(srcAddr.getHostAddress());
+                SettableFuture<RpcResult<GetMacOutput>> future = macAddrs.remove(srcAddr.getHostAddress());
                 if (future == null) {
                     LOG.trace("There are no pending mac requests.");
                     return;
                 }
                 outputBuilder = new GetMacOutputBuilder().setMacaddress(new PhysAddress(srcMac));
-                resultBuilder = RpcResultBuilder.success(outputBuilder.build());
+                future.set(RpcResultBuilder.success(outputBuilder.build()).build());
                 if (LOG.isTraceEnabled()) {
                     LOG.trace("sent the mac response for ip {}", srcAddr.getHostAddress());
                 }
             } catch (UnknownHostException e) {
                 LOG.error("failed to send mac response", e);
-                resultBuilder = RpcResultBuilder.<GetMacOutput>failed().withError(ErrorType.APPLICATION, e.getMessage(),
-                        e);
             }
-            future.set(resultBuilder.build());
         }
     }
 
@@ -535,37 +513,5 @@ public class ArpUtilImpl extends AbstractLifecycle implements OdlArputilService,
             builder.setMacaddress(mac);
             notificationPublishService.putNotification(builder.build());
         }
-    }
-
-    private InstanceIdentifier<Interface> buildInterfaceId(String interfaceName) {
-        InstanceIdentifierBuilder<Interface> idBuilder = InstanceIdentifier.builder(Interfaces.class)
-                .child(Interface.class, new InterfaceKey(interfaceName));
-        return idBuilder.build();
-    }
-
-    private NodeConnectorId getNodeConnectorFromInterfaceName(String interfaceName) {
-        InstanceIdentifierBuilder<org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508
-            .interfaces.state.Interface> idBuilder = InstanceIdentifier.builder(InterfacesState.class)
-            .child(org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508
-            .interfaces.state.Interface.class, new org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf
-            .interfaces.rev140508.interfaces.state.InterfaceKey(interfaceName));
-
-        InstanceIdentifier<org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf
-            .interfaces.rev140508.interfaces.state.Interface> ifStateId = idBuilder.build();
-
-        Optional<org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf
-            .interfaces.rev140508.interfaces.state.Interface> ifStateOptional = MDSALUtil
-                .read(dataBroker, LogicalDatastoreType.OPERATIONAL, ifStateId);
-
-        if (ifStateOptional.isPresent()) {
-            org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508
-                .interfaces.state.Interface ifState = ifStateOptional
-                    .get();
-            List<String> lowerLayerIf = ifState.getLowerLayerIf();
-            if (!lowerLayerIf.isEmpty()) {
-                return new NodeConnectorId(lowerLayerIf.get(0));
-            }
-        }
-        return null;
     }
 }
