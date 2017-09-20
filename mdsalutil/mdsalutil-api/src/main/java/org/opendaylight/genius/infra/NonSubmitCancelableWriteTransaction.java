d1acdebb9b6a755a43625b444c900804bc8027c5
Michael Vorburger <vorburger@redhat.com>
2017-09-20 20:05:09 +0200

    ManagedTransactionRunner utility to help close transactions
    inspired by
    https://git.opendaylight.org/gerrit/#/q/topic:transaction-helper, but:
    
      (1) instead of the cancel-if-lambda-didn't-submit design, forbid
    lambda from doing submit() or cancel(), and instead let new transaction
    utility do that.  This is better because it already opens the door for
    non-single transactions in the future (see upcoming next chained change)
    
      (2) allow lambda to throw checked exceptions (see CheckedConsumer),
    which will, obviously, lead to the Transaction to be canceled. This is
    important as it helps avoid errors where using code forget this.
    
    TODO: Make all places which topic:transaction-helper converted to using
    TransactionHelper use this change's ManagedNewTransactionRunner instead.
    
    Change-Id: I613ab7720e226c09d46c90329f0bc767f4642530
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/infra/NonSubmitCancelableWriteTransaction.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.infra;
+
+import com.google.common.util.concurrent.CheckedFuture;
+import com.google.common.util.concurrent.ListenableFuture;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.controller.md.sal.common.api.TransactionStatus;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+
+/**
+ * WriteTransaction which cannot be {@link WriteTransaction#cancel()},
+ * {@link WriteTransaction#commit()} or {@link WriteTransaction#submit()}.
+ *
+ * @author Michael Vorburger.ch
+ */
+@SuppressWarnings("deprecation")
+// intentionally package local, for now
+class NonSubmitCancelableWriteTransaction extends DelegatingWriteTransaction {
+
+    // TODO if we could finally reach consensus on https://git.opendaylight.org/gerrit/#/c/46684/, then this could probably be removed?
+    // see also https://git.opendaylight.org/gerrit/#/c/46335/ for the earlier take on it - controller, mdsal, controller, mdsal... ;-)
+
+    NonSubmitCancelableWriteTransaction(WriteTransaction delegate) {
+        super(delegate);
+    }
+
+    @Override
+    public boolean cancel() {
+        throw new UnsupportedOperationException("cancel() cannot be used inside a Managed[New]TransactionRunner");
+    }
+
+    @Override
+    public ListenableFuture<RpcResult<TransactionStatus>> commit() {
+        throw new UnsupportedOperationException("commit() cannot be used inside a Managed[New]TransactionRunner");
+    }
+
+    @Override
+    public CheckedFuture<Void, TransactionCommitFailedException> submit() {
+        throw new UnsupportedOperationException("submit() cannot be used inside a Managed[New]TransactionRunner");
+    }
+}
