a88fdecf71a0877c8c371194c80583fb7aaa24ed
David Suarez <david.suarez.fuentes@gmail.com>
2017-09-23 21:47:03 +0200

    Improve the Genius Listeners Framework
    Improve the Listeners Framework created in [1] to ease registration
    and un-registration processes for entities.
    
    [1] https://git.opendaylight.org/gerrit/#/c/59177/
    
    Change-Id: I497903950026dd30037ca19fb0928ff455b07682
    Signed-off-by: David Suarez <david.suarez.fuentes@gmail.com>

--- mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/datastoreutils/listeners/AbstractAsyncDataTreeChangeListener.java
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/datastoreutils/listeners/AbstractAsyncDataTreeChangeListener.java
@@ -9,32 +9,44 @@ package org.opendaylight.genius.datastoreutils.listeners;
 
 import java.util.Collection;
 import java.util.concurrent.ExecutorService;
+import javax.annotation.Nonnull;
 import javax.inject.Inject;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.DataTreeChangeListener;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
 import org.opendaylight.controller.md.sal.binding.api.DataTreeModification;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
 /**
- * Abstract class providing some common functionality to specific listeners.
- * This listener launches the received notifications in a different thread by
- * using the queuing functionality of the {@link ExecutorService}.
+ * Abstract class providing some common functionality to specific listeners. This listener launches the received
+ * notifications in a different thread by using the an {@link ExecutorService}.
  *
+ * @param <T> type of the data object the listener is registered to.
  * @author David Suárez (david.suarez.fuentes@gmail.com)
- *
- * @param <T>
- *            type of the data object the listener is registered to.
  */
-public abstract class AbstractAsyncDataTreeChangeListener<T extends DataObject>
-        implements DataTreeChangeListenerActions<T>, DataTreeChangeListener<T> {
+public abstract class AbstractAsyncDataTreeChangeListener<T extends DataObject> extends
+        AbstractDataTreeChangeListener<T> implements DataTreeChangeListenerActions<T>, DataTreeChangeListener<T> {
     private final ExecutorService executorService;
 
     @Inject
-    public AbstractAsyncDataTreeChangeListener(ExecutorService executorService) {
+    public AbstractAsyncDataTreeChangeListener(DataBroker dataBroker, DataTreeIdentifier<T> dataTreeIdentifier,
+                                               ExecutorService executorService) {
+        super(dataBroker, dataTreeIdentifier);
+        this.executorService = executorService;
+    }
+
+    @Inject
+    public AbstractAsyncDataTreeChangeListener(DataBroker dataBroker, LogicalDatastoreType datastoreType,
+                                               InstanceIdentifier<T> instanceIdentifier,
+                                               ExecutorService executorService) {
+        super(dataBroker, datastoreType, instanceIdentifier);
         this.executorService = executorService;
     }
 
     @Override
-    public final void onDataTreeChanged(Collection<DataTreeModification<T>> collection) {
-        executorService.submit(() -> DataTreeChangeListenerActions.super.onDataTreeChanged(collection));
+    public final void onDataTreeChanged(@Nonnull Collection<DataTreeModification<T>> collection) {
+        executorService.execute(() -> DataTreeChangeListenerActions.super.onDataTreeChanged(collection));
     }
 }
---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/datastoreutils/listeners/AbstractDataTreeChangeListener.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2017 Ericsson S.A. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.datastoreutils.listeners;
+
+import javax.inject.Inject;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeChangeListener;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+abstract class AbstractDataTreeChangeListener<T extends DataObject> implements DataTreeChangeListener<T>,
+        AutoCloseable {
+    private final DataBroker dataBroker;
+    private final ListenerRegistration<AbstractDataTreeChangeListener<T>> dataChangeListenerRegistration;
+
+    @Inject
+    AbstractDataTreeChangeListener(DataBroker dataBroker, DataTreeIdentifier<T> dataTreeIdentifier) {
+        this.dataBroker = dataBroker;
+        dataChangeListenerRegistration = dataBroker.registerDataTreeChangeListener(dataTreeIdentifier, this);
+    }
+
+    @Inject
+    AbstractDataTreeChangeListener(DataBroker dataBroker, LogicalDatastoreType datastoreType,
+                                   InstanceIdentifier<T> instanceIdentifier) {
+        this(dataBroker, new DataTreeIdentifier<>(datastoreType, instanceIdentifier));
+    }
+
+    public DataBroker getDataBroker() {
+        return dataBroker;
+    }
+
+    @Override
+    public void close() {
+        dataChangeListenerRegistration.close();
+    }
+}
+
