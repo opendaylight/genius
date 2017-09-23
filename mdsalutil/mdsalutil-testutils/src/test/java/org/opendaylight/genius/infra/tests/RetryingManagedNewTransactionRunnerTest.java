d6e84f487654be5dd97d928d86b3d86217c7f997
Michael Vorburger <vorburger@redhat.com>
2017-09-23 02:50:27 +0200

    RetryingManagedNewTransactionRunner for OptimisticLockFailedException
    TODO in a future follow-up changes: Start fixing (many)
    OptimisticLockFailedException that appear in logs by using this... ;)
    
    Change-Id: I8fee2b50b562dc2c0f416d59082ad4bded0cdf58
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ mdsalutil/mdsalutil-testutils/src/test/java/org/opendaylight/genius/infra/tests/RetryingManagedNewTransactionRunnerTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.infra.tests;
+
+import static org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType.OPERATIONAL;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.OptimisticLockFailedException;
+import org.opendaylight.genius.infra.ManagedNewTransactionRunner;
+import org.opendaylight.genius.infra.RetryingManagedNewTransactionRunner;
+
+/**
+ * Test for {@link RetryingManagedNewTransactionRunner}.
+ * Note that this test (intentionally) extends the {@link ManagedNewTransactionRunnerImplTest}.
+ *
+ * @author Michael Vorburger.ch
+ */
+public class RetryingManagedNewTransactionRunnerTest extends ManagedNewTransactionRunnerImplTest {
+
+    @Override
+    protected ManagedNewTransactionRunner createManagedNewTransactionRunnerToTest(DataBroker dataBroker) {
+        return new RetryingManagedNewTransactionRunner(super.createManagedNewTransactionRunnerToTest(dataBroker));
+    }
+
+    @Override
+    public void testCallWithNewWriteOnlyTransactionOptimisticLockFailedException() throws Exception {
+        // contrary to the super() test implementation for (just) ManagedNewTransactionRunnerImpl, in the parent class
+        // here we expect the x2 OptimisticLockFailedException to be retried, and then eventually succeed:
+        testableDataBroker.failSubmits(2, new OptimisticLockFailedException("bada boum bam!"));
+        managedNewTransactionRunner.callWithNewWriteOnlyTransactionAndSubmit(writeTx -> {
+            writeTx.put(LogicalDatastoreType.OPERATIONAL, TEST_PATH, newTestDataObject());
+        }).get();
+        singleTransactionDataBroker.syncRead(OPERATIONAL, TEST_PATH);
+    }
+}
