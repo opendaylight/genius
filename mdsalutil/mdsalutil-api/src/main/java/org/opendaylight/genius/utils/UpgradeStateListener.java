d95a9bdb2840f874c398ea4c1e4e239e88e1e09a
Josh <jhershbe@redhat.com>
2017-11-08 10:17:25 +0200

    Upgrading flag
    This commit introduces a small utility that tracks whether or not
    an upgrade is in progress. The purpose of this state is so that
    code paths required during upgrade need not be entered during
    regular application flow. The upgrade in progress state is toggled
    via REST (or config xml). I tested it like this:
    
    > curl -v -u admin:admin -X PUT -d @put-data -H \
    "Content-Type: application/json" \
    http://127.0.0.1:8181/restconf/config/genius-mdsalutil:config
    
    ...where the contents of the put-data file are:
    {
    "config": {
            "upgradeInProgress": false
             }
    }
    
    The REST call to retrieve the status is:
    
    > curl -u admin:admin http://127.0.0.1:8181/restconf/config/genius-mdsalutil:config
    
    Change-Id: I96d4255645b8650ee97154ff58c1afc52d1ac9f0
    Signed-off-by: Josh <jhershbe@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/utils/UpgradeStateListener.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.genius.utils;
+
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.datastoreutils.listeners.AbstractClusteredSyncDataTreeChangeListener;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.mdsalutil.rev170830.Config;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class UpgradeStateListener extends AbstractClusteredSyncDataTreeChangeListener<Config> implements UpgradeState {
+    private static final Logger LOG = LoggerFactory.getLogger(UpgradeStateListener.class);
+
+    private AtomicBoolean isUpgradeInProgress = new AtomicBoolean(false);
+
+    @Inject
+    public UpgradeStateListener(final DataBroker dataBroker) {
+        super(dataBroker, new DataTreeIdentifier<>(
+                LogicalDatastoreType.CONFIGURATION, InstanceIdentifier.create(Config.class)));
+        LOG.info("UpgradeStateListener(): isUpgradeInProgress = {}", this.isUpgradeInProgress.get());
+        register();
+    }
+
+    public InstanceIdentifier<Config> getWildCardPath() {
+        return InstanceIdentifier.create(Config.class);
+    }
+
+    public UpgradeStateListener getDataTreeChangeListener() {
+        return this;
+    }
+
+    @Override
+    public boolean isUpgradeInProgress() {
+        return isUpgradeInProgress.get();
+    }
+
+    @Override
+    public void add(@Nonnull Config newDataObject) {
+        isUpgradeInProgress.set(newDataObject.isUpgradeInProgress());
+        LOG.info("UpgradeStateListener.add: isUpgradeInProgress = {}", newDataObject.isUpgradeInProgress());
+    }
+
+    @Override
+    public void remove(@Nonnull Config removedDataObject) {
+        isUpgradeInProgress.set(false);
+        LOG.info("UpgradeStateListener.update: isUpgradeInProgress = {}", false);
+    }
+
+    @Override
+    public void update(@Nonnull Config originalDataObject, Config updatedDataObject) {
+        isUpgradeInProgress.set(updatedDataObject.isUpgradeInProgress());
+        LOG.info("UpgradeStateListener.remove: isUpgradeInProgress = {}", updatedDataObject.isUpgradeInProgress());
+    }
+}
