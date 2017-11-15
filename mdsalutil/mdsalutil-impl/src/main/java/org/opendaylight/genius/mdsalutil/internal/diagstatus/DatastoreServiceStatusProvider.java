b98712ad03dfff9871056d139cb85576c234aed7
Nidhi Adhvaryu <nidhi.adhvaryu@ericsson.com>
2017-11-15 18:26:47 +0530

    Diagstatus integration for datastore service
    Change-Id: I915e13debbd3c423f978871232901cd0c7b2fa6b
    Signed-off-by: Nidhi Adhvaryu <nidhi.adhvaryu@ericsson.com>

---
+++ mdsalutil/mdsalutil-impl/src/main/java/org/opendaylight/genius/mdsalutil/internal/diagstatus/DatastoreServiceStatusProvider.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.internal.diagstatus;
+
+import javax.annotation.PreDestroy;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.opendaylight.infrautils.diagstatus.DiagStatusService;
+import org.opendaylight.infrautils.diagstatus.MBeanUtils;
+import org.opendaylight.infrautils.diagstatus.ServiceDescriptor;
+import org.opendaylight.infrautils.diagstatus.ServiceState;
+import org.opendaylight.infrautils.diagstatus.ServiceStatusProvider;
+import org.ops4j.pax.cdi.api.OsgiService;
+import org.ops4j.pax.cdi.api.OsgiServiceProvider;
+
+@Singleton
+@OsgiServiceProvider(classes = ServiceStatusProvider.class)
+public class DatastoreServiceStatusProvider implements ServiceStatusProvider {
+
+    private static final String DATASTORE_SERVICE_NAME = "DATASTORE";
+    private volatile ServiceDescriptor serviceDescriptor;
+    private DiagStatusService diagStatusService;
+
+    @Inject
+    public DatastoreServiceStatusProvider(@OsgiService DiagStatusService diagStatusService) {
+        this.diagStatusService = diagStatusService;
+        diagStatusService.register(DATASTORE_SERVICE_NAME);
+        serviceDescriptor = new ServiceDescriptor(DATASTORE_SERVICE_NAME, ServiceState.OPERATIONAL,
+                "Service started");
+        diagStatusService.report(serviceDescriptor);
+    }
+
+    @PreDestroy
+    public void close() {
+        serviceDescriptor = new ServiceDescriptor(DATASTORE_SERVICE_NAME, ServiceState.UNREGISTERED,
+                "Service Closed");
+        diagStatusService.report(serviceDescriptor);
+    }
+
+    @Override
+    public ServiceDescriptor getServiceDescriptor() {
+        ServiceState dataStoreServiceState = ServiceState.ERROR;
+        String statusDesc;
+        boolean operSyncStatusValue = (boolean) MBeanUtils.readMBeanAttribute("org.opendaylight.controller:type="
+                        + "DistributedOperationalDatastore,Category=ShardManager,name=shard-manager-operational",
+                "SyncStatus");
+        boolean configSyncStatusValue = (boolean) MBeanUtils.readMBeanAttribute("org.opendaylight.controller:type="
+                        + "DistributedConfigDatastore,Category=ShardManager,name=shard-manager-config",
+                "SyncStatus");
+        if (operSyncStatusValue != false && configSyncStatusValue != false) {
+            if (operSyncStatusValue &&  configSyncStatusValue) {
+                dataStoreServiceState =  ServiceState.OPERATIONAL;
+                statusDesc = dataStoreServiceState.name();
+            } else {
+                statusDesc = "datastore out of sync";
+            }
+        } else {
+            statusDesc = "Unable to obtain the datastore status";
+        }
+        return new ServiceDescriptor(DATASTORE_SERVICE_NAME, dataStoreServiceState, statusDesc);
+    }
+}
