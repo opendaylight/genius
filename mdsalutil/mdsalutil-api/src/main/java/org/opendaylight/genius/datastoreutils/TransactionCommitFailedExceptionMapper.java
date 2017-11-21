d7337ed95e678e650e575208bfa9313671fb9eb2
Michael Vorburger <vorburger@redhat.com>
2017-11-21 19:34:25 +0100

    SingleTransactionDataBroker with RetryingManagedNewTransactionRunner
    built-in retries transparently handle OptimisticLockFailedException
    
    This probably will squash a number of bugs from
    https://jira.opendaylight.org/browse/NETVIRT-994?jql=text%20~%20"OptimisticLockFailedException"%20and%20status%20%3D%20Open
    in one go.
    
    Change-Id: I7a3762d9563935f66975f0e6e000609f39b05733
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/datastoreutils/TransactionCommitFailedExceptionMapper.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.datastoreutils;
+
+import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
+import org.opendaylight.yangtools.util.concurrent.ExceptionMapper;
+
+/**
+ * An {@link ExceptionMapper} for {@link TransactionCommitFailedException}.
+ *
+ * <p>Like org.opendaylight.controller.md.sal.dom.broker.impl.TransactionCommitFailedExceptionMapper,
+ * but that is in a bundle internal private implementation package, so not accessible for us here.
+ *
+ * <p>And org.opendaylight.mdsal.dom.broker.TransactionCommitFailedExceptionMapper
+ * is for org.opendaylight.mdsal.common.api.TransactionCommitFailedException instead of for
+ * org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException, of course.
+ *
+ * @author Michael Vorburger.ch
+ */
+// intentionally only package-local, for now
+class TransactionCommitFailedExceptionMapper extends ExceptionMapper<TransactionCommitFailedException> {
+
+    static final TransactionCommitFailedExceptionMapper SUBMIT_MAPPER = create("submit");
+
+    static TransactionCommitFailedExceptionMapper create(String opName) {
+        return new TransactionCommitFailedExceptionMapper(opName);
+    }
+
+    TransactionCommitFailedExceptionMapper(String opName) {
+        super(opName, TransactionCommitFailedException.class);
+    }
+
+    @Override
+    protected TransactionCommitFailedException newWithCause(String message, Throwable cause) {
+        return new TransactionCommitFailedException(message, cause);
+    }
+
+}
