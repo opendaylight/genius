9a743445d3bc08f8434a652bad8b39b3d9e0cfa1
Michael Vorburger <vorburger@redhat.com>
2017-11-24 15:58:53 +0100

    DataInstanceChangeListenerRegisterer
    Phase 0 is just the API.
    
    Phase 1 will be first simple impl of this interface service API.
    
    Phase 2 will be an optimized implementation.
    
    Change-Id: I37d0b787874b00646d9e7ea93f3a91664a69db4a
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/datastoreutils/listeners/DataTreeEventCallbackRegistrar.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.datastoreutils.listeners;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+/**
+ * Service to register callbacks for data tree changes for a fixed instance.
+ *
+ * @author Michael Vorburger.ch, based on an idea and with design feedback from Josh
+ */
+public interface DataTreeEventCallbackRegistrar {
+
+    enum UnregisterOrCallAgain { unregister, call_again }
+
+    <T extends DataObject> void onAdd(LogicalDatastoreType type, InstanceIdentifier<T> dataTreeIdentifier,
+            Function<T, UnregisterOrCallAgain> callback);
+
+    <T extends DataObject> void onUpdate(LogicalDatastoreType type, InstanceIdentifier<T> dataTreeIdentifier,
+            BiFunction<T, T, UnregisterOrCallAgain> callback);
+
+    <T extends DataObject> void onRemove(LogicalDatastoreType type, InstanceIdentifier<T> dataTreeIdentifier,
+            Function<T, UnregisterOrCallAgain> callback);
+
+}
