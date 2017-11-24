daadea35f9e472e929e54fa83d4161a26b7cfb8f
Michael Vorburger <vorburger@redhat.com>
2017-11-24 15:58:53 +0100

    DataTreeEventCallbackRegistrar for data tree instance call backs
    Phase 1 is this first interface service API & simple implementation.
    
    Phase 2 will be an optimized implementation (post usage).
    
    Change-Id: I37d0b787874b00646d9e7ea93f3a91664a69db4a
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/datastoreutils/listeners/DataTreeEventCallbackRegistrar.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.datastoreutils.listeners;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+/**
+ * Service to register callbacks for data tree changes for a fixed instance.
+ *
+ * @author Josh original idea and design feedback
+ * @author Michael Vorburger.ch API design and implementation
+ * @author Tom Pantelis feedback on concurrency issue in implementation
+ */
+public interface DataTreeEventCallbackRegistrar {
+
+    enum UnregisterOrCallAgain {
+        /**
+         * When the callback function returns this, then it is unregistered and will never be invoked anymore.
+         */
+        UNREGISTER,
+
+        /**
+         * When the callback function returns this, then it will be called again for add, update OR remove
+         * on the given store &amp; path.
+         */
+        CALL_AGAIN
+    }
+
+    <T extends DataObject> void onAdd(LogicalDatastoreType store, InstanceIdentifier<T> path,
+            Function<T, UnregisterOrCallAgain> callback);
+
+    <T extends DataObject> void onUpdate(LogicalDatastoreType store, InstanceIdentifier<T> path,
+            BiFunction<T, T, UnregisterOrCallAgain> callback);
+
+    <T extends DataObject> void onRemove(LogicalDatastoreType store, InstanceIdentifier<T> path,
+            Function<T, UnregisterOrCallAgain> callback);
+
+}
