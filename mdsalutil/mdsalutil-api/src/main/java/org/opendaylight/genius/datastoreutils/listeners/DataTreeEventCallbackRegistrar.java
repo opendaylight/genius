cab9ba7dc84d873ad3cef9dea35df4d9902765c4
Michael Vorburger <vorburger@redhat.com>
2017-11-24 15:58:53 +0100

    DataTreeEventCallbackRegistrar for data tree instance call backs
    Phase 1 is this first interface service API & simple implementation.
    
    Phase 2 will be an optimized implementation (post usage).
    
    Change-Id: I37d0b787874b00646d9e7ea93f3a91664a69db4a
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/datastoreutils/listeners/DataTreeEventCallbackRegistrar.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.datastoreutils.listeners;
+
+import static org.opendaylight.genius.datastoreutils.listeners.DataTreeEventCallbackRegistrar.UnregisterOrCallAgain.UNREGISTER;
+
+import com.google.common.annotations.Beta;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+/**
+ * Service to register callbacks for data tree changes for a fixed instance.
+ *
+ * @author Michael Vorburger.ch, based on an idea and with design feedback from Josh
+ */
+@Beta // we may still change this API
+public interface DataTreeEventCallbackRegistrar {
+
+    enum UnregisterOrCallAgain {
+        /**
+         * When the callback function returns this, then it is unregistered and will never be invoked anymore.
+         */
+        UNREGISTER,
+
+        /**
+         * When the callback function returns this, then it will be called again for add, update OR remove
+         * on the given store &amp; path.
+         */
+        CALL_AGAIN
+    }
+
+    <T extends DataObject> void onAdd(LogicalDatastoreType store, InstanceIdentifier<T> path,
+            Function<T, UnregisterOrCallAgain> callback);
+
+    default <T extends DataObject> void onAdd(LogicalDatastoreType store, InstanceIdentifier<T> path,
+            Consumer<T> callback) {
+        onAdd(store, path, (Function<T, UnregisterOrCallAgain>) t -> {
+            callback.accept(t);
+            return UNREGISTER;
+        });
+    }
+
+    <T extends DataObject> void onUpdate(LogicalDatastoreType store, InstanceIdentifier<T> path,
+            BiFunction<T, T, UnregisterOrCallAgain> callback);
+
+    default <T extends DataObject> void onUpdate(LogicalDatastoreType store, InstanceIdentifier<T> path,
+            BiConsumer<T, T> callback) {
+        onUpdate(store, path, (BiFunction<T, T, UnregisterOrCallAgain>) (t1, t2) -> {
+            callback.accept(t1, t2);
+            return UNREGISTER;
+        });
+    }
+
+    <T extends DataObject> void onRemove(LogicalDatastoreType store, InstanceIdentifier<T> path,
+            Function<T, UnregisterOrCallAgain> callback);
+
+    default <T extends DataObject> void onRemove(LogicalDatastoreType store, InstanceIdentifier<T> path,
+            Consumer<T> callback) {
+        onRemove(store, path, (Function<T, UnregisterOrCallAgain>) t -> {
+            callback.accept(t);
+            return UNREGISTER;
+        });
+    }
+
+}
---
+++ mdsalutil/mdsalutil-impl/src/main/java/org/opendaylight/genius/datastoreutils/listeners/internal/DataTreeEventCallbackRegistrarImpl.java
@@ -0,0 +1,168 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.datastoreutils.listeners.internal;
+
+import java.util.Collection;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataObjectModification;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeChangeListener;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeModification;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.datastoreutils.listeners.DataTreeEventCallbackRegistrar;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.ops4j.pax.cdi.api.OsgiService;
+import org.ops4j.pax.cdi.api.OsgiServiceProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Implementation of DataTreeEventCallbackRegistrar.
+ *
+ * @author Michael Vorburger.ch
+ */
+@Singleton
+@OsgiServiceProvider(classes = DataTreeEventCallbackRegistrar.class)
+public class DataTreeEventCallbackRegistrarImpl implements DataTreeEventCallbackRegistrar {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DataTreeEventCallbackRegistrarImpl.class);
+
+    // This implementation is, intentionally, kept very simple and thin.  If during usage we see
+    // that registering many listeners to the DataBroker causes any sort of real overhead, then we will
+    // change this implementation to register 1 single listener to the DataBroker, and instead do the
+    // dispatching etc. ourselves within this implementation.
+
+    private final DataBroker dataBroker;
+
+    @Inject
+    public DataTreeEventCallbackRegistrarImpl(@OsgiService DataBroker dataBroker) {
+        this.dataBroker = dataBroker;
+    }
+
+    @Override
+    public <T extends DataObject> void onUpdate(LogicalDatastoreType store, InstanceIdentifier<T> path,
+            BiFunction<T, T, UnregisterOrCallAgain> callback) {
+        on(Operation.UPDATE, store, path, callback);
+    }
+
+    @Override
+    public <T extends DataObject> void onAdd(LogicalDatastoreType store, InstanceIdentifier<T> path,
+            Function<T, UnregisterOrCallAgain> callback) {
+        on(Operation.ADD, store, path, biify(callback));
+    }
+
+    @Override
+    public <T extends DataObject> void onRemove(LogicalDatastoreType store, InstanceIdentifier<T> path,
+            Function<T, UnregisterOrCallAgain> callback) {
+        on(Operation.REMOVE, store, path, biify(callback));
+    }
+
+    private <T, U, R> BiFunction<T, T, R> biify(Function<T, R> function) {
+        return (first, second) -> function.apply(first);
+    }
+
+    @SuppressWarnings("resource") // thanks but we're good
+    private <T extends DataObject> void on(Operation op, LogicalDatastoreType store, InstanceIdentifier<T> path,
+            BiFunction<T, T, UnregisterOrCallAgain> cb) {
+        DataTreeIdentifier<T> dtid = new DataTreeIdentifier<>(store, path);
+        DataTreeEventCallbackChangeListener<T> listener = new DataTreeEventCallbackChangeListener<>(Operation.ADD, cb);
+        ListenerRegistration<DataTreeEventCallbackChangeListener<T>> reg
+            = dataBroker.registerDataTreeChangeListener(dtid, listener);
+        listener.setRegistration(reg);
+    }
+
+    private static final class DataTreeEventCallbackChangeListener<T extends DataObject>
+            implements DataTreeChangeListener<T> {
+
+        private final Operation operation;
+        private final BiFunction<T, T, UnregisterOrCallAgain> callback;
+        private ListenerRegistration<DataTreeEventCallbackChangeListener<T>> listenerRegistration;
+
+        DataTreeEventCallbackChangeListener(Operation operation, BiFunction<T, T, UnregisterOrCallAgain> callback) {
+            this.operation = operation;
+            this.callback = callback;
+        }
+
+        void setRegistration(ListenerRegistration<DataTreeEventCallbackChangeListener<T>> reg) {
+            this.listenerRegistration = reg;
+        }
+
+        @Override
+        public void onDataTreeChanged(Collection<DataTreeModification<T>> changes) {
+            // Code almost identical to org.opendaylight.genius.datastoreutils.listeners.DataTreeChangeListenerActions
+            for (final DataTreeModification<T> dataTreeModification : changes) {
+                final DataObjectModification<T> dataObjectModification = dataTreeModification.getRootNode();
+                final T dataBefore = dataObjectModification.getDataBefore();
+                final T dataAfter = dataObjectModification.getDataAfter();
+
+                UnregisterOrCallAgain unregisterOrCallAgain;
+                switch (dataObjectModification.getModificationType()) {
+                    case SUBTREE_MODIFIED:
+                        unregisterOrCallAgain = update(dataBefore, dataAfter);
+                        break; // switch
+                    case DELETE:
+                        unregisterOrCallAgain = remove(dataBefore);
+                        break; // switch
+                    case WRITE:
+                        if (dataBefore == null) {
+                            unregisterOrCallAgain = add(dataAfter);
+                        } else {
+                            unregisterOrCallAgain = update(dataBefore, dataAfter);
+                        }
+                        break; // switch
+                    default:
+                        unregisterOrCallAgain = UnregisterOrCallAgain.UNREGISTER;
+                        break; // switch
+                }
+
+                if (unregisterOrCallAgain.equals(UnregisterOrCallAgain.UNREGISTER)) {
+                    if (listenerRegistration != null) {
+                        listenerRegistration.close();
+                    } else {
+                        LOG.error("Cannot close() listenerRegistration, because UNREGISTER *before* setRegistration()");
+                    }
+                    break; // for loop
+                }
+            }
+        }
+
+        UnregisterOrCallAgain add(T newDataObject) {
+            if (operation == Operation.ADD) {
+                return callback.apply(newDataObject, null);
+            } else {
+                return UnregisterOrCallAgain.CALL_AGAIN;
+            }
+        }
+
+        UnregisterOrCallAgain remove(T removedDataObject) {
+            if (operation == Operation.REMOVE) {
+                return callback.apply(removedDataObject, null);
+            } else {
+                return UnregisterOrCallAgain.CALL_AGAIN;
+            }
+        }
+
+        UnregisterOrCallAgain update(T originalDataObject, T updatedDataObject) {
+            if (operation == Operation.UPDATE) {
+                return callback.apply(originalDataObject, updatedDataObject);
+            } else {
+                return UnregisterOrCallAgain.CALL_AGAIN;
+            }
+        }
+
+    }
+
+    private enum Operation { ADD, UPDATE, REMOVE }
+
+}
