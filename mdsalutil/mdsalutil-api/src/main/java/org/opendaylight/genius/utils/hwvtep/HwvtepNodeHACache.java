a0d2386e72c1ae14e3975265809500004c65f61e
Tom Pantelis <tompantelis@gmail.com>
2017-12-28 11:15:28 -0500

    Add HwvtepNodeHACache interface
    This is the first in a series of patches to eliminate the
    static HwvtepHACache. Added an HwvtepNodeHACache interface
    which HwvtepHACache temporarily implements until netvirt
    is migrated to use HwvtepNodeHACache in the next patch.
    The static HwvtepHACache instance is also temporarily
    advertised as the HwvtepNodeHACache service.
    
    Change-Id: I67154a0bfa21655d1158ba81b472e6501ac1ce76
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/utils/hwvtep/HwvtepNodeHACache.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2017 Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.utils.hwvtep;
+
+import java.util.Map;
+import java.util.Set;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+/**
+ * Caches hwvtep Node HA info.
+ *
+ * @author Thomas Pantelis
+ */
+public interface HwvtepNodeHACache {
+    void addChild(InstanceIdentifier<Node> parentId, InstanceIdentifier<Node> childId);
+
+    boolean isHAEnabledDevice(InstanceIdentifier<?> nodeId);
+
+    boolean isHAParentNode(InstanceIdentifier<Node> nodeId);
+
+    Set<InstanceIdentifier<Node>> getChildrenForHANode(InstanceIdentifier<Node> parentId);
+
+    Set<InstanceIdentifier<Node>> getHAParentNodes();
+
+    Set<InstanceIdentifier<Node>> getHAChildNodes();
+
+    InstanceIdentifier<Node> getParent(InstanceIdentifier<Node> childId);
+
+    void removeParent(InstanceIdentifier<Node> parentId);
+
+    void updateConnectedNodeStatus(InstanceIdentifier<Node> nodeId);
+
+    void updateDisconnectedNodeStatus(InstanceIdentifier<Node> nodeId);
+
+    Map<String, Boolean> getNodeConnectionStatuses();
+}
