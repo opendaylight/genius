18e1f919726e3d7ced37ee9d6279fa553f799a38
Ankit <ankit.kumar@altencalsoftlabs.com>
2018-01-18 13:12:46 +0530

    Migrating FCAPSApplication PM Counters to Infrautils Counters
    Change-Id: I2a4edeeb9deb72de15b8e4b75adf225843c4ddfb
    Signed-off-by: Ankit <ank123it@gmail.com>

---
+++ fcapsapplication/fcapsapplication-impl/src/main/java/org/opendaylight/genius/fcapsapp/FcapsUtil.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2016, 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.fcapsapp;
+
+/**
+ * Created by ankit.kumar on 22-01-2018.
+ */
+public final class FcapsUtil {
+
+
+    private FcapsUtil() {
+    }
+
+    /**
+     * This method provide the counter name in below pattern.
+     * @param dpnId datapath id value
+     * @return counter key string
+     */
+    public static String getCounterName(String dpnId, String counterName) {
+        String dpnName = FcapsConstants.MODULENAME
+                + FcapsConstants.ENTITY_TYPE_OFSWITCH + "<switchid=" + dpnId + ">." + counterName;
+        return dpnName;
+    }
+}
--- fcapsapplication/fcapsapplication-impl/src/main/java/org/opendaylight/genius/fcapsapp/performancecounter/NodeUpdateCounter.java
+++ fcapsapplication/fcapsapplication-impl/src/main/java/org/opendaylight/genius/fcapsapp/performancecounter/NodeUpdateCounter.java
@@ -7,11 +7,14 @@
  */
 package org.opendaylight.genius.fcapsapp.performancecounter;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import org.opendaylight.genius.fcapsapp.FcapsUtil;
+import org.opendaylight.infrautils.metrics.Counter;
+import org.opendaylight.infrautils.metrics.MetricProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,33 +25,31 @@ public class NodeUpdateCounter {
     private String nodeListEFSCountStr;
     private static HashSet<String> dpnList = new HashSet<>();
     public final PMAgent agent;
-    private final Map<String, String> countersMap = new HashMap<>();
+    private final MetricProvider metricProvider;
+    private Map<String,Counter> counterMap = new ConcurrentHashMap<>();
+    private String counterName = "switchespernode";
 
     @Inject
-    public NodeUpdateCounter(final PMAgent agent) {
+    public NodeUpdateCounter(final PMAgent agent, final MetricProvider metricProvider) {
         this.agent = agent;
+        this.metricProvider = metricProvider;
     }
 
     public void nodeAddedNotification(String node, String hostName) {
         dpnList.add(node);
-        sendNodeUpdation(dpnList.size(), hostName);
+        String counterKey = FcapsUtil.getCounterName(String.valueOf(hostName),counterName)
+                .replace("switchid","hostid");
+        counterMap.computeIfAbsent(counterKey,
+                (counter -> metricProvider.newCounter(this, counterKey))).increment();
     }
 
     public void nodeRemovedNotification(String node, String hostName) {
         dpnList.remove(node);
-        sendNodeUpdation(dpnList.size(), hostName);
-    }
-
-    private void sendNodeUpdation(Integer count, String hostName) {
-
-        if (hostName != null) {
-            nodeListEFSCountStr = "Node_" + hostName + "_NumberOfEFS";
-            LOG.debug("NumberOfEFS: {} dpnList.size {}", nodeListEFSCountStr, count);
-
-            countersMap.put("NumberOfEFS:" + nodeListEFSCountStr, "" + count);
-            agent.connectToPMAgent(countersMap);
-        } else {
-            LOG.error("Hostname is null upon NumberOfEFS counter");
+        String counterKey = FcapsUtil.getCounterName(String.valueOf(hostName),counterName)
+                .replace("switchid","hostid");
+        if (counterMap.containsKey(counterKey)) {
+            counterMap.get(counterKey).close();
+            counterMap.remove(counterKey);
         }
     }
 
