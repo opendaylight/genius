b5e556ff140a1af4663c330d411b06b3ebdba135
Vishal Thapar <vishal.thapar@ericsson.com>
2018-01-29 18:21:30 +0530

    ITM Scalability caches
    Change-Id: I69e23ca3f7a533ee665b0fe58b1c7bb53c619334
    Signed-off-by: Vishal Thapar <vishal.thapar@ericsson.com>

--- itm/itm-impl/src/main/java/org/opendaylight/genius/itm/confighelpers/ItmTunnelStateAddHelper.java
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/confighelpers/ItmTunnelStateAddHelper.java
@@ -10,15 +10,19 @@ package org.opendaylight.genius.itm.confighelpers;
 import com.google.common.util.concurrent.ListenableFuture;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
 import org.opendaylight.genius.interfacemanager.interfaces.IInterfaceManager;
 import org.opendaylight.genius.itm.impl.ITMBatchingUtils;
+import org.opendaylight.genius.itm.impl.ItmTepUtils;
 import org.opendaylight.genius.itm.impl.ItmUtils;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.state.Interface;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.TunnelOperStatus;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.tunnels_state.StateTunnelList;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.tunnels_state.StateTunnelListKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbTerminationPointAugmentation;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -70,4 +74,41 @@ public final class ItmTunnelStateAddHelper {
         return Collections.singletonList(writeTransaction.submit());
     }
 
+    @SuppressWarnings("checkstyle:IllegalCatch")
+    public static List<ListenableFuture<Void>> addTunnel(OvsdbTerminationPointAugmentation tp,
+                                                         Map<String, String> externalIds,
+                                                         ItmTepUtils itmTepUtils) {
+
+        LOG.debug("Invoking ItmTunnelStateAddHelper for {} ", tp.getName());
+        StateTunnelListKey tlKey = new StateTunnelListKey(tp.getName());
+        InstanceIdentifier<StateTunnelList> stListId = ItmUtils.buildStateTunnelListId(tlKey);
+        StateTunnelList tunnelStateList;
+        TunnelOperStatus tunnelOperStatus;
+        //TODO: Handle state as per BFD enable/disable
+        tunnelOperStatus = TunnelOperStatus.Up;
+        boolean tunnelState = true;
+
+        // Create new Tunnel State
+        try {
+            /*
+             * FIXME: A defensive try-catch to find issues without
+             * disrupting existing behavior.
+             */
+            String srcIp = itmTepUtils.getOption(itmTepUtils.TUNNEL_OPTIONS_LOCAL_IP, tp.getOptions());
+            String dstIp = itmTepUtils.getOption(itmTepUtils.TUNNEL_OPTIONS_REMOTE_IP, tp.getOptions());
+            tunnelStateList = ItmUtils.buildStateTunnelList(itmTepUtils.getDataBroker(), tlKey, tp.getName(),
+                tunnelState, tunnelOperStatus,
+                new IpAddress(srcIp.toCharArray()),
+                new IpAddress((dstIp.toCharArray())),
+                externalIds.get(itmTepUtils.IFACE_EXTERNAL_ID_DPNID),
+                externalIds.get(itmTepUtils.IFACE_EXTERNAL_ID_PEER_ID),
+                ItmUtils.TUNNEL_TYPE_MAP.get(externalIds.get(itmTepUtils.IFACE_EXTERNAL_ID_TUNNEL_TYPE)));
+            LOG.trace("Batching the Creation of tunnel_state: {} for Id: {}", tunnelStateList, stListId);
+            ITMBatchingUtils.write(stListId, tunnelStateList, ITMBatchingUtils.EntityType.DEFAULT_OPERATIONAL);
+        } catch (Exception e) {
+            LOG.warn("Exception trying to create tunnel state for {}", tp.getName(), e);
+        }
+
+        return Collections.EMPTY_LIST;
+    }
 }
