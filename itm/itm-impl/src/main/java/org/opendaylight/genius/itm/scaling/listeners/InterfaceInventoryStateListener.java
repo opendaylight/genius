7dcc0e39546fa18642804952c40d052690d55b7b
Edw7n <n.edwin.anthony@ericsson.com>
2018-02-01 14:35:20 +0530

    WIP: Itm-direct-tunnels
    Change-Id: I8ea1c8da6fc1ad842c06ccae02bca56226c8c620
    Signed-off-by: Edw7n <n.edwin.anthony@ericsson.com>

---
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/scaling/listeners/InterfaceInventoryStateListener.java
@@ -0,0 +1,306 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.itm.scaling.listeners;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import java.util.List;
+import java.util.concurrent.Callable;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.datastoreutils.AsyncClusteredDataTreeChangeListenerBase;
+import org.opendaylight.genius.interfacemanager.interfaces.IInterfaceManager;
+import org.opendaylight.genius.itm.cache.DPNTEPsInfoCache;
+import org.opendaylight.genius.itm.cache.TunnelStateCache;
+import org.opendaylight.genius.itm.globals.ITMConstants;
+import org.opendaylight.genius.itm.scaling.renderer.ovs.statehelpers.OvsInterfaceStateAddHelper;
+import org.opendaylight.genius.itm.scaling.renderer.ovs.statehelpers.OvsInterfaceStateRemoveHelper;
+import org.opendaylight.genius.itm.scaling.renderer.ovs.statehelpers.OvsInterfaceStateUpdateHelper;
+import org.opendaylight.genius.itm.scaling.renderer.ovs.utilities.ItmScaleUtils;
+import org.opendaylight.genius.itm.scaling.renderer.ovs.utilities.NodeConnectorInfo;
+import org.opendaylight.genius.itm.scaling.renderer.ovs.utilities.TunnelUtils;
+import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
+import org.opendaylight.genius.utils.clustering.EntityOwnershipUtils;
+import org.opendaylight.infrautils.jobcoordinator.JobCoordinator;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNodeConnector;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.port.rev130925.PortReason;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.IdManagerService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeConnectorId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.node.NodeConnector;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This Class is a Data Change Listener for FlowCapableNodeConnector updates.
+ * This creates an entry in the tunnels-state OperDS for every node-connector used.
+ */
+@Singleton
+public class InterfaceInventoryStateListener extends AsyncClusteredDataTreeChangeListenerBase<FlowCapableNodeConnector,
+        InterfaceInventoryStateListener> {
+    private static final Logger LOG = LoggerFactory.getLogger(InterfaceInventoryStateListener.class);
+
+    private final DataBroker dataBroker;
+    private final IdManagerService idManager;
+    private final IMdsalApiManager mdsalApiManager;
+    private final JobCoordinator coordinator;
+    private final EntityOwnershipUtils entityOwnershipUtils;
+    private final DPNTEPsInfoCache dpntePsInfoCache;
+    private final TunnelStateCache tunnelStateCache;
+
+    @Inject
+    public InterfaceInventoryStateListener(final DataBroker dataBroker, final IdManagerService idManagerService,
+                                           final IMdsalApiManager mdsalApiManager,
+                                           final IInterfaceManager interfaceManager,
+                                           final JobCoordinator coordinator,
+                                           final EntityOwnershipUtils entityOwnershipUtils,
+                                           final DPNTEPsInfoCache dpntePsInfoCache,
+                                           final TunnelStateCache tunnelStateCache) {
+        super(FlowCapableNodeConnector.class, InterfaceInventoryStateListener.class);
+        this.dataBroker = dataBroker;
+        this.idManager = idManagerService;
+        this.mdsalApiManager = mdsalApiManager;
+        this.coordinator = coordinator;
+        this.entityOwnershipUtils = entityOwnershipUtils;
+        this.dpntePsInfoCache = dpntePsInfoCache;
+        this.tunnelStateCache = tunnelStateCache;
+        if (interfaceManager.isItmDirectTunnelsEnabled()) {
+            LOG.debug("ITM Direct Tunnels is Enabled, hence registering this listener");
+            this.registerListener(LogicalDatastoreType.OPERATIONAL, dataBroker);
+        } else {
+            LOG.debug("ITM Direct Tunnels is not Enabled, therefore not registering this listener");
+        }
+    }
+
+    @Override
+    protected InstanceIdentifier<FlowCapableNodeConnector> getWildCardPath() {
+        return InstanceIdentifier.create(Nodes.class).child(Node.class).child(NodeConnector.class)
+                .augmentation(FlowCapableNodeConnector.class);
+    }
+
+    @Override
+    protected InterfaceInventoryStateListener getDataTreeChangeListener() {
+        return InterfaceInventoryStateListener.this;
+    }
+
+    @Override
+    protected void remove(InstanceIdentifier<FlowCapableNodeConnector> key,
+                          FlowCapableNodeConnector flowCapableNodeConnectorOld) {
+        String portName = flowCapableNodeConnectorOld.getName();
+        LOG.debug("InterfaceInventoryState Remove for {}", portName);
+        // ITM Direct Tunnels Return if its not tunnel port and if its not Internal
+        if (!TunnelUtils.TUNNEL_PORT_PREDICATE.test(portName)) {
+            LOG.debug("Node Connector Remove - {} Interface is not a tunnel I/f, so no-op", portName);
+            return;
+        } else if (!ItmScaleUtils.isInternalBasedOnState(portName, dataBroker, tunnelStateCache)) {
+            LOG.debug("Node Connector Remove {} Interface is not a internal tunnel I/f, so no-op", portName);
+            return;
+        }
+        if (!entityOwnershipUtils.isEntityOwner(ITMConstants.ITM_CONFIG_ENTITY, ITMConstants.ITM_CONFIG_ENTITY)) {
+            return;
+        }
+        LOG.debug("Received NodeConnector Remove Event: {}, {}", key, flowCapableNodeConnectorOld);
+        NodeConnectorId nodeConnectorId = InstanceIdentifier.keyOf(key.firstIdentifierOf(NodeConnector.class)).getId();
+        remove(nodeConnectorId, null, flowCapableNodeConnectorOld, portName, true);
+    }
+
+    private void remove(NodeConnectorId nodeConnectorIdNew, NodeConnectorId nodeConnectorIdOld,
+                        FlowCapableNodeConnector fcNodeConnectorNew, String portName, boolean isNetworkEvent) {
+        LOG.debug("InterfaceInventoryState REMOVE for {}", portName);
+        InterfaceStateRemoveWorker portStateRemoveWorker = new InterfaceStateRemoveWorker(idManager, nodeConnectorIdNew,
+                nodeConnectorIdOld, fcNodeConnectorNew, portName, portName, isNetworkEvent, true, tunnelStateCache);
+        coordinator.enqueueJob(portName, portStateRemoveWorker, ITMConstants.JOB_MAX_RETRIES);
+    }
+
+    @Override
+    protected void update(InstanceIdentifier<FlowCapableNodeConnector> key, FlowCapableNodeConnector fcNodeConnectorOld,
+                          FlowCapableNodeConnector fcNodeConnectorNew) {
+        String portName = fcNodeConnectorNew.getName();
+        if (!TunnelUtils.TUNNEL_PORT_PREDICATE.test(portName)) {
+            LOG.debug("Node Connector Update - {} Interface is not a tunnel I/f, so no-op", portName);
+            return;
+        } else if (!ItmScaleUtils.isInternal(portName)) {
+            LOG.debug("Node Connector Update {} Interface is not a internal tunnel I/f, so no-op", portName);
+            return;
+        }
+        if (fcNodeConnectorNew.getReason() == PortReason.Delete
+            || !entityOwnershipUtils.isEntityOwner(ITMConstants.ITM_CONFIG_ENTITY, ITMConstants.ITM_CONFIG_ENTITY)) {
+            return;
+        }
+        LOG.debug("Received NodeConnector Update Event: {}, {}, {}", key, fcNodeConnectorOld, fcNodeConnectorNew);
+
+        InterfaceStateUpdateWorker portStateUpdateWorker = new InterfaceStateUpdateWorker(key, fcNodeConnectorOld,
+                fcNodeConnectorNew, portName, tunnelStateCache);
+        coordinator.enqueueJob(portName, portStateUpdateWorker, ITMConstants.JOB_MAX_RETRIES);
+    }
+
+    @Override
+    protected void add(InstanceIdentifier<FlowCapableNodeConnector> key, FlowCapableNodeConnector fcNodeConnectorNew) {
+        String portName = fcNodeConnectorNew.getName();
+        LOG.debug("InterfaceInventoryState ADD for {}", portName);
+        // SF419 Return if its not tunnel port and if its not Internal
+        if (!TunnelUtils.TUNNEL_PORT_PREDICATE.test(portName)) {
+            LOG.debug("Node Connector Add {} Interface is not a tunnel I/f, so no-op", portName);
+            return;
+        }
+        if (!entityOwnershipUtils.isEntityOwner(ITMConstants.ITM_CONFIG_ENTITY, ITMConstants.ITM_CONFIG_ENTITY)) {
+            return;
+        }
+        if (!ItmScaleUtils.isConfigAvailable(portName)) {
+            // Park the notification
+            LOG.debug("Unable to process the NodeConnector ADD event for {} as Config not available."
+                    + "Hence parking it", portName);
+            NodeConnectorInfo nodeConnectorInfo = new NodeConnectorInfo(key, fcNodeConnectorNew);
+            ItmScaleUtils.addNodeConnectorInfoToCache(portName, nodeConnectorInfo);
+            return;
+        } else if (!ItmScaleUtils.isInternal(portName)) {
+            LOG.debug("{} Interface is not a internal tunnel I/f, so no-op", portName);
+            return;
+        }
+
+        LOG.debug("Received NodeConnector Add Event: {}, {}", key, fcNodeConnectorNew);
+        if (TunnelUtils.TUNNEL_PORT_PREDICATE.test(portName) && ItmScaleUtils.isInternal(portName)) {
+            //NodeConnectorId nodeConnectorId =
+            // InstanceIdentifier.keyOf(key.firstIdentifierOf(NodeConnector.class)).getId();
+
+
+            InterfaceStateAddWorker ifStateAddWorker = new InterfaceStateAddWorker(idManager, key,
+                    fcNodeConnectorNew, portName, dpntePsInfoCache);
+            coordinator.enqueueJob(portName, ifStateAddWorker, ITMConstants.JOB_MAX_RETRIES);
+        }
+    }
+
+
+
+    // ITM Direct Tunnels Maybe this is not required -- CHECK
+    private String getDpnPrefixedPortName(NodeConnectorId nodeConnectorId, String portName) {
+        portName = (ItmScaleUtils.getDpnFromNodeConnectorId(nodeConnectorId)).toString()
+                + ITMConstants.OF_URI_SEPARATOR + portName;
+        return portName;
+    }
+
+    private class InterfaceStateAddWorker implements Callable {
+        private final InstanceIdentifier<FlowCapableNodeConnector> key;
+        private final FlowCapableNodeConnector fcNodeConnectorNew;
+        private final String interfaceName;
+        private final IdManagerService idManager;
+        private final DPNTEPsInfoCache dpntePsInfoCache;
+
+        InterfaceStateAddWorker(IdManagerService idManager, InstanceIdentifier<FlowCapableNodeConnector> key,
+                                FlowCapableNodeConnector fcNodeConnectorNew, String portName,
+                                DPNTEPsInfoCache dpntePsInfoCache) {
+            this.key = key;
+            this.fcNodeConnectorNew = fcNodeConnectorNew;
+            this.interfaceName = portName;
+            this.idManager = idManager;
+            this.dpntePsInfoCache = dpntePsInfoCache;
+        }
+
+        @Override
+        public Object call() throws Exception {
+            // If another renderer(for eg : CSS) needs to be supported, check can be performed here
+            // to call the respective helpers.
+            List<ListenableFuture<Void>> futures = OvsInterfaceStateAddHelper.addState(dataBroker, idManager,
+                    mdsalApiManager, key, interfaceName, fcNodeConnectorNew, dpntePsInfoCache);
+            return futures;
+        }
+
+        @Override
+        public String toString() {
+            return "InterfaceStateAddWorker{fcNodeConnectorIdentifier=" + key + ", fcNodeConnectorNew="
+                    + fcNodeConnectorNew + ", interfaceName='" + interfaceName + '\'' + '}';
+        }
+    }
+
+    private class InterfaceStateUpdateWorker implements Callable {
+        private InstanceIdentifier<FlowCapableNodeConnector> key;
+        private final FlowCapableNodeConnector fcNodeConnectorOld;
+        private final FlowCapableNodeConnector fcNodeConnectorNew;
+        private final TunnelStateCache tunnelStateCache;
+        private String interfaceName;
+
+
+
+        InterfaceStateUpdateWorker(InstanceIdentifier<FlowCapableNodeConnector> key,
+                                   FlowCapableNodeConnector fcNodeConnectorOld,
+                                   FlowCapableNodeConnector fcNodeConnectorNew,
+                                   String portName, TunnelStateCache tunnelStateCache) {
+            this.key = key;
+            this.fcNodeConnectorOld = fcNodeConnectorOld;
+            this.fcNodeConnectorNew = fcNodeConnectorNew;
+            this.interfaceName = portName;
+            this.tunnelStateCache = tunnelStateCache;
+        }
+
+        @Override
+        public Object call() throws Exception {
+            // If another renderer(for eg : CSS) needs to be supported, check can be performed here
+            // to call the respective helpers.
+            List<ListenableFuture<Void>> futures = OvsInterfaceStateUpdateHelper.updateState(key, dataBroker,
+                    interfaceName, fcNodeConnectorNew, fcNodeConnectorOld, tunnelStateCache);
+            return futures;
+        }
+
+        @Override
+        public String toString() {
+            return "InterfaceStateUpdateWorker{key=" + key + ", fcNodeConnectorOld=" + fcNodeConnectorOld
+                    + ", fcNodeConnectorNew=" + fcNodeConnectorNew + ", interfaceName='" + interfaceName + '\'' + '}';
+        }
+    }
+
+    private class InterfaceStateRemoveWorker implements Callable {
+        private final NodeConnectorId nodeConnectorIdNew;
+        private NodeConnectorId nodeConnectorIdOld;
+        FlowCapableNodeConnector fcNodeConnectorOld;
+        private final String interfaceName;
+        private final String parentInterface;
+        private final IdManagerService idManager;
+        private final boolean isNetworkEvent;
+        private final boolean isParentInterface;
+        private final TunnelStateCache tunnelStateCache;
+
+        InterfaceStateRemoveWorker(IdManagerService idManager, NodeConnectorId nodeConnectorIdNew,
+                                   NodeConnectorId nodeConnectorIdOld, FlowCapableNodeConnector fcNodeConnectorOld,
+                                   String interfaceName, String parentInterface, boolean isNetworkEvent,
+                                   boolean isParentInterface, TunnelStateCache tunnelStateCache) {
+            this.nodeConnectorIdNew = nodeConnectorIdNew;
+            this.nodeConnectorIdOld = nodeConnectorIdOld;
+            this.fcNodeConnectorOld = fcNodeConnectorOld;
+            this.interfaceName = interfaceName;
+            this.parentInterface = parentInterface;
+            this.idManager = idManager;
+            this.isNetworkEvent = isNetworkEvent;
+            this.isParentInterface = isParentInterface;
+            this.tunnelStateCache = tunnelStateCache;
+        }
+
+        @Override
+        public Object call() throws Exception {
+            // If another renderer(for eg : CSS) needs to be supported, check can be performed here
+            // to call the respective helpers.
+
+            List<ListenableFuture<Void>> futures = null;
+
+            futures = OvsInterfaceStateRemoveHelper.removeInterfaceStateConfiguration(idManager, mdsalApiManager,
+                nodeConnectorIdNew, nodeConnectorIdOld, dataBroker, interfaceName,
+                fcNodeConnectorOld, parentInterface, tunnelStateCache);
+
+            return futures;
+        }
+
+        @Override
+        public String toString() {
+            return "InterfaceStateRemoveWorker{nodeConnectorIdNew=" + nodeConnectorIdNew + ", nodeConnectorIdOld="
+                    + nodeConnectorIdOld + ", fcNodeConnectorOld=" + fcNodeConnectorOld + ", interfaceName='"
+                    + interfaceName + '\'' + '}';
+        }
+    }
+}
---
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/scaling/renderer/ovs/statehelpers/OvsTunnelTopologyStateAddHelper.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.itm.scaling.renderer.ovs.statehelpers;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.genius.itm.cache.DPNTEPsInfoCache;
+import org.opendaylight.genius.itm.impl.TunnelMonitoringConfig;
+import org.opendaylight.genius.itm.scaling.renderer.ovs.utilities.ItmScaleUtils;
+import org.opendaylight.genius.itm.scaling.renderer.ovs.utilities.SouthboundUtils;
+import org.opendaylight.genius.itm.scaling.renderer.ovs.utilities.TunnelMetaUtils;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.meta.rev171210.bridge.tunnel.info.OvsBridgeEntry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbBridgeAugmentation;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OvsTunnelTopologyStateAddHelper {
+    private static final Logger LOG = LoggerFactory.getLogger(OvsTunnelTopologyStateAddHelper.class);
+
+    public static List<ListenableFuture<Void>> addPortToBridge(InstanceIdentifier<OvsdbBridgeAugmentation> bridgeIid,
+                                                               OvsdbBridgeAugmentation bridgeNew,
+                                                               DataBroker dataBroker,
+                                                               TunnelMonitoringConfig tunnelMonitoringConfig,
+                                                               DPNTEPsInfoCache dpntePsInfoCache) {
+        List<ListenableFuture<Void>> futures = new ArrayList<>();
+
+        if (bridgeNew.getDatapathId() == null) {
+            LOG.info("DataPathId found as null for Bridge Augmentation: {}... returning...", bridgeNew);
+            return futures;
+        }
+
+        BigInteger dpnId = ItmScaleUtils.getDpnId(bridgeNew.getDatapathId());
+        LOG.debug("adding bridge references for bridge: {}, dpn: {}", bridgeNew, dpnId);
+
+        // create bridge reference entry in interface meta operational DS
+        WriteTransaction writeTransaction = dataBroker.newWriteOnlyTransaction();
+        TunnelMetaUtils.createOvsBridgeRefEntry(dpnId, bridgeIid, writeTransaction);
+
+        // handle pre-provisioning of tunnels for the newly connected dpn
+        OvsBridgeEntry bridgeEntry = TunnelMetaUtils.getOvsBridgeEntryFromConfigDS(dpnId, dataBroker);
+        if (bridgeEntry == null) {
+            LOG.debug("Bridge entry not found in config DS for dpn: {}", dpnId);
+            futures.add(writeTransaction.submit());
+            return futures;
+        }
+        futures.add(writeTransaction.submit());
+        SouthboundUtils.addAllPortsToBridge(bridgeEntry, dataBroker, bridgeIid, bridgeNew, futures,
+                tunnelMonitoringConfig, dpntePsInfoCache);
+        return futures;
+    }
+}
