78ba1ed652495de1fad73cbaf7ea69c60e956dae
Edw7n <n.edwin.anthony@ericsson.com>
2018-02-01 14:35:20 +0530

    WIP: Itm-direct-tunnels
    Change-Id: I8ea1c8da6fc1ad842c06ccae02bca56226c8c620
    Signed-off-by: Edw7n <n.edwin.anthony@ericsson.com>

---
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/scaling/listeners/TerminationPointStateListener.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.itm.scaling.listeners;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import java.util.List;
+import java.util.concurrent.Callable;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.datastoreutils.AsyncClusteredDataTreeChangeListenerBase;
+import org.opendaylight.genius.itm.globals.ITMConstants;
+import org.opendaylight.genius.itm.scaling.renderer.ovs.statehelpers.OvsTunnelTopologyStateUpdateHelper;
+import org.opendaylight.genius.itm.scaling.renderer.ovs.utilities.ItmScaleUtils;
+import org.opendaylight.genius.itm.scaling.renderer.ovs.utilities.TunnelUtils;
+import org.opendaylight.infrautils.jobcoordinator.JobCoordinator;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbTerminationPointAugmentation;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.node.TerminationPoint;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+public class TerminationPointStateListener
+        extends AsyncClusteredDataTreeChangeListenerBase<OvsdbTerminationPointAugmentation,
+        TerminationPointStateListener> {
+    private static final Logger LOG = LoggerFactory.getLogger(TerminationPointStateListener.class);
+
+    private final DataBroker dataBroker;
+    private final JobCoordinator coordinator;
+    private final OvsTunnelTopologyStateUpdateHelper ovsTunnelTopologyStateUpdateHelper;
+
+    @Inject
+    public TerminationPointStateListener(final DataBroker dataBroker, final JobCoordinator coordinator, final
+                                         OvsTunnelTopologyStateUpdateHelper ovsTunnelTopologyStateUpdateHelper) {
+        super(OvsdbTerminationPointAugmentation.class, TerminationPointStateListener.class);
+        this.dataBroker = dataBroker;
+        this.coordinator = coordinator;
+        this.ovsTunnelTopologyStateUpdateHelper = ovsTunnelTopologyStateUpdateHelper;
+        this.registerListener(LogicalDatastoreType.OPERATIONAL, this.dataBroker);
+    }
+
+    @Override
+    protected InstanceIdentifier<OvsdbTerminationPointAugmentation> getWildCardPath() {
+        return InstanceIdentifier.builder(NetworkTopology.class).child(Topology.class)
+                .child(Node.class).child(TerminationPoint.class).augmentation(OvsdbTerminationPointAugmentation.class)
+                .build();
+    }
+
+    @Override
+    protected TerminationPointStateListener getDataTreeChangeListener() {
+        return TerminationPointStateListener.this;
+    }
+
+    @Override
+    protected void remove(InstanceIdentifier<OvsdbTerminationPointAugmentation> identifier,
+                          OvsdbTerminationPointAugmentation tpOld) {
+        if (TunnelUtils.TUNNEL_PORT_PREDICATE.test(tpOld.getName()) && ItmScaleUtils.isInternal(tpOld.getName())) {
+            LOG.debug("Received remove DataChange Notification for ovsdb termination point {}", tpOld.getName());
+            if (tpOld.getInterfaceBfdStatus() != null) {
+                LOG.debug("Received termination point removed notification with bfd status values {}", tpOld.getName());
+                RendererStateRemoveWorker rendererStateRemoveWorker = new RendererStateRemoveWorker(tpOld);
+                coordinator.enqueueJob(tpOld.getName(), rendererStateRemoveWorker);
+            }
+        }
+    }
+
+    @Override
+    protected void update(InstanceIdentifier<OvsdbTerminationPointAugmentation> identifier,
+                          OvsdbTerminationPointAugmentation tpOld,
+                          OvsdbTerminationPointAugmentation tpNew) {
+        if (TunnelUtils.TUNNEL_PORT_PREDICATE.test(tpNew.getName()) && ItmScaleUtils.isInternal(tpNew.getName())) {
+            LOG.debug("Received Update DataChange Notification for ovsdb termination point {}", tpNew.getName());
+            if (tpNew.getInterfaceBfdStatus() != null
+                    && !tpNew.getInterfaceBfdStatus().equals(tpOld.getInterfaceBfdStatus())) {
+                LOG.info("Bfd Status changed for ovsdb termination point identifier: {},  old: {}, new: {}.",
+                        identifier, tpOld, tpNew);
+                RendererStateUpdateWorker rendererStateAddWorker = new RendererStateUpdateWorker(identifier, tpNew);
+                coordinator.enqueueJob(tpNew.getName(), rendererStateAddWorker, ITMConstants.JOB_MAX_RETRIES);
+            }
+        }
+    }
+
+    @Override
+    protected void add(InstanceIdentifier<OvsdbTerminationPointAugmentation> identifier,
+                       OvsdbTerminationPointAugmentation tpNew) {
+        if (TunnelUtils.TUNNEL_PORT_PREDICATE.test(tpNew.getName()) && ItmScaleUtils.isInternal(tpNew.getName())) {
+            LOG.debug("Received add DataChange Notification for ovsdb termination point {}", tpNew.getName());
+            if (tpNew.getInterfaceBfdStatus() != null) {
+                LOG.debug("Received termination point added notification with bfd status values {}", tpNew.getName());
+                RendererStateUpdateWorker rendererStateUpdateWorker = new RendererStateUpdateWorker(identifier, tpNew);
+                coordinator.enqueueJob(tpNew.getName(), rendererStateUpdateWorker, ITMConstants.JOB_MAX_RETRIES);
+            }
+        }
+    }
+
+    private class RendererStateUpdateWorker implements Callable<List<ListenableFuture<Void>>> {
+        InstanceIdentifier<OvsdbTerminationPointAugmentation> instanceIdentifier;
+        OvsdbTerminationPointAugmentation terminationPointNew;
+
+
+        RendererStateUpdateWorker(InstanceIdentifier<OvsdbTerminationPointAugmentation> instanceIdentifier,
+                                         OvsdbTerminationPointAugmentation tpNew) {
+            this.instanceIdentifier = instanceIdentifier;
+            this.terminationPointNew = tpNew;
+        }
+
+        @Override
+        public List<ListenableFuture<Void>> call() throws Exception {
+            // If another renderer(for eg : CSS) needs to be supported, check can be performed here
+            // to call the respective helpers.
+            return ovsTunnelTopologyStateUpdateHelper.updateTunnelState(terminationPointNew);
+        }
+    }
+
+    private class RendererStateRemoveWorker implements Callable<List<ListenableFuture<Void>>> {
+        OvsdbTerminationPointAugmentation terminationPointOld;
+
+
+        RendererStateRemoveWorker(OvsdbTerminationPointAugmentation tpNew) {
+            this.terminationPointOld = tpNew;
+        }
+
+        @Override
+        public List<ListenableFuture<Void>> call() throws Exception {
+            LOG.debug("Removing bfd state from cache, if any, for {}", terminationPointOld.getName());
+            TunnelUtils.removeBfdStateFromCache(terminationPointOld.getName());
+            return null;
+        }
+    }
+}
---
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/scaling/renderer/ovs/statehelpers/OvsTunnelTopologyStateUpdateHelper.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright (c) 2016 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.itm.scaling.renderer.ovs.statehelpers;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.genius.itm.cache.DPNTEPsInfoCache;
+import org.opendaylight.genius.itm.cache.TunnelStateCache;
+import org.opendaylight.genius.itm.globals.ITMConstants;
+import org.opendaylight.genius.itm.impl.TunnelMonitoringConfig;
+import org.opendaylight.genius.itm.scaling.renderer.ovs.utilities.ItmClusterUtils;
+import org.opendaylight.genius.itm.scaling.renderer.ovs.utilities.ItmScaleUtils;
+import org.opendaylight.genius.itm.scaling.renderer.ovs.utilities.SouthboundUtils;
+import org.opendaylight.genius.itm.scaling.renderer.ovs.utilities.TunnelMetaUtils;
+import org.opendaylight.genius.itm.scaling.renderer.ovs.utilities.TunnelUtils;
+import org.opendaylight.genius.utils.clustering.EntityOwnershipUtils;
+import org.opendaylight.infrautils.jobcoordinator.JobCoordinator;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.state.Interface;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.meta.rev161113.bridge.tunnel.info.OvsBridgeEntry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.TunnelOperStatus;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.tunnels_state.StateTunnelList;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbBridgeAugmentation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbTerminationPointAugmentation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.port._interface.attributes.InterfaceBfdStatus;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@Singleton
+public class OvsTunnelTopologyStateUpdateHelper {
+    private static final Logger LOG = LoggerFactory.getLogger(OvsTunnelTopologyStateUpdateHelper.class);
+
+    private final DataBroker dataBroker;
+    private final JobCoordinator coordinator;
+    private final EntityOwnershipUtils entityOwnershipUtils;
+    private final DPNTEPsInfoCache dpntePsInfoCache;
+    private final TunnelStateCache tunnelStateCache;
+    private final TunnelMonitoringConfig tunnelMonitoringConfig;
+
+    @Inject
+    public OvsTunnelTopologyStateUpdateHelper(final DataBroker dataBroker, final JobCoordinator coordinator,
+                                              final EntityOwnershipUtils entityOwnershipUtils,
+                                              final DPNTEPsInfoCache dpntePsInfoCache,
+                                              final TunnelStateCache tunnelStateCache,
+                                              final TunnelMonitoringConfig tunnelMonitoringConfig) {
+        this.dataBroker = dataBroker;
+        this.coordinator = coordinator;
+        this.entityOwnershipUtils = entityOwnershipUtils;
+        this.dpntePsInfoCache = dpntePsInfoCache;
+        this.tunnelStateCache = tunnelStateCache;
+        this.tunnelMonitoringConfig = tunnelMonitoringConfig;
+
+    }
+
+    /*
+     *  This code is used to handle only a dpnId change scenario for a particular change,
+     * which is not expected to happen in usual cases.
+     */
+    public List<ListenableFuture<Void>> updateOvsBridgeRefEntry(InstanceIdentifier<OvsdbBridgeAugmentation> bridgeIid,
+                                                                OvsdbBridgeAugmentation bridgeNew,
+                                                                OvsdbBridgeAugmentation bridgeOld) {
+
+        List<ListenableFuture<Void>> futures = new ArrayList<>();
+        WriteTransaction writeTransaction = dataBroker.newWriteOnlyTransaction();
+
+        BigInteger dpnIdNew = ItmScaleUtils.getDpnId(bridgeNew.getDatapathId());
+        BigInteger dpnIdOld = ItmScaleUtils.getDpnId(bridgeOld.getDatapathId());
+
+        LOG.debug("updating bridge references for bridge: {}, dpnNew: {}, dpnOld: {}", bridgeNew,
+            dpnIdNew, dpnIdOld);
+        //delete bridge reference entry for the old dpn in interface meta operational DS
+        TunnelMetaUtils.deleteOvsBridgeRefEntry(dpnIdOld, writeTransaction);
+
+        // create bridge reference entry in interface meta operational DS
+        TunnelMetaUtils.createOvsBridgeRefEntry(dpnIdNew, bridgeIid, writeTransaction);
+
+        // handle pre-provisioning of tunnels for the newly connected dpn
+        OvsBridgeEntry bridgeEntry = TunnelMetaUtils.getOvsBridgeEntryFromConfigDS(dpnIdNew, dataBroker);
+        if (bridgeEntry == null) {
+            futures.add(writeTransaction.submit());
+            return futures;
+        }
+
+        SouthboundUtils.addAllPortsToBridge(bridgeEntry, dataBroker, bridgeIid, bridgeNew, futures,
+                tunnelMonitoringConfig, dpntePsInfoCache);
+
+        futures.add(writeTransaction.submit());
+        return futures;
+    }
+
+    public List<ListenableFuture<Void>> updateTunnelState(OvsdbTerminationPointAugmentation terminationPointNew) {
+        final String interfaceName = terminationPointNew.getName();
+        final Interface.OperStatus interfaceBfdStatus = getTunnelOpState(terminationPointNew.getInterfaceBfdStatus());
+        TunnelOperStatus tunnelState = ItmScaleUtils.convertInterfaceToTunnelOperState(interfaceBfdStatus);
+        TunnelUtils.addBfdStateToCache(interfaceName, interfaceBfdStatus);
+        if (!ItmClusterUtils.isEntityOwner(ITMConstants.ITM_CONFIG_ENTITY, entityOwnershipUtils)) {
+            return null;
+        }
+
+        coordinator.enqueueJob(interfaceName, () -> {
+            // update opstate of interface if TEP has gone down/up as a result of BFD monitoring
+            final StateTunnelList stateTnl = TunnelUtils
+                .getTunnelFromOperationalDS(interfaceName, dataBroker, tunnelStateCache);
+            if (stateTnl != null && stateTnl.getOperState() != TunnelOperStatus.Unknown
+                && stateTnl.getOperState() != tunnelState) {
+                LOG.debug("updating tunnel state for interface {} as {}", interfaceName,
+                    stateTnl.getOperState());
+                WriteTransaction transaction = dataBroker.newWriteOnlyTransaction();
+                TunnelUtils.updateOpState(transaction, interfaceName, tunnelState);
+                return Collections.singletonList(transaction.submit());
+            }
+            return Collections.emptyList();
+        });
+        return null;
+    }
+
+    private static Interface.OperStatus getTunnelOpState(List<InterfaceBfdStatus> tunnelBfdStatus) {
+        Interface.OperStatus livenessState = Interface.OperStatus.Down;
+        if (tunnelBfdStatus != null && !tunnelBfdStatus.isEmpty()) {
+            for (InterfaceBfdStatus bfdState : tunnelBfdStatus) {
+                if (bfdState.getBfdStatusKey().equalsIgnoreCase(SouthboundUtils.BFD_OP_STATE)) {
+                    String bfdOpState = bfdState.getBfdStatusValue();
+                    livenessState = SouthboundUtils.BFD_STATE_UP.equalsIgnoreCase(bfdOpState)
+                            ? Interface.OperStatus.Up : Interface.OperStatus.Down;
+                    break;
+                }
+            }
+        }
+        return livenessState;
+    }
+}
