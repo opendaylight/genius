7bad9c1d361293955d08b44defe78c7d4e9eb063
Hema Gopalakrishnan <hema.gopalkrishnan@ericsson.com>
2018-02-16 16:21:02 +0530

    Fix for ITM reporting wrong tunnel status
    When the tunnel is down and the controller undergoes a reboot, the tunnel
    BFD states come as UP and DOWN back to back and this results in the DOWN
    state not getting reflected correctly in ITM.
    Fix is to cache the tunnel status that comes as DOWN in "update" and
    process the add DTCN and then process the update tunnel state DTCN
    Fixed checkstyle issues
    
    Change-Id: Iee89e482b8d55f806264f23904e74c5122c0c399
    Signed-off-by: Hema Gopalakrishnan <hema.gopalkrishnan@ericsson.com>

--- itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cli/TepCommandHelper.java
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cli/TepCommandHelper.java
@@ -529,6 +529,9 @@ public class TepCommandHelper {
                 case ITMConstants.EXTERNAL_TUNNEL_CACHE_NAME:
                     cacheContent = ItmUtils.ITM_CACHE.getAllExternalInterfaces();
                     break;
+                case ITMConstants.UNPROCESSED_TUNNELS_CACHE_NAME:
+                    cacheContent = ItmUtils.ITM_CACHE.getAllUnprocessedTunnels();
+                    break;
                 default:
                     cacheContent = Collections.emptyList();
             }
@@ -546,7 +549,8 @@ public class TepCommandHelper {
     public boolean isInMemoryCacheNameValid(String name) {
         boolean valid = false;
         valid = name.equals(ITMConstants.INTERNAL_TUNNEL_CACHE_NAME)
-                || name.equals(ITMConstants.EXTERNAL_TUNNEL_CACHE_NAME);
+                || name.equals(ITMConstants.EXTERNAL_TUNNEL_CACHE_NAME)
+                || name.equals(ITMConstants.UNPROCESSED_TUNNELS_CACHE_NAME);
         return valid;
     }
 
