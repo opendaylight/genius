b4aa1be4556452a5a7ba98ee877154bdfda8e44a
Edw7n <n.edwin.anthony@ericsson.com>
2018-02-17 18:34:18 +0530

    Utils for performance enhancements
    Change-Id: I5d2c2bc2867d934518de682c9e79d7cd2c8ae8c3
    Depends-on: Icdceb46e6453b6b1e503d14d89bd1b201f9a183b
    Signed-off-by: Edw7n <n.edwin.anthony@ericsson.com>

---
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/scaling/renderer/ovs/utilities/DpnTepInterfaceInfo.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.itm.scaling.renderer.ovs.utilities;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rev160406.TunnelTypeBase;
+
+public final class DpnTepInterfaceInfo {
+
+    private Class<? extends TunnelTypeBase> tunnelType;
+    private long groupId;
+    private String tunnelName;
+    private boolean monitorEnabled;
+    boolean internal;
+
+    public boolean isInternal() {
+        return internal;
+    }
+
+    public void setInternal(boolean internal) {
+        this.internal = internal;
+    }
+
+    public Class<? extends TunnelTypeBase>   getTunnelType() {
+        return tunnelType;
+    }
+
+    public void setTunnelType(Class<? extends TunnelTypeBase> tunnelType) {
+        this.tunnelType = tunnelType;
+    }
+
+    public long getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(long groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getTunnelName() {
+        return tunnelName;
+    }
+
+    public void setTunnelName(String tunnelName) {
+        this.tunnelName = tunnelName;
+    }
+
+    public boolean isMonitorEnabled() {
+        return monitorEnabled;
+    }
+
+    public void setMonitorEnabled(boolean monitorEnabled) {
+        this.monitorEnabled = monitorEnabled;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        DpnTepInterfaceInfo that = (DpnTepInterfaceInfo) obj;
+
+        if (getGroupId() != that.getGroupId()) {
+            return false;
+        }
+        return getTunnelName() != null ? getTunnelName().equals(that.getTunnelName()) : that.getTunnelName() == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (getGroupId() ^ (getGroupId() >>> 32));
+        result = 31 * result + (getTunnelName() != null ? getTunnelName().hashCode() : 0);
+        return result;
+    }
+}
