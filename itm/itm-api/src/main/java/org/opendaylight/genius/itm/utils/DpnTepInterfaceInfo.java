3264106a8858359bcdbc8a7a7c3349559f3aaa2d
Edw7n <n.edwin.anthony@ericsson.com>
2018-03-02 10:53:32 +0530

    Immutables for performance enhancements
    Change-Id: I150d8edc58ad91724679dba15a1f269bd9e2e674
    Depends-on: I250b75fea3ae29e8adb351693e7eeb0a596e18fe
    Signed-off-by: Edw7n <n.edwin.anthony@ericsson.com>
    (cherry picked from commit 472a2c7f07d92295d082509cf2ede81ca9ac57f6)

---
+++ itm/itm-api/src/main/java/org/opendaylight/genius/itm/utils/DpnTepInterfaceInfo.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2018 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.itm.utils;
+
+import org.immutables.value.Value;
+import org.opendaylight.genius.infra.OpenDaylightImmutableStyle;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rev160406.TunnelTypeBase;
+
+@Value.Immutable
+@OpenDaylightImmutableStyle
+public interface DpnTepInterfaceInfo {
+
+    Class<? extends TunnelTypeBase> getTunnelType();
+
+    long getGroupId();
+
+    String getTunnelName();
+
+    boolean isMonitoringEnabled();
+
+    boolean isInternal();
+}
