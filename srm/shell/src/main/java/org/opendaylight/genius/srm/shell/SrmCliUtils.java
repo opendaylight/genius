0a43287be655bba928a220d460be1416421e3e2e
Shashidhar Raja <shashidharr@altencalsoftlabs.com>
2018-03-02 11:58:17 +0530

    Service-Recovery: Support needed for ACL feature introduced in SRM framework.
    (a) srm-types.yang updated with 'netvirt-acl', 'netvirt-acl-interface' and
    'netvirt-acl-instance' definitions to support ACL service and instance
    recovery operations. 'netvirt-acl-interface' and
    'netvirt-acl-instance' are the two instance recovery options supported.
    (b) SrmRpcUtils.java and SrmCliUtils.java updated to add required ACL definitions
    to the internal maps maintained to support recovery operations.
    
    Change-Id: I4c91cd52e7595b92b494356dfa715dbdf15da3c5
    Signed-off-by: Shashidhar Raja <shashidharr@altencalsoftlabs.com>

--- srm/shell/src/main/java/org/opendaylight/genius/srm/shell/SrmCliUtils.java
+++ srm/shell/src/main/java/org/opendaylight/genius/srm/shell/SrmCliUtils.java
@@ -19,6 +19,9 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.srm.types.rev170711.
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.srm.types.rev170711.GeniusItm;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.srm.types.rev170711.GeniusItmTep;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.srm.types.rev170711.GeniusItmTz;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.srm.types.rev170711.NetvirtAcl;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.srm.types.rev170711.NetvirtAclInstance;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.srm.types.rev170711.NetvirtAclInterface;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.srm.types.rev170711.NetvirtElan;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.srm.types.rev170711.NetvirtElanInstance;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.srm.types.rev170711.NetvirtQos;
@@ -50,6 +53,7 @@ public final class SrmCliUtils {
             .put("IFM", GeniusIfm.class)
             .put("VPN", NetvirtVpn.class)
             .put("ELAN", NetvirtElan.class)
+            .put("ACL", NetvirtAcl.class)
             .put("OFPLUGIN", Ofplugin.class)
             .put("QOS", NetvirtQos.class)
             .build();
@@ -62,6 +66,8 @@ public final class SrmCliUtils {
             .put("VPN-INSTANCE", NetvirtVpnInstance.class)
             .put("ELAN-INSTANCE", NetvirtElanInstance.class)
             .put("QOS-POLICY-INSTANCE", NetvirtQosPolicyInstance.class)
+            .put("ACL-INTERFACE", NetvirtAclInterface.class)
+            .put("ACL-INSTANCE", NetvirtAclInstance.class)
             .build();
 
     /**
