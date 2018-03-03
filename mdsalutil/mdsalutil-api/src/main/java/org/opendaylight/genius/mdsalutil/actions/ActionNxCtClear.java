2fcfe846e81086e4ff0bec613d480b1608f87f6b
Aswin Suryanarayanan <asuryana@redhat.com>
2018-03-03 10:36:14 +0530

    NETVIRT-430 Ping responder on tenant network and FIP-FIP traffic between vm in same n/w in same compute is not working in stateful SG mode.
    Added Ct Clear action
    
    Change-Id: If86203e73a5ec516ff487f3f5c33fbd7dbfd12e1
    Signed-off-by: Aswin Suryanarayanan <asuryana@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/actions/ActionNxCtClear.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright © 2018 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.actions;
+
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nodes.node.table.flow.instructions.instruction.instruction.apply.actions._case.apply.actions.action.action.NxActionCtClearNodesNodeTableFlowApplyActionsCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.ct.clear.grouping.NxCtClearBuilder;
+
+/**
+ * Conntrack Clear action.
+ */
+public class ActionNxCtClear extends ActionInfo {
+
+    public ActionNxCtClear() {
+        this(0);
+    }
+
+    public ActionNxCtClear(int actionKey) {
+        super(actionKey);
+    }
+
+    @Override
+    public Action buildAction() {
+        return buildAction(getActionKey());
+    }
+
+    @Override
+    public Action buildAction(int newActionKey) {
+        return new ActionBuilder().setAction(
+                new NxActionCtClearNodesNodeTableFlowApplyActionsCaseBuilder().setNxCtClear(
+                        new NxCtClearBuilder().build()).build())
+                .setKey(new ActionKey(newActionKey)).build();
+    }
+}
