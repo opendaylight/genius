19603c0eed704c58ae000fa299219bdf4c6d2122
Edw7n <n.edwin.anthony@ericsson.com>
2018-03-06 11:41:17 +0530

    Utils for itm performance enhancements
    Change-Id: Id0400bd56473550ebc49d2968024632fcdf5b265
    Signed-off-by: Edw7n <n.edwin.anthony@ericsson.com>

---
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cache/UnprocessedNodeConnectorCache.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2018 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.itm.cache;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import javax.inject.Singleton;
+import org.opendaylight.genius.itm.utils.NodeConnectorInfo;
+
+@Singleton
+public class UnprocessedNodeConnectorCache {
+
+    private final ConcurrentMap<String, NodeConnectorInfo> unprocessedNodeConnectorMap = new ConcurrentHashMap();
+
+    public void add(String tunnelName, NodeConnectorInfo ncInfo) {
+        unprocessedNodeConnectorMap.put(tunnelName, ncInfo);
+    }
+
+    public NodeConnectorInfo get(String tunnelName) {
+        return unprocessedNodeConnectorMap.get(tunnelName);
+    }
+
+    public void remove(String tunnelName) {
+        unprocessedNodeConnectorMap.remove(tunnelName);
+    }
+}
