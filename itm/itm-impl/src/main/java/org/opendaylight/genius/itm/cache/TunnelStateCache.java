17dd93e463245cd70115e2d24c0a0fabb4b6d907
Edw7n <n.edwin.anthony@ericsson.com>
2018-03-06 11:41:17 +0530

    Utils for itm performance enhancements
    Change-Id: Id0400bd56473550ebc49d2968024632fcdf5b265
    Signed-off-by: Edw7n <n.edwin.anthony@ericsson.com>

--- itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cache/TunnelStateCache.java
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cache/TunnelStateCache.java
@@ -7,15 +7,23 @@
  */
 package org.opendaylight.genius.itm.cache;
 
+import com.google.common.base.Optional;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
+import org.opendaylight.genius.itm.globals.ITMConstants;
+import org.opendaylight.genius.itm.impl.ItmUtils;
 import org.opendaylight.genius.mdsalutil.cache.InstanceIdDataObjectCache;
 import org.opendaylight.infrautils.caches.CacheProvider;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.TepTypeInternal;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.TunnelsState;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.tunnels_state.StateTunnelList;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.tunnels_state.StateTunnelListKey;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Caches StateTunnelList objects.
@@ -25,9 +33,54 @@ import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 @Singleton
 public class TunnelStateCache extends InstanceIdDataObjectCache<StateTunnelList> {
 
+    private static final Logger LOG = LoggerFactory.getLogger(TunnelStateCache.class);
+
+    private final DataBroker dataBroker;
+
     @Inject
     public TunnelStateCache(DataBroker dataBroker, CacheProvider cacheProvider) {
         super(StateTunnelList.class, dataBroker, LogicalDatastoreType.OPERATIONAL,
                 InstanceIdentifier.builder(TunnelsState.class).child(StateTunnelList.class).build(), cacheProvider);
+        this.dataBroker = dataBroker;
+    }
+
+    public boolean isInternalBasedOnState(String tunnelName) {
+        StateTunnelList stateTunnelList = getTunnelFromOperationalDS(tunnelName);
+        return stateTunnelList != null && TepTypeInternal.class.equals(stateTunnelList.getDstInfo().getTepDeviceType());
+    }
+
+    public long getNodeConnectorIdFromInterface(String interfaceName) {
+        StateTunnelList stateTnl = getTunnelFromOperationalDS(interfaceName);
+        if (stateTnl != null) {
+            return Long.parseLong(stateTnl.getPortNumber());
+        } else {
+            return ITMConstants.INVALID_PORT_NO;
+        }
+    }
+
+    public StateTunnelList getTunnelFromOperationalDS(String tunnelName) {
+
+        try {
+            Optional<StateTunnelList> stateTunnelList =
+                    get(getStateTunnelListIdentifier(new StateTunnelListKey(tunnelName)));
+            if (stateTunnelList.isPresent()) {
+                return stateTunnelList.get();
+            }
+        } catch (ReadFailedException exception) {
+            LOG.debug("read failed for tunnel {}, while reading TunnelStateCache", tunnelName);
+        }
+        InstanceIdentifier<StateTunnelList> stateTnlII =
+                ItmUtils.buildStateTunnelListId(new StateTunnelListKey(tunnelName));
+
+        Optional<StateTunnelList> tnlStateOptional =
+                ItmUtils.read(LogicalDatastoreType.OPERATIONAL, stateTnlII, dataBroker);
+        if (!tnlStateOptional.isPresent()) {
+            return null;
+        }
+        return tnlStateOptional.get();
+    }
+
+    private InstanceIdentifier<StateTunnelList> getStateTunnelListIdentifier(StateTunnelListKey key) {
+        return InstanceIdentifier.builder(TunnelsState.class).child(StateTunnelList.class, key).build();
     }
 }
