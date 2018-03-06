ab23f6269392bfb026a5b2c8e5eae9dd12ba5b64
Edw7n <n.edwin.anthony@ericsson.com>
2018-03-06 11:41:17 +0530

    Utils for itm performance enhancements
    Change-Id: Id0400bd56473550ebc49d2968024632fcdf5b265
    Signed-off-by: Edw7n <n.edwin.anthony@ericsson.com>

---
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cache/BfdStateCache.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2018 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.itm.cache;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import javax.inject.Singleton;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.state.Interface;
+
+@Singleton
+public class BfdStateCache {
+
+    private final ConcurrentMap<String, Interface.OperStatus> bfdStateMap = new ConcurrentHashMap<>();
+
+    public void add(String interfaceName, Interface.OperStatus operStatus) {
+        bfdStateMap.put(interfaceName, operStatus);
+    }
+
+    public Interface.OperStatus remove(String interfaceName) {
+        return bfdStateMap.remove(interfaceName);
+    }
+
+    public Interface.OperStatus get(String interfaceName) {
+        return bfdStateMap.get(interfaceName);
+    }
+}
