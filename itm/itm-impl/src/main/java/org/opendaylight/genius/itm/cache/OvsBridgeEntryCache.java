18c6be5dedc247ec5f04a3e2506cc2b56ab61f47
Edw7n <n.edwin.anthony@ericsson.com>
2018-03-06 11:41:17 +0530

    Utils for itm performance enhancements
    Change-Id: Id0400bd56473550ebc49d2968024632fcdf5b265
    Signed-off-by: Edw7n <n.edwin.anthony@ericsson.com>

--- itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cache/OvsBridgeEntryCache.java
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cache/OvsBridgeEntryCache.java
@@ -7,20 +7,29 @@
  */
 package org.opendaylight.genius.itm.cache;
 
+import com.google.common.base.Optional;
 import java.math.BigInteger;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
+import org.opendaylight.genius.itm.impl.ItmUtils;
 import org.opendaylight.genius.mdsalutil.cache.DataObjectCache;
 import org.opendaylight.infrautils.caches.CacheProvider;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.meta.rev171210.BridgeTunnelInfo;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.meta.rev171210.bridge.tunnel.info.OvsBridgeEntry;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.meta.rev171210.bridge.tunnel.info.OvsBridgeEntryKey;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Singleton
 public class OvsBridgeEntryCache extends DataObjectCache<BigInteger, OvsBridgeEntry> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(OvsBridgeRefEntryCache.class);
+
+    private final DataBroker dataBroker;
 
     @Inject
     public OvsBridgeEntryCache(DataBroker dataBroker, CacheProvider cacheProvider) {
@@ -29,5 +38,27 @@ public class OvsBridgeEntryCache extends DataObjectCache<BigInteger, OvsBridgeEn
             (iid, ovsBridgeEntry) -> ovsBridgeEntry.getKey().getDpid(),
             dpId -> InstanceIdentifier.builder(BridgeTunnelInfo.class)
                     .child(OvsBridgeEntry.class, new OvsBridgeEntryKey(dpId)).build());
+        this.dataBroker = dataBroker;
+    }
+
+    public OvsBridgeEntry getOvsBridgeEntryFromConfigDS(BigInteger dpnId) {
+
+        try {
+            Optional<OvsBridgeEntry> bridgeEntry = get(dpnId);
+            if (bridgeEntry.isPresent()) {
+                return bridgeEntry.get();
+            }
+        } catch (ReadFailedException e) {
+            LOG.error("OvsBridgeEntry cache read failed for {}", dpnId);
+        }
+        OvsBridgeEntryKey bridgeEntryKey = new OvsBridgeEntryKey(dpnId);
+        InstanceIdentifier<OvsBridgeEntry> bridgeEntryInstanceIdentifier = getOvsBridgeEntryIdentifier(bridgeEntryKey);
+        LOG.debug("Trying to retrieve bridge entry from config for Id: {}", bridgeEntryInstanceIdentifier);
+        return ItmUtils.read(LogicalDatastoreType.CONFIGURATION, bridgeEntryInstanceIdentifier, dataBroker).orNull();
+    }
+
+    private InstanceIdentifier<OvsBridgeEntry> getOvsBridgeEntryIdentifier(OvsBridgeEntryKey bridgeEntryKey) {
+        return InstanceIdentifier.builder(BridgeTunnelInfo.class).child(OvsBridgeEntry.class, bridgeEntryKey)
+                .build();
     }
 }
