f9eb0db6df4e990a2e0ec41546fabe0799f6f393
Edw7n <n.edwin.anthony@ericsson.com>
2018-03-06 11:41:17 +0530

    Utils for itm performance enhancements
    Change-Id: Id0400bd56473550ebc49d2968024632fcdf5b265
    Signed-off-by: Edw7n <n.edwin.anthony@ericsson.com>

--- itm/itm-api/src/main/java/org/opendaylight/genius/itm/globals/ITMConstants.java
+++ itm/itm-api/src/main/java/org/opendaylight/genius/itm/globals/ITMConstants.java
@@ -64,4 +64,14 @@ public interface ITMConstants {
     String EXT_ID_TEP_PARAM_KEY_TZNAME = "tzname";
     String EXT_ID_TEP_PARAM_KEY_BR_NAME = "br-name";
     String EXT_ID_TEP_PARAM_KEY_OF_TUNNEL = "of-tunnel";
+
+    // ITM DIRECT TUNNELS RELATED CONSTANTS
+    String ITM_CONFIG_ENTITY = "itm_config";
+    int INVALID_PORT_NO = -1;
+    int DEFAULT_FLOW_PRIORITY = 5;
+    int MAC_STRING_LENGTH = 17;
+    String OF_URI_SEPARATOR = ":";
+    String DEAD_BEEF_MAC_PREFIX = "DEADBEEF";
+    String INVALID_MAC = "00:00:00:00:00:00";
+    String MAC_SEPARATOR = ":";
 }
