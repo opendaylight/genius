d031cdb95810a2bd76caa9e598a933911c99773f
David Suarez <david.suarez.fuentes@gmail.com>
2018-03-09 00:33:26 +0100

    Add InstanceIdentifier to method add/update/remove in Genius' listeners
    Some use cases require the Genius' listeners to provide the
    InstanceIdentifier as a parameter in add, update and remove methods.
    
    The change is made on the DataTreeChangeListenerActions interface by
    making the new methods the default ones. The current methods are
    deprecated and they will be removed once all the current implementations
    are converted.
    
    The AbstractDataTreeChangeListener will implement the old methods until
    the migration to the new ones finishes.
    
    Change-Id: I979e5f3e2480f441828f2ee87e917543df6504b0
    Signed-off-by: David Suarez <david.suarez.fuentes@gmail.com>

--- mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/datastoreutils/listeners/DataTreeChangeListenerActions.java
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/datastoreutils/listeners/DataTreeChangeListenerActions.java
@@ -14,6 +14,7 @@ import org.opendaylight.controller.md.sal.binding.api.DataObjectModification;
 import org.opendaylight.controller.md.sal.binding.api.DataTreeModification;
 import org.opendaylight.genius.utils.metrics.DataStoreMetrics;
 import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
 /**
  * Interface to be implemented by classes interested in receiving notifications
@@ -31,13 +32,14 @@ interface DataTreeChangeListenerActions<T extends DataObject> {
      * Default method invoked upon data tree change, in turn it calls the
      * appropriate method (add, update, remove) depending on the type of change.
      *
-     * @param changes collection of changes
+     * @param changes          collection of changes
      * @param dataStoreMetrics data store metrics
      */
     default void onDataTreeChanged(@Nonnull Collection<DataTreeModification<T>> changes,
                                    DataStoreMetrics dataStoreMetrics) {
         // This code is also in DataTreeEventCallbackRegistrarImpl and any changes should be applied there as well
         for (final DataTreeModification<T> dataTreeModification : changes) {
+            final InstanceIdentifier<T> instanceIdentifier = dataTreeModification.getRootPath().getRootIdentifier();
             final DataObjectModification<T> dataObjectModification = dataTreeModification.getRootNode();
             final T dataBefore = dataObjectModification.getDataBefore();
             final T dataAfter = dataObjectModification.getDataAfter();
@@ -47,25 +49,25 @@ interface DataTreeChangeListenerActions<T extends DataObject> {
                     if (dataStoreMetrics != null) {
                         dataStoreMetrics.incrementUpdated();
                     }
-                    update(dataBefore, dataAfter);
+                    update(instanceIdentifier, dataBefore, dataAfter);
                     break;
                 case DELETE:
                     if (dataStoreMetrics != null) {
                         dataStoreMetrics.incrementDeleted();
                     }
-                    remove(dataBefore);
+                    remove(instanceIdentifier, dataBefore);
                     break;
                 case WRITE:
                     if (dataBefore == null) {
                         if (dataStoreMetrics != null) {
                             dataStoreMetrics.incrementAdded();
                         }
-                        add(dataAfter);
+                        add(instanceIdentifier, dataAfter);
                     } else {
                         if (dataStoreMetrics != null) {
                             dataStoreMetrics.incrementUpdated();
                         }
-                        update(dataBefore, dataAfter);
+                        update(instanceIdentifier, dataBefore, dataAfter);
                     }
                     break;
                 default:
@@ -75,18 +77,52 @@ interface DataTreeChangeListenerActions<T extends DataObject> {
     }
 
     /**
-     * Invoked when new data object added.
+     * Invoked when a new data object is added.
+     *
+     * @param instanceIdentifier instance id for this data object
+     * @param newDataObject      newly added object
+     */
+    default void add(@Nonnull InstanceIdentifier<T> instanceIdentifier, @Nonnull T newDataObject) {
+        add(newDataObject);
+    }
+
+    /**
+     * Invoked when a new data object added.
      *
      * @param newDataObject newly added object
      */
+    @Deprecated
     void add(@Nonnull T newDataObject);
+
+    /**
+     * Invoked when the data object has been removed.
+     *
+     * @param instanceIdentifier instance id for this data object
+     * @param removedDataObject  existing object being removed
+     */
+    default void remove(@Nonnull InstanceIdentifier<T> instanceIdentifier, @Nonnull T removedDataObject) {
+        remove(removedDataObject);
+    }
 
     /**
      * Invoked when the data object has been removed.
      *
      * @param removedDataObject existing object being removed
      */
+    @Deprecated
     void remove(@Nonnull T removedDataObject);
+
+    /**
+     * Invoked when there is a change in the data object.
+     *
+     * @param instanceIdentifier instance id for this data object
+     * @param originalDataObject existing object being modified
+     * @param updatedDataObject  modified data object
+     */
+    default void update(@Nonnull InstanceIdentifier<T> instanceIdentifier, @Nonnull T originalDataObject,
+                        @Nonnull T updatedDataObject) {
+        update(originalDataObject, updatedDataObject);
+    }
 
     /**
      * Invoked when there is a change in the data object.
@@ -94,5 +130,6 @@ interface DataTreeChangeListenerActions<T extends DataObject> {
      * @param originalDataObject existing object being modified
      * @param updatedDataObject  modified data object
      */
+    @Deprecated
     void update(@Nonnull T originalDataObject, @Nonnull T updatedDataObject);
 }
