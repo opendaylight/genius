b54aa3bf5532348fbddbb0514d3f52fb42a85ec5
Tom Pantelis <tompantelis@gmail.com>
2018-03-08 07:42:36 -0500

    Add InstanceIdDataObjectCache
    DataObjectCache will be extended to allow for custom key so
    add implementation that keys by InstanceIdentifier.
    
    Change-Id: I16450c794512afde8d1091dc88f43a43ef8639b5
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cache/DPNTEPsInfoCache.java
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cache/DPNTEPsInfoCache.java
@@ -11,7 +11,7 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
-import org.opendaylight.genius.mdsalutil.cache.DataObjectCache;
+import org.opendaylight.genius.mdsalutil.cache.InstanceIdDataObjectCache;
 import org.opendaylight.infrautils.caches.CacheProvider;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.DpnEndpoints;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.dpn.endpoints.DPNTEPsInfo;
@@ -24,7 +24,7 @@ import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
  */
 @SuppressWarnings("checkstyle:AbbreviationAsWordInName")
 @Singleton
-public class DPNTEPsInfoCache extends DataObjectCache<DPNTEPsInfo> {
+public class DPNTEPsInfoCache extends InstanceIdDataObjectCache<DPNTEPsInfo> {
 
     @Inject
     public DPNTEPsInfoCache(DataBroker dataBroker, CacheProvider cacheProvider) {
---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/cache/InstanceIdDataObjectCache.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2018 Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.cache;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.infrautils.caches.CacheProvider;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+/**
+ * A DataObjectCache keyed by InstanceIdentifier.
+ *
+ * @author Thomas Pantelis
+ */
+public class InstanceIdDataObjectCache<V extends DataObject> extends DataObjectCache<V> {
+    public InstanceIdDataObjectCache(Class<V> dataObjectClass, DataBroker dataBroker,
+            LogicalDatastoreType datastoreType, InstanceIdentifier<V> listetenerRegistrationPath,
+            CacheProvider cacheProvider) {
+        super(dataObjectClass, dataBroker, datastoreType, listetenerRegistrationPath, cacheProvider);
+    }
+}
