12a67e36fa71297f56b7e29fca7a9a16f94310a1
Faseela K <faseela.k@ericsson.com>
2018-03-15 17:35:29 +0530

    Re-enable interface-manager l2vlan component tests
    Change-Id: I63d0a93da481003b701c5a1843cece7d50cf51d1
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

---
+++ mdsalutil/mdsalutil-api/src/test/java/org/opendaylight/genius/mdsalutil/interfaces/testutils/FlowAssertTestUtils.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2016, 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsalutil.interfaces.testutils;
+
+import static org.opendaylight.mdsal.binding.testutils.AssertDataObjects.assertEqualBeans;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.Flow;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.FlowBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.flow.InstructionsBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.list.Instruction;
+
+/**
+ * This class is a place holder for various openflow flow/group related test utilities.
+ *
+ * @autor Faseela K
+ */
+public class FlowAssertTestUtils {
+
+    // The problem we're fixing here is that, in MD-SAL binding v1, YANG lists are represented
+    // as Java lists but they don't preserve order (unless they specify “ordered-by user”).
+    // YANG keyed lists in particular are backed by maps, so you can store such a list in the
+    // MD-SAL and get it back in a different order.
+    // When comparing beans involving such lists, we need to sort the lists before comparing
+    // them.
+    // Hence, we rebuild instances of Flow, and sort the affected lists
+    // in the augmentations.
+    public void assertFlowsInAnyOrder(Flow expected, Flow actual) {
+        // Re-create the flows to avoid re-ordering problems
+        assertEqualBeans(rebuildFlow(expected), rebuildFlow(actual));
+    }
+
+    private Flow rebuildFlow(Flow flow) {
+        FlowBuilder flowBuilder = new FlowBuilder(flow);
+        InstructionsBuilder instructionsBuilder = new InstructionsBuilder();
+        if (flow.getInstructions() != null) {
+            ArrayList<Instruction> instructionList = new ArrayList<>(flow.getInstructions().getInstruction());
+            instructionList.sort(Comparator.comparing(o -> o.getKey().toString()));
+            instructionsBuilder.setInstruction(instructionList);
+            flowBuilder.setInstructions(instructionsBuilder.build());
+        }
+        return flowBuilder.build();
+    }
+}
