7705979e041892dc6495cd51081826edc3052a24
Stephen Kitt <skitt@redhat.com>
2018-03-21 16:02:56 +0100

    Flow-based listeners: use top-level transactions
    This changes the listener to use a single transaction for its
    top-level operations.
    
    Change-Id: I739ba6ce11b881331945d77ea2a9d7c3b2120206
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- interfacemanager/interfacemanager-impl/src/main/java/org/opendaylight/genius/interfacemanager/servicebindings/flowbased/state/helpers/FlowBasedEgressServicesStateBindHelper.java
+++ interfacemanager/interfacemanager-impl/src/main/java/org/opendaylight/genius/interfacemanager/servicebindings/flowbased/state/helpers/FlowBasedEgressServicesStateBindHelper.java
@@ -14,6 +14,7 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
 import org.opendaylight.genius.interfacemanager.IfmUtil;
 import org.opendaylight.genius.interfacemanager.commons.InterfaceManagerCommonUtils;
 import org.opendaylight.genius.interfacemanager.servicebindings.flowbased.utilities.FlowBasedServicesUtils;
@@ -39,35 +40,33 @@ public class FlowBasedEgressServicesStateBindHelper extends AbstractFlowBasedSer
     }
 
     @Override
-    public void bindServicesOnInterface(List<ListenableFuture<Void>> futures, List<BoundServices> allServices,
+    public void bindServicesOnInterface(WriteTransaction tx, List<BoundServices> allServices,
                                         Interface ifState) {
         LOG.info("bind all egress services for interface: {}", ifState.getName());
         NodeConnectorId nodeConnectorId = FlowBasedServicesUtils.getNodeConnectorIdFromInterface(ifState);
         BigInteger dpId = IfmUtil.getDpnFromNodeConnectorId(nodeConnectorId);
-        futures.add(getTxRunner().callWithNewWriteOnlyTransactionAndSubmit(tx -> {
-            allServices.sort(Comparator.comparing(BoundServices::getServicePriority));
-            BoundServices highestPriority = allServices.remove(0);
-            short nextServiceIndex = (short) (allServices.size() > 0 ? allServices.get(0).getServicePriority()
-                    : highestPriority.getServicePriority() + 1);
-            org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang
-                    .ietf.interfaces.rev140508.interfaces.Interface iface = interfaceManagerCommonUtils
-                    .getInterfaceFromConfigDS(ifState.getName());
-            FlowBasedServicesUtils.installEgressDispatcherFlows(dpId, highestPriority, ifState.getName(), tx,
-                    ifState.getIfIndex(), NwConstants.DEFAULT_SERVICE_INDEX, nextServiceIndex, iface);
-            BoundServices prev = null;
-            for (BoundServices boundService : allServices) {
-                if (prev != null) {
-                    FlowBasedServicesUtils.installEgressDispatcherFlows(dpId, prev, ifState.getName(), tx,
-                            ifState.getIfIndex(), prev.getServicePriority(), boundService.getServicePriority(), iface);
-                }
-                prev = boundService;
-            }
+        allServices.sort(Comparator.comparing(BoundServices::getServicePriority));
+        BoundServices highestPriority = allServices.remove(0);
+        short nextServiceIndex = (short) (allServices.size() > 0 ? allServices.get(0).getServicePriority()
+                : highestPriority.getServicePriority() + 1);
+        org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang
+                .ietf.interfaces.rev140508.interfaces.Interface iface = interfaceManagerCommonUtils
+                .getInterfaceFromConfigDS(ifState.getName());
+        FlowBasedServicesUtils.installEgressDispatcherFlows(dpId, highestPriority, ifState.getName(), tx,
+                ifState.getIfIndex(), NwConstants.DEFAULT_SERVICE_INDEX, nextServiceIndex, iface);
+        BoundServices prev = null;
+        for (BoundServices boundService : allServices) {
             if (prev != null) {
                 FlowBasedServicesUtils.installEgressDispatcherFlows(dpId, prev, ifState.getName(), tx,
-                        ifState.getIfIndex(), prev.getServicePriority(), (short) (prev.getServicePriority() + 1),
-                        iface);
+                        ifState.getIfIndex(), prev.getServicePriority(), boundService.getServicePriority(), iface);
             }
-        }));
+            prev = boundService;
+        }
+        if (prev != null) {
+            FlowBasedServicesUtils.installEgressDispatcherFlows(dpId, prev, ifState.getName(), tx,
+                    ifState.getIfIndex(), prev.getServicePriority(), (short) (prev.getServicePriority() + 1),
+                    iface);
+        }
     }
 
     @Override
