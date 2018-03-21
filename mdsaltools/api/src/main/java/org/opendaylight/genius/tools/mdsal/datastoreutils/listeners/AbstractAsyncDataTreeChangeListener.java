73ce8d189c0a3b4d6ffa00cbee2038fd3172f8fa
Faseela K <faseela.k@ericsson.com>
2018-03-21 18:13:48 +0530

    WIP - Moving datastore related utils from mdsalutil to new "mdsaltools"
    Change-Id: I7d59273851c51b02fe43afcb1525c94ced1c5088
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

---
+++ mdsaltools/api/src/main/java/org/opendaylight/genius/tools/mdsal/datastoreutils/listeners/AbstractAsyncDataTreeChangeListener.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2017 Ericsson S.A. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.tools.mdsal.datastoreutils.listeners;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeModification;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.infrautils.metrics.MetricProvider;
+import org.opendaylight.yangtools.util.concurrent.SpecialExecutors;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+import java.util.Collection;
+import java.util.concurrent.ExecutorService;
+
+/**
+ * Abstract class providing some common functionality to specific listeners. This listener launches the received
+ * notifications by using an {@link ExecutorService}.
+ *
+ * <p>The {@link ExecutorService} passed to the constructor will depend on the use case. Here we have some examples:
+ *
+ * <p>- If the listener is fast enough and non-blocking: MoreExecutors.directExecutor might be used, or even better,
+ * just use the {@link AbstractSyncDataTreeChangeListener}.
+ *
+ * <p>- If the listener is heavy or could be blocked: use a multi-threaded executor.
+ * We recommend using one of the factory methods in {@link SpecialExecutors}.
+ * You could and probably should share such an Executor among several listeners in your project.
+ *
+ * <p>- If the listener needs to preserve the order of notifications, then (only) use a single thread executor typically
+ * an {@link org.opendaylight.infrautils.utils.concurrent.Executors#newSingleThreadExecutor(String, org.slf4j.Logger)}.
+ *
+ * <p>- If there are multiple listeners: they could even share an Executor as the ones in {@link SpecialExecutors},
+ *
+ * <p>Subclasses are also encouraged to, in addition to passing the ExecutorService for use in
+ * production (by Blueprint wiring) based on above via super(), expose a public constructor letting tests specify
+ * an alternative ExecutorService; this is useful e.g. to inject infrautils' AwaitableExecutorService for testing.
+ *
+ * @param <T> type of the data object the listener is registered to.
+ *
+ * @author David Suárez (david.suarez.fuentes@gmail.com)
+ */
+public abstract class AbstractAsyncDataTreeChangeListener<T extends DataObject> extends
+        AbstractDataTreeChangeListener<T> {
+
+    private final ExecutorService executorService;
+
+    @Inject
+    public AbstractAsyncDataTreeChangeListener(DataBroker dataBroker, DataTreeIdentifier<T> dataTreeIdentifier,
+                                               ExecutorService executorService) {
+        super(dataBroker, dataTreeIdentifier);
+        this.executorService = executorService;
+    }
+
+    @Inject
+    public AbstractAsyncDataTreeChangeListener(DataBroker dataBroker, LogicalDatastoreType datastoreType,
+                                               InstanceIdentifier<T> instanceIdentifier,
+                                               ExecutorService executorService) {
+        super(dataBroker, datastoreType, instanceIdentifier);
+        this.executorService = executorService;
+    }
+
+    @Inject
+    public AbstractAsyncDataTreeChangeListener(DataBroker dataBroker,
+                                               LogicalDatastoreType datastoreType,
+                                               InstanceIdentifier<T> instanceIdentifier,
+                                               ExecutorService executorService,
+                                               MetricProvider metricProvider) {
+        super(dataBroker, datastoreType, instanceIdentifier, metricProvider);
+        this.executorService = executorService;
+    }
+
+    @Override
+    public final void onDataTreeChanged(@Nonnull Collection<DataTreeModification<T>> collection) {
+        executorService.execute(() -> super.onDataTreeChanged(collection,
+                getDataStoreMetrics()));
+    }
+
+    /**
+     * Returns the ExecutorService provided when constructing this instance. If the subclass owns the
+     * ExecutorService, it should be shut down when closing the listener using this getter.
+     *
+     * @return executor service
+     */
+    protected ExecutorService getExecutorService() {
+        return executorService;
+    }
+}
---
+++ mdsaltools/api/src/main/java/org/opendaylight/genius/tools/mdsal/datastoreutils/listeners/AbstractDataTreeChangeListener.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2017 Ericsson S.A. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.tools.mdsal.datastoreutils.listeners;
+
+import javax.annotation.Nonnull;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Inject;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeChangeListener;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.utils.metrics.DataStoreMetrics;
+import org.opendaylight.infrautils.metrics.MetricProvider;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+/**
+ * Abstract class providing some common functionality to abstract listeners. This class is not designed to be
+ * extended by the specific listeners, that's why it is package-private. It provides subclasses with access to the
+ * {@link DataBroker} passed as constructor argument, listener registration/de-registration and a shutdown method to
+ * be implemented if needed by the subclasses (e.g. shutting down services, closing resources, etc.)
+ *
+ * @param <T> type of the data object the listener is registered to.
+ * @author David Suárez (david.suarez.fuentes@gmail.com)
+ */
+abstract class AbstractDataTreeChangeListener<T extends DataObject> implements DataTreeChangeListener<T>,
+        DataTreeChangeListenerActions<T>, AutoCloseable {
+
+    private final DataBroker dataBroker;
+    private final DataTreeIdentifier<T> dataTreeIdentifier;
+    private ListenerRegistration<AbstractDataTreeChangeListener<T>> dataChangeListenerRegistration;
+    private DataStoreMetrics dataStoreMetrics;
+
+    @Inject
+    AbstractDataTreeChangeListener(DataBroker dataBroker, DataTreeIdentifier<T> dataTreeIdentifier) {
+        this.dataBroker = dataBroker;
+        this.dataTreeIdentifier = dataTreeIdentifier;
+    }
+
+    @Inject
+    AbstractDataTreeChangeListener(DataBroker dataBroker, LogicalDatastoreType datastoreType,
+                                   InstanceIdentifier<T> instanceIdentifier) {
+        this(dataBroker, new DataTreeIdentifier<>(datastoreType, instanceIdentifier));
+    }
+
+    @Inject
+    AbstractDataTreeChangeListener(DataBroker dataBroker,
+                                   LogicalDatastoreType datastoreType,
+                                   InstanceIdentifier<T> instanceIdentifier,
+                                   MetricProvider metricProvider) {
+        this(dataBroker, new DataTreeIdentifier<>(datastoreType, instanceIdentifier));
+        this.dataStoreMetrics = new DataStoreMetrics(metricProvider, getClass());
+    }
+
+    @PostConstruct
+    public void register() {
+        this.dataChangeListenerRegistration = dataBroker.registerDataTreeChangeListener(dataTreeIdentifier, this);
+    }
+
+    protected DataBroker getDataBroker() {
+        return dataBroker;
+    }
+
+    protected DataStoreMetrics getDataStoreMetrics() {
+        return dataStoreMetrics;
+    }
+
+    @Override
+    @PreDestroy
+    public void close() {
+        if (dataChangeListenerRegistration != null) {
+            dataChangeListenerRegistration.close();
+        }
+    }
+
+    @Override
+    @Deprecated
+    public void add(@Nonnull T newDataObject) {
+        // TODO: to be removed after all listeners migrated to use the new methods
+    }
+
+    @Override
+    @Deprecated
+    public void remove(@Nonnull T removedDataObject) {
+        // TODO: to be removed after all listeners migrated to use the new methods
+    }
+
+    @Override
+    @Deprecated
+    public void update(@Nonnull T originalDataObject, @Nonnull T updatedDataObject) {
+        // TODO: to be removed after all listeners migrated to use the new methods
+    }
+}
+
