5cf2803e1b034b2ca90f7da06738e82024f82847
Faseela K <faseela.k@ericsson.com>
2018-03-21 18:13:48 +0530

    Moving datastore related utils from mdsalutil to new "genius.tools"
    There are several requirements coming in where users are asking for a
    simple feature with only datastore related utils. Currently all the utils
    are parked in mdsalutil which has even openflowplugin related utils, and is embedded
    in a heavier genius feature. This patch aims to move of these heavily used utils
    to a new module, and add new features to expose the same.
    
    Change-Id: I7d59273851c51b02fe43afcb1525c94ced1c5088
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

--- tools/api/src/main/java/org/opendaylight/genius/tools/mdsal/listener/AbstractSyncDataTreeChangeListener.java
+++ tools/api/src/main/java/org/opendaylight/genius/tools/mdsal/listener/AbstractSyncDataTreeChangeListener.java
@@ -5,27 +5,32 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.genius.datastoreutils.listeners;
+package org.opendaylight.genius.tools.mdsal.listener;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.util.Collection;
+import javax.annotation.Nonnull;
 import javax.inject.Inject;
-
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeModification;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.infrautils.metrics.MetricProvider;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
 /**
- * Abstract class providing some common functionality to specific listeners.
- * @deprecated Please use {@link org.opendaylight.genius.tools.mdsal.listener.AbstractSyncDataTreeChangeListener}
- *     instead of this!
+ * Abstract class providing some common functionality to specific listeners. This should be the most common parent
+ * class for any listener that does not need to be either cluster-aware (see
+ * {@link AbstractClusteredSyncDataTreeChangeListener}) or to process notifications asynchronously
+ * (see {@link AbstractAsyncDataTreeChangeListener}) or even both characteristics (see
+ * {@link AbstractClusteredAsyncDataTreeChangeListener}).
+ *
+ * @param <T> type of the data object the listener is registered to.
+ *
+ * @author David Suárez (david.suarez.fuentes@gmail.com)
  */
-@SuppressFBWarnings("NM_SAME_SIMPLE_NAME_AS_SUPERCLASS")
-@Deprecated
 public abstract class AbstractSyncDataTreeChangeListener<T extends DataObject> extends
-        org.opendaylight.genius.tools.mdsal.listener.AbstractSyncDataTreeChangeListener<T> {
+        AbstractDataTreeChangeListener<T> {
 
     @Inject
     public AbstractSyncDataTreeChangeListener(DataBroker dataBroker, DataTreeIdentifier<T> dataTreeIdentifier) {
@@ -43,5 +48,10 @@ public abstract class AbstractSyncDataTreeChangeListener<T extends DataObject> e
                                               InstanceIdentifier<T> instanceIdentifier,
                                               MetricProvider metricProvider) {
         super(dataBroker, datastoreType, instanceIdentifier, metricProvider);
+    }
+
+    @Override
+    public final void onDataTreeChanged(@Nonnull Collection<DataTreeModification<T>> collection) {
+        super.onDataTreeChanged(collection, getDataStoreMetrics());
     }
 }
