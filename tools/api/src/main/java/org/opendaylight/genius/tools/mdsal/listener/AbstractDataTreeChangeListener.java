feeebf99e1f6943a8e3b6802743952b8a49eff9f
Faseela K <faseela.k@ericsson.com>
2018-03-21 18:13:48 +0530

    Moving datastore related utils from mdsalutil to new "genius.tools"
    There are several requirements coming in where users are asking for a
    simple feature with only datastore related utils. Currently all the utils
    are parked in mdsalutil which has even openflowplugin related utils, and is embedded
    in a heavier genius feature. This patch aims to move of these heavily used utils
    to a new module, and add new features to expose the same.
    
    Change-Id: I7d59273851c51b02fe43afcb1525c94ced1c5088
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

--- tools/api/src/main/java/org/opendaylight/genius/tools/mdsal/listener/AbstractDataTreeChangeListener.java
+++ tools/api/src/main/java/org/opendaylight/genius/tools/mdsal/listener/AbstractDataTreeChangeListener.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.genius.datastoreutils.listeners;
+package org.opendaylight.genius.tools.mdsal.listener;
 
 import javax.annotation.Nonnull;
 import javax.annotation.PostConstruct;
@@ -15,7 +15,7 @@ import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.DataTreeChangeListener;
 import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
-import org.opendaylight.genius.utils.metrics.DataStoreMetrics;
+import org.opendaylight.genius.tools.mdsal.metrics.DataStoreMetrics;
 import org.opendaylight.infrautils.metrics.MetricProvider;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.yang.binding.DataObject;
