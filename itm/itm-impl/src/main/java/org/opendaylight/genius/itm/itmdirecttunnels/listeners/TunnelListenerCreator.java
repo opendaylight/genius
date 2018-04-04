20e69b5a90f433066b6ef9fb678845764f9729d4
Edw7n <n.edwin.anthony@ericsson.com>
2018-04-04 19:41:17 +0530

    Support for tunnel creation directly by ITM
    Implemented creation of tunnels directly by ITM when the switch connects.
    Conditional creation of listener if the itm-direct-tunnel flag is enabled.
    Removed unused constants from ITMConstants.
    Fixed some issues while testing
    
    Change-Id: Ib5bef52ca85f7589057fadc2614e97de14e0109a
    Signed-off-by: Edw7n <n.edwin.anthony@ericsson.com>

---
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/itmdirecttunnels/listeners/TunnelListenerCreator.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2018 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.itm.itmdirecttunnels.listeners;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.genius.interfacemanager.interfaces.IInterfaceManager;
+import org.opendaylight.genius.itm.cache.DPNTEPsInfoCache;
+import org.opendaylight.genius.itm.cache.DpnTepStateCache;
+import org.opendaylight.genius.itm.cache.OvsBridgeEntryCache;
+import org.opendaylight.genius.itm.cache.UnprocessedNodeConnectorCache;
+import org.opendaylight.genius.itm.itmdirecttunnels.renderer.ovs.utilities.DirectTunnelUtils;
+import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
+import org.opendaylight.genius.utils.clustering.EntityOwnershipUtils;
+import org.opendaylight.infrautils.jobcoordinator.JobCoordinator;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.IdManagerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+public class TunnelListenerCreator implements AutoCloseable {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TunnelListenerCreator.class);
+
+    private final TunnelTopologyStateListener tunnelTopologyStateListener;
+
+    @Inject
+    public TunnelListenerCreator(final DataBroker dataBroker,
+                                 final JobCoordinator coordinator,
+                                 final EntityOwnershipUtils entityOwnershipUtils,
+                                 final IdManagerService idManager,
+                                 final IMdsalApiManager mdsalApiManager,
+                                 final IInterfaceManager interfaceManager,
+                                 final DirectTunnelUtils directTunnelUtils,
+                                 final DpnTepStateCache dpnTepStateCache,
+                                 final DPNTEPsInfoCache dpntePsInfoCache,
+                                 final OvsBridgeEntryCache ovsBridgeEntryCache,
+                                 final UnprocessedNodeConnectorCache unprocessedNodeConnectorCache) {
+
+        if (interfaceManager.isItmDirectTunnelsEnabled()) {
+            LOG.trace("ITM Direct Tunnels is enabled. Initializing the listeners");
+            this.tunnelTopologyStateListener = new TunnelTopologyStateListener(dataBroker, coordinator,
+                    entityOwnershipUtils, idManager, mdsalApiManager, directTunnelUtils, dpnTepStateCache,
+                    dpntePsInfoCache, ovsBridgeEntryCache, unprocessedNodeConnectorCache);
+        } else {
+            LOG.trace("ITM Direct Tunnels is disabled. Listeners are not registered");
+            this.tunnelTopologyStateListener = null;
+        }
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (tunnelTopologyStateListener != null) {
+            this.tunnelTopologyStateListener.close();
+        }
+    }
+}
