17843472e75a0f0cbcce5ae58e3ba2ed3fa4a968
Faseela K <faseela.k@ericsson.com>
2018-04-13 12:01:28 +0530

    Findbugs violation in master
    Change-Id: I6d5e905119dfa3f57b9b6a0ce2f61f16fb924e4a
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

--- mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/utils/batching/ResourceBatchingManager.java
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/utils/batching/ResourceBatchingManager.java
@@ -15,6 +15,7 @@ import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.google.common.util.concurrent.SettableFuture;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -27,6 +28,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
@@ -327,6 +329,7 @@ public class ResourceBatchingManager implements AutoCloseable {
         }
 
         @SuppressWarnings("unchecked")
+        @SuppressFBWarnings("NP_NONNULL_PARAM_VIOLATION")
         public void process() {
             LOG.trace("Picked up 3 size {} of resourceType {}", actResourceList.size(), resourceType);
             Pair<BlockingQueue<ActionableResource>, ResourceHandler> resMapper =
