b46c0d176ea927237d044dcecbe6151f4d0deb8c
Edw7n <n.edwin.anthony@ericsson.com>
2018-04-25 09:04:21 +0530

    Adding API support for ELAN
    Support for fetching Interface information from tunnelName
    
    Change-Id: Ibc067c3c05b02fc30cc6bd0e20040457744c3cd4
    Signed-off-by: Edw7n <n.edwin.anthony@ericsson.com>

--- itm/itm-impl/src/main/java/org/opendaylight/genius/itm/impl/ItmProvider.java
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/impl/ItmProvider.java
@@ -24,6 +24,7 @@ import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
 import org.opendaylight.genius.itm.api.IITMProvider;
+import org.opendaylight.genius.itm.cache.DpnTepStateCache;
 import org.opendaylight.genius.itm.cli.TepCommandHelper;
 import org.opendaylight.genius.itm.cli.TepException;
 import org.opendaylight.genius.itm.globals.ITMConstants;
@@ -42,6 +43,7 @@ import org.opendaylight.mdsal.eos.binding.api.EntityOwnershipCandidateRegistrati
 import org.opendaylight.mdsal.eos.binding.api.EntityOwnershipService;
 import org.opendaylight.mdsal.eos.common.api.CandidateAlreadyRegisteredException;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.Interface;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.CreateIdPoolInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.CreateIdPoolInputBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.IdManagerService;
@@ -79,6 +81,7 @@ public class ItmProvider implements AutoCloseable, IITMProvider /*,ItmStateServi
     static short flag = 0;
     private final TunnelMonitoringConfig tunnelMonitoringConfig;
     private EntityOwnershipCandidateRegistration registryCandidate;
+    private final DpnTepStateCache dpnTepStateCache;
 
     @Inject
     public ItmProvider(DataBroker dataBroker,
@@ -93,7 +96,8 @@ public class ItmProvider implements AutoCloseable, IITMProvider /*,ItmStateServi
                        VtepConfigSchemaListener vtepConfigSchemaListener,
                        OvsdbNodeListener ovsdbNodeListener,
                        TunnelMonitoringConfig tunnelMonitoringConfig,
-                       EntityOwnershipService entityOwnershipService) {
+                       EntityOwnershipService entityOwnershipService,
+                       DpnTepStateCache dpnTepStateCache) {
         LOG.info("ItmProvider Before register MBean");
         this.dataBroker = dataBroker;
         this.idManager = idManagerService;
@@ -108,6 +112,7 @@ public class ItmProvider implements AutoCloseable, IITMProvider /*,ItmStateServi
         this.ovsdbChangeListener = ovsdbNodeListener;
         this.tunnelMonitoringConfig = tunnelMonitoringConfig;
         this.entityOwnershipService = entityOwnershipService;
+        this.dpnTepStateCache = dpnTepStateCache;
         ITMBatchingUtils.registerWithBatchManager(this.dataBroker);
     }
 
@@ -348,5 +353,10 @@ public class ItmProvider implements AutoCloseable, IITMProvider /*,ItmStateServi
         Pattern pattern = Pattern.compile(PTRN);
         Matcher matcher = pattern.matcher(ip);
         return matcher.matches();
+    }
+
+    @Override
+    public Interface getInterface(String tunnelName) {
+        return dpnTepStateCache.getInterfaceFromCache(tunnelName);
     }
 }
