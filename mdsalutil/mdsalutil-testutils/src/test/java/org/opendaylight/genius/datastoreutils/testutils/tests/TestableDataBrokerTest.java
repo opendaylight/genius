9f54b3a8a69349f492ce04057063666a293345cf
Michael Vorburger <vorburger@redhat.com>
2018-04-30 13:35:39 +0200

    introduce TestableDataBroker (with TestableDataBrokerTest)
    JIRA: GENIUS-122
    Change-Id: Ibebecdd2b387cc33079f2c669199b6f590866e8a
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ mdsalutil/mdsalutil-testutils/src/test/java/org/opendaylight/genius/datastoreutils/testutils/tests/TestableDataBrokerTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2018 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.datastoreutils.testutils.tests;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.opendaylight.infrautils.testutils.Asserts.assertThrows;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.junit.Test;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeChangeListener;
+import org.opendaylight.genius.datastoreutils.testutils.TestableDataBroker;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+
+/**
+ * Unit test for the {@link TestableDataBroker}.
+ *
+ * @author Michael Vorburger.ch
+ */
+public class TestableDataBrokerTest {
+
+    @Test
+    public void testListener() {
+        AtomicBoolean hasChanged = new AtomicBoolean(false);
+        TestableDataBroker tdb = TestableDataBroker.newInstance();
+        tdb.registerDataTreeChangeListener(null, (DataTreeChangeListener<DataObject>) changes -> hasChanged.set(true));
+        assertThat(hasChanged.get()).isFalse();
+        tdb.fireDataTreeChangeListener();
+        assertThat(hasChanged.get()).isTrue();
+    }
+
+    @Test
+    public void testListenerRegistration() {
+        AtomicBoolean hasChanged = new AtomicBoolean(false);
+        TestableDataBroker tdb = TestableDataBroker.newInstance();
+        tdb.registerDataTreeChangeListener(null, (DataTreeChangeListener<DataObject>) changes -> hasChanged.set(true))
+                .close();
+        tdb.fireDataTreeChangeListener();
+        assertThat(hasChanged.get()).isFalse();
+        tdb.registerDataTreeChangeListener(null, (DataTreeChangeListener<DataObject>) changes -> hasChanged.set(true));
+        tdb.fireDataTreeChangeListener();
+        assertThat(hasChanged.get()).isTrue();
+    }
+
+    @Test
+    public void testSingleListenerRegistration() {
+        TestableDataBroker tdb = TestableDataBroker.newInstance();
+        tdb.registerDataTreeChangeListener(null, changes -> { });
+        assertThrows(IllegalStateException.class, () -> tdb.registerDataTreeChangeListener(null, changes -> { }));
+    }
+}
