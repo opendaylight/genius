a34c39e46b965451c3f13eb3478a66300bb2c71a
David Suarez <david.suarez.fuentes@gmail.com>
2018-05-04 00:50:34 +0200

    Migrate InterfaceConfigListener to use the new listeners
    - Code simplification by using lambdas
    - Remove unused services injected
    - Other minor changes
    
    Change-Id: Ie60556f42b553618c13205191e1ac3e77ddf860d
    Signed-off-by: David Suarez <david.suarez.fuentes@gmail.com>

--- interfacemanager/interfacemanager-impl/src/main/java/org/opendaylight/genius/interfacemanager/listeners/InterfaceConfigListener.java
+++ interfacemanager/interfacemanager-impl/src/main/java/org/opendaylight/genius/interfacemanager/listeners/InterfaceConfigListener.java
@@ -8,16 +8,11 @@
 
 package org.opendaylight.genius.interfacemanager.listeners;
 
-import com.google.common.util.concurrent.ListenableFuture;
-
-import java.util.List;
-import java.util.concurrent.Callable;
+import javax.annotation.Nonnull;
 import javax.inject.Inject;
 import javax.inject.Singleton;
-
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
-import org.opendaylight.genius.datastoreutils.AsyncClusteredDataTreeChangeListenerBase;
 import org.opendaylight.genius.interfacemanager.IfmConstants;
 import org.opendaylight.genius.interfacemanager.InterfacemgrProvider;
 import org.opendaylight.genius.interfacemanager.commons.InterfaceManagerCommonUtils;
@@ -26,15 +21,13 @@ import org.opendaylight.genius.interfacemanager.renderer.ovs.confighelpers.OvsIn
 import org.opendaylight.genius.interfacemanager.renderer.ovs.confighelpers.OvsInterfaceConfigRemoveHelper;
 import org.opendaylight.genius.interfacemanager.renderer.ovs.confighelpers.OvsInterfaceConfigUpdateHelper;
 import org.opendaylight.genius.interfacemanager.renderer.ovs.utilities.SouthboundUtils;
-import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
 import org.opendaylight.genius.srm.RecoverableListener;
 import org.opendaylight.genius.srm.ServiceRecoveryRegistry;
+import org.opendaylight.genius.tools.mdsal.listener.AbstractClusteredSyncDataTreeChangeListener;
 import org.opendaylight.genius.utils.clustering.EntityOwnershipUtils;
 import org.opendaylight.infrautils.jobcoordinator.JobCoordinator;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.Interfaces;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.Interface;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.alivenessmonitor.rev160411.AlivenessMonitorService;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.IdManagerService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rev160406.IfTunnel;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rev160406.ParentRefs;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
@@ -47,14 +40,13 @@ import org.slf4j.LoggerFactory;
  */
 @Singleton
 public class InterfaceConfigListener
-        extends AsyncClusteredDataTreeChangeListenerBase<Interface, InterfaceConfigListener>
+        extends AbstractClusteredSyncDataTreeChangeListener<Interface>
         implements RecoverableListener {
 
     private static final Logger LOG = LoggerFactory.getLogger(InterfaceConfigListener.class);
 
     private final InterfacemgrProvider interfaceMgrProvider;
     private final EntityOwnershipUtils entityOwnershipUtils;
-    private final DataBroker dataBroker;
     private final JobCoordinator coordinator;
     private final InterfaceManagerCommonUtils interfaceManagerCommonUtils;
     private final OvsInterfaceConfigRemoveHelper ovsInterfaceConfigRemoveHelper;
@@ -62,19 +54,18 @@ public class InterfaceConfigListener
     private final OvsInterfaceConfigUpdateHelper ovsInterfaceConfigUpdateHelper;
 
     @Inject
-    public InterfaceConfigListener(final DataBroker dataBroker, final IdManagerService idManager,
-                                   final IMdsalApiManager mdsalApiManager,
-                                   final InterfacemgrProvider interfaceMgrProvider,
-                                   final AlivenessMonitorService alivenessMonitorService,
-                                   final EntityOwnershipUtils entityOwnershipUtils,
-                                   final JobCoordinator coordinator,
-                                   final InterfaceManagerCommonUtils interfaceManagerCommonUtils,
-                                   final OvsInterfaceConfigRemoveHelper ovsInterfaceConfigRemoveHelper,
-                                   final OvsInterfaceConfigAddHelper ovsInterfaceConfigAddHelper,
-                                   final OvsInterfaceConfigUpdateHelper ovsInterfaceConfigUpdateHelper,
-                                   final InterfaceServiceRecoveryHandler interfaceServiceRecoveryHandler,
-                                   final ServiceRecoveryRegistry serviceRecoveryRegistry) {
-        super(Interface.class, InterfaceConfigListener.class);
+    public InterfaceConfigListener(DataBroker dataBroker,
+                                   InterfacemgrProvider interfaceMgrProvider,
+                                   EntityOwnershipUtils entityOwnershipUtils,
+                                   JobCoordinator coordinator,
+                                   InterfaceManagerCommonUtils interfaceManagerCommonUtils,
+                                   OvsInterfaceConfigRemoveHelper ovsInterfaceConfigRemoveHelper,
+                                   OvsInterfaceConfigAddHelper ovsInterfaceConfigAddHelper,
+                                   OvsInterfaceConfigUpdateHelper ovsInterfaceConfigUpdateHelper,
+                                   InterfaceServiceRecoveryHandler interfaceServiceRecoveryHandler,
+                                   ServiceRecoveryRegistry serviceRecoveryRegistry) {
+        super(dataBroker, LogicalDatastoreType.CONFIGURATION,
+              InstanceIdentifier.create(Interfaces.class).child(Interface.class));
         this.interfaceMgrProvider = interfaceMgrProvider;
         this.entityOwnershipUtils = entityOwnershipUtils;
         this.coordinator = coordinator;
@@ -82,7 +73,6 @@ public class InterfaceConfigListener
         this.ovsInterfaceConfigRemoveHelper = ovsInterfaceConfigRemoveHelper;
         this.ovsInterfaceConfigAddHelper = ovsInterfaceConfigAddHelper;
         this.ovsInterfaceConfigUpdateHelper = ovsInterfaceConfigUpdateHelper;
-        this.dataBroker = dataBroker;
         registerListener();
         serviceRecoveryRegistry.addRecoverableListener(interfaceServiceRecoveryHandler.buildServiceRegistryKey(),
                 this);
@@ -90,17 +80,92 @@ public class InterfaceConfigListener
 
     @Override
     public void registerListener() {
-        this.registerListener(LogicalDatastoreType.CONFIGURATION, dataBroker);
+        super.register();
     }
 
     @Override
-    protected InstanceIdentifier<Interface> getWildCardPath() {
-        return InstanceIdentifier.create(Interfaces.class).child(Interface.class);
+    public void deregisterListener() {
+        close();
     }
 
     @Override
-    protected InterfaceConfigListener getDataTreeChangeListener() {
-        return InterfaceConfigListener.this;
+    public void remove(@Nonnull InstanceIdentifier<Interface> instanceIdentifier, @Nonnull Interface removedInterface) {
+        interfaceManagerCommonUtils.removeFromInterfaceCache(removedInterface);
+
+        if (!entityOwnershipUtils.isEntityOwner(IfmConstants.INTERFACE_CONFIG_ENTITY,
+                IfmConstants.INTERFACE_CONFIG_ENTITY)) {
+            return;
+        }
+        LOG.debug("Received Interface Remove Event: {}, {}", instanceIdentifier, removedInterface);
+        ParentRefs parentRefs = removedInterface.getAugmentation(ParentRefs.class);
+        if (parentRefs == null
+                || parentRefs.getDatapathNodeIdentifier() == null && parentRefs.getParentInterface() == null) {
+            LOG.debug("parent refs not specified for {}", removedInterface.getName());
+            return;
+        }
+        boolean isTunnelInterface = InterfaceManagerCommonUtils.isTunnelInterface(removedInterface);
+        String synchronizationKey = isTunnelInterface ? parentRefs.getDatapathNodeIdentifier().toString()
+                : parentRefs.getParentInterface();
+        coordinator.enqueueJob(synchronizationKey,
+            () -> ovsInterfaceConfigRemoveHelper.removeConfiguration(removedInterface, parentRefs),
+            IfmConstants.JOB_MAX_RETRIES);
+    }
+
+    @Override
+    public void update(@Nonnull InstanceIdentifier<Interface> instanceIdentifier, @Nonnull Interface originalInterface,
+                       @Nonnull Interface updatedInterface) {
+        interfaceManagerCommonUtils.addInterfaceToCache(updatedInterface);
+
+        if (!entityOwnershipUtils.isEntityOwner(IfmConstants.INTERFACE_CONFIG_ENTITY,
+                IfmConstants.INTERFACE_CONFIG_ENTITY)) {
+            return;
+        }
+        LOG.debug("Received Interface Update Event: {}, {}, {}", instanceIdentifier, originalInterface,
+                  updatedInterface);
+        ParentRefs parentRefs = updatedInterface.getAugmentation(ParentRefs.class);
+        if (parentRefs == null || parentRefs.getParentInterface() == null
+                && !InterfaceManagerCommonUtils.isTunnelInterface(updatedInterface)) {
+            // If parentRefs are missing, try to find a matching parent and
+            // update - this will trigger another DCN
+            updateInterfaceParentRefs(updatedInterface);
+        }
+
+        if (parentRefs == null
+                || parentRefs.getDatapathNodeIdentifier() == null && parentRefs.getParentInterface() == null) {
+            LOG.debug("parent refs not specified for {}, or parentRefs {} missing DPN/parentInterface",
+                    updatedInterface.getName(), parentRefs);
+            return;
+        }
+        String synchronizationKey = getSynchronizationKey(updatedInterface, parentRefs);
+        coordinator.enqueueJob(synchronizationKey, () -> ovsInterfaceConfigUpdateHelper
+                .updateConfiguration(updatedInterface, originalInterface), IfmConstants.JOB_MAX_RETRIES);
+    }
+
+    @Override
+    public void add(@Nonnull InstanceIdentifier<Interface> instanceIdentifier, @Nonnull Interface newInterface) {
+        interfaceManagerCommonUtils.addInterfaceToCache(newInterface);
+
+        if (!entityOwnershipUtils.isEntityOwner(IfmConstants.INTERFACE_CONFIG_ENTITY,
+                IfmConstants.INTERFACE_CONFIG_ENTITY)) {
+            return;
+        }
+        LOG.debug("Received Interface Add Event: {}, {}", instanceIdentifier, newInterface);
+        ParentRefs parentRefs = newInterface.getAugmentation(ParentRefs.class);
+        if (parentRefs == null || parentRefs.getParentInterface() == null) {
+            // If parentRefs are missing, try to find a matching parent and
+            // update - this will trigger another DCN
+            updateInterfaceParentRefs(newInterface);
+        }
+
+        if (parentRefs == null
+                || parentRefs.getDatapathNodeIdentifier() == null && parentRefs.getParentInterface() == null) {
+            LOG.debug("parent refs not specified for {}", newInterface.getName());
+            return;
+        }
+        String synchronizationKey = getSynchronizationKey(newInterface, parentRefs);
+        coordinator.enqueueJob(synchronizationKey,
+            () -> ovsInterfaceConfigAddHelper.addConfiguration(parentRefs, newInterface),
+            IfmConstants.JOB_MAX_RETRIES);
     }
 
     private void updateInterfaceParentRefs(Interface iface) {
@@ -112,89 +177,8 @@ public class InterfaceConfigListener
             return;
         }
         LOG.debug("retrieved parent ref {} for interface {} from southbound, updating parentRef in datastore",
-                parentRefName, ifName);
+                  parentRefName, ifName);
         interfaceMgrProvider.updateInterfaceParentRef(ifName, parentRefName, false);
-        return;
-    }
-
-    @Override
-    protected void remove(InstanceIdentifier<Interface> key, Interface interfaceOld) {
-        interfaceManagerCommonUtils.removeFromInterfaceCache(interfaceOld);
-
-        if (!entityOwnershipUtils.isEntityOwner(IfmConstants.INTERFACE_CONFIG_ENTITY,
-                IfmConstants.INTERFACE_CONFIG_ENTITY)) {
-            return;
-        }
-        LOG.debug("Received Interface Remove Event: {}, {}", key, interfaceOld);
-        ParentRefs parentRefs = interfaceOld.getAugmentation(ParentRefs.class);
-        if (parentRefs == null
-                || parentRefs.getDatapathNodeIdentifier() == null && parentRefs.getParentInterface() == null) {
-            LOG.debug("parent refs not specified for {}", interfaceOld.getName());
-            return;
-        }
-        boolean isTunnelInterface = InterfaceManagerCommonUtils.isTunnelInterface(interfaceOld);
-        RendererConfigRemoveWorker configWorker = new RendererConfigRemoveWorker(key, interfaceOld,
-                interfaceOld.getName(), parentRefs);
-        String synchronizationKey = isTunnelInterface ? parentRefs.getDatapathNodeIdentifier().toString()
-                : parentRefs.getParentInterface();
-        coordinator.enqueueJob(synchronizationKey, configWorker, IfmConstants.JOB_MAX_RETRIES);
-    }
-
-    @Override
-    protected void update(InstanceIdentifier<Interface> key, Interface interfaceOld, Interface interfaceNew) {
-        interfaceManagerCommonUtils.addInterfaceToCache(interfaceNew);
-
-        if (!entityOwnershipUtils.isEntityOwner(IfmConstants.INTERFACE_CONFIG_ENTITY,
-                IfmConstants.INTERFACE_CONFIG_ENTITY)) {
-            return;
-        }
-        LOG.debug("Received Interface Update Event: {}, {}, {}", key, interfaceOld, interfaceNew);
-        ParentRefs parentRefs = interfaceNew.getAugmentation(ParentRefs.class);
-        if (parentRefs == null || parentRefs.getParentInterface() == null
-                && !InterfaceManagerCommonUtils.isTunnelInterface(interfaceNew)) {
-            // If parentRefs are missing, try to find a matching parent and
-            // update - this will trigger another DCN
-            updateInterfaceParentRefs(interfaceNew);
-        }
-
-        if (parentRefs == null
-                || parentRefs.getDatapathNodeIdentifier() == null && parentRefs.getParentInterface() == null) {
-            LOG.debug("parent refs not specified for {}, or parentRefs {} missing DPN/parentInterface",
-                    interfaceNew.getName(), parentRefs);
-            return;
-        }
-        RendererConfigUpdateWorker configWorker = new RendererConfigUpdateWorker(key, interfaceOld, interfaceNew,
-                interfaceNew.getName());
-        String synchronizationKey = getSynchronizationKey(interfaceNew, parentRefs);
-        coordinator.enqueueJob(synchronizationKey, configWorker, IfmConstants.JOB_MAX_RETRIES);
-    }
-
-    @Override
-    protected void add(InstanceIdentifier<Interface> key, Interface interfaceNew) {
-        interfaceManagerCommonUtils.addInterfaceToCache(interfaceNew);
-
-        if (!entityOwnershipUtils.isEntityOwner(IfmConstants.INTERFACE_CONFIG_ENTITY,
-                IfmConstants.INTERFACE_CONFIG_ENTITY)) {
-            return;
-        }
-        LOG.debug("Received Interface Add Event: {}, {}", key, interfaceNew);
-        ParentRefs parentRefs = interfaceNew.getAugmentation(ParentRefs.class);
-        if (parentRefs == null || parentRefs.getParentInterface() == null) {
-            // If parentRefs are missing, try to find a matching parent and
-            // update - this will trigger another DCN
-            updateInterfaceParentRefs(interfaceNew);
-        }
-
-        if (parentRefs == null
-                || parentRefs.getDatapathNodeIdentifier() == null && parentRefs.getParentInterface() == null) {
-            LOG.debug("parent refs not specified for {}", interfaceNew.getName());
-            return;
-        }
-
-        RendererConfigAddWorker configWorker = new RendererConfigAddWorker(key, interfaceNew, parentRefs,
-                interfaceNew.getName());
-        String synchronizationKey = getSynchronizationKey(interfaceNew, parentRefs);
-        coordinator.enqueueJob(synchronizationKey, configWorker, IfmConstants.JOB_MAX_RETRIES);
     }
 
     private String getSynchronizationKey(Interface theInterface, ParentRefs theParentRefs) {
@@ -206,91 +190,6 @@ public class InterfaceConfigListener
             return theInterface.getName();
         } else {
             return theParentRefs.getParentInterface();
-        }
-    }
-
-    private class RendererConfigAddWorker implements Callable<List<ListenableFuture<Void>>> {
-        InstanceIdentifier<Interface> key;
-        Interface interfaceNew;
-        String portName;
-        ParentRefs parentRefs;
-
-        RendererConfigAddWorker(InstanceIdentifier<Interface> key, Interface interfaceNew,
-                                ParentRefs parentRefs,
-                                String portName) {
-            this.key = key;
-            this.interfaceNew = interfaceNew;
-            this.portName = portName;
-            this.parentRefs = parentRefs;
-        }
-
-        @Override
-        public List<ListenableFuture<Void>> call() {
-            // If another renderer(for eg: another OpenFlow based switch) needs to be supported, check
-            // can be performed here to call the respective helpers.
-            return ovsInterfaceConfigAddHelper.addConfiguration(parentRefs, interfaceNew);
-        }
-
-        @Override
-        public String toString() {
-            return "RendererConfigAddWorker{" + "key=" + key + ", interfaceNew=" + interfaceNew + ", portName='"
-                    + portName + '\'' + '}';
-        }
-    }
-
-    private class RendererConfigUpdateWorker implements Callable {
-        InstanceIdentifier<Interface> key;
-        Interface interfaceOld;
-        Interface interfaceNew;
-        String portNameNew;
-
-        RendererConfigUpdateWorker(InstanceIdentifier<Interface> key, Interface interfaceOld,
-                                   Interface interfaceNew, String portNameNew) {
-            this.key = key;
-            this.interfaceOld = interfaceOld;
-            this.interfaceNew = interfaceNew;
-            this.portNameNew = portNameNew;
-        }
-
-        @Override
-        public List<ListenableFuture<Void>> call() {
-            // If another renderer(for eg: another OpenFlow based switch) needs to be supported, check
-            // can be performed here to call the respective helpers.
-            return ovsInterfaceConfigUpdateHelper.updateConfiguration(interfaceNew, interfaceOld);
-        }
-
-        @Override
-        public String toString() {
-            return "RendererConfigUpdateWorker{" + "key=" + key + ", interfaceOld=" + interfaceOld + ", interfaceNew="
-                    + interfaceNew + ", portNameNew='" + portNameNew + '\'' + '}';
-        }
-    }
-
-    private class RendererConfigRemoveWorker implements Callable {
-        InstanceIdentifier<Interface> key;
-        Interface interfaceOld;
-        String portName;
-        ParentRefs parentRefs;
-
-        RendererConfigRemoveWorker(InstanceIdentifier<Interface> key, Interface interfaceOld, String portName,
-                                   ParentRefs parentRefs) {
-            this.key = key;
-            this.interfaceOld = interfaceOld;
-            this.portName = portName;
-            this.parentRefs = parentRefs;
-        }
-
-        @Override
-        public List<ListenableFuture<Void>> call() {
-            // If another renderer(for eg: HWVTEP) needs to be supported, check
-            // can be performed here to call the respective helpers.
-            return ovsInterfaceConfigRemoveHelper.removeConfiguration(interfaceOld, parentRefs);
-        }
-
-        @Override
-        public String toString() {
-            return "RendererConfigRemoveWorker{" + "key=" + key + ", interfaceOld=" + interfaceOld + ", portName='"
-                    + portName + '\'' + '}';
         }
     }
 }
