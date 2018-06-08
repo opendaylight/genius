38d56c0f8e9c6594b6f9f01cf96c893f441794b3
Michael Vorburger <vorburger@redhat.com>
2018-06-08 14:46:13 +0200

    look up MBean in DatastoreServiceStatusProvider constructor
    instead of every time in getServiceDescriptor()
    
    JIRA: GENIUS-138
    Change-Id: I8590242f1e9c20a690a544803265be96b9c82369
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- mdsalutil/mdsalutil-impl/src/main/java/org/opendaylight/genius/mdsalutil/diagstatus/internal/DatastoreServiceStatusProvider.java
+++ mdsalutil/mdsalutil-impl/src/main/java/org/opendaylight/genius/mdsalutil/diagstatus/internal/DatastoreServiceStatusProvider.java
@@ -7,10 +7,13 @@
  */
 package org.opendaylight.genius.mdsalutil.diagstatus.internal;
 
+import java.util.Objects;
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import javax.management.MalformedObjectNameException;
 import org.opendaylight.controller.cluster.datastore.shardmanager.ShardManagerInfoMBean;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.infrautils.diagstatus.DiagStatusService;
 import org.opendaylight.infrautils.diagstatus.MBeanUtils;
 import org.opendaylight.infrautils.diagstatus.ServiceDescriptor;
@@ -30,11 +33,25 @@ public class DatastoreServiceStatusProvider implements ServiceStatusProvider {
     private static final String DATASTORE_SERVICE_NAME = "DATASTORE";
 
     private final DiagStatusService diagStatusService;
+    private final ShardManagerInfoMBean operationalShardManagerInfo;
+    private final ShardManagerInfoMBean configShardManagerInfo;
 
     @Inject
-    public DatastoreServiceStatusProvider(@OsgiService DiagStatusService diagStatusService) {
+    public DatastoreServiceStatusProvider(@OsgiService DiagStatusService diagStatusService,
+            @OsgiService DataBroker dataBroker) throws MalformedObjectNameException {
         this.diagStatusService = diagStatusService;
         diagStatusService.register(DATASTORE_SERVICE_NAME);
+
+        // We don't really require a DataBroker, but we do this to enforce a dependency
+        // which will make sure that this happens after the datastore is up and running.
+        Objects.requireNonNull(dataBroker);
+        operationalShardManagerInfo = MBeanUtils.getMBean("org.opendaylight.controller:type="
+                + "DistributedOperationalDatastore,Category=ShardManager,name=shard-manager-operational",
+                ShardManagerInfoMBean.class);
+        configShardManagerInfo = MBeanUtils.getMBean("org.opendaylight.controller:type="
+                + "DistributedConfigDatastore,Category=ShardManager,name=shard-manager-config",
+                ShardManagerInfoMBean.class);
+
         diagStatusService.report(getServiceDescriptor());
     }
 
@@ -48,14 +65,7 @@ public class DatastoreServiceStatusProvider implements ServiceStatusProvider {
     @SuppressWarnings("checkstyle:IllegalCatch")
     public ServiceDescriptor getServiceDescriptor() {
         try {
-            ShardManagerInfoMBean operationalShardManagerInfo = MBeanUtils.getMBean("org.opendaylight.controller:type="
-                    + "DistributedOperationalDatastore,Category=ShardManager,name=shard-manager-operational",
-                    ShardManagerInfoMBean.class);
             boolean operSyncStatusValue = operationalShardManagerInfo.getSyncStatus();
-
-            ShardManagerInfoMBean configShardManagerInfo = MBeanUtils.getMBean("org.opendaylight.controller:type="
-                    + "DistributedConfigDatastore,Category=ShardManager,name=shard-manager-config",
-                    ShardManagerInfoMBean.class);
             boolean configSyncStatusValue = configShardManagerInfo.getSyncStatus();
 
             String statusDesc;
