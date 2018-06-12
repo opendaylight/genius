d3518de9b309cb033c5bdd1a0a1009bcbf497c82
Edw7n <n.edwin.anthony@ericsson.com>
2018-06-12 11:22:49 +0530

    Fixing ItmRpcService to return tunnelName - requires TestModule, as IITMProvider will be injected in elanItmUtils
    Change-Id: I5d4c22682200ed63d3edb88d0769d7622328813b
    Signed-off-by: Edw7n <n.edwin.anthony@ericsson.com>

---
+++ commons/testutils/src/main/java/org/opendaylight/genius/testutils/TestItmProvider.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright © 2018 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.testutils;
+
+import static org.opendaylight.yangtools.testutils.mockito.MoreAnswers.realOrException;
+
+import org.opendaylight.genius.itm.api.IITMProvider;
+import org.mockito.Mockito;
+
+public abstract class TestItmProvider implements IITMProvider{
+
+    public static TestItmProvider newInstance() {
+        return Mockito.mock(TestItmProvider.class, realOrException());
+    }
+}
