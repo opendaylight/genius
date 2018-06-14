e11f2a43b838c7c879ede257a66074b54add577e
Faseela K <faseela.k@ericsson.com>
2018-06-14 05:55:08 +0530

    Add IfIndex-Interface Cache
    In an incident of heavy packet-in messages(30K events per minute),
    each packet-in was making an RPC call to getinterfaceNameForIfIndex(),
    which was internally doing a datastore read. In a particular case, all these
    READS ended up being remote in a 3 node cluster, which was causing trouble
    for the cluster.
    
    Change-Id: I4f7f3adb2ed2c076a954cde25bd7186cb581d63c
    Signed-off-by: Faseela K <faseela.k@ericsson.com>
    (cherry picked from commit f0750a964ed4fb27531c290f0dc5b1c24607f254)

---
+++ interfacemanager/interfacemanager-impl/src/main/java/org/opendaylight/genius/interfacemanager/listeners/IfIndexInterfaceCache.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2017, 2018 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.genius.interfacemanager.listeners;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.mdsalutil.cache.DataObjectCache;
+import org.opendaylight.infrautils.caches.CacheProvider;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.meta.rev160406.IfIndexesInterfaceMap;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.meta.rev160406._if.indexes._interface.map.IfIndexInterface;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.meta.rev160406._if.indexes._interface.map.IfIndexInterfaceKey;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+@Singleton
+public class IfIndexInterfaceCache extends DataObjectCache<Integer, IfIndexInterface> {
+
+    @Inject
+    public IfIndexInterfaceCache(DataBroker dataBroker, CacheProvider cacheProvider) {
+        super(IfIndexInterface.class, dataBroker, LogicalDatastoreType.OPERATIONAL,
+                InstanceIdentifier.builder(IfIndexesInterfaceMap.class).child(IfIndexInterface.class).build(),
+                cacheProvider, (iid, ifIndexInterface) -> ifIndexInterface.getKey().getIfIndex(),
+            ifIndex -> InstanceIdentifier.builder(IfIndexesInterfaceMap.class)
+                .child(IfIndexInterface.class, new IfIndexInterfaceKey(ifIndex)).build());
+    }
+}
