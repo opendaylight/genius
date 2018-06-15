821d69c173779b027d7c59d83ab760567bbc10ae
Stephen Kitt <skitt@redhat.com>
2018-06-15 13:58:21 +0200

    Add datastore-constrained transactions
    This introduces new transactions which constrain the datastore being
    used, and thus no longer require it to be specified with each
    operation.
    
    Since this involves adding new methods to ManagedNewTransactionRunner,
    this patch also switches to FluentFuture (for the new methods only).
    
    JIRA: NETVIRT-1321
    Change-Id: I4f5f46f3a3eb144827f131d31cf4b8f776b6be57
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/infra/DatastoreReadTransaction.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright © 2018 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.infra;
+
+import com.google.common.base.Optional;
+import com.google.common.util.concurrent.CheckedFuture;
+import org.opendaylight.controller.md.sal.binding.api.ReadTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+public interface DatastoreReadTransaction extends ReadTransaction {
+    @Override
+    @Deprecated
+    <T extends DataObject> CheckedFuture<Optional<T>, ReadFailedException> read(
+            LogicalDatastoreType store, InstanceIdentifier<T> path);
+
+    <T extends DataObject> CheckedFuture<Optional<T>, ReadFailedException> read(InstanceIdentifier<T> path);
+}
---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/infra/DatastoreReadWriteTransaction.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright © 2018 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.infra;
+
+public interface DatastoreReadWriteTransaction<D extends Datastore>
+        extends DatastoreReadTransaction, DatastoreWriteTransaction<D> {
+}
