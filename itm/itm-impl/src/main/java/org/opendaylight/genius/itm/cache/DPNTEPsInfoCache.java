7a137534bff76b97abb1dbb49958c3ad0f8546d8
Tarun Thakur <tarun.t@altencalsoftlabs.com>
2018-06-19 17:40:05 +0530

    GENIUS-204: TEP is part of old and new TZ when tz is updated after  del-manager
    Fail scenario::
    1. TZB was present and TEPs were part of it.
    2. ovsdb connection is removed by running delete-manager command on
    switch
    3. Change transport-zone of TEPs from TZB to TZC
    4. ovsdb connection is set again by running set-manager command on
    switch
    5. TEPs go under teps-not-hosted since TZC is not yet present. It is
    noted here that TEPs are still part of TZB in ITM config DS.
    6. TZC is created from northbound
    7. TEPs are moved from teps-not-hosted to TZC.
    8. In the end, TEPs are part of TZB and TZC both. This is issue.
    
    Fix provided::
    When TEPs add notification come again with updated TZC, then
    check if same TEP (key: dpid) is already present in any other TZ.
    If present, then remove TEP from old TZ and then add TEP into newly
    configured TZ.
    
    Change-Id: If5ad27860b68cae3e9441834d5936d484b90e269
    Signed-off-by: Tarun Thakur <tarun.t@altencalsoftlabs.com>

--- itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cache/DPNTEPsInfoCache.java
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cache/DPNTEPsInfoCache.java
@@ -12,6 +12,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
@@ -20,6 +22,8 @@ import org.opendaylight.genius.mdsalutil.cache.InstanceIdDataObjectCache;
 import org.opendaylight.infrautils.caches.CacheProvider;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.DpnEndpoints;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.dpn.endpoints.DPNTEPsInfo;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.dpn.endpoints.dpn.teps.info.TunnelEndPoints;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.dpn.endpoints.dpn.teps.info.tunnel.end.points.TzMembership;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
 /**
@@ -30,11 +34,26 @@ import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 @SuppressWarnings("checkstyle:AbbreviationAsWordInName")
 @Singleton
 public class DPNTEPsInfoCache extends InstanceIdDataObjectCache<DPNTEPsInfo> {
+    private final ConcurrentMap<BigInteger, List<TzMembership>> tepTransportZonesMap = new ConcurrentHashMap<>();
 
     @Inject
     public DPNTEPsInfoCache(DataBroker dataBroker, CacheProvider cacheProvider) {
         super(DPNTEPsInfo.class, dataBroker, LogicalDatastoreType.CONFIGURATION,
                 InstanceIdentifier.builder(DpnEndpoints.class).child(DPNTEPsInfo.class).build(), cacheProvider);
+    }
+
+    @Override
+    protected void added(InstanceIdentifier<DPNTEPsInfo> path, DPNTEPsInfo dpnTepsInfo) {
+        for (TunnelEndPoints tep : dpnTepsInfo.getTunnelEndPoints()) {
+            tepTransportZonesMap.put(dpnTepsInfo.getDPNID(), tep.getTzMembership());
+            // since there can be only one tunnel-endpoint, so come out of loop.
+            break;
+        }
+    }
+
+    @Override
+    protected void removed(InstanceIdentifier<DPNTEPsInfo> path, DPNTEPsInfo dpnTepsInfo) {
+        tepTransportZonesMap.remove(dpnTepsInfo.getDPNID());
     }
 
     public List<DPNTEPsInfo> getDPNTepListFromDPNId(List<BigInteger> dpnIds) {
@@ -53,5 +72,9 @@ public class DPNTEPsInfoCache extends InstanceIdDataObjectCache<DPNTEPsInfo> {
     public Optional<DPNTEPsInfo> getDPNTepFromDPNId(BigInteger dpnId) {
         Collection<DPNTEPsInfo> meshedDpnList = this.getAllPresent() ;
         return meshedDpnList.stream().filter(info -> dpnId.equals(info.getDPNID())).findFirst();
+    }
+
+    public List<TzMembership> getTransportZoneOfVtep(BigInteger dpnId) {
+        return tepTransportZonesMap.get(dpnId);
     }
 }
--- itm/itm-impl/src/test/java/org/opendaylight/genius/itm/tests/ItmTepAutoConfigTest.java
+++ itm/itm-impl/src/test/java/org/opendaylight/genius/itm/tests/ItmTepAutoConfigTest.java
@@ -24,13 +24,17 @@ import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
 import org.opendaylight.genius.datastoreutils.SingleTransactionDataBroker;
 import org.opendaylight.genius.datastoreutils.testutils.JobCoordinatorTestModule;
+import org.opendaylight.genius.itm.cache.DPNTEPsInfoCache;
 import org.opendaylight.genius.itm.cache.UnprocessedTunnelsStateCache;
 import org.opendaylight.genius.itm.cli.TepCommandHelper;
+import org.opendaylight.genius.itm.confighelpers.OvsdbTepAddConfigHelper;
 import org.opendaylight.genius.itm.globals.ITMConstants;
 import org.opendaylight.genius.itm.impl.ItmUtils;
 import org.opendaylight.genius.itm.tests.xtend.ExpectedDefTransportZoneObjects;
 import org.opendaylight.genius.itm.tests.xtend.ExpectedTepNotHostedTransportZoneObjects;
 import org.opendaylight.genius.itm.tests.xtend.ExpectedTransportZoneObjects;
+import org.opendaylight.infrautils.caches.baseimpl.internal.CacheManagersRegistryImpl;
+import org.opendaylight.infrautils.caches.guava.internal.GuavaCacheProvider;
 import org.opendaylight.infrautils.caches.testutils.CacheModule;
 import org.opendaylight.infrautils.inject.guice.testutils.GuiceRule;
 import org.opendaylight.infrautils.testutils.LogRule;
@@ -70,6 +74,7 @@ public class ItmTepAutoConfigTest {
 
     @Inject DataBroker dataBroker;
     private UnprocessedTunnelsStateCache unprocessedTunnelsStateCache;
+    private OvsdbTepAddConfigHelper ovsdbTepAddConfigHelper;
 
     @Before
     public void start() throws InterruptedException {
@@ -79,6 +84,8 @@ public class ItmTepAutoConfigTest {
         transportZoneList.add(transportZone);
         transportZones = new TransportZonesBuilder().setTransportZone(transportZoneList).build();
         unprocessedTunnelsStateCache = new UnprocessedTunnelsStateCache();
+        ovsdbTepAddConfigHelper = new OvsdbTepAddConfigHelper(new DPNTEPsInfoCache(dataBroker,
+                new GuavaCacheProvider(new CacheManagersRegistryImpl())));
     }
 
     // Common method created for code-reuse
@@ -267,7 +274,7 @@ public class ItmTepAutoConfigTest {
         CheckedFuture<Void, TransactionCommitFailedException> futures =
             ItmTepAutoConfigTestUtil.addTep(ItmTestConstants.DEF_TZ_TEP_IP,
             ItmTestConstants.DEF_BR_DPID,
-            ITMConstants.DEFAULT_TRANSPORT_ZONE, false, dataBroker);
+            ITMConstants.DEFAULT_TRANSPORT_ZONE, false, dataBroker, ovsdbTepAddConfigHelper);
         futures.get();
 
         InstanceIdentifier<Vteps> vtepPath = ItmTepAutoConfigTestUtil.getTepIid(subnetMaskObj,
@@ -305,7 +312,8 @@ public class ItmTepAutoConfigTest {
         // add tep
         CheckedFuture<Void, TransactionCommitFailedException> futures =
             ItmTepAutoConfigTestUtil.addTep(ItmTestConstants.NB_TZ_TEP_IP,
-                ItmTestConstants.DEF_BR_DPID, ItmTestConstants.TZ_NAME, false, dataBroker);
+                ItmTestConstants.DEF_BR_DPID, ItmTestConstants.TZ_NAME, false, dataBroker,
+                    ovsdbTepAddConfigHelper);
         futures.get();
 
         IpPrefix subnetMaskObj = ItmUtils.getDummySubnet();
@@ -596,7 +604,8 @@ public class ItmTepAutoConfigTest {
         // add into not hosted list
         CheckedFuture<Void, TransactionCommitFailedException> future =
             ItmTepAutoConfigTestUtil.addTep(ItmTestConstants.NOT_HOSTED_TZ_TEP_IP,
-                ItmTestConstants.NOT_HOSTED_TZ_TEPDPN_ID, ItmTestConstants.NOT_HOSTED_TZ_NAME, false, dataBroker);
+                ItmTestConstants.NOT_HOSTED_TZ_TEPDPN_ID, ItmTestConstants.NOT_HOSTED_TZ_NAME, false,
+                    dataBroker, ovsdbTepAddConfigHelper);
         future.get();
         InstanceIdentifier<TepsInNotHostedTransportZone> notHostedPath =
             ItmTepAutoConfigTestUtil.getTepNotHostedInTZIid(ItmTestConstants.NOT_HOSTED_TZ_NAME);
@@ -612,7 +621,8 @@ public class ItmTepAutoConfigTest {
         // add into not hosted list
         CheckedFuture<Void, TransactionCommitFailedException> future =
             ItmTepAutoConfigTestUtil.addTep(ItmTestConstants.NOT_HOSTED_TZ_TEP_IP,
-                ItmTestConstants.NOT_HOSTED_TZ_TEPDPN_ID, ItmTestConstants.NOT_HOSTED_TZ_NAME, false, dataBroker);
+                ItmTestConstants.NOT_HOSTED_TZ_TEPDPN_ID, ItmTestConstants.NOT_HOSTED_TZ_NAME, false,
+                    dataBroker, ovsdbTepAddConfigHelper);
         future.get();
         InstanceIdentifier<TepsInNotHostedTransportZone> notHostedPath =
             ItmTepAutoConfigTestUtil.getTepNotHostedInTZIid(ItmTestConstants.NOT_HOSTED_TZ_NAME);
@@ -638,7 +648,7 @@ public class ItmTepAutoConfigTest {
         CheckedFuture<Void, TransactionCommitFailedException> future =
             ItmTepAutoConfigTestUtil.addTep(ItmTestConstants.NOT_HOSTED_TZ_TEP_IP,
                 ItmTestConstants.NOT_HOSTED_TZ_TEPDPN_ID, ItmTestConstants.NOT_HOSTED_TZ_NAME, false,
-                dataBroker);
+                dataBroker, ovsdbTepAddConfigHelper);
         future.get();
         InstanceIdentifier<TepsInNotHostedTransportZone> notHostedPath =
             ItmTepAutoConfigTestUtil.getTepNotHostedInTZIid(ItmTestConstants.NOT_HOSTED_TZ_NAME);
