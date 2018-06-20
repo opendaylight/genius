8f8d36d6d0caf31b67a1f405fcc2e70299624b19
Michael Vorburger <vorburger@redhat.com>
2018-06-20 19:42:35 +0200

    make new Datastore type a class instead of an interface
    with a private constructor, just to prevent any users
    from doing new Datastore() { } by accident or confusion.
    
    also include a simple unit test for its conversion methods.
    
    Change-Id: Ifa0b8d8ba81a9599d4e68cf519a5ccdaa88852d4
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ mdsalutil/mdsalutil-testutils/src/test/java/org/opendaylight/genius/infra/tests/DatastoreTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2018 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.infra.tests;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.Test;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.infra.Datastore;
+import org.opendaylight.infrautils.testutils.Asserts;
+
+public class DatastoreTest {
+
+    @Test
+    public void testDatastore() {
+        assertThat(Datastore.toType(Datastore.CONFIGURATION)).isEqualTo(LogicalDatastoreType.CONFIGURATION);
+        assertThat(Datastore.toType(Datastore.OPERATIONAL)).isEqualTo(LogicalDatastoreType.OPERATIONAL);
+        Asserts.assertThrows(NullPointerException.class, () -> Datastore.toType(null));
+
+        assertThat(Datastore.toClass(LogicalDatastoreType.CONFIGURATION)).isEqualTo(Datastore.CONFIGURATION);
+        assertThat(Datastore.toClass(LogicalDatastoreType.OPERATIONAL)).isEqualTo(Datastore.OPERATIONAL);
+        Asserts.assertThrows(NullPointerException.class, () -> Datastore.toClass(null));
+    }
+
+}
