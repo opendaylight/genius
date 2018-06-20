f82ef8e349593fef97a1570fb5be70ad7fa1d448
Stephen Kitt <skitt@redhat.com>
2018-06-20 17:11:02 +0200

    Add datastore-constrained transactions
    This introduces new transactions which constrain the datastore being
    used, and thus no longer require it to be specified with each
    operation.
    
    Since this involves adding new methods to ManagedNewTransactionRunner,
    this patch also switches to FluentFuture (for the new methods only).
    
    JIRA: NETVIRT-1321
    Change-Id: I4f5f46f3a3eb144827f131d31cf4b8f776b6be57
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/infra/TypedReadWriteTransaction.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright © 2018 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.infra;
+
+/**
+ * Read-write transaction which is specific to a single logical datastore (configuration or operational). Designed
+ * for use with {@link ManagedNewTransactionRunner} (it doesn’t support explicit cancel or commit operations).
+ *
+ * @param <D> The logical datastore handled by the transaction.
+ * @see org.opendaylight.controller.md.sal.binding.api.ReadWriteTransaction
+ */
+public interface TypedReadWriteTransaction<D extends Datastore>
+        extends TypedReadTransaction<D>, TypedWriteTransaction<D> {
+}
