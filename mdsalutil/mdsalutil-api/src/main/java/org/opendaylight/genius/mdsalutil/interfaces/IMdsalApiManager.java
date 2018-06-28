1668a44348e92d0cf95dd7e6b1fc08b1b7eec770
Stephen Kitt <skitt@redhat.com>
2018-06-28 15:23:21 +0200

    mdsalutil datastore-constrained migration, part 1
    This adds constrained methods to IMdsalApiManager and HwvtepUtils, in
    particular to allow their use in NetVirt.
    
    Change-Id: Ifeb6a43dd263583e2158ddf4b0b82f9e9a0597fd
    JIRA: GENIUS-168
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/interfaces/IMdsalApiManager.java
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/interfaces/IMdsalApiManager.java
@@ -15,20 +15,42 @@ import java.util.List;
 
 import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
+import org.opendaylight.genius.infra.Datastore.Configuration;
+import org.opendaylight.genius.infra.TypedReadWriteTransaction;
+import org.opendaylight.genius.infra.TypedWriteTransaction;
 import org.opendaylight.genius.mdsalutil.ActionInfo;
 import org.opendaylight.genius.mdsalutil.FlowEntity;
 import org.opendaylight.genius.mdsalutil.GroupEntity;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.Flow;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.FlowKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.group.types.rev131018.group.buckets.Bucket;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.group.types.rev131018.groups.Group;
 
 public interface IMdsalApiManager {
 
+    /**
+     * Adds a flow.
+     *
+     * @deprecated Use {@link #addFlow(TypedWriteTransaction, FlowEntity)}.
+     */
+    @Deprecated
     CheckedFuture<Void,TransactionCommitFailedException> installFlow(FlowEntity flowEntity);
 
+    /**
+     * Adds a flow.
+     *
+     * @deprecated Use {@link #addFlow(TypedWriteTransaction, BigInteger, Flow)}.
+     */
+    @Deprecated
     CheckedFuture<Void,TransactionCommitFailedException> installFlow(BigInteger dpId, Flow flowEntity);
 
+    /**
+     * Adds a flow.
+     *
+     * @deprecated Use {@link #addFlow(TypedWriteTransaction, FlowEntity)}.
+     */
+    @Deprecated
     CheckedFuture<Void,TransactionCommitFailedException> installFlow(BigInteger dpId, FlowEntity flowEntity);
 
     /**
@@ -39,7 +61,9 @@ public interface IMdsalApiManager {
      *            Flow being added
      * @param tx
      *            batched transaction
+     * @deprecated Use {@link #addFlow(TypedWriteTransaction, FlowEntity)}.
      */
+    @Deprecated
     void addFlowToTx(FlowEntity flowEntity, WriteTransaction tx);
 
     /**
@@ -52,24 +76,128 @@ public interface IMdsalApiManager {
      *            Flow being added
      * @param tx
      *            batched transaction
+     * @deprecated Use {@link #addFlow(TypedWriteTransaction, BigInteger, Flow)}.
      */
+    @Deprecated
     void addFlowToTx(BigInteger dpId, Flow flow, WriteTransaction tx);
 
+    /**
+     * Adds the given flow.
+     *
+     * @param tx The transaction to use.
+     * @param flowEntity The flow entity.
+     */
+    void addFlow(TypedWriteTransaction<Configuration> tx, FlowEntity flowEntity);
+
+    /**
+     * Adds the given flow.
+     *
+     * @param tx The transaction to use.
+     * @param dpId The DPN identifier.
+     * @param flow The flow.
+     */
+    void addFlow(TypedWriteTransaction<Configuration> tx, BigInteger dpId, Flow flow);
+
+    /**
+     * Adds a flow.
+     *
+     * @deprecated Use {@link #addFlow(TypedWriteTransaction, FlowEntity)}.
+     */
+    @Deprecated
     void batchedAddFlow(BigInteger dpId, FlowEntity flowEntity);
 
+    /**
+     * Adds a flow.
+     *
+     * @deprecated Use {@link #addFlow(TypedWriteTransaction, BigInteger, Flow)}.
+     */
+    @Deprecated
     void batchedAddFlow(BigInteger dpId, Flow flow);
 
+    /**
+     * Removes a flow.
+     *
+     * @deprecated Use {@link #removeFlow(TypedReadWriteTransaction, FlowEntity)}.
+     */
+    @Deprecated
     void batchedRemoveFlow(BigInteger dpId, FlowEntity flowEntity);
 
+    /**
+     * Removes a flow.
+     *
+     * @deprecated Use {@link #removeFlow(TypedReadWriteTransaction, BigInteger, Flow)}.
+     */
+    @Deprecated
     void batchedRemoveFlow(BigInteger dpId, Flow flow);
 
+    /**
+     * Removes a flow.
+     *
+     * @deprecated Use {@link #removeFlow(TypedReadWriteTransaction, BigInteger, String, short)}.
+     */
+    @Deprecated
     ListenableFuture<Void> removeFlow(BigInteger dpId, short tableId, FlowId flowId);
 
+    /**
+     * Removes a flow.
+     *
+     * @deprecated Use {@link #removeFlow(TypedReadWriteTransaction, FlowEntity)}.
+     */
+    @Deprecated
     CheckedFuture<Void,TransactionCommitFailedException> removeFlow(FlowEntity flowEntity);
 
+    /**
+     * Removes a flow.
+     *
+     * @deprecated Use {@link #removeFlow(TypedReadWriteTransaction, BigInteger, Flow)}.
+     */
+    @Deprecated
     CheckedFuture<Void,TransactionCommitFailedException> removeFlow(BigInteger dpId, Flow flowEntity);
 
+    /**
+     * Removes a flow.
+     *
+     * @deprecated Use {@link #removeFlow(TypedReadWriteTransaction, FlowEntity)}.
+     */
+    @Deprecated
     CheckedFuture<Void,TransactionCommitFailedException> removeFlow(BigInteger dpId, FlowEntity flowEntity);
+
+    /**
+     * Removes the given flow.
+     *
+     * @param tx The transaction to use.
+     * @param dpId The DPN identifier.
+     * @param flow The flow.
+     */
+    void removeFlow(TypedReadWriteTransaction<Configuration> tx, BigInteger dpId, Flow flow);
+
+    /**
+     * Removes the given flow.
+     *
+     * @param tx The transaction to use.
+     * @param flowEntity The flow entity.
+     */
+    void removeFlow(TypedReadWriteTransaction<Configuration> tx, FlowEntity flowEntity);
+
+    /**
+     * Removes the given flow.
+     *
+     * @param tx The transaction to use.
+     * @param dpId The DPN identifier.
+     * @param flowKey The flow key.
+     * @param tableId The table identifier.
+     */
+    void removeFlow(TypedReadWriteTransaction<Configuration> tx, BigInteger dpId, FlowKey flowKey, short tableId);
+
+    /**
+     * Removes the given flow.
+     *
+     * @param tx The transaction to use.
+     * @param dpId The DPN identifier.
+     * @param flowId The flow identifier.
+     * @param tableId The table identifier.
+     */
+    void removeFlow(TypedReadWriteTransaction<Configuration> tx, BigInteger dpId, String flowId, short tableId);
 
     /**
      * Remove a Flow using batched transaction.
@@ -81,7 +209,9 @@ public interface IMdsalApiManager {
      *            Flow being removed
      * @param tx
      *            batched transaction
+     * @deprecated Use {@link #removeFlow(TypedReadWriteTransaction, BigInteger, Flow)}.
      */
+    @Deprecated
     void removeFlowToTx(BigInteger dpId, Flow flow, WriteTransaction tx);
 
     /**
@@ -92,9 +222,17 @@ public interface IMdsalApiManager {
      *            Flow being removed
      * @param tx
      *            batched transaction
+     * @deprecated Use {@link #removeFlow(TypedReadWriteTransaction, BigInteger, Flow)}.
      */
+    @Deprecated
     void removeFlowToTx(FlowEntity flowEntity, WriteTransaction tx);
 
+    /**
+     * Adds a group.
+     *
+     * @deprecated Use {@link #addGroup(TypedWriteTransaction, GroupEntity)}.
+     */
+    @Deprecated
     void installGroup(GroupEntity groupEntity);
 
     /**
@@ -105,7 +243,9 @@ public interface IMdsalApiManager {
      *            group being added
      * @param tx
      *            batched transaction
+     * @deprecated Use {@link #addGroup(TypedWriteTransaction, GroupEntity)}.
      */
+    @Deprecated
     void addGroupToTx(GroupEntity groupEntity, WriteTransaction tx);
 
     /**
@@ -118,15 +258,35 @@ public interface IMdsalApiManager {
      *            group being added
      * @param tx
      *            batched transaction
+     * @deprecated Use {@link #addGroup(TypedWriteTransaction, BigInteger, Group)}.
      */
+    @Deprecated
     void addGroupToTx(BigInteger dpId, Group group, WriteTransaction tx);
 
+    /**
+     * Adds the given group using the given transaction.
+     *
+     * @param tx The transaction to use.
+     * @param groupEntity The group to add.
+     */
+    void addGroup(TypedWriteTransaction<Configuration> tx, GroupEntity groupEntity);
+
+    /**
+     * Adds the given group using the given transaction.
+     *
+     * @param tx The transaction to use.
+     * @param dpId The DPN identifier.
+     * @param group The group to add.
+     */
+    void addGroup(TypedWriteTransaction<Configuration> tx, BigInteger dpId, Group group);
+
+    @Deprecated
     void modifyGroup(GroupEntity groupEntity);
 
     /**
      * Remove a group.
      *
-     * @deprecated Use {@link #removeGroup(BigInteger, long)}
+     * @deprecated Use {@link #removeGroup(TypedReadWriteTransaction, GroupEntity)}
      */
     @Deprecated
     void removeGroup(GroupEntity groupEntity);
@@ -136,7 +296,9 @@ public interface IMdsalApiManager {
      *
      * @param dpnId The DP id.
      * @param groupId The group id.
+     * @deprecated Use {@link #removeGroup(TypedReadWriteTransaction, BigInteger, long)}.
      */
+    @Deprecated
     void removeGroup(BigInteger dpnId, long groupId);
 
     /**
@@ -145,8 +307,37 @@ public interface IMdsalApiManager {
      * @param dpnId The DP id.
      * @param groupId The group id.
      * @param tx The transaction.
+     * @deprecated Use {@link #removeGroup(TypedReadWriteTransaction, BigInteger, long)}.
      */
+    @Deprecated
     void removeGroup(BigInteger dpnId, long groupId, WriteTransaction tx);
+
+    /**
+     * Remove a group using the given transaction.
+     *
+     * @param tx The transaction to use.
+     * @param groupEntity The group to remove.
+     * @deprecated Use {@link #removeGroup(TypedReadWriteTransaction, GroupEntity)}.
+     */
+    void removeGroup(TypedReadWriteTransaction<Configuration> tx, GroupEntity groupEntity);
+
+    /**
+     * Remove a group using the given transaction.
+     *
+     * @param tx The transaction to use.
+     * @param dpId The DPN identifier.
+     * @param group The group to remove.
+     */
+    void removeGroup(TypedReadWriteTransaction<Configuration> tx, BigInteger dpId, Group group);
+
+    /**
+     * Remove a group using the given transaction.
+     *
+     * @param tx The transaction to use.
+     * @param dpId The DPN identifier.
+     * @param groupId The group identifier of the group to remove.
+     */
+    void removeGroup(TypedReadWriteTransaction<Configuration> tx, BigInteger dpId, long groupId);
 
     /**
      * Remove a Group using batched transaction
@@ -157,7 +348,7 @@ public interface IMdsalApiManager {
      * @param tx
      *            batched transaction
      *
-     * @deprecated Use {@link #removeGroup(BigInteger, long, WriteTransaction)}
+     * @deprecated Use {@link #removeGroup(TypedReadWriteTransaction, GroupEntity)}
      */
     @Deprecated
     void removeGroupToTx(GroupEntity groupEntity, WriteTransaction tx);
@@ -173,7 +364,7 @@ public interface IMdsalApiManager {
      * @param tx
      *            batched transaction
      *
-     * @deprecated Use {@link #removeGroup(BigInteger, long, WriteTransaction)}
+     * @deprecated Use {@link #removeGroup(TypedReadWriteTransaction, BigInteger, Group)}
      */
     @Deprecated
     void removeGroupToTx(BigInteger dpId, Group group, WriteTransaction tx);
@@ -187,8 +378,10 @@ public interface IMdsalApiManager {
      *            OF group id
      * @return true if group exists and false otherwise
      */
+    @Deprecated
     boolean groupExists(BigInteger dpId, long groupId);
 
+    @Deprecated
     void sendPacketOut(BigInteger dpnId, int groupId, byte[] payload);
 
     /**
@@ -199,61 +392,97 @@ public interface IMdsalApiManager {
     @Deprecated
     void sendPacketOutWithActions(BigInteger dpnId, long groupId, byte[] payload, List<ActionInfo> actionInfos);
 
+    @Deprecated
     void sendPacketOutWithActions(BigInteger dpnId, byte[] payload, List<ActionInfo> actionInfos);
 
+    @Deprecated
     void sendARPPacketOutWithActions(BigInteger dpnId, byte[] payload, List<ActionInfo> actionInfo);
 
     /**
      * API to remove the flow on Data Plane Node synchronously. It internally waits for
      * Flow Change Notification to confirm flow delete request is being sent with-in delayTime.
      *
-     * @deprecated Use {@link #syncRemoveFlow(FlowEntity)}.
+     * @deprecated Use {@link #removeFlow(TypedReadWriteTransaction, FlowEntity)}.
      */
     @Deprecated
     void syncRemoveFlow(FlowEntity flowEntity, long delayTime);
 
+    /**
+     * Removes a flow.
+     *
+     * @deprecated Use {@link #removeFlow(TypedReadWriteTransaction, FlowEntity)}.
+     */
+    @Deprecated
     void syncRemoveFlow(FlowEntity flowEntity);
 
     /**
      * Install a flow.
      *
-     * @deprecated Use {@link #syncInstallFlow(FlowEntity)}.
+     * @deprecated Use {@link #addFlow(TypedWriteTransaction, FlowEntity)}.
      */
     @Deprecated
     void syncInstallFlow(FlowEntity flowEntity, long delayTime);
 
+    /**
+     * Installs a flow.
+     *
+     * @deprecated Use {@link #addFlow(TypedWriteTransaction, FlowEntity)}.
+     */
+    @Deprecated
     void syncInstallFlow(FlowEntity flowEntity);
 
     /**
      * API to install the Group on Data Plane Node synchronously. It internally waits for
      * Group Change Notification to confirm group mod request is being sent with-in delayTime
      *
-     * @deprecated Use {@link #syncInstallGroup(GroupEntity)}.
+     * @deprecated Use {@link #addGroup(TypedWriteTransaction, GroupEntity)}.
      */
     @Deprecated
     void syncInstallGroup(GroupEntity groupEntity, long delayTime);
 
+    /**
+     * Installs a group.
+     *
+     * @deprecated Use {@link #addGroup(TypedWriteTransaction, GroupEntity)}.
+     */
+    @Deprecated
     void syncInstallGroup(GroupEntity groupEntity);
 
     /**
      * Install a group.
      *
-     * @deprecated Use {@link #syncInstallGroup(BigInteger, Group)}.
+     * @deprecated Use {@link #addGroup(TypedWriteTransaction, BigInteger, Group)}.
      */
     @Deprecated
     void syncInstallGroup(BigInteger dpId, Group group, long delayTime);
 
+    /**
+     * Installs a group.
+     *
+     * @deprecated Use {@link #addGroup(TypedWriteTransaction, BigInteger, Group)}.
+     */
+    @Deprecated
     void syncInstallGroup(BigInteger dpId, Group group);
 
     /**
      * API to remove the Group on Data Plane Node synchronously. It internally waits for
      * Group Change Notification to confirm group delete request is being sent.
+     * @deprecated Use {@link #removeGroup(TypedReadWriteTransaction, GroupEntity)}.
      */
+    @Deprecated
     void syncRemoveGroup(GroupEntity groupEntity);
 
+    /**
+     * Removes a group.
+     *
+     * @deprecated Use {@link #removeGroup(TypedReadWriteTransaction, GroupEntity)}.
+     */
+    @Deprecated
     void syncRemoveGroup(BigInteger dpId, Group groupEntity);
 
+    @Deprecated
     void addBucketToTx(BigInteger dpId, long groupId, Bucket bucket, WriteTransaction tx);
 
+    @Deprecated
     void removeBucketToTx(BigInteger dpId, long groupId, long bucketId, WriteTransaction tx);
 }
