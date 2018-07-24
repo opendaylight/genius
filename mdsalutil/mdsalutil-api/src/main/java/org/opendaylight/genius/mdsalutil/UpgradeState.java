b0f2494ff4bda1186b522399660acc963466273c
Michael Vorburger <vorburger@redhat.com>
2018-07-24 17:15:00 +0200

    fix minor JavaDoc typos in UpgradeState
    Change-Id: I75471387a0be7281f6e984321f35171778950735
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/UpgradeState.java
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/UpgradeState.java
@@ -5,13 +5,14 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.genius.mdsalutil;
 
 /**
- * This service is only intented to be used by code that
- * needs to accomodate the full replay based upgrade.
+ * This service is only intended to be used by code that
+ * needs to accommodate the full replay based upgrade.
  */
 public interface UpgradeState {
+
     boolean isUpgradeInProgress();
+
 }
