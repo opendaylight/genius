4e98cd5c7df7ebe76b90ea8f8724991d8455d378
Faseela K <faseela.k@ericsson.com>
2018-07-28 11:07:10 +0530

    netvirt 3node CSIT - Unable to initialize bean tepCommandHelper
    In a 3 node setup, TepCommandHelper bean initialization fails, as all
    3 nodes are writing to the same DS, resulting in ConflicitngModificationException.
    The bigger question here, whether we really want to do such a heavy method
    configureTunnelType() in the bundle bringup thread. It is having several datastore
    reads and writes.
    
    JIRA: GENIUS-192
    
    Change-Id: I6607997478cebc4c7d1fc7ebf18ae89ae2eaa857
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

--- itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cli/TepCommandHelper.java
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cli/TepCommandHelper.java
@@ -22,13 +22,18 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 import javax.inject.Singleton;
+
+import com.google.common.util.concurrent.AbstractScheduledService;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.net.util.SubnetUtils;
 import org.apache.felix.service.command.CommandSession;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.infra.Datastore;
 import org.opendaylight.genius.infra.ManagedNewTransactionRunner;
 import org.opendaylight.genius.infra.ManagedNewTransactionRunnerImpl;
+import org.opendaylight.genius.infra.RetryingManagedNewTransactionRunner;
 import org.opendaylight.genius.itm.globals.ITMConstants;
 import org.opendaylight.genius.itm.impl.ItmUtils;
 import org.opendaylight.genius.utils.cache.DataStoreCache;
@@ -78,7 +83,7 @@ public class TepCommandHelper {
     private static final AtomicInteger CHECK = new AtomicInteger();
 
     private final DataBroker dataBroker;
-    private final ManagedNewTransactionRunner txRunner;
+    private final RetryingManagedNewTransactionRunner txRunner;
     private final ItmConfig itmConfig;
 
     /*
@@ -97,7 +102,7 @@ public class TepCommandHelper {
     @Inject
     public TepCommandHelper(final DataBroker dataBroker, final ItmConfig itmConfig) {
         this.dataBroker = dataBroker;
-        this.txRunner = new ManagedNewTransactionRunnerImpl(dataBroker);
+        this.txRunner = new RetryingManagedNewTransactionRunner(dataBroker);
         this.itmConfig = itmConfig;
     }
 
@@ -831,7 +836,6 @@ public class TepCommandHelper {
         tunnelType = StringUtils.upperCase(tunnelType);
         tunType = ItmUtils.TUNNEL_TYPE_MAP.get(tunnelType);
 
-        TransportZones transportZones = null;
         List<TransportZone> tzList = null;
         InstanceIdentifier<TransportZones> path = InstanceIdentifier.builder(TransportZones.class).build();
         Optional<TransportZones> tzones = ItmUtils.read(LogicalDatastoreType.CONFIGURATION, path, dataBroker);
@@ -847,8 +851,9 @@ public class TepCommandHelper {
             tzList = new ArrayList<>();
         }
         tzList.add(tzone);
-        transportZones = new TransportZonesBuilder().setTransportZone(tzList).build();
-        ItmUtils.syncWrite(LogicalDatastoreType.CONFIGURATION, path, transportZones, dataBroker);
+        TransportZones transportZones = new TransportZonesBuilder().setTransportZone(tzList).build();
+        txRunner.callWithNewWriteOnlyTransactionAndSubmit(tx -> tx.put(LogicalDatastoreType.CONFIGURATION,
+                path, transportZones, WriteTransaction.CREATE_MISSING_PARENTS));
 
     }
 
