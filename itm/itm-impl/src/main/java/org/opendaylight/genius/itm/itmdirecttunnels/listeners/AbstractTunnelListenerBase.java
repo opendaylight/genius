74f84e278cbb5c6a3e74783935ea15b9d3ad5261
Chintan Apte <chintan.apte@ericsson.com>
2018-08-02 16:21:51 +0530

    Bug: Genius-167
    Change-Id: Id078d379d0fe4824e13d156abe55e09dd92097af
    Signed-off-by: Chintan Apte <chintan.apte@ericsson.com>

--- itm/itm-impl/src/main/java/org/opendaylight/genius/itm/itmdirecttunnels/listeners/AbstractTunnelListenerBase.java
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/itmdirecttunnels/listeners/AbstractTunnelListenerBase.java
@@ -98,7 +98,7 @@ abstract class AbstractTunnelListenerBase<T extends DataObject> extends Abstract
         NodeConnectorId nodeConnectorId = InstanceIdentifier.keyOf(key.firstIdentifierOf(NodeConnector.class)).getId();
         long portNo = DirectTunnelUtils.getPortNumberFromNodeConnectorId(nodeConnectorId);
         if (portNo == ITMConstants.INVALID_PORT_NO) {
-            LOG.trace("Cannot derive port number, not proceeding with Interface State "
+            LOG.error("Cannot derive port number, not proceeding with Interface State "
                     + "addition for interface: {}", interfaceName);
             return null;
         }
@@ -125,7 +125,7 @@ abstract class AbstractTunnelListenerBase<T extends DataObject> extends Abstract
                         new BigInteger(tunnelEndPointInfo.getDstEndPointInfo()));
             }
             if (tunnelEndPointInfo == null || dpnTepConfigInfo == null) {
-                LOG.debug("Unable to process the NodeConnector ADD event for {} as Config not available."
+                LOG.info("Unable to process the NodeConnector ADD event for {} as Config not available."
                         + "Hence parking it", interfaceName);
                 unprocessedNCCache.add(interfaceName, nodeConnectorInfo);
                 return Collections.emptyList();
@@ -172,7 +172,7 @@ abstract class AbstractTunnelListenerBase<T extends DataObject> extends Abstract
             srcDpnTepsInfo = dpntePsInfoCache
                     .getDPNTepFromDPNId(new BigInteger(tunnelEndPointInfo.getSrcEndPointInfo()));
             if (!srcDpnTepsInfo.isPresent()) {
-                LOG.debug("Unable to add State for tunnel {}. Hence Parking with key {}",
+                LOG.info("Unable to add State for tunnel {}. Hence Parking with key {}",
                         interfaceName, tunnelEndPointInfo.getSrcEndPointInfo());
                 unprocessedNodeConnectorEndPointCache.add(tunnelEndPointInfo.getSrcEndPointInfo(), nodeConnectorInfo);
             }
@@ -185,9 +185,9 @@ abstract class AbstractTunnelListenerBase<T extends DataObject> extends Abstract
             dstDpnTePsInfo = dpntePsInfoCache
                     .getDPNTepFromDPNId(new BigInteger(tunnelEndPointInfo.getDstEndPointInfo()));
             if (!dstDpnTePsInfo.isPresent()) {
-                LOG.debug("Unable to add State for tunnel {}. Hence Parking with key {}",
+                LOG.info("Unable to add State for tunnel {}. Hence Parking with key {}",
                         interfaceName, tunnelEndPointInfo.getDstEndPointInfo());
-                unprocessedNodeConnectorEndPointCache.add(tunnelEndPointInfo.getSrcEndPointInfo(), nodeConnectorInfo);
+                unprocessedNodeConnectorEndPointCache.add(tunnelEndPointInfo.getDstEndPointInfo(), nodeConnectorInfo);
             }
         } finally {
             directTunnelUtils.getTunnelLocks().unlock(tunnelEndPointInfo.getDstEndPointInfo());
