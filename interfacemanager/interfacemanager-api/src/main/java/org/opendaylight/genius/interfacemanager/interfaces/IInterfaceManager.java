ca7e2b3f302d852f054a649c00bf6137432f4b5c
Hema Gopalakrishnan <hema.gopalkrishnan@ericsson.com>
2018-08-07 14:09:52 +0530

    GRE Config removal doesn't remove tunnel from Oper DS
    When GRE config is removed, GRE external tunnel is not cleaned up on the
    switch.
    Fix is to cache the internal tunnel in InterfaceManager so that it knows
    the internal tunnels to be ignored and only process the external tunnels.
    This cache is built across the cluster
    
    Change-Id: I776b1b11418377c8d3fee378a48734063fefe607
    Signed-off-by: Hema Gopalakrishnan <hema.gopalkrishnan@ericsson.com>

--- interfacemanager/interfacemanager-api/src/main/java/org/opendaylight/genius/interfacemanager/interfaces/IInterfaceManager.java
+++ interfacemanager/interfacemanager-api/src/main/java/org/opendaylight/genius/interfacemanager/interfaces/IInterfaceManager.java
@@ -190,4 +190,10 @@ public interface IInterfaceManager {
     long getLogicalTunnelSelectGroupId(int lportTag);
 
     boolean isItmDirectTunnelsEnabled();
+
+    void addInternalTunnelToIgnoreCache(String tunnelName);
+
+    String removeInternalTunnelFromIgnoreCache(String tunnelName);
+
+    boolean isInternalTunnelInIgnoreCache(String tunnelName);
 }
