28549b4682b8a87d0ea035372482a8e91fa0cf67
Stephen Kitt <skitt@redhat.com>
2018-08-17 18:52:45 +0200

    Make read-only transactions cancel themselves
    When a read-write transaction is only used to read from the datastore,
    there's no point in committing it; this patch changes managed
    read-write transactions so they are cancelled rather than submitted
    when appropriate.
    
    Change-Id: Ic7175574097702a45290c7500fcad2c37475a228
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/infra/WriteTrackingReadWriteTransaction.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright © 2018 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.infra;
+
+import org.opendaylight.controller.md.sal.binding.api.ForwardingReadWriteTransaction;
+import org.opendaylight.controller.md.sal.binding.api.ReadWriteTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+/**
+ * Read-write transaction which keeps track of writes.
+ */
+class WriteTrackingReadWriteTransaction extends ForwardingReadWriteTransaction {
+    // This is volatile to ensure we get the latest value; transactions aren't supposed to be used in multiple threads,
+    // but the cost here is tiny (one read penalty at the end of a transaction) so we play it safe
+    private volatile boolean written;
+
+    WriteTrackingReadWriteTransaction(ReadWriteTransaction delegate) {
+        super(delegate);
+    }
+
+    @Override
+    public <T extends DataObject> void put(LogicalDatastoreType store, InstanceIdentifier<T> path, T data) {
+        super.put(store, path, data);
+        written = true;
+    }
+
+    @Override
+    public <T extends DataObject> void put(LogicalDatastoreType store, InstanceIdentifier<T> path, T data,
+        boolean createMissingParents) {
+        super.put(store, path, data, createMissingParents);
+        written = true;
+    }
+
+    @Override
+    public <T extends DataObject> void merge(LogicalDatastoreType store, InstanceIdentifier<T> path, T data) {
+        super.merge(store, path, data);
+        written = true;
+    }
+
+    @Override
+    public <T extends DataObject> void merge(LogicalDatastoreType store, InstanceIdentifier<T> path, T data,
+        boolean createMissingParents) {
+        super.merge(store, path, data, createMissingParents);
+        written = true;
+    }
+
+    @Override
+    public void delete(LogicalDatastoreType store, InstanceIdentifier<?> path) {
+        super.delete(store, path);
+        written = true;
+    }
+
+    boolean isWritten() {
+        return written;
+    }
+}
