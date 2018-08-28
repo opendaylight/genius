aad90a6a4ff125b7a210f8635c811113d991fbfd
Michael Vorburger <vorburger@redhat.com>
2018-08-28 20:33:42 +0200

    use un-clustered UpgradeStateListener
    doing this with a clustered DTCL only leads to
    OptimisticLockFailedException (OLFE) on nodes, but there's really no
    need for this to be clustered; it's sufficient for 1 node (leader) to
    ract to changes and update the flag.
    
    JIRA: NETVIRT-1324
    Change-Id: I08601afb77ca9faaa29e2839aefcdfbcbbb66f5e
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- mdsalutil/mdsalutil-impl/src/main/java/org/opendaylight/genius/mdsalutil/internal/UpgradeStateListener.java
+++ mdsalutil/mdsalutil-impl/src/main/java/org/opendaylight/genius/mdsalutil/internal/UpgradeStateListener.java
@@ -20,7 +20,7 @@ import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
 import org.opendaylight.genius.datastoreutils.SingleTransactionDataBroker;
 import org.opendaylight.genius.mdsalutil.UpgradeState;
-import org.opendaylight.serviceutils.tools.mdsal.listener.AbstractClusteredSyncDataTreeChangeListener;
+import org.opendaylight.serviceutils.tools.mdsal.listener.AbstractSyncDataTreeChangeListener;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.mdsalutil.rev170830.Config;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.ops4j.pax.cdi.api.OsgiService;
@@ -31,7 +31,7 @@ import org.slf4j.LoggerFactory;
 @Singleton
 @Named("geniusUpgradeStateListener") // to distinguish the <bean id=".."> from serviceutils' UpgradeStateListener
 @OsgiServiceProvider(classes = UpgradeState.class)
-public class UpgradeStateListener extends AbstractClusteredSyncDataTreeChangeListener<Config> implements UpgradeState {
+public class UpgradeStateListener extends AbstractSyncDataTreeChangeListener<Config> implements UpgradeState {
     private static final Logger LOG = LoggerFactory.getLogger(UpgradeStateListener.class);
 
     private final AtomicBoolean isUpgradeInProgress = new AtomicBoolean(false);
