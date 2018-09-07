99a374d7868fadc4fc87b898fd6bc3f3f682ee76
Edw7n <n.edwin.anthony@ericsson.com>
2018-09-07 23:35:28 +0530

    GENIUS-185: Removing usage of databroker tx
    - replaces most uses of databroker tx
    - few worker classes have been refactored into their respective listeneres
    - some classes ommitted in /itm-impl/..../cli/
        would lead to other comprehensive changes which are out of scope
    
    Change-Id: I8d2218636471923b7fb9e61dd2b42b6d80207bcf
    Signed-off-by: Edw7n <n.edwin.anthony@ericsson.com>

--- itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cli/TepDeleteDatastore.java
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cli/TepDeleteDatastore.java
@@ -7,12 +7,15 @@
  */
 package org.opendaylight.genius.itm.cli;
 
+import com.google.common.util.concurrent.Futures;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.karaf.shell.commands.Command;
 import org.apache.karaf.shell.console.OsgiCommandSupport;
-import org.opendaylight.controller.md.sal.binding.api.DataBroker;
-import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.infra.Datastore;
+import org.opendaylight.genius.infra.ManagedNewTransactionRunner;
+import org.opendaylight.genius.infra.ManagedNewTransactionRunnerImpl;
 import org.opendaylight.genius.itm.api.IITMProvider;
 import org.opendaylight.genius.itm.impl.ItmUtils;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.Interfaces;
@@ -21,12 +24,9 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.rev160406.Transp
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Command(scope = "tep", name = "deleteDatastore", description = "deleting all tep data from datastore")
 public class TepDeleteDatastore<T extends DataObject>  extends OsgiCommandSupport {
-    private static final Logger LOG = LoggerFactory.getLogger(TepDeleteDatastore.class);
     private IITMProvider itmProvider;
 
     public void setItmProvider(IITMProvider itmProvider) {
@@ -34,8 +34,8 @@ public class TepDeleteDatastore<T extends DataObject>  extends OsgiCommandSuppor
     }
 
     @Override
+    @SuppressFBWarnings("UWF_FIELD_NOT_INITIALIZED_IN_CONSTRUCTOR")
     protected Object doExecute() {
-        final DataBroker dataBroker =  itmProvider.getDataBroker();
         InstanceIdentifier<TransportZones> itmConfigPath =
                 InstanceIdentifier.builder(TransportZones.class).build();
         InstanceIdentifier<Interfaces> interfacesConfigPath =
@@ -44,20 +44,19 @@ public class TepDeleteDatastore<T extends DataObject>  extends OsgiCommandSuppor
                 InstanceIdentifier.builder(InterfacesState.class).build();
         InstanceIdentifier<Nodes> frmConfigPath =
                 InstanceIdentifier.builder(Nodes.class).build();
-        List<InstanceIdentifier<T>> allConfigPaths =
-                        new ArrayList<>();
+        List<InstanceIdentifier<T>> allConfigPaths = new ArrayList<>();
         allConfigPaths.add((InstanceIdentifier<T>) itmConfigPath);
         allConfigPaths.add((InstanceIdentifier<T>) interfacesConfigPath);
         allConfigPaths.add((InstanceIdentifier<T>) frmConfigPath);
         //allConfigPaths.add((InstanceIdentifier<T>) tunnelsConfigPath);
-        ItmUtils.asyncBulkRemove(dataBroker, LogicalDatastoreType.CONFIGURATION,allConfigPaths,
-                        ItmUtils.DEFAULT_CALLBACK);
-        List<InstanceIdentifier<T>> allOperationalPaths =
-                        new ArrayList<>();
+        final ManagedNewTransactionRunner txrunner = new ManagedNewTransactionRunnerImpl(itmProvider.getDataBroker());
+        Futures.addCallback(txrunner.callWithNewWriteOnlyTransactionAndSubmit(Datastore.CONFIGURATION,
+            tx -> allConfigPaths.forEach(tx::delete)), ItmUtils.DEFAULT_WRITE_CALLBACK);
+        List<InstanceIdentifier<T>> allOperationalPaths = new ArrayList<>();
         // allOperationalPaths.add((InstanceIdentifier<T>) tnStateOpPath);
         allOperationalPaths.add((InstanceIdentifier<T>) ifStateOpPath);
-        ItmUtils.asyncBulkRemove(dataBroker, LogicalDatastoreType.OPERATIONAL, allOperationalPaths,
-                        ItmUtils.DEFAULT_CALLBACK);
+        Futures.addCallback(txrunner.callWithNewWriteOnlyTransactionAndSubmit(Datastore.OPERATIONAL,
+            tx -> allOperationalPaths.forEach(tx::delete)), ItmUtils.DEFAULT_WRITE_CALLBACK);
         return null;
     }
 }
