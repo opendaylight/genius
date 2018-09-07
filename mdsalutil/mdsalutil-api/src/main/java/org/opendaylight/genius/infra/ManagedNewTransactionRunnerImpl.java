6baf62ef04d2ad231965f22311cfe03dbe868321
Stephen Kitt <skitt@redhat.com>
2018-09-07 16:58:51 +0200

    Refactor ManagedTransactionFactory to reduce dupes
    This reworks the factory and runner to have a single method in charge
    of each type of behaviour.
    
    Change-Id: Ibbeb1f5d78eadfeb6cc35cbef4e308c43e8acdd1
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/infra/ManagedNewTransactionRunnerImpl.java
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/infra/ManagedNewTransactionRunnerImpl.java
@@ -11,7 +11,6 @@ import com.google.common.annotations.Beta;
 import com.google.common.util.concurrent.FluentFuture;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
-import com.google.common.util.concurrent.MoreExecutors;
 import java.util.function.Function;
 import javax.inject.Inject;
 import org.opendaylight.controller.md.sal.binding.api.BindingTransactionChain;
@@ -23,6 +22,7 @@ import org.opendaylight.controller.md.sal.common.api.data.TransactionChain;
 import org.opendaylight.controller.md.sal.common.api.data.TransactionChainListener;
 import org.opendaylight.infrautils.utils.function.InterruptibleCheckedConsumer;
 import org.opendaylight.infrautils.utils.function.InterruptibleCheckedFunction;
+import org.opendaylight.mdsal.common.api.CommitInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -49,45 +49,32 @@ public class ManagedNewTransactionRunnerImpl extends ManagedTransactionFactoryIm
     @Override
     public <E extends Exception> ListenableFuture<Void>
             callWithNewWriteOnlyTransactionAndSubmit(InterruptibleCheckedConsumer<WriteTransaction, E> txConsumer) {
-        WriteTransaction realTx = broker.newWriteOnlyTransaction();
-        WriteTransaction wrappedTx = new NonSubmitCancelableWriteTransaction(realTx);
-        return tryCatchCancel(() -> {
-            txConsumer.accept(wrappedTx);
-            return realTx.commit().transform(v -> null, MoreExecutors.directExecutor());
-        }, realTx);
+        return callWithNewTransactionAndSubmit(Datastore.class, broker::newWriteOnlyTransaction,
+            (datastoreType, realTx) -> new NonSubmitCancelableWriteTransaction(realTx),
+            (realTx, wrappedTx) -> realTx.commit(), txConsumer::accept);
     }
 
     @Override
     public <E extends Exception> ListenableFuture<Void>
             callWithNewReadWriteTransactionAndSubmit(InterruptibleCheckedConsumer<ReadWriteTransaction, E> txConsumer) {
-        ReadWriteTransaction realTx = broker.newReadWriteTransaction();
-        WriteTrackingReadWriteTransaction wrappedTx = new NonSubmitCancelableReadWriteTransaction(realTx);
-        return tryCatchCancel(() -> {
-            txConsumer.accept(wrappedTx);
-            return commit(realTx, null, wrappedTx);
-        }, realTx);
+        return callWithNewTransactionAndSubmit(Datastore.class, broker::newReadWriteTransaction,
+            (datastoreType, realTx) -> new NonSubmitCancelableReadWriteTransaction(realTx), this::commit,
+            txConsumer::accept);
     }
 
     @Override
     public <D extends Datastore, E extends Exception> FluentFuture<Void>
         callWithNewReadWriteTransactionAndSubmit(Class<D> datastoreType,
             InterruptibleCheckedConsumer<TypedReadWriteTransaction<D>, E> txConsumer) {
-        ReadWriteTransaction realTx = broker.newReadWriteTransaction();
-        WriteTrackingTypedReadWriteTransactionImpl<D> wrappedTx =
-            new WriteTrackingTypedReadWriteTransactionImpl<>(datastoreType, realTx);
-        return tryCatchCancel(() -> {
-            txConsumer.accept(wrappedTx);
-            return commit(realTx, null, wrappedTx);
-        }, realTx);
+        return callWithNewTransactionAndSubmit(datastoreType, broker::newReadWriteTransaction,
+            WriteTrackingTypedReadWriteTransactionImpl<D>::new, this::commit, txConsumer::accept);
     }
 
     @Override
     public <D extends Datastore, E extends Exception, R> FluentFuture<R> applyWithNewReadWriteTransactionAndSubmit(
             Class<D> datastoreType, InterruptibleCheckedFunction<TypedReadWriteTransaction<D>, R, E> txFunction) {
-        ReadWriteTransaction realTx = broker.newReadWriteTransaction();
-        WriteTrackingTypedReadWriteTransactionImpl<D> wrappedTx =
-                new WriteTrackingTypedReadWriteTransactionImpl<>(datastoreType, realTx);
-        return tryCatchCancel(() -> commit(realTx, txFunction.apply(wrappedTx), wrappedTx), realTx);
+        return super.applyWithNewTransactionAndSubmit(datastoreType, broker::newReadWriteTransaction,
+            WriteTrackingTypedReadWriteTransactionImpl<D>::new, this::commit, txFunction::apply);
     }
 
     @Override
@@ -108,14 +95,14 @@ public class ManagedNewTransactionRunnerImpl extends ManagedTransactionFactoryIm
         }
     }
 
-    private <R> FluentFuture<R> commit(ReadWriteTransaction realTx, R result, WriteTrackingTransaction wrappedTx) {
+    private FluentFuture<? extends CommitInfo> commit(WriteTransaction realTx, WriteTrackingTransaction wrappedTx) {
         if (wrappedTx.isWritten()) {
             // The transaction contains changes, commit it
-            return realTx.commit().transform(v -> result, MoreExecutors.directExecutor());
+            return realTx.commit();
         } else {
             // The transaction only handled reads, cancel it
             realTx.cancel();
-            return FluentFuture.from(Futures.immediateFuture(result));
+            return FluentFuture.from(Futures.immediateFuture(CommitInfo.empty()));
         }
     }
 }
