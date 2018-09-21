6af50d8c5876896a2349084179f63bb548edc853
Tarun Thakur <tarun.t@altencalsoftlabs.com>
2018-09-21 13:01:01 +0530

    GENIUS-212: TEP IP Update does not work in ITM auto-tunnel
    Issue: During local_ip update, old-tep is being set.
    
    Fix: New tep ip is getting passed during tep addition after old tep is
    removed.
    
    Change-Id: I37573b69178188ce9e809805f63d0dca0b1cefdb
    Signed-off-by: Tarun Thakur <tarun.t@altencalsoftlabs.com>

--- itm/itm-impl/src/test/java/org/opendaylight/genius/itm/tests/ItmTest.java
+++ itm/itm-impl/src/test/java/org/opendaylight/genius/itm/tests/ItmTest.java
@@ -7,12 +7,15 @@
  */
 package org.opendaylight.genius.itm.tests;
 
+import javax.inject.Inject;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.MethodRule;
+import org.opendaylight.genius.datastoreutils.testutils.JobCoordinatorEventsWaiter;
 import org.opendaylight.genius.datastoreutils.testutils.JobCoordinatorTestModule;
+import org.opendaylight.genius.datastoreutils.testutils.TestableDataTreeChangeListenerModule;
 import org.opendaylight.genius.itm.impl.ItmTestUtils;
 import org.opendaylight.genius.utils.cache.CacheTestUtil;
 import org.opendaylight.infrautils.caches.testutils.CacheModule;
@@ -27,8 +30,10 @@ public class ItmTest {
 
     public @Rule LogRule logRule = new LogRule();
     public @Rule LogCaptureRule logCaptureRule = new LogCaptureRule();
-    public @Rule MethodRule guice = new GuiceRule(ItmTestModule.class, JobCoordinatorTestModule.class,
-            CacheModule.class);
+    public @Rule MethodRule guice = new GuiceRule(ItmTestModule.class,  TestableDataTreeChangeListenerModule.class,
+            JobCoordinatorTestModule.class, CacheModule.class);
+
+    private @Inject JobCoordinatorEventsWaiter coordinatorEventsWaiter;
 
     @Before
     public void before() {
@@ -52,6 +57,7 @@ public class ItmTest {
 
     @Test
     public void testWiring() {
+        coordinatorEventsWaiter.awaitEventsConsumption();
         // do nothing; still valuable to check that ItmTestModule works and everything comes up
     }
 }
