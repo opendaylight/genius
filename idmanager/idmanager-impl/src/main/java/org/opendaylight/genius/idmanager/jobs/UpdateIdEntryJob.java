04c90d8c60e1a63cb5cf519c9f4eb5d4a3b5028c
Stephen Kitt <skitt@redhat.com>
2018-10-24 10:16:23 +0200

    Use plain @Nullable
    The @NullableDecl reference added in
    https://git.opendaylight.org/gerrit/75824 breaks when upgrading Guava;
    use @Nullable instead.
    
    Change-Id: I62b726bb21cdd26e113e284d04cd108a7ec1c45f
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- idmanager/idmanager-impl/src/main/java/org/opendaylight/genius/idmanager/jobs/UpdateIdEntryJob.java
+++ idmanager/idmanager-impl/src/main/java/org/opendaylight/genius/idmanager/jobs/UpdateIdEntryJob.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
-import org.checkerframework.checker.nullness.compatqual.NullableDecl;
+import javax.annotation.Nullable;
 import org.opendaylight.genius.idmanager.IdUtils;
 import org.opendaylight.genius.infra.ManagedNewTransactionRunner;
 import org.opendaylight.infrautils.utils.concurrent.Executors;
@@ -70,7 +70,7 @@ public class UpdateIdEntryJob implements Callable<List<ListenableFuture<Void>>> 
         });
         future.addCallback(new FutureCallback<Void>() {
             @Override
-            public void onSuccess(@NullableDecl Void result) {
+            public void onSuccess(@Nullable Void result) {
                 cleanUp();
             }
 
