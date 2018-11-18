38d1e1bfd58f01cb96ef087b9355d8cab47712ef
Robert Varga <robert.varga@pantheon.tech>
2018-11-18 15:43:03 +0100

    Add JvmGlobalLocks
    As explained in NETVIRT-1510, the String.intern()-based locking is
    a thing of pure horror.
    
    Provide a migration class, where the semantics are explained, so
    that we can catch all places performing this funkiness. This will
    bind the locking domain, which we can then split into finer domains,
    properly expressing our locking.
    
    JIRA: NETVIRT-1510
    Change-Id: I82b1ad1c150ad191055ca3049525d99af0612e8e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/utils/NamedReentrantLock.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.utils;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.base.MoreObjects;
+import java.util.concurrent.locks.ReentrantLock;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.concepts.Identifiable;
+
+final class NamedReentrantLock<T> extends ReentrantLock implements Identifiable<T> {
+    private static final long serialVersionUID = 1L;
+
+    private final @NonNull T name;
+
+    NamedReentrantLock(final T name) {
+        super();
+        this.name = requireNonNull(name);
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this).add("name", name).toString();
+    }
+
+    @Override
+    public @NonNull T getIdentifier() {
+        return name;
+    }
+}
