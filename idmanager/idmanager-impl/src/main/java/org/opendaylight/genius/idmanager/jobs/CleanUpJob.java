fa607bc6cafafede1f03dffaef7237a9675186a2
Michael Vorburger <vorburger@redhat.com>
2018-11-22 17:47:34 +0100

    use TX runner in IdManager CleanUpJob
    This makes the read and put happen in an atomic transaction.
    
    The RetryingManagedNewTransactionRunner is used to duplicate exactly
    what the SingleTransactionDataBroker.syncWrite() does today.
    
    Change-Id: I28307feadee6141d01a7c23a6c7f1badde48c846
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- idmanager/idmanager-impl/src/main/java/org/opendaylight/genius/idmanager/jobs/CleanUpJob.java
+++ idmanager/idmanager-impl/src/main/java/org/opendaylight/genius/idmanager/jobs/CleanUpJob.java
@@ -7,27 +7,26 @@
  */
 package org.opendaylight.genius.idmanager.jobs;
 
-import static org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType.CONFIGURATION;
-
 import com.google.common.base.Optional;
+import com.google.common.util.concurrent.FluentFuture;
 import com.google.common.util.concurrent.ListenableFuture;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Callable;
-import org.opendaylight.controller.md.sal.binding.api.DataBroker;
-import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
 import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
-import org.opendaylight.genius.datastoreutils.SingleTransactionDataBroker;
 import org.opendaylight.genius.idmanager.IdLocalPool;
 import org.opendaylight.genius.idmanager.IdManagerException;
 import org.opendaylight.genius.idmanager.IdUtils;
 import org.opendaylight.genius.idmanager.ReleasedIdHolder;
+import org.opendaylight.genius.infra.Datastore;
 import org.opendaylight.genius.infra.ManagedNewTransactionRunner;
+import org.opendaylight.genius.infra.RetryingManagedNewTransactionRunner;
 import org.opendaylight.infrautils.jobcoordinator.JobCoordinator;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.id.pools.id.pool.ReleasedIdsHolder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.id.pools.id.pool.ReleasedIdsHolderBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.lockmanager.rev160413.LockManagerService;
+import org.opendaylight.yangtools.util.concurrent.FluentFutures;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,19 +37,19 @@ public class CleanUpJob implements Callable<List<ListenableFuture<Void>>> {
 
     private final IdLocalPool idLocalPool;
     private final ManagedNewTransactionRunner txRunner;
-    private final DataBroker broker;
+    private final RetryingManagedNewTransactionRunner retryingTxRunner;
     private final String parentPoolName;
     private final int blockSize;
     private final LockManagerService lockManager;
     private final IdUtils idUtils;
     private final JobCoordinator jobCoordinator;
 
-    public CleanUpJob(IdLocalPool idLocalPool, ManagedNewTransactionRunner txRunner, DataBroker broker,
-            String parentPoolName, int blockSize,
+    public CleanUpJob(IdLocalPool idLocalPool, ManagedNewTransactionRunner txRunner,
+            RetryingManagedNewTransactionRunner retryingTxRunner, String parentPoolName, int blockSize,
             LockManagerService lockManager, IdUtils idUtils, JobCoordinator jobCoordinator) {
         this.idLocalPool = idLocalPool;
         this.txRunner = txRunner;
-        this.broker = broker;
+        this.retryingTxRunner = retryingTxRunner;
         this.parentPoolName = parentPoolName;
         this.blockSize = blockSize;
         this.lockManager = lockManager;
@@ -60,11 +59,10 @@ public class CleanUpJob implements Callable<List<ListenableFuture<Void>>> {
 
     @Override
     public List<ListenableFuture<Void>> call() throws Exception {
-        cleanupExcessIds();
-        return Collections.emptyList();
+        return Collections.singletonList(cleanupExcessIds());
     }
 
-    private void cleanupExcessIds()
+    private FluentFuture<Void> cleanupExcessIds()
             throws IdManagerException, ReadFailedException, TransactionCommitFailedException {
         // We can update the availableCount here... and update it in DS using IdHolderSyncJob
         long totalAvailableIdCount = idLocalPool.getAvailableIds().getAvailableIdCount()
@@ -82,25 +80,29 @@ public class CleanUpJob implements Callable<List<ListenableFuture<Void>>> {
             // cannot rely on DSJC because that is not cluster-aware
             try {
                 idUtils.lock(lockManager, parentPoolNameIntern);
-                Optional<ReleasedIdsHolder> releasedIdsHolder = SingleTransactionDataBroker.syncReadOptional(broker,
-                        CONFIGURATION, releasedIdInstanceIdentifier);
-                if (!releasedIdsHolder.isPresent()) {
-                    LOG.error("ReleasedIds not present in parent pool. Unable to cleanup excess ids");
-                    return;
-                }
-                if (LOG.isDebugEnabled()) {
-                    LOG.debug("Releasing excesss Ids from local pool");
-                }
-                ReleasedIdHolder releasedIds = (ReleasedIdHolder) idLocalPool.getReleasedIds();
-                ReleasedIdsHolderBuilder releasedIdsParent = new ReleasedIdsHolderBuilder(releasedIdsHolder.get());
-                idUtils.freeExcessAvailableIds(releasedIds, releasedIdsParent, totalAvailableIdCount - blockSize * 2);
-                IdHolderSyncJob job = new IdHolderSyncJob(idLocalPool.getPoolName(), releasedIds, txRunner, idUtils);
-                jobCoordinator.enqueueJob(idLocalPool.getPoolName(), job, IdUtils.RETRY_COUNT);
-                SingleTransactionDataBroker.syncWrite(broker, LogicalDatastoreType.CONFIGURATION,
-                        releasedIdInstanceIdentifier, releasedIdsParent.build());
+                return retryingTxRunner.callWithNewReadWriteTransactionAndSubmit(Datastore.CONFIGURATION, tx -> {
+                    Optional<ReleasedIdsHolder> releasedIdsHolder = tx.read(releasedIdInstanceIdentifier).get();
+                    if (!releasedIdsHolder.isPresent()) {
+                        LOG.error("ReleasedIds not present in parent pool. Unable to cleanup excess ids");
+                        return;
+                    }
+                    if (LOG.isDebugEnabled()) {
+                        LOG.debug("Releasing excesss Ids from local pool");
+                    }
+                    ReleasedIdHolder releasedIds = (ReleasedIdHolder) idLocalPool.getReleasedIds();
+                    ReleasedIdsHolderBuilder releasedIdsParent = new ReleasedIdsHolderBuilder(releasedIdsHolder.get());
+                    idUtils.freeExcessAvailableIds(releasedIds, releasedIdsParent,
+                        totalAvailableIdCount - blockSize * 2);
+                    IdHolderSyncJob job = new IdHolderSyncJob(idLocalPool.getPoolName(), releasedIds, txRunner,
+                            idUtils);
+                    jobCoordinator.enqueueJob(idLocalPool.getPoolName(), job, IdUtils.RETRY_COUNT);
+                    tx.put(releasedIdInstanceIdentifier, releasedIdsParent.build(), true);
+                });
             } finally {
                 idUtils.unlock(lockManager, parentPoolNameIntern);
             }
+        } else {
+            return FluentFutures.immediateNullFluentFuture();
         }
     }
 }
