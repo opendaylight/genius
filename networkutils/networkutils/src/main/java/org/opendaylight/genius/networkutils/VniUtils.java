64a57970d21e2241f6bea313c2a5b94fce60a228
Faseela K <faseela.k@ericsson.com>
2018-11-30 16:36:49 +0530

    Move IdManager based VNI Carving to a centralized module
    JIRA: GENIUS-250
    
    Netvirt currently has a vni carving logic which is
    tightly coupled with NAT. There are more netvirt
    services which are in need of this utility now, and
    hence we need to move this to a centralized place.
    Adding a new VniUtils in genius, to which later
    netvirt services can be migrated to.
    
    Change-Id: I68c2f6cfe74d03c741e519e86704490043787d95
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

---
+++ networkutils/networkutils/src/main/java/org/opendaylight/genius/networkutils/VniUtils.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright © 2018 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.genius.networkutils;
+
+import java.math.BigInteger;
+import java.util.concurrent.ExecutionException;
+import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.id.pools.IdPool;
+
+public interface VniUtils {
+
+    BigInteger getVNI(String vniKey) throws ExecutionException, InterruptedException;
+
+    void releaseVNI(String vniKey) throws ExecutionException, InterruptedException;
+
+    IdPool getVxlanVniPool() throws ReadFailedException;
+}
