5d53063f33151d81d74b0d5ea2d4571604b50630
Faseela K <faseela.k@ericsson.com>
2018-11-30 16:36:49 +0530

    Move IdManager based VNI Carving to a centralized module
    JIRA: GENIUS-250
    
    Netvirt currently has a vni carving logic which is
    tightly coupled with NAT. There are more netvirt
    services which are in need of this utility now, and
    hence we need to move this to a centralized place.
    Adding a new VniUtils in genius, to which later
    netvirt services can be migrated to.
    
    Change-Id: I68c2f6cfe74d03c741e519e86704490043787d95
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

---
+++ networkutils/networkutils/src/test/java/org/opendaylight/genius/networkutils/test/NetworkUtilTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright © 2018 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.networkutils.test;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.concurrent.ExecutionException;
+import javax.inject.Inject;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.MethodRule;
+import org.opendaylight.controller.md.sal.binding.test.AbstractConcurrentDataBrokerTest;
+import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
+import org.opendaylight.genius.datastoreutils.testutils.JobCoordinatorTestModule;
+import org.opendaylight.genius.mdsalutil.NwConstants;
+import org.opendaylight.genius.networkutils.VniUtils;
+import org.opendaylight.infrautils.inject.guice.testutils.GuiceRule;
+import org.opendaylight.infrautils.testutils.LogCaptureRule;
+import org.opendaylight.infrautils.testutils.LogRule;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.id.pools.IdPool;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Test for {@link VniUtils}.
+ *
+ * @author Faseela K
+ */
+public class NetworkUtilTest extends AbstractConcurrentDataBrokerTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(NetworkUtilTest.class);
+
+    public @Rule LogRule logRule = new LogRule();
+    public @Rule LogCaptureRule logCaptureRule = new LogCaptureRule();
+    public @Rule MethodRule guice = new GuiceRule(NetworkUtilTestModule.class, JobCoordinatorTestModule.class);
+
+    private @Inject VniUtils vniUtils;
+
+    @Test
+    public void testDefaultVniPoolCreated() throws ReadFailedException {
+        IdPool idPool = vniUtils.getVxlanVniPool();
+        assertThat(idPool.getPoolName()).isEqualTo(NwConstants.ODL_VNI_POOL_NAME);
+    }
+
+    @Test
+    public void testGetVNI() throws ExecutionException, InterruptedException {
+        assertThat(vniUtils.getVNI("test").longValue()).isEqualTo(NwConstants.VNI_DEFAULT_LOW_VALUE);
+    }
+}
