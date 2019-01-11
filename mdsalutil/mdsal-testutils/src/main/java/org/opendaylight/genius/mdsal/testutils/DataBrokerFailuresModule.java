ff3fbb423e55be7ff8e590d7049f71c1a357ce23
Stephen Kitt <skitt@redhat.com>
2019-01-11 11:02:54 +0100

    WIP: Add mdsal 3 DataBroker test modules
    Change-Id: Id2a34b49bd144e6a2b6e930cdf28965515839954
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ mdsalutil/mdsal-testutils/src/main/java/org/opendaylight/genius/mdsal/testutils/DataBrokerFailuresModule.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright © 2019 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.mdsal.testutils;
+
+import com.google.inject.AbstractModule;
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.mdsal.binding.testutils.DataBrokerFailures;
+import org.opendaylight.mdsal.binding.testutils.DataBrokerFailuresImpl;
+
+/**
+ * Guice Module which correctly binds the {@link DataBrokerFailures}.
+ *
+ * @author Michael Vorburger.ch
+ */
+public class DataBrokerFailuresModule extends AbstractModule {
+
+    private final DataBroker realDataBroker;
+
+    public DataBrokerFailuresModule(DataBroker realDataBroker) {
+        this.realDataBroker = realDataBroker;
+    }
+
+    public DataBrokerFailuresModule() {
+        this(DataBrokerTestModule.dataBroker());
+    }
+
+    @Override
+    protected void configure() {
+        DataBrokerFailuresImpl testableDataBroker = new DataBrokerFailuresImpl(realDataBroker);
+        bind(DataBroker.class).toInstance(testableDataBroker);
+        // bind(DataBroker.class).annotatedWith(Reference.class).toInstance(testableDataBroker);
+        bind(DataBrokerFailures.class).toInstance(testableDataBroker);
+    }
+}
