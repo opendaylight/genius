15c0b16a69546d78da44529357cec1ab183337d8
Dimple Jain <dimple.jain@ericsson.com>
2019-01-23 16:31:14 +0530

    GENIUS-145: Creation of OFPort on ITM Direct Tunnels.
    Change-Id: Ic6d229f22eb540b4859d61450b5b3f53cee9faaa
    Signed-off-by: Dimple Jain <dimple.jain@ericsson.com>

---
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cache/OfEndPointCache.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2019 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.itm.cache;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import javax.inject.Singleton;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+public class OfEndPointCache {
+
+    private static final Logger LOG = LoggerFactory.getLogger(OfEndPointCache.class);
+
+    private final ConcurrentMap<String, String> ofEndPtMap =
+            new ConcurrentHashMap<>();
+
+    public void add(String dpnId, String ofTunnelName) {
+        ofEndPtMap.put(dpnId, ofTunnelName);
+    }
+
+    public String get(String dpnId) {
+        return ofEndPtMap.get(dpnId);
+    }
+
+    public String remove(String dpnId) {
+        return ofEndPtMap.remove(dpnId);
+    }
+
+    public Set<String> getAll(String dpnId) {
+        return ofEndPtMap.keySet();
+    }
+}
--- itm/itm-impl/src/main/java/org/opendaylight/genius/itm/recovery/impl/ItmTepInstanceRecoveryHandler.java
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/recovery/impl/ItmTepInstanceRecoveryHandler.java
@@ -22,6 +22,7 @@ import org.opendaylight.genius.infra.ManagedNewTransactionRunnerImpl;
 import org.opendaylight.genius.interfacemanager.interfaces.IInterfaceManager;
 import org.opendaylight.genius.itm.cache.DPNTEPsInfoCache;
 import org.opendaylight.genius.itm.cache.DpnTepStateCache;
+import org.opendaylight.genius.itm.cache.OfEndPointCache;
 import org.opendaylight.genius.itm.cache.OvsBridgeEntryCache;
 import org.opendaylight.genius.itm.cache.OvsBridgeRefEntryCache;
 import org.opendaylight.genius.itm.cache.TunnelStateCache;
@@ -85,6 +86,7 @@ public class ItmTepInstanceRecoveryHandler implements ServiceRecoveryInterface {
                                          IInterfaceManager interfaceManager,
                                          ServiceRecoveryRegistry serviceRecoveryRegistry,
                                          EntityOwnershipUtils entityOwnershipUtils,
+                                         OfEndPointCache ofEndPointCache,
                                          DataTreeEventCallbackRegistrar eventCallbacks) {
         this.dataBroker = dataBroker;
         this.itmConfig = itmConfig;
@@ -95,8 +97,10 @@ public class ItmTepInstanceRecoveryHandler implements ServiceRecoveryInterface {
         this.eventCallbacks = eventCallbacks;
         this.txRunner = new ManagedNewTransactionRunnerImpl(dataBroker);
         this.itmInternalTunnelAddWorker = new ItmInternalTunnelAddWorker(dataBroker, jobCoordinator,
-                tunnelMonitoringConfig, itmConfig, directTunnelUtils, interfaceManager, ovsBridgeRefEntryCache);
-        this.itmExternalTunnelAddWorker = new ItmExternalTunnelAddWorker(itmConfig, dpntePsInfoCache);
+                tunnelMonitoringConfig, itmConfig, directTunnelUtils, interfaceManager,
+                ovsBridgeRefEntryCache, ofEndPointCache);
+        this.itmExternalTunnelAddWorker = new ItmExternalTunnelAddWorker(itmConfig,
+                dpntePsInfoCache);
         this.itmInternalTunnelDeleteWorker = new ItmInternalTunnelDeleteWorker(dataBroker, jobCoordinator,
                 tunnelMonitoringConfig, interfaceManager, dpnTepStateCache, ovsBridgeEntryCache,
                 ovsBridgeRefEntryCache, tunnelStateCache, directTunnelUtils);
