f28a516d71a88f567e43903aa9b5120d0fdb4d8b
Tarun Thakur <tarun.t@altencalsoftlabs.com>
2019-01-23 18:53:12 +0530

    GENIUS-248: ITM-Vtem Auto tunnel feature- Tunnels not coming up after deleting external id's in OVS
    Issue:
    When exterenal_id is deleted from OVS, then TEPs are supposed to move in def-TZ.
    When TEP's movement to default-transport-zone from TZA happens, tunnels do not come up.
    Main issue is during movement, TEP is added followed by TEP deletion
    which makes TEP not available in tep-show-state. This happens because
    TEP is keyed on dpn-id in DpnTepsInfo and tz-membership do not help in synchronization
    issues.
    
    Fix:
    Introduced DataTreeEventCallbackRegistrar on DpnTepsInfo to check if TEP is deleted and
    when it is removed and event is notified, then only perform TEP add in TZ movement case.
    
    Change-Id: Iadbcc2a23ac971722237d0dd8f0b9d603d31a8ff
    Signed-off-by: Tarun Thakur <tarun.t@altencalsoftlabs.com>

--- itm/itm-impl/src/test/java/org/opendaylight/genius/itm/tests/ItmTestModule.java
+++ itm/itm-impl/src/test/java/org/opendaylight/genius/itm/tests/ItmTestModule.java
@@ -12,6 +12,8 @@ import static org.mockito.Mockito.mock;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.test.DataBrokerTestModule;
 import org.opendaylight.daexim.DataImportBootReady;
+import org.opendaylight.genius.datastoreutils.listeners.DataTreeEventCallbackRegistrar;
+import org.opendaylight.genius.datastoreutils.listeners.internal.DataTreeEventCallbackRegistrarImpl;
 import org.opendaylight.genius.datastoreutils.testutils.JobCoordinatorEventsWaiter;
 import org.opendaylight.genius.datastoreutils.testutils.TestableJobCoordinatorEventsWaiter;
 import org.opendaylight.genius.idmanager.IdManager;
@@ -88,6 +90,9 @@ public class ItmTestModule extends AbstractGuiceJsr250Module {
         DataBroker dataBroker = dataBrokerTestModule.getDataBroker();
         bind(DataBroker.class).toInstance(dataBroker);
         bind(DataBroker.class).annotatedWith(OsgiService.class).toInstance(dataBroker);
+        DataTreeEventCallbackRegistrar dataTreeEventCallbackRegistrar =
+                new DataTreeEventCallbackRegistrarImpl(dataBroker);
+        bind(DataTreeEventCallbackRegistrar.class).toInstance(dataTreeEventCallbackRegistrar);
         bind(InterfaceManagerService.class).to(InterfaceManagerServiceImpl.class);
         bind(OdlInterfaceRpcService.class).to(InterfaceManagerRpcService.class);
         bind(IInterfaceManager.class).to(InterfacemgrProvider.class);
