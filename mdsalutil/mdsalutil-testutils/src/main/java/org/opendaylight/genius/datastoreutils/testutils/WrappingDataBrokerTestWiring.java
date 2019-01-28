3caaad4df7d9bbb03691e16a7d73173f1e2eaf8c
Stephen Kitt <skitt@redhat.com>
2019-01-28 10:51:46 +0100

    Add a wrapping DataBroker test wiring ...
    which wraps an MD-SAL DataBroker with a Controller DataBroker for test
    purposes. (This will be removed once the migration is complete in
    Genius and NetVirt.)
    
    Change-Id: I7b8e9316d9e12f6c4252c6d64dfaeb3f482616c3
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ mdsalutil/mdsalutil-testutils/src/main/java/org/opendaylight/genius/datastoreutils/testutils/WrappingDataBrokerTestWiring.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright © 2019 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.datastoreutils.testutils;
+
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+import java.util.concurrent.Executors;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.test.AbstractBaseDataBrokerTest;
+import org.opendaylight.controller.md.sal.binding.test.AbstractDataBrokerTestCustomizer;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataBroker;
+import org.opendaylight.controller.sal.core.compat.LegacyDOMDataBrokerAdapter;
+
+/**
+ * Legacy (Controller) DataBroker test wiring which wraps an MD-SAL DataBroker.
+ */
+public class WrappingDataBrokerTestWiring {
+    private final AbstractBaseDataBrokerTest test;
+
+    public WrappingDataBrokerTestWiring(org.opendaylight.mdsal.dom.api.DOMDataBroker domDataBroker) throws Exception {
+        test = new AbstractBaseDataBrokerTest() {
+            @Override
+            protected AbstractDataBrokerTestCustomizer createDataBrokerTestCustomizer() {
+                return new AbstractDataBrokerTestCustomizer() {
+                    @Override
+                    public ListeningExecutorService getCommitCoordinatorExecutor() {
+                        return MoreExecutors.listeningDecorator(Executors.newCachedThreadPool());
+                    }
+
+                    @Override
+                    public DOMDataBroker createDOMDataBroker() {
+                        return new LegacyDOMDataBrokerAdapter(domDataBroker);
+                    }
+                };
+            }
+        };
+        test.setup();
+    }
+
+    public DataBroker getDataBroker() {
+        return test.getDataBroker();
+    }
+}
