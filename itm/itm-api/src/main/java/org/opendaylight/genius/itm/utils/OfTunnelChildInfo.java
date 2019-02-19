17a92bfa643bcbe1c2bc7695210b2abd8e25e831
Dimple Jain <dimple.jain@ericsson.com>
2019-02-19 10:50:49 +0530

    GENIUS-271: Updating Tunnel state operational DS with all of the child Tunnels mapped to the OFTunnel port.
    Change-Id: I0c22d54f556dcf20257ddebb3113f560bbac22a7
    Signed-off-by: Dimple Jain <dimple.jain@ericsson.com>

---
+++ itm/itm-api/src/main/java/org/opendaylight/genius/itm/utils/OfTunnelChildInfo.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2018 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.itm.utils;
+
+import java.math.BigInteger;
+import java.util.List;
+import org.apache.commons.lang3.tuple.Pair;
+import org.eclipse.jdt.annotation.Nullable;
+import org.immutables.value.Value;
+import org.opendaylight.genius.infra.OpenDaylightImmutableStyle;
+
+@Value.Immutable
+@OpenDaylightImmutableStyle
+public interface OfTunnelChildInfo {
+
+    @Nullable
+    String getOfTunnelName();
+
+    @Nullable
+    List<String> getOutChildTunnels();
+
+    @Nullable
+    List<Pair<BigInteger, String>> getInDpnChildTunnels();
+}
---
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/itmdirecttunnels/workers/OFTunnelStateAddWorkerForNodeConnector.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2018 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.itm.itmdirecttunnels.workers;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import org.opendaylight.genius.itm.utils.OfTunnelChildInfo;
+import org.opendaylight.genius.itm.utils.TunnelStateInfo;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+
+public class OFTunnelStateAddWorkerForNodeConnector implements Callable<List<ListenableFuture<Void>>> {
+    private final OFTunnelStateAddWorker ofTunnelStateAddWorker;
+    private final TunnelStateInfo tunnelStateInfo;
+    private final OfTunnelChildInfo ofTunnelChildInfo;
+
+    public OFTunnelStateAddWorkerForNodeConnector(OFTunnelStateAddWorker ofTunnelStateAddWorker,
+                                                  TunnelStateInfo tunnelStateInfo,
+                                                  OfTunnelChildInfo ofTunnelChildInfo) {
+        this.ofTunnelStateAddWorker = ofTunnelStateAddWorker;
+        this.tunnelStateInfo = tunnelStateInfo;
+        this.ofTunnelChildInfo = ofTunnelChildInfo;
+    }
+
+    @Override
+    public List<ListenableFuture<Void>> call() throws Exception {
+        // If another renderer(for eg : OVS) needs to be supported, check can be performed here
+        // to call the respective helpers.
+        return ofTunnelStateAddWorker.addState(tunnelStateInfo, ofTunnelChildInfo);
+    }
+
+    @Override
+    public String toString() {
+        return "TunnelStateAddWorkerForNodeConnector{tunnelStateInfo=" + tunnelStateInfo + '}';
+    }
+}
