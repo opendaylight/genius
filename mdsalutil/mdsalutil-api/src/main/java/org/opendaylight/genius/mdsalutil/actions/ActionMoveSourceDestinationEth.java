48a64ec5b3a654aa79fab221b91fd56da1d3b5ee
R P Karthika <r.p.karthika@ericsson.com>
2019-04-10 15:33:54 +0530

    Bump mdsal to 3.0.0
    Bump mdsal version to 3.0.0 and codegen changes
    
    Change-Id: Idcfb62212acb989848fa1d77e822183165dd7826
    Signed-off-by: R P Karthika <r.p.karthika@ericsson.com>

--- mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/actions/ActionMoveSourceDestinationEth.java
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/actions/ActionMoveSourceDestinationEth.java
@@ -18,7 +18,7 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.ni
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.move.grouping.nx.reg.move.DstBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.nx.action.reg.move.grouping.nx.reg.move.SrcBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.action.rev140714.src.choice.grouping.src.choice.SrcOfEthSrcCaseBuilder;
-
+import org.opendaylight.yangtools.yang.common.Empty;
 
 /**
  * Move source/destination Ethernet action.
@@ -43,11 +43,11 @@ public class ActionMoveSourceDestinationEth extends ActionInfo {
         ActionBuilder ab = new ActionBuilder();
         NxRegMove regMove = new NxRegMoveBuilder()
             .setSrc(new SrcBuilder()
-                .setSrcChoice(new SrcOfEthSrcCaseBuilder().setOfEthSrc(Boolean.TRUE).build())
+                .setSrcChoice(new SrcOfEthSrcCaseBuilder().setOfEthSrc(Empty.getInstance()).build())
                 .setStart(0)
                 .build())
             .setDst(new DstBuilder()
-                .setDstChoice(new DstOfEthDstCaseBuilder().setOfEthDst(Boolean.TRUE).build())
+                .setDstChoice(new DstOfEthDstCaseBuilder().setOfEthDst(Empty.getInstance()).build())
                 .setStart(0)
                 .setEnd(47)
                 .build())
