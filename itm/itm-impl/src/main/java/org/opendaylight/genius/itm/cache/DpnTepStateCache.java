d63f4f80dcad8704687c5e5cfd4021ad09bdb50c
Nidhi <nidhi.adhvaryu@ericsson.com>
2019-05-03 12:16:50 +0530

    In ITM Scale-In-Out when same IP is used, some stale tunnels hanging in new mesh creation.
    When computes were scaled-in and scaled-out with the same vtep ip, ITM
    full mesh was not created.During scale-in, ITM caches were notcompletely
    cleaned up resulting in stale DPN in ITM. This prevented the full mesh
    creation.This patch add solution to this problem.
    
    Change-Id: If712ca51893f56e6d0efd260260387242fab667a
    Signed-off-by: Nidhi <nidhi.adhvaryu@ericsson.com>

--- itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cache/DpnTepStateCache.java
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cache/DpnTepStateCache.java
@@ -159,8 +159,11 @@ public class DpnTepStateCache extends DataObjectCache<BigInteger, DpnsTeps> {
     @Override
     protected void removed(InstanceIdentifier<DpnsTeps> path, DpnsTeps dpnsTeps) {
         for (RemoteDpns remoteDpns : dpnsTeps.nonnullRemoteDpns()) {
-            dpnTepInterfaceMap.remove(getDpnId(dpnsTeps.getSourceDpnId(), remoteDpns.getDestinationDpnId()));
+            String fwkey = getDpnId(dpnsTeps.getSourceDpnId(), remoteDpns.getDestinationDpnId());
+            dpnTepInterfaceMap.remove(fwkey);
             tunnelEndpointMap.remove(remoteDpns.getTunnelName());
+            String revkey = getDpnId(remoteDpns.getDestinationDpnId(), dpnsTeps.getSourceDpnId());
+            dpnTepInterfaceMap.remove(revkey);
         }
     }
 
@@ -301,5 +304,9 @@ public class DpnTepStateCache extends DataObjectCache<BigInteger, DpnsTeps> {
 
     public List<String> getOfTunnelChildInfoFromCache(String tunnelName) {
         return ofTunnelChildMap.get(tunnelName);
+    }
+
+    public void removeFromTunnelEndPointMap(String tunnelName) {
+        tunnelEndpointMap.remove(tunnelName);
     }
 }
