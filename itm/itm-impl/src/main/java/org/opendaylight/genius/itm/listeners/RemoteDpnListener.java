19dee3f7277630abd54db3c058c1b4a0f9c5e14a
Nidhi <nidhi.adhvaryu@ericsson.com>
2019-05-03 12:16:50 +0530

    In ITM Scale-In-Out when same IP is used, some stale tunnels hanging in new mesh creation.
    When computes were scaled-in and scaled-out with the same vtep ip, ITM
    full mesh was not created.During scale-in, ITM caches were notcompletely
    cleaned up resulting in stale DPN in ITM. This prevented the full mesh
    creation.This patch add solution to this problem.
    
    Change-Id: If712ca51893f56e6d0efd260260387242fab667a
    Signed-off-by: Nidhi <nidhi.adhvaryu@ericsson.com>
    (cherry picked from commit d63f4f80dcad8704687c5e5cfd4021ad09bdb50c)

---
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/listeners/RemoteDpnListener.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2019 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.genius.itm.listeners;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.itm.cache.DpnTepStateCache;
+import org.opendaylight.serviceutils.tools.mdsal.listener.AbstractClusteredSyncDataTreeChangeListener;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.DpnTepsState;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.dpn.teps.state.DpnsTeps;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.dpn.teps.state.dpns.teps.RemoteDpns;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RemoteDpnListener extends AbstractClusteredSyncDataTreeChangeListener<RemoteDpns> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RemoteDpnListener.class);
+
+    private final DpnTepStateCache dpnTepStateCache;
+
+    public RemoteDpnListener(final DataBroker dataBroker, final DpnTepStateCache dpnTepStateCache) {
+        super(dataBroker, LogicalDatastoreType.CONFIGURATION,
+                InstanceIdentifier.create(DpnTepsState.class).child(DpnsTeps.class).child(RemoteDpns.class));
+        this.dpnTepStateCache = dpnTepStateCache;
+        super.register();
+    }
+
+    public void remove(InstanceIdentifier<RemoteDpns> key, RemoteDpns remoteDpns) {
+        final String tunnelName = remoteDpns.getTunnelName();
+        LOG.debug("Removing TunnelEndPointInfoCache on RemoteDpns removal for tunnelName {}", tunnelName);
+        dpnTepStateCache.removeFromTunnelEndPointMap(tunnelName);
+    }
+}
