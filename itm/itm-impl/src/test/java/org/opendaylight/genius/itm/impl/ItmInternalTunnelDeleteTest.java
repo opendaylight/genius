086626c602f1521d0f9a822513b79d513aecd43f
Apurba Mukherjee <apurba.mukherjee@ericsson.com>
2019-07-04 12:12:18 +0530

    GENIUS-266: Deletion of OF-Port along with status - takes care of deleting of-Port - takes case of deleting tunnels to and from this port - takes care of deleting ingress and egress flows
    Change-Id: I8d42b02134d163e5ebf6b18aa7734cc65237ab33
    Signed-off-by: Apurba Mukherjee <apurba.mukherjee@ericsson.com>

--- itm/itm-impl/src/test/java/org/opendaylight/genius/itm/impl/ItmInternalTunnelDeleteTest.java
+++ itm/itm-impl/src/test/java/org/opendaylight/genius/itm/impl/ItmInternalTunnelDeleteTest.java
@@ -28,13 +28,7 @@ import org.opendaylight.controller.md.sal.binding.api.ReadOnlyTransaction;
 import org.opendaylight.controller.md.sal.binding.api.ReadWriteTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.genius.interfacemanager.interfaces.IInterfaceManager;
-import org.opendaylight.genius.itm.cache.DPNTEPsInfoCache;
-import org.opendaylight.genius.itm.cache.DpnTepStateCache;
-import org.opendaylight.genius.itm.cache.OvsBridgeEntryCache;
-import org.opendaylight.genius.itm.cache.OvsBridgeRefEntryCache;
-import org.opendaylight.genius.itm.cache.TunnelStateCache;
-import org.opendaylight.genius.itm.cache.UnprocessedNodeConnectorCache;
-import org.opendaylight.genius.itm.cache.UnprocessedNodeConnectorEndPointCache;
+import org.opendaylight.genius.itm.cache.*;
 import org.opendaylight.genius.itm.confighelpers.ItmInternalTunnelDeleteWorker;
 import org.opendaylight.genius.itm.itmdirecttunnels.renderer.ovs.utilities.DirectTunnelUtils;
 import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
@@ -125,6 +119,7 @@ public class ItmInternalTunnelDeleteTest {
     ItmInternalTunnelDeleteWorker itmInternalTunnelDeleteWorker;
     UnprocessedNodeConnectorCache unprocessedNodeConnectorCache;
     UnprocessedNodeConnectorEndPointCache unprocessedNodeConnectorEndPointCache;
+    OfEndPointCache ofEndPointCache;
 
 
     Optional<TunnelMonitorParams> tunnelMonitorParamsOptional;
@@ -168,7 +163,7 @@ public class ItmInternalTunnelDeleteTest {
             new OvsBridgeEntryCache(dataBroker, new GuavaCacheProvider(new CacheManagersRegistryImpl())),
             new OvsBridgeRefEntryCache(dataBroker, new GuavaCacheProvider(new CacheManagersRegistryImpl())),
             new TunnelStateCache(dataBroker, new GuavaCacheProvider(new CacheManagersRegistryImpl())),
-            directTunnelUtils);
+            directTunnelUtils, ofEndPointCache, itmConfig);
     }
 
     @After
@@ -210,6 +205,7 @@ public class ItmInternalTunnelDeleteTest {
         doReturn(mockReadWriteTx).when(dataBroker).newReadWriteTransaction();
         lenient().doReturn(Futures.immediateCheckedFuture(null)).when(mockReadWriteTx).submit();
         doReturn(true).when(mockReadWriteTx).cancel();
+        doReturn(false).when(itmConfig).isUseOfTunnels();
     }
 
     // Since all the unit test cases will be written to the new way, this should be taken care then.
