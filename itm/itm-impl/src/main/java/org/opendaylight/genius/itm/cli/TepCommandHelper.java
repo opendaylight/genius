53da8af731b31f3097c34c3ad5bcec33ef1e158b
Nishchya Gupta <nishchyag@altencalsoftlabs.com>
2020-02-03 18:05:01 +0530

    Randomly build failure because of test failure in ITM
    This Fail test case create a TZ(transport zone) then deletes it and again
    recreates the same TZ. After creation when it deletes TZ , it involves two
    steps 1. Tz gets deleted from TransportZones config ds and then 2. It get
    added to Not Hosted TZ oper ds. So after delete, when it is trying to get
    that TZ from notHostedTz it is failing randomly. Issue is that step 2 is
    still not completed(addition of TZ to notHostedTZ) and we are querying for
    TZ from notHostedTZ. For solving this, we have add a small delay.
    
    Apart from this, In place of put, we are using merge, so that creation of
    DefaultTZ will not impact the exsisting TZ.
    
    Signed-off-by: Nishchya Gupta <nishchyag@altencalsoftlabs.com>
    Change-Id: If85f5d1ea74a9c9b6538c58b98fa830f9687c9eb

--- itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cli/TepCommandHelper.java
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cli/TepCommandHelper.java
@@ -807,7 +807,7 @@ public class TepCommandHelper {
         }
         tzList.add(tzone);
         TransportZones transportZones = new TransportZonesBuilder().setTransportZone(tzList).build();
-        txRunner.callWithNewWriteOnlyTransactionAndSubmit(tx -> tx.put(LogicalDatastoreType.CONFIGURATION,
+        txRunner.callWithNewWriteOnlyTransactionAndSubmit(tx -> tx.merge(LogicalDatastoreType.CONFIGURATION,
                 path, transportZones, WriteTransaction.CREATE_MISSING_PARENTS)).get();
 
     }
