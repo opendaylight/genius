e7ec75b44230b3e565ce33124e581c0f2cad5bb3
Apurba Mukherjee <apurba.mukherjee@ericsson.com>
2020-07-02 16:10:07 +0530

    OF port creation and update config side implementation
    Change-Id: Ia542b73cde03453ccd90041d6596ea3cf5f5f5dc
    Signed-off-by: Apurba Mukherjee <apurba.mukherjee@ericsson.com>

---
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cache/OfTepStateCache.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2020 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.itm.cache;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.opendaylight.genius.mdsalutil.cache.DataObjectCache;
+import org.opendaylight.infrautils.caches.CacheProvider;
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.OfTepsState;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.of.teps.state.OfTep;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.of.teps.state.OfTepKey;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@Singleton
+public class OfTepStateCache extends DataObjectCache<String, OfTep> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(OfTepStateCache.class);
+
+    @Inject
+    public OfTepStateCache(DataBroker dataBroker, CacheProvider cacheProvider) {
+        super(OfTep.class, dataBroker, LogicalDatastoreType.OPERATIONAL,
+                InstanceIdentifier.builder(OfTepsState.class).child(OfTep.class).build(), cacheProvider,
+                (iid, ofTepList) -> ofTepList.getOfPortName(), ofPortName -> InstanceIdentifier.builder(OfTepsState.class)
+                        .child(OfTep.class, new OfTepKey(ofPortName)).build());
+    }
+}
---
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/confighelpers/ItmOfPortRemoveWorker.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2020 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.genius.itm.confighelpers;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.Callable;
+import java.util.List;
+import java.util.Map;
+
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.dpn.tep.config.OfDpnTep;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.dpn.tep.config.OfDpnTepKey;
+
+public class ItmOfPortRemoveWorker implements Callable<List<? extends ListenableFuture<?>>> {
+    public ItmOfPortRemoveWorker(Map<OfDpnTepKey, OfDpnTep> oldDpnTepList, DataBroker dataBroker,
+                                 ItmOfTunnelDeleteWorker itmOfTunnelDeleteWorker) {
+    }
+
+    @Override
+    public List<ListenableFuture<Void>> call() throws Exception {
+        return null;
+    }
+}
