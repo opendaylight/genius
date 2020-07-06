3e2abe882568532a076b72fc7008cca97ceb3f80
Apurba Mukherjee <apurba.mukherjee@ericsson.com>
2020-07-06 15:48:19 +0530

    Of Tunnel creation Oper Implementation
    Change-Id: I535021fbe9b1cc69c8da173ab60b304245c1d4bc
    Signed-off-by: Apurba Mukherjee <apurba.mukherjee@ericsson.com>

--- itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cache/OfTepStateCache.java
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/cache/OfTepStateCache.java
@@ -7,8 +7,14 @@
  */
 package org.opendaylight.genius.itm.cache;
 
+import com.google.common.util.concurrent.ListenableFuture;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import org.opendaylight.genius.infra.ManagedNewTransactionRunner;
+import org.opendaylight.genius.infra.ManagedNewTransactionRunnerImpl;
+import org.opendaylight.genius.itm.servicebinding.BindServiceUtils;
 import org.opendaylight.genius.mdsalutil.cache.DataObjectCache;
 import org.opendaylight.infrautils.caches.CacheProvider;
 import org.opendaylight.mdsal.binding.api.DataBroker;
@@ -24,6 +30,8 @@ import org.slf4j.LoggerFactory;
 @Singleton
 public class OfTepStateCache extends DataObjectCache<String, OfTep> {
 
+    private final ManagedNewTransactionRunner txRunner;
+    List<ListenableFuture<Void>> futures = new ArrayList<>();
     private static final Logger LOG = LoggerFactory.getLogger(OfTepStateCache.class);
 
     @Inject
@@ -32,5 +40,13 @@ public class OfTepStateCache extends DataObjectCache<String, OfTep> {
             InstanceIdentifier.builder(OfTepsState.class).child(OfTep.class).build(), cacheProvider,
             (iid, ofTepList) -> ofTepList.getOfPortName(), ofPortName -> InstanceIdentifier.builder(OfTepsState.class)
                     .child(OfTep.class, new OfTepKey(ofPortName)).build());
+        this.txRunner =  new ManagedNewTransactionRunnerImpl(dataBroker);
+    }
+
+    protected void added(InstanceIdentifier<OfTep> path, OfTep ofTep) {
+        LOG.info("Binding default egress dispatcher service for{}", ofTep.getOfPortName());
+        BindServiceUtils.bindDefaultEgressDispatcherService(txRunner, futures, "VXLAN_TRUNK_INTERFACE",
+                String.valueOf(ofTep.getPortNumber()), ofTep.getOfPortName(),
+                ofTep.getIfIndex());
     }
 }
