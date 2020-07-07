ef31bd13508262aa862294fb4f3a9d87ca47b64c
Apurba Mukherjee <apurba.mukherjee@ericsson.com>
2020-07-07 21:46:52 +0530

    OF port deletion complete implementation
    Change-Id: I485909dd804b049808366c0374a35a3a41511f0e
    Signed-off-by: Apurba Mukherjee <apurba.mukherjee@ericsson.com>

--- itm/itm-impl/src/main/java/org/opendaylight/genius/itm/confighelpers/ItmOfPortRemoveWorker.java
+++ itm/itm-impl/src/main/java/org/opendaylight/genius/itm/confighelpers/ItmOfPortRemoveWorker.java
@@ -9,20 +9,33 @@
 package org.opendaylight.genius.itm.confighelpers;
 
 import com.google.common.util.concurrent.ListenableFuture;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
-import org.opendaylight.mdsal.binding.api.DataBroker;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.dpn.tep.config.OfDpnTep;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.dpn.tep.config.OfDpnTepKey;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ItmOfPortRemoveWorker implements Callable<List<? extends ListenableFuture<?>>> {
-    public ItmOfPortRemoveWorker(Map<OfDpnTepKey, OfDpnTep> oldDpnTepList, DataBroker dataBroker,
+
+    private static final Logger LOG = LoggerFactory.getLogger(ItmOfPortRemoveWorker.class);
+
+    private Map<OfDpnTepKey, OfDpnTep> oldDpnTepMap;
+    private final ItmOfTunnelDeleteWorker itmOfTunnelDeleteWorker;
+
+    public ItmOfPortRemoveWorker(Map<OfDpnTepKey, OfDpnTep> oldDpnTepMap,
                                  ItmOfTunnelDeleteWorker itmOfTunnelDeleteWorker) {
+        this.itmOfTunnelDeleteWorker = itmOfTunnelDeleteWorker;
+        this.oldDpnTepMap = oldDpnTepMap;
     }
 
     @Override
     public List<ListenableFuture<Void>> call() throws Exception {
-        return null;
+        List<ListenableFuture<Void>> futures = new ArrayList<>();
+        futures.addAll(itmOfTunnelDeleteWorker.deleteOfTeps(oldDpnTepMap.values()));
+        LOG.debug("Invoking OfTep delete method with OfTep list to be deleted {}", oldDpnTepMap.values());
+        return futures;
     }
 }
