39cdbed74ec78d0ed4ecb4498499efef17b80b5e
Apurba Mukherjee <apurba.mukherjee@ericsson.com>
2020-07-23 11:48:00 +0530

    CLI to display Of-Port status
    Change-Id: I21401deb23bffd004bfa8acf0fa9cdf5320b1884
    Signed-off-by: Apurba Mukherjee <apurba.mukherjee@ericsson.com>

--- itm/itm-api/src/main/java/org/opendaylight/genius/itm/api/IITMProvider.java
+++ itm/itm-api/src/main/java/org/opendaylight/genius/itm/api/IITMProvider.java
@@ -16,6 +16,7 @@ import org.opendaylight.mdsal.common.api.ReadFailedException;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.Interface;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rev160406.TunnelTypeBase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.of.teps.state.OfTep;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.tunnels_state.StateTunnelList;
 import org.opendaylight.yangtools.yang.common.Uint64;
 
@@ -54,4 +55,6 @@ public interface IITMProvider {
     Interface getInterface(String tunnelName);
 
     Optional<StateTunnelList> getTunnelState(String interfaceName) throws ReadFailedException;
+
+    void showOfPorts(Collection<OfTep> ofTeps);
 }
