47bbb54f1ade061363931ed807e478d02d6e693c
Chetan Arakere Gowdru <chetan.arakere@altencalsoftlabs.com>
2020-08-04 13:51:29 +0530

    WIP : L2GW HwvtepHACache add
    Signed-off-by: Chetan Arakere Gowdru <chetan.arakere@altencalsoftlabs.com>
    Change-Id: I494c34be054f45e5bd020f9e30a5a6bfdf0d557f

---
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/utils/hwvtep/HwvtepHACache.java
@@ -0,0 +1,182 @@
+/*
+ * Copyright (c) 2016 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.genius.utils.hwvtep;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HwvtepHACache {
+
+    private static final Logger LOG = LoggerFactory.getLogger(HwvtepHACache.class);
+
+    private static final int MAX_EVENT_BUFFER_SIZE = 500000;
+    private static final int EVENT_DRAIN_BUFFER_SIZE = 100000;
+
+    private static HwvtepHACache instance = new HwvtepHACache();
+
+    private final ConcurrentHashMap<InstanceIdentifier<Node>, Set<InstanceIdentifier<Node>>>
+        parentToChildMap = new ConcurrentHashMap<>();
+
+    private final ConcurrentHashMap<InstanceIdentifier<Node>, InstanceIdentifier<Node>>
+        childToParentMap = new ConcurrentHashMap<>();
+
+    private final ConcurrentHashMap<String, Boolean> childNodeIds = new ConcurrentHashMap<>();
+
+    private final ConcurrentHashMap<String, Boolean> connectedNodes = new ConcurrentHashMap<>();
+
+    private final LinkedBlockingQueue<DebugEvent> debugEvents = new LinkedBlockingQueue<>(MAX_EVENT_BUFFER_SIZE);
+    private Map<InstanceIdentifier<Node>, IpAddress> tepIps = new ConcurrentHashMap();
+    private Map<IpAddress, InstanceIdentifier<Node>> nodeIds = new ConcurrentHashMap();
+
+    public HwvtepHACache() {
+    }
+
+    public static HwvtepHACache getInstance() {
+        return instance;
+    }
+
+    public synchronized void addChild(InstanceIdentifier<Node> parent, InstanceIdentifier<Node> child) {
+        if (parent == null || child == null) {
+            return;
+        }
+
+        parentToChildMap.computeIfAbsent(parent, key -> new HashSet<>()).add(child);
+        childToParentMap.put(child, parent);
+        String childNodeId = child.firstKeyOf(Node.class).getNodeId().getValue();
+        childNodeIds.put(childNodeId, Boolean.TRUE);
+        addDebugEvent(new NodeEvent.ChildAddedEvent(childNodeId));
+    }
+
+    public IpAddress getTepIpOfNode(InstanceIdentifier<Node> iid) {
+        return (IpAddress)this.tepIps.get(iid);
+    }
+
+    public InstanceIdentifier<Node> getNodeIdFromTepIp(IpAddress ipAddress) {
+        return (InstanceIdentifier)this.nodeIds.get(ipAddress);
+    }
+
+    public void setTepIpOfNode(InstanceIdentifier<Node> iid, IpAddress ipAddress) {
+        this.tepIps.put(iid, ipAddress);
+        if (this.isHAParentNode(iid)) {
+            this.nodeIds.put(ipAddress, iid);
+        } else {
+            this.nodeIds.putIfAbsent(ipAddress, iid);
+        }
+
+    }
+
+    public boolean isHAEnabledDevice(InstanceIdentifier<?> iid) {
+        if (iid == null) {
+            return false;
+        }
+        boolean enabled = childToParentMap.containsKey(iid.firstIdentifierOf(Node.class));
+        if (!enabled) {
+            String psNodeId = iid.firstKeyOf(Node.class).getNodeId().getValue();
+            int idx = psNodeId.indexOf(HwvtepSouthboundConstants.PSWITCH_URI_PREFIX);
+            if (idx > 0) {
+                String globalNodeId = psNodeId.substring(0, idx - 1);
+                return childNodeIds.containsKey(globalNodeId);
+            }
+        }
+        return enabled;
+    }
+
+    public  boolean isHAParentNode(InstanceIdentifier<Node> node) {
+        return parentToChildMap.containsKey(node);
+    }
+
+    public Set<InstanceIdentifier<Node>> getChildrenForHANode(InstanceIdentifier<Node> parent) {
+        if (parent != null && parentToChildMap.containsKey(parent)) {
+            return new HashSet<>(parentToChildMap.get(parent));
+        } else {
+            return Collections.emptySet();
+        }
+    }
+
+    public Set<InstanceIdentifier<Node>> getHAParentNodes() {
+        return parentToChildMap.keySet();
+    }
+
+    public Set<InstanceIdentifier<Node>> getHAChildNodes() {
+        return childToParentMap.keySet();
+    }
+
+    public InstanceIdentifier<Node> getParent(InstanceIdentifier<Node> child) {
+        if (child != null) {
+            return childToParentMap.get(child);
+        }
+        return null;
+    }
+
+    public synchronized void cleanupParent(InstanceIdentifier<Node> parent) {
+        if (parent == null) {
+            return;
+        }
+
+        if (parentToChildMap.get(parent) != null) {
+            Set<InstanceIdentifier<Node>> childs = parentToChildMap.get(parent);
+            for (InstanceIdentifier<Node> child : childs) {
+                childToParentMap.remove(child);
+                String childNodeId = child.firstKeyOf(Node.class).getNodeId().getValue();
+                childNodeIds.remove(childNodeId);
+            }
+        }
+        parentToChildMap.remove(parent);
+    }
+
+    public void updateConnectedNodeStatus(InstanceIdentifier<Node> iid) {
+        String nodeId = iid.firstKeyOf(Node.class).getNodeId().getValue();
+        connectedNodes.put(nodeId, true);
+        DebugEvent event = new NodeEvent.NodeConnectedEvent(nodeId);
+        addDebugEvent(event);
+    }
+
+    public void updateDisconnectedNodeStatus(InstanceIdentifier<Node> iid) {
+        String nodeId = iid.firstKeyOf(Node.class).getNodeId().getValue();
+        connectedNodes.put(nodeId, false);
+        DebugEvent event = new NodeEvent.NodeDisconnectedEvent(nodeId);
+        addDebugEvent(event);
+    }
+
+    public Map<String, Boolean> getConnectedNodes() {
+        return ImmutableMap.copyOf(connectedNodes);
+    }
+
+    public void addDebugEvent(DebugEvent debugEvent) {
+        //Try adding the event to event queue
+        if (!debugEvents.offer(debugEvent)) {
+            //buffer is exhausted
+            Collection<DebugEvent> list = new ArrayList<>();
+            //do not clear all events , make some place by clearing few old events
+            debugEvents.drainTo(list, EVENT_DRAIN_BUFFER_SIZE);
+
+            if (!debugEvents.offer(debugEvent)) {
+                LOG.debug("Unable to add debug event");
+            }
+        }
+    }
+
+    public List<DebugEvent> getNodeEvents() {
+        return ImmutableList.copyOf(debugEvents);
+    }
+}
