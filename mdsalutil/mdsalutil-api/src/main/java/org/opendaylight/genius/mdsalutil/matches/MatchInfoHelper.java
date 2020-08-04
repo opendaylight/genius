5d699536958e4a42b6ed31f441ca2be845eacc2b
Robert Varga <robert.varga@pantheon.tech>
2020-08-04 13:07:40 +0200

    Migrate users of Class.newInstance()
    This method has been deprecated for removal since Java 9, migrate
    the three call sites.
    
    Change-Id: I7527ae2958c22e4f83e436c3293cc06abc1e4f65
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/matches/MatchInfoHelper.java
+++ mdsalutil/mdsalutil-api/src/main/java/org/opendaylight/genius/mdsalutil/matches/MatchInfoHelper.java
@@ -28,8 +28,8 @@ public abstract class MatchInfoHelper<T extends DataObject, B extends Builder<T>
     public void createInnerMatchBuilder(Map<Class<?>, Object> mapMatchBuilder) {
         populateBuilder((B) mapMatchBuilder.computeIfAbsent(builderClass, key -> {
             try {
-                return builderClass.newInstance();
-            } catch (InstantiationException | IllegalAccessException e) {
+                return builderClass.getDeclaredConstructor().newInstance();
+            } catch (ReflectiveOperationException e) {
                 throw new IllegalStateException("Unable to create an instance of " + builderClass, e);
             }
         }));
